b47a4d6e9f4f10765f892350a81edfe34572eb8d
jentzsch <anja@anjeve.de>
2010-05-31 11:17:45 +0000

    - calculate average aggregation value

--- framework/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/aggegration/AverageAggregation.scala
+++ framework/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/aggegration/AverageAggregation.scala
@@ -5,5 +5,15 @@ import de.fuberlin.wiwiss.silk.Instance
 
 class AverageAggregation(val weight : Int, val operators : Traversable[Operator]) extends Aggregation
 {
-    override def evaluate(sourceInstance : Instance, targetInstance : Instance) =  Traversable(0.0)
+    override def evaluate(sourceInstance : Instance, targetInstance : Instance) =
+    {
+        var count = 0
+        var result = 0.0
+        for (operator <- operators; val operatorValues = operator.evaluate(sourceInstance, targetInstance); operatorValue <- operatorValues)
+        {
+            result += operator.weight * operatorValue
+            count += 1
+        }
+        if (count > 0) Traversable(result.toDouble/count) else Traversable()
+    }
 }
