2a0d52cdbb214bced871f677c21093691715ad9e
jentzsch <anja@anjeve.de>
2010-06-03 11:36:08 +0000

    - bugfix in Jaro (common character strings of unequal size are allowed)

--- framework/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/metric/JaroDistanceMetric.scala
+++ framework/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/metric/JaroDistanceMetric.scala
@@ -2,7 +2,7 @@ package de.fuberlin.wiwiss.silk.metric
 
 import de.fuberlin.wiwiss.silk.linkspec.Metric
 
-class JaroDistanceMetric(val params: Map[String, String]) extends Metric
+class JaroDistanceMetric(val params: Map[String, String] = Map()) extends Metric
 {
     override def evaluate(str1 : String, str2 : String) =
     {
@@ -15,7 +15,7 @@ object JaroDinstanceMetric
     def jaro(string1: String, string2: String): Double =
     {
         //get half the length of the string rounded up - (this is the distance used for acceptable transpositions)
-        val halflen: Int = ((Math.min(string1.length, string2.length)) / 2) + ((Math.min(string1.length, string2.length)) % 2)
+        val halflen: Int = ((math.min(string1.length, string2.length)) / 2) + ((math.min(string1.length, string2.length)) % 2)
 
         //get common characters
         val common1: StringBuffer = getCommonCharacters(string1, string2, halflen)
@@ -25,16 +25,17 @@ object JaroDinstanceMetric
         if (common1.length == 0 || common2.length == 0) {
             return 0.0
         }
-
+        /*
         //check for same length common strings returning 0.0 is not the same
         if (common1.length != common2.length) {
             return 0.0
         }
+        */
 
         //get the number of transpositions
         var transpositions: Int = 0
 
-        for (i <- 0 to common1.length)
+        for (i <- 0 to math.min(common1.length-1, common2.length-1))
         {
             if (common1.charAt(i) != common2.charAt(i))
             {
@@ -45,7 +46,7 @@ object JaroDinstanceMetric
         transpositions = transpositions / 2
 
         //calculate jaro metric
-        return (common1.length / (string1.length.asInstanceOf[Double]) + common2.length / (string2.length.asInstanceOf[Double]) + (common1.length - transpositions) / (common1.length.asInstanceOf[Double])) / 3.0
+        return (common1.length / (string1.length.toDouble) + common2.length / (string2.length.toDouble) + (common1.length - transpositions) / (common1.length.toDouble)) / 3.0
     }
 
 
@@ -67,18 +68,16 @@ object JaroDinstanceMetric
         for (string1Char <- string1)
         {
             var foundIt: Boolean = false
-            for (string2Char <- copy.toString)
+            var j: Int = math.max(0, i - distanceSep)
+            while (!foundIt && j < math.min(i + distanceSep + 1, string2.length))
             {
-                var j: Int = Math.max(0, i - distanceSep)
-                while (!foundIt && j < Math.min(i + distanceSep, string2.length - 1))
+                if (copy.charAt(j) == string1Char)
                 {
-                    if (copy.charAt(j) == string1Char)
-                    {
-                        foundIt = true
-                        returnCommons.append(string1Char)
-                        copy.setCharAt(j, '0')
-                    }
+                    foundIt = true
+                    returnCommons.append(string1Char)
+                    copy.setCharAt(j, 0.toChar)
                 }
+                j += 1
             }
             i += 1
         }
--- framework/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/metric/JaroWinklerMetric.scala
+++ framework/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/metric/JaroWinklerMetric.scala
@@ -3,7 +3,7 @@ package de.fuberlin.wiwiss.silk.metric
 import de.fuberlin.wiwiss.silk.linkspec.Metric
 import de.fuberlin.wiwiss.silk.metric._
 
-class JaroWinklerMetric(val params: Map[String, String]) extends Metric
+class JaroWinklerMetric(val params: Map[String, String] = Map()) extends Metric
 {
     require(params.contains("str1"), "Parameter 'str1' is required")
     require(params.contains("str2"), "Parameter 'str2' is required")
@@ -16,7 +16,7 @@ class JaroWinklerMetric(val params: Map[String, String]) extends Metric
 
     override def evaluate(str1 : String, str2 : String) =
     {
-        jaroWinkler(str1, str2)
+        evaluateDistance(str1, str2)
     }
 
     /**
@@ -26,7 +26,7 @@ class JaroWinklerMetric(val params: Map[String, String]) extends Metric
      * @param string2
      * @return 0 -1 similarity measure of the JaroWinkler metric
      */
-    private def jaroWinkler(string1: String, string2: String): Double =
+    def evaluateDistance(string1: String, string2: String): Double =
     {
         val dist = JaroDinstanceMetric.jaro(string1, string2)
         val prefixLength: Int = getPrefixLength(string1, string2)
@@ -41,7 +41,7 @@ class JaroWinklerMetric(val params: Map[String, String]) extends Metric
      * @return the prefix length found of common characters at the begining of the strings
      */
     private def getPrefixLength(string1: String, string2: String): Int = {
-        val max: Int = Math.min(MINPREFIXTESTLENGTH, Math.min(string1.length, string2.length))
+        val max: Int = math.min(MINPREFIXTESTLENGTH, math.min(string1.length, string2.length))
 
         var i: Int = 0
         while (i < max)
