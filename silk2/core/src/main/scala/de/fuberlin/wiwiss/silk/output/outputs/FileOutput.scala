d16e03bd1f41c2d99ab8aafc238188232bb94c0e
robertisele <robertisele@googlemail.com>
2010-06-21 17:16:55 +0000

    FileOutput: Log the number of links which have been written

--- silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/output/outputs/FileOutput.scala
+++ silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/output/outputs/FileOutput.scala
@@ -3,12 +3,15 @@ package de.fuberlin.wiwiss.silk.output.outputs
 import de.fuberlin.wiwiss.silk.output.{Formatter, Link, Output}
 import de.fuberlin.wiwiss.silk.util.StringUtils._
 import java.io.{Writer, OutputStreamWriter, FileOutputStream, OutputStream}
+import java.util.logging.Logger
 
 /**
  * A file output.
  */
 class FileOutput(val params : Map[String, String]) extends Output
 {
+    private val logger = Logger.getLogger(classOf[FileOutput].getName)
+
     private val formatter = Formatter(params.get("format").getOrElse(throw new IllegalArgumentException("No format specified")))
 
     private val minConfidence = params.get("minConfidence").map
@@ -27,10 +30,13 @@ class FileOutput(val params : Map[String, String]) extends Output
 
     private var out : Writer = null
 
+    private var linkCount : Int = 0
+
     override def open : Unit =
     {
         out = new OutputStreamWriter(new FileOutputStream(outputFile), "UTF-8")
         out.write(formatter.header)
+        linkCount = 0
     }
 
     override def write(link : Link, predicateUri : String) : Unit =
@@ -41,6 +47,7 @@ class FileOutput(val params : Map[String, String]) extends Output
            (maxConfidence.isEmpty || link.confidence <= maxConfidence.get))
         {
             out.write(formatter.format(link, predicateUri))
+            linkCount += 1
         }
     }
 
@@ -48,5 +55,6 @@ class FileOutput(val params : Map[String, String]) extends Output
     {
         out.write(formatter.footer)
         out.close()
+        logger.info("Wrote " + linkCount + " links to " + outputFile)
     }
 }
