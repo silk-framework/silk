53d5ca632710cd9b57bbf9b7e6d65cad0ce57691
robertisele <robertisele@googlemail.com>
2010-06-25 08:59:20 +0000

    Renamed Alignment to Link

--- silk2/evaluation/src/main/scala/de/fuberlin/wiwiss/silk/evaluation/EvaluationResult.scala
+++ silk2/evaluation/src/main/scala/de/fuberlin/wiwiss/silk/evaluation/EvaluationResult.scala
@@ -1,6 +1,5 @@
 package de.fuberlin.wiwiss.silk.evaluation
 
-
 class EvaluationResult(val precision : Double, val recall : Double)
 {
     def fMeassure = 2.0 * precision * recall / (precision + recall)
