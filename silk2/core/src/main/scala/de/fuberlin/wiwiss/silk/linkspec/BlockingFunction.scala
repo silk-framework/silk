3db7416bb3f657a26d641835f9ccaeb3f990a780
robertisele <robertisele@googlemail.com>
2010-06-28 15:04:40 +0000

    Added Numeric blocking function

--- silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/BlockingFunction.scala
+++ silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/BlockingFunction.scala
@@ -1,11 +1,12 @@
 package de.fuberlin.wiwiss.silk.linkspec
 
-import blocking.AlphaNumBlockingFunction
+import blocking.{NumBlockingFunction, AlphaNumBlockingFunction}
 import de.fuberlin.wiwiss.silk.util.{Factory, Strategy}
 
 trait BlockingFunction extends Strategy with (String => Double)
 
 object BlockingFunction extends Factory[BlockingFunction]
 {
+    register("numeric", classOf[NumBlockingFunction])
     register("alphanumeric", classOf[AlphaNumBlockingFunction])
 }
