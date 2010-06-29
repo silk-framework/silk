bf87b875e4d427dd3a0aad21e74f456ac4e39051
robertisele <robertisele@googlemail.com>
2010-06-29 10:30:47 +0000

    Update to allow global output definitions

--- silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/config/Configuration.scala
+++ silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/config/Configuration.scala
@@ -2,8 +2,9 @@ package de.fuberlin.wiwiss.silk.config
 
 import de.fuberlin.wiwiss.silk.datasource.DataSource
 import de.fuberlin.wiwiss.silk.linkspec.LinkSpecification
+import de.fuberlin.wiwiss.silk.output.Output
 
 class Configuration(val prefixes : Map[String, String], val dataSources : Map[String, DataSource],
-                    val linkSpecs : Map[String, LinkSpecification])
+                    val linkSpecs : Map[String, LinkSpecification], val outputs : Traversable[Output] = Traversable.empty)
 {
 }
