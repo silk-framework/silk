a545de2482172276d55b942593c30d4fe0f57a61
robertisele <robertisele@googlemail.com>
2010-07-09 09:03:24 +0000

    Updates to ConfigWriter

---
+++ silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/util/XMLUtils.scala
@@ -0,0 +1,47 @@
+package de.fuberlin.wiwiss.silk.util
+
+import java.io._
+import xml.{PrettyPrinter, NodeSeq}
+
+/**
+ * Defines additional methods on XML, which are missing in the standard library.
+ */
+object XMLUtils
+{
+    implicit def toXMLUtils(xml : NodeSeq) = new XMLUtils(xml)
+}
+
+/**
+ * Defines additional methods on XML, which are missing in the standard library.
+ */
+class XMLUtils(xml : NodeSeq)
+{
+    def toFormattedString =
+    {
+        val stringWriter = new StringWriter()
+        write(stringWriter)
+        stringWriter.toString
+    }
+
+    def write(file : File)
+    {
+        val fileWriter= new OutputStreamWriter(new FileOutputStream(file), "UTF-8")
+        try
+        {
+            write(fileWriter)
+        }
+        finally
+        {
+            fileWriter.close()
+        }
+    }
+
+    def write(writer : Writer)
+    {
+        val printer = new PrettyPrinter(140, 2)
+
+        writer.write(printer.formatNodes(xml))
+        writer.write("\n")
+        writer.flush()
+    }
+}
