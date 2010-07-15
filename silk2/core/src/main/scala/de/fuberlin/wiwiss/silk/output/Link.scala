d330ab9629daf3bcae29fe3b2c87b8bf6cfea00f
robertisele <robertisele@googlemail.com>
2010-07-15 10:51:04 +0000

    Extended ConfigLoader.load to accept streams
    Former-commit-id: f46fe6b9ea1448ae6d12a9cfb80f64748630882c

--- silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/output/Link.scala
+++ silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/output/Link.scala
@@ -2,9 +2,15 @@ package de.fuberlin.wiwiss.silk.output
 
 /**
  * Represents a link between two instances.
+ *
+ * @sourceUri the source URI
+ * @targetUri the target URI
+ * @confidence the confidence that this link is correct. Allowed values: [0.0, 1.0]
  */
 class Link(val sourceUri : String, val targetUri : String, val confidence : Double)
 {
+    require(confidence >= 0.0 && confidence <= 1.0, "confidence >= 0.0 && confidence <= 1.0 (confidence=" + confidence)
+
     override def toString = "<" + sourceUri + ">  <" + targetUri + "> (" + confidence + ")"
 
     /**
