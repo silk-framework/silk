693d9cee48d315ef45e94483c4ada7b049e18473
robertisele <robertisele@googlemail.com>
2010-07-27 17:38:13 +0000

    Distinguish between Resources and BlankNodes and dont generate Links for BlankNodes
    Former-commit-id: aa6d72d505a421ea963de79a361e7afd89884e10

--- silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/instance/Path.scala
+++ silk2/core/src/main/scala/de/fuberlin/wiwiss/silk/instance/Path.scala
@@ -1,17 +1,13 @@
 package de.fuberlin.wiwiss.silk.instance
 
+import collection.mutable.{SynchronizedMap, WeakHashMap}
 import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * Represents an RDF path.
  */
-case class Path(variable : String, operators : List[PathOperator])
+case class Path(variable : String, operators : List[PathOperator], id : Int = Path.currentId.getAndIncrement())
 {
-    /**
-     * Unique ID of this path.
-     */
-    val id = Path.currentId.getAndIncrement()
-
     /**
      * Serializes this path using the Silk RDF path language.
      */
@@ -27,7 +23,24 @@ case class Path(variable : String, operators : List[PathOperator])
 
 object Path
 {
-    private var currentId : AtomicInteger = new AtomicInteger(0)
+    private val pathCache = new WeakHashMap[String, Path]() with SynchronizedMap[String, Path]
 
-    def parse(path : String) = PathParser.parse(path)
+    private val currentId = new AtomicInteger(0)
+
+    /**
+     * Parses a path string.
+     * May return a cached copy.
+     */
+    def parse(pathStr : String) =
+    {
+        //Split the path into the variable and the operators
+        val variable = pathStr.tail.takeWhile(_ != '/')
+        val operators = pathStr.dropWhile(_ != '/')
+
+        //Try to retrieve a cached copy with the same operators. If not found, parse the path
+        val path = pathCache.getOrElseUpdate(operators, PathParser.parse(pathStr))
+
+        //Set the variable as the cached copy might use another variable
+        path.copy(variable = variable)
+    }
 }
