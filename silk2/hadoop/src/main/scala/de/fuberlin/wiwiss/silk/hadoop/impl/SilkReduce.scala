5366fb2d29aa9bc07129d7771747fd27ee87bff9
robertisele <robertisele@googlemail.com>
2010-09-25 11:32:18 +0000

    Hadoop configuration handling

--- silk2/hadoop/src/main/scala/de/fuberlin/wiwiss/silk/hadoop/impl/SilkReduce.scala
+++ silk2/hadoop/src/main/scala/de/fuberlin/wiwiss/silk/hadoop/impl/SilkReduce.scala
@@ -3,14 +3,16 @@ package de.fuberlin.wiwiss.silk.hadoop.impl
 import org.apache.hadoop.mapreduce.Reducer
 import scala.collection.JavaConversions._
 import org.apache.hadoop.io.Text
-import de.fuberlin.wiwiss.silk.hadoop.Silk
+import de.fuberlin.wiwiss.silk.hadoop.SilkConfiguration
 
 class SilkReduce extends Reducer[Text, InstanceSimilarity, Text, InstanceSimilarity]
 {
     protected override def reduce(sourceUri : Text, instanceSimilarities : java.lang.Iterable[InstanceSimilarity],
                                   context : Reducer[Text, InstanceSimilarity, Text, InstanceSimilarity]#Context)
     {
-        Silk.linkSpec.filter.limit match
+        val config = SilkConfiguration.get(context.getConfiguration)
+
+        config.linkSpec.filter.limit match
         {
             case Some(limit) =>
             {
