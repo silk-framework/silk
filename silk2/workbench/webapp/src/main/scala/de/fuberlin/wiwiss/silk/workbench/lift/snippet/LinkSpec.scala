749c40032682fb2f7de4968d8ce9a5d6d93cf1eb
robertisele <robertisele@googlemail.com>
2010-12-20 19:35:56 +0000

    Updates

--- silk2/workbench/webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/LinkSpec.scala
+++ silk2/workbench/webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/LinkSpec.scala
@@ -4,35 +4,26 @@ import xml.{PrettyPrinter, NodeSeq}
 import net.liftweb.http.{S, SHtml}
 import de.fuberlin.wiwiss.silk.workbench.project.Project
 import de.fuberlin.wiwiss.silk.config.{ConfigWriter, ConfigReader}
-import java.io.ByteArrayInputStream
+import net.liftweb.http.js.JE.JsRaw
+import net.liftweb.http.js.JsCmds.{Script, OnLoad}
 import net.liftweb.util.Helpers._
+import scala.xml.Utility.escape
 
 class LinkSpec
 {
   def render(xhtml : NodeSeq) : NodeSeq =
   {
-    var configStr = new PrettyPrinter(120, 2).format(ConfigWriter.serializeConfig(Project().config))
+//    def apply()
+//    {
+//      val config = ConfigReader.read(new ByteArrayInputStream(configStr.getBytes))
+//      Project.updateConfig(config)
+//      S.redirectTo("alignment")
+//    }
 
-    def apply()
-    {
-      val config = ConfigReader.read(new ByteArrayInputStream(configStr.getBytes))
-      Project.updateConfig(config)
-      S.redirectTo("alignment")
-    }
+    val linkConditionStr = ConfigWriter.serializeLinkCondition(Project().linkSpec.condition).toString.replace("\n", " ")
+    val linkConditionVar = "var linkCondition = '" + linkConditionStr + "';"
 
     bind("entry", xhtml,
-         "properties" -> properties(),
-         "linkSpec" -> SHtml.textarea(configStr, configStr = _, "rows" -> "30", "cols" -> "100"),
-         "submit" -> SHtml.submit("Apply", apply))
-  }
-
-  private def properties() : NodeSeq =
-  {
-    <ul>{
-      for(path <- Project().cache.instanceSpecs.source.paths) yield
-      {
-        <li>{path.toString}</li>
-      }
-    }</ul>
+         "linkSpec" -> Script(JsRaw(linkConditionVar).cmd))
   }
 }
--- silk2/workbench/webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/PrefixEditor.scala
+++ silk2/workbench/webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/PrefixEditor.scala
@@ -1,6 +1,51 @@
 package de.fuberlin.wiwiss.silk.workbench.lift.snippet
 
-class PrefixEditor
-{
+import xml.NodeSeq
+import net.liftweb.http.SHtml
+import net.liftweb.http.js.JsCmd
+import net.liftweb.http.js.JE.JsRaw
 
+object PrefixEditor
+{
+  def readPrefixes(f : Map[String, String] => Unit) : JsCmd =
+  {
+    def update(str : String) =
+    {
+      val prefixes = for(Array(prefix, namespace) <- str.split(',').grouped(2)) yield (prefix, namespace)
+
+      f(prefixes.toMap)
+
+      JsRaw("").cmd
+    }
+
+    SHtml.ajaxCall(JsRaw("$('#prefixTable tr td input').toArray().map(function (a) { return a.value; })"), update)._2.cmd
+  }
+
+  def prefixEditor(prefixes : Map[String, String] = Map.empty) : NodeSeq =
+  {
+    def addRow() =
+    {
+      JsRaw("$('#prefixTable').append(\"<tr><td><input type='text' /></td><td><input type='text' /></td></tr>\");").cmd
+    }
+
+    <table id="prefixTable">
+      <tr>
+        <th>Prefix</th>
+        <th>Namespace</th>
+      </tr>
+      {
+        for((prefix, namespace) <- prefixes) yield
+        {
+          <tr>
+            <td>{prefix}</td>
+            <td>{namespace}</td>
+          </tr>
+        }
+      }
+      <tr>
+        <td></td>
+        <td>{SHtml.ajaxButton("add", addRow _)}</td>
+      </tr>
+    </table>
+  }
 }
