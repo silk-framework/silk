490080ea6db60b7ffd53b816924e4541b0fb7d28
jentzsch <anja@anjeve.de>
2011-01-07 15:27:58 +0000

    - added annotation
    Former-commit-id: 44171a3b588dfa7773dc4d8b1d30974af698d662

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/UpperCaseTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/UpperCaseTransformer.scala
@@ -1,7 +1,9 @@
 package de.fuberlin.wiwiss.silk.impl.transformer
 
 import de.fuberlin.wiwiss.silk.linkspec.input.Transformer
+import de.fuberlin.wiwiss.silk.util.strategy.StrategyAnnotation
 
+@StrategyAnnotation(id = "upperCase", label = "Upper case", description = "Converts a string to upper case.")
 class UpperCaseTransformer(val params: Map[String, String] = Map()) extends Transformer
 {
     override def evaluate(strings : Seq[String]) =
