eae10b3ba557a2648edb0f3d04e56e128d022aec
robertisele <robertisele@googlemail.com>
2011-01-11 17:07:15 +0000

    LinkSpec editor receives the complete link condition now

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SourceTargetPair.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SourceTargetPair.scala
@@ -4,6 +4,8 @@ class SourceTargetPair[T](sourceValue : T, targetValue : T) extends Pair[T, T](s
 {
   def source = _1
   def target = _2
+
+  def map[U](f : (T) => U) = SourceTargetPair(f(sourceValue), f(targetValue))
 }
 
 object SourceTargetPair
