a8afd9607ef7ff53b508777832a92f6e6e56a53a
jentzsch <anja@anjeve.de>
2011-01-11 09:59:34 +0000

    - changed description

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/RemoveBlanksTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/RemoveBlanksTransformer.scala
@@ -3,5 +3,5 @@ package de.fuberlin.wiwiss.silk.impl.transformer
 import de.fuberlin.wiwiss.silk.linkspec.input.Transformer
 import de.fuberlin.wiwiss.silk.util.strategy.StrategyAnnotation
 
-@StrategyAnnotation(id = "replace", label = "Replace", description = "Replace all occurrences of a string \"search\" with \"replace\" in a string.")
+@StrategyAnnotation(id = "replace", label = "Replace", description = "Remove whitespace from a string.")
 class RemoveBlanksTransformer() extends ReplaceTransformer(" ", "")
