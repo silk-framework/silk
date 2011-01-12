7c7bffa764a5f6914d966a48017bbee6d1dac8de
robertisele <robertisele@googlemail.com>
2011-01-12 12:23:10 +0000

    Bugfixes
    Former-commit-id: bf9e353202d3afd856838b4cf2a5dc66e9377571

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/LogarithmTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/LogarithmTransformer.scala
@@ -2,6 +2,7 @@ package de.fuberlin.wiwiss.silk.impl.transformer
 
 import de.fuberlin.wiwiss.silk.util.strategy.StrategyAnnotation
 import de.fuberlin.wiwiss.silk.util.StringUtils._
+import de.fuberlin.wiwiss.silk.linkspec.input.Transformer
 
 @StrategyAnnotation(id = "logarithm", label = "Logarithm", description = "Transforms all numbers by applying the logarithm function. Non-numeric values are left unchanged.")
 class LogarithmTransformer(base : Int = 10) extends Transformer
@@ -10,7 +11,7 @@ class LogarithmTransformer(base : Int = 10) extends Transformer
   {
     strings.head match
     {
-      case DoubleLiteral(d) => math.log(d) / math.log(base)
+      case DoubleLiteral(d) => (math.log(d) / math.log(base)).toString
       case str => str
     }
   }
