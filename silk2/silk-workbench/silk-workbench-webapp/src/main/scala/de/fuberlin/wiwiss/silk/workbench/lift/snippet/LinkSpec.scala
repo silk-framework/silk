428d09863aa86d8b7a0bab27ae4113cf2076fa39
robertisele <robertisele@googlemail.com>
2011-01-20 12:36:07 +0000

    Separated toolbar and content elements in the template

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/LinkSpec.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/LinkSpec.scala
@@ -5,7 +5,7 @@ import net.liftweb.http.js.JE.{JsRaw}
 import net.liftweb.http.js.JsCmds.Script
 import net.liftweb.util.Helpers._
 import net.liftweb.http.js.JE.{Call, Str, Num, JsArray}
-import xml.{XML, NodeSeq}
+import xml.NodeSeq
 import net.liftweb.http.{S, SHtml}
 import net.liftweb.http.js.{JsObj, JsCmds}
 import de.fuberlin.wiwiss.silk.config.{ConfigWriter, ConfigReader}
@@ -13,34 +13,39 @@ import de.fuberlin.wiwiss.silk.instance.Path
 
 class LinkSpec
 {
-  def render(xhtml : NodeSeq) : NodeSeq =
+  def toolbar(xhtml : NodeSeq) : NodeSeq =
   {
-    /**
-     * Updates the Link Specification
-     */
-    def updateLinkSpec(linkSpecStr : String) =
-    {
-      try
-      {
-        val config = Project().config
-        val sourceMap = config.sources.map(source => (source.id, source)).toMap
-
-        val linkSpecXml = ConfigReader.readXML(linkSpecStr)
-        val linkSpec = ConfigReader.readLinkSpecification(linkSpecXml, config.prefixes, sourceMap)
-
-        Project.updateLinkSpec(linkSpec)
-        JsRaw("alert('Updated Link Specification')").cmd
-      }
-      catch
-      {
-        case ex : Exception => JsRaw("alert('Error updating Link Specification. Details: " + ex.getMessage.encJs + "')").cmd
-      }
-    }
-
     bind("entry", xhtml,
          "update" -> SHtml.ajaxButton("Update", () => SHtml.ajaxCall(Call("serializeLinkSpec"), updateLinkSpec)._2.cmd),
-         "download" -> SHtml.submit("Download", () => S.redirectTo("config")),
-         "linkSpec" -> Script(generateLinkSpecVar & generatePathsFunction))
+         "download" -> SHtml.submit("Download", () => S.redirectTo("config")))
+  }
+
+  def content(xhtml : NodeSeq) : NodeSeq =
+  {
+    bind("entry", xhtml,
+         "linkSpecVar" -> Script(generateLinkSpecVar))
+  }
+
+  /**
+   * Updates the Link Specification
+   */
+  private def updateLinkSpec(linkSpecStr : String) =
+  {
+    try
+    {
+      val config = Project().config
+      val sourceMap = config.sources.map(source => (source.id, source)).toMap
+
+      val linkSpecXml = ConfigReader.readXML(linkSpecStr)
+      val linkSpec = ConfigReader.readLinkSpecification(linkSpecXml, config.prefixes, sourceMap)
+
+      Project.updateLinkSpec(linkSpec)
+      JsRaw("alert('Updated Link Specification')").cmd
+    }
+    catch
+    {
+      case ex : Exception => JsRaw("alert('Error updating Link Specification. Details: " + ex.getMessage.encJs + "')").cmd
+    }
   }
 
   private def generateLinkSpecVar() =
