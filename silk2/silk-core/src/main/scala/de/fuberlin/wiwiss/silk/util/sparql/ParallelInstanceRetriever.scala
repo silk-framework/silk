a555ab51ba9d44c1030037acb6034690bbbbe70e
robertisele <robertisele@googlemail.com>
2011-01-23 18:51:43 +0000

    Added parallel instance retriever
    Former-commit-id: 1c70480b44ead66ed305222ccd1c0eabc44ad9ff

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/sparql/ParallelInstanceRetriever.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/sparql/ParallelInstanceRetriever.scala
@@ -1,170 +1,155 @@
-//package de.fuberlin.wiwiss.silk.util.sparql
-//
-//import de.fuberlin.wiwiss.silk.instance.{Path, Instance, InstanceSpecification}
-//
-//class ParallelInstanceRetriever(endpoint : SparqlEndpoint, pageSize : Int = 1000, graphUri : Option[String] = None)
-//{
-//  private val varPrefix = "v"
-//
-//  /**
-//   * Retrieves instances with a given instance specification.
-//   *
-//   * @param instanceSpec The instance specification
-//   * @param instances The URIs of the instances to be retrieved. If empty, all instances will be retrieved.
-//   * @return The retrieved instances
-//   */
-////  def retrieve(instanceSpec : InstanceSpecification, instances : Seq[String]) : Traversable[Instance] =
-////  {
-////    //if(instances.isEmpty)
-//////    {
-//////      retrieveAll(instanceSpec)
-//////    }
-////    //        else
-////    //        {
-////    //            retrieveList(instances, instanceSpec)
-////    //        }
-////  }
-//
-//  /**
-//   * Retrieves all instances with a given instance specification.
-//   *
-//   * @param instanceSpec The instance specification
-//   * @return The retrieved instances
-//   */
-////  def retrieveAll(instanceSpec : InstanceSpecification) : Traversable[Instance] =
-////  {
-////     val pathQueries = instanceSpec.paths.map(path => (path.id, queryPath(instanceSpec, path))).toMap
-////
-////     new InstanceTraversable(pathQueries, instanceSpec, None)
-////  }
-//
-//  private def queryPath(instanceSpec : InstanceSpecification, path : Path) =
-//  {
-//    //Prefixes
-//    var sparql = instanceSpec.prefixes.map{case (prefix, uri) => "PREFIX " + prefix + ": <" + uri + ">\n"}.mkString
-//
-//    //Select
-//    sparql += "SELECT DISTINCT "
-//    sparql += "?" + instanceSpec.variable + " "
-//    sparql += instanceSpec.paths.map("?" + varPrefix + _.id).mkString(" ") + "\n"
-//
-//    //Graph
-//    for(graph <- graphUri) sparql += "FROM <" + graph + ">\n"
-//
-//    //Body
-//    sparql += "WHERE {\n"
-//    if(instanceSpec.restrictions.isEmpty && instanceSpec.paths.isEmpty)
-//    {
-//      sparql += "?" + instanceSpec.variable + " ?" + varPrefix + "_p ?" + varPrefix + "_o "
-//    }
-//    else
-//    {
-//      sparql += instanceSpec.restrictions + "\n"
-//      sparql += SparqlPathBuilder(path :: Nil, "?" + instanceSpec.variable, "?" + varPrefix)
-//    }
-//    sparql += "}"
-//
-//    endpoint.query(sparql)
-//  }
-//
-//  /**
-//   * Wraps a Traversable of SPARQL results and retrieves instances from them.
-//   */
-//  private class InstanceTraversable(sparqlResults : Map[Int, Traversable[Map[String, Node]]], instanceSpec : InstanceSpecification, subject : Option[String]) extends Traversable[Instance]
-//  {
-//    override def foreach[U](f : Instance => U) : Unit =
-//    {
-////      //Remember current subject
-////      var curSubject : Option[String] = subject
-////
-////      //Collect values of the current subject
-////      val values = collection.mutable.HashMap[Int, Set[String]]()
-////
-////      while(true)
-//
-////      {
-////        for((id, results) <- sparqlResults) yield
-////        {
-////
-////        }
-////      }
-////
-////      for(result <- sparqlResults)
-////      {
-////        //If the subject is unknown, find binding for subject variable
-////        if(subject.isEmpty)
-////        {
-////          //Check if we are still reading values for the current subject
-////          val resultSubject = result.get(instanceSpec.variable) match
-////          {
-////            case Some(Resource(value)) => Some(value)
-////            case _ => None
-////          }
-////
-////          if(resultSubject != curSubject)
-////          {
-////            for(curSubjectUri <- curSubject)
-////            {
-////              f(new Instance(instanceSpec.variable, curSubjectUri, values.toMap))
-////            }
-////
-////            curSubject = resultSubject
-////            values.clear()
-////          }
-////        }
-////
-////        //Find results for values for the current subject
-////        if(curSubject.isDefined)
-////        {
-////          for((variable, node) <- result if variable.startsWith(varPrefix))
-////          {
-////            val id = variable.substring(varPrefix.length).toInt
-////
-////            val oldVarValues = values.get(id).getOrElse(Set())
-////
-////            values(id) = oldVarValues + node.value
-////          }
-////        }
-////      }
-////
-////      for(curSubjectUri <- curSubject)
-////      {
-////        f(new Instance(instanceSpec.variable, curSubjectUri, values.toMap))
-////      }
-//    }
-//  }
-//
-//  private class PathTraversable(variable : String, sparqlResults : Traversable[Map[String, Node]]) extends Traversable[Traversable[String]]
-//  {
-//    override def foreach[U](f : Traversable[String] => U) : Unit =
-//    {
-//      var currentSubject : String = null
-//      var currentValues : List[String] = Nil
-//
-//      for(result <- sparqlResults)
-//      {
-//        //Check if we are still reading values for the current subject
-//        val Resource(subject) = result(variable)
-//
-//        if(currentSubject == null)
-//        {
-//          currentSubject = subject
-//        }
-//
-//        if(subject != currentSubject)
-//        {
-//          f(currentValues)
-//
-//          currentSubject = subject
-//          currentValues = Nil
-//        }
-//        else
-//        {
-//          currentValues ::= subject
-//        }
-//      }
-//
-//      f(currentValues)
-//    }
-//  }
-//}
+package de.fuberlin.wiwiss.silk.util.sparql
+
+import de.fuberlin.wiwiss.silk.instance.{Path, Instance, InstanceSpecification}
+import collection.mutable.SynchronizedQueue
+
+class ParallelInstanceRetriever(endpoint : SparqlEndpoint, pageSize : Int = 1000, graphUri : Option[String] = None)
+{
+  private val varPrefix = "v"
+
+  private val maxQueueSize = 1000
+
+  /**
+   * Retrieves instances with a given instance specification.
+   *
+   * @param instanceSpec The instance specification
+   * @param instances The URIs of the instances to be retrieved. If empty, all instances will be retrieved.
+   * @return The retrieved instances
+   */
+  def retrieve(instanceSpec : InstanceSpecification, instances : Seq[String]) : Traversable[Instance] =
+  {
+    new InstanceTraversable(instanceSpec, instances)
+  }
+
+  /**
+   * Wraps a Traversable of SPARQL results and retrieves instances from them.
+   */
+  private class InstanceTraversable(instanceSpec : InstanceSpecification, instanceUris : Seq[String]) extends Traversable[Instance]
+  {
+    override def foreach[U](f : Instance => U) : Unit =
+    {
+      val pathRetrievers = for(path <- instanceSpec.paths) yield new PathRetriever(instanceUris, instanceSpec, path)
+
+      pathRetrievers.foreach(_.start())
+
+      while(pathRetrievers.forall(_.hasNext))
+      {
+        val values = for(pathRetriever <- pathRetrievers) yield pathRetriever.next()
+
+        f(new Instance(values.head.uri, values.map(_.values).toIndexedSeq, instanceSpec))
+      }
+    }
+  }
+
+  private class PathRetriever(instanceUris : Seq[String], instanceSpec : InstanceSpecification, path : Path) extends Thread
+  {
+    private val queue = new SynchronizedQueue[PathValues]()
+
+    def hasNext() : Boolean =
+    {
+      while(isAlive && queue.isEmpty)
+      {
+        Thread.sleep(100)
+      }
+
+      !queue.isEmpty
+    }
+
+    def next() : PathValues =
+    {
+      queue.dequeue
+    }
+
+    override def run()
+    {
+      if(instanceUris.isEmpty)
+      {
+        //Query for all instances
+        val sparqlResults = queryPath()
+        parseResults(sparqlResults)
+      }
+      else
+      {
+        //Query for a list of instances
+        for(instanceUri <- instanceUris)
+        {
+          val sparqlResults = queryPath(Some(instanceUri))
+          parseResults(sparqlResults, Some(instanceUri))
+        }
+      }
+    }
+
+    private def queryPath(fixedSubject : Option[String] = None) =
+    {
+      //Prefixes
+      var sparql = instanceSpec.prefixes.map{case (prefix, uri) => "PREFIX " + prefix + ": <" + uri + ">\n"}.mkString
+
+      //Select
+      sparql += "SELECT DISTINCT "
+      sparql += "?" + instanceSpec.variable + " "
+      sparql += "?" + varPrefix + "0\n"
+
+      //Graph
+      for(graph <- graphUri) sparql += "FROM <" + graph + ">\n"
+
+      //Body
+      sparql += "WHERE {\n"
+      fixedSubject match
+      {
+        case Some(subjectUri) =>
+        {
+          sparql += SparqlPathBuilder(path :: Nil, "<" + subjectUri + ">", "?" + varPrefix)
+        }
+        case None =>
+        {
+          sparql += instanceSpec.restrictions + "\n"
+          sparql += SparqlPathBuilder(path :: Nil, "?" + instanceSpec.variable, "?" + varPrefix)
+        }
+      }
+      sparql += "}"
+
+      endpoint.query(sparql)
+    }
+
+    private def parseResults(sparqlResults : Traversable[Map[String, Node]], fixedSubject : Option[String] = None)
+    {
+      var currentSubject : Option[String] = fixedSubject
+      var currentValues : Set[String] = Set.empty
+
+      for(result <- sparqlResults)
+      {
+        if(!fixedSubject.isDefined)
+        {
+          //Check if we are still reading values for the current subject
+          val Resource(subject) = result(instanceSpec.variable)
+
+          if(currentSubject.isEmpty)
+          {
+            currentSubject = Some(subject)
+          }
+          else if(subject != currentSubject.get)
+          {
+            while(queue.size > maxQueueSize)
+            {
+              Thread.sleep(100)
+            }
+
+            queue.enqueue(PathValues(currentSubject.get, currentValues))
+
+            currentSubject = Some(subject)
+            currentValues = Set.empty
+          }
+        }
+
+        for(node <- result.get(varPrefix + "0"))
+        {
+          currentValues += node.value
+        }
+      }
+
+      queue.enqueue(PathValues(currentSubject.get, currentValues))
+    }
+  }
+
+  private case class PathValues(uri : String, values : Set[String])
+}
