77b5b405f8e1b9706a293b0b26b445938ef8b01c
robertisele <robertisele@googlemail.com>
2011-01-24 00:07:10 +0000

    Added dbepdia_drugbank_drugs example project

--- silk2/silk-server/src/main/scala/de/fuberlin/wiwiss/silk/server/Boot.scala
+++ silk2/silk-server/src/main/scala/de/fuberlin/wiwiss/silk/server/Boot.scala
@@ -3,20 +3,19 @@ package bootstrap.liftweb
 import net.liftweb.http.LiftRules
 import de.fuberlin.wiwiss.silk.server.view.RestApi
 import de.fuberlin.wiwiss.silk.server.model.Server
-import java.io.File
-import de.fuberlin.wiwiss.silk.config.ConfigReader
 
 /**
-  * A class that's instantiated early and run.  It allows the application
-  * to modify lift's environment
-  */
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
 class Boot
 {
-    def boot
-    {
-        Server.start()
+  def boot
+  {
+    Server.start()
 
-        LiftRules.addToPackages("de.fuberlin.wiwiss.silk.server.view")
-        LiftRules.dispatch.prepend(RestApi.dispatch)
-    }
+    LiftRules.addToPackages("de.fuberlin.wiwiss.silk.server.view")
+    LiftRules.dispatch.prepend(RestApi.dispatch)
+  }
 }
+
