ffa2c56136a85fc0d8733ddb41c7b935537d25dd
robertisele <robertisele@googlemail.com>
2011-01-25 15:08:27 +0000

    Updated Project API
    Former-commit-id: 2f5d616deb3bb6208176a3f0efebe64e9f4135f1

--- silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/workspace/Module.scala
+++
@@ -1,8 +0,0 @@
-package de.fuberlin.wiwiss.silk.workbench.workspace
-
-trait Module
-{
-  /** The unique name of this module */
-  val name : String
-}
-
---
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/workspace/modules/Module.scala
@@ -0,0 +1,32 @@
+package de.fuberlin.wiwiss.silk.workbench.workspace.modules
+
+/**
+ * A project module.
+ */
+trait Module[ConfigType <: ModuleConfig, TaskType <: ModuleTask]
+{
+  /**
+   * The configuration of this module
+   */
+  def config : ConfigType
+
+  /**
+   * Updates the configuration of this module.
+   */
+  def config_=(c : ConfigType) : Unit
+
+  /**
+   *  Retrieves the tasks in this module.
+   */
+  def tasks : Traversable[TaskType]
+
+  /**
+   * Updates a specific task.
+   */
+  def update(task : TaskType) : Unit
+
+  /**
+   * Removes a task from this project.
+   */
+  def remove(task : TaskType) : Unit
+}
