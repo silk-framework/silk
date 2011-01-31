4da8aec8975532f9d3fb879aabfc7648cb4833c3
robertisele <robertisele@googlemail.com>
2011-01-31 15:45:08 +0000

    Added workspaceVar

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/Workspace.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/Workspace.scala
@@ -2,12 +2,50 @@ package de.fuberlin.wiwiss.silk.workbench.lift.snippet
 
 import xml.NodeSeq
 import net.liftweb.util.Helpers._
+import net.liftweb.http.SHtml
+import net.liftweb.json.JsonAST.{JArray, JValue}
+import net.liftweb.http.js.JE.{JsRaw, JsVar}
+import net.liftweb.json.JsonDSL._
+import net.liftweb.http.js.JsCmds.{Script, JsReturn}
+import de.fuberlin.wiwiss.silk.workbench.workspace.User
+import net.liftweb.http.js.{JE, JsCmds}
+import net.liftweb.json.JsonAST
 
 class Workspace
 {
   def content(xhtml : NodeSeq) : NodeSeq =
   {
     bind("entry", xhtml,
-         "workspaceVar" -> "TODO")
+         "workspaceVar" -> Script(JsRaw("var workspaceVar = " + pretty(JsonAST.render(generateJson)) + ";").cmd))
+  }
+
+  private def generateJson : JValue =
+  {
+    val project = User().project
+
+    val sources : JArray = for(task <- project.sourceModule.tasks.toSeq) yield
+    {
+      ("name" -> task.name) ~
+      ("url" -> task.source.dataSource.toString)
+    }
+
+    val linkingTasks : JArray = for(task <- project.linkingModule.tasks.toSeq) yield
+    {
+      ("name" -> task.name) ~
+      ("source" -> task.linkSpec.datasets.source.sourceId) ~
+      ("target" -> task.linkSpec.datasets.target.sourceId) ~
+      ("sourceDataset" -> task.linkSpec.datasets.source.restriction) ~
+      ("targetDataset" -> task.linkSpec.datasets.target.restriction)
+    }
+
+    ("workspace" ->
+      ("project" ->
+        (
+          ("name" -> "project") ~
+          ("dataSource" -> sources) ~
+          ("linkingTask" -> linkingTasks)
+        )
+      )
+    )
   }
 }
