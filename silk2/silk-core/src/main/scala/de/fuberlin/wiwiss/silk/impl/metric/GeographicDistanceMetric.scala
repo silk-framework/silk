904d456c5280c24d46aef83ea44c4af3bd10109d
robertisele <robertisele@googlemail.com>
2011-01-31 10:03:06 +0000

    Geographic Distance metric: set km as default unit.
    Former-commit-id: 473141cceecf430439bab2c3af46d536f15d5ec3

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/metric/GeographicDistanceMetric.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/metric/GeographicDistanceMetric.scala
@@ -20,8 +20,10 @@ import de.fuberlin.wiwiss.silk.util.strategy.StrategyAnnotation
   id = "wgs84",
   label = "Geographical distance",
   description = "Computes the geographical distance between two points. Author: Konrad Höffner (MOLE subgroup of Research Group AKSW, University of Leipzig)")
-class GeographicDistanceMetric(threshold : Double, unit : String = "", curveStyle : String = "linear") extends Metric
+class GeographicDistanceMetric(threshold : Double, unit : String = "km", curveStyle : String = "linear") extends Metric
 {
+  require(threshold > 0.0, "Threshold must be positive.")
+
   val multipliers = Map("km"->0.001,"kilometer"->0.001,"meter"->1.0,"m"->1.0)
 
   val unitMultiplier : Double = multipliers.get(unit).getOrElse(1)
