652b7fdb7df346a9ce5cc572671b619967781936
robertisele <robertisele@googlemail.com>
2011-02-03 16:27:48 +0000

    Improved CreateLinkingTaskDialog: Validation + Autocomplete
    Former-commit-id: 12e8f367097c686723f52256ef8bb10fe8c34bef

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/CreateLinkingTaskDialog.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/CreateLinkingTaskDialog.scala
@@ -11,6 +11,7 @@ import de.fuberlin.wiwiss.silk.workbench.workspace.modules.linking.LinkingTask
 import de.fuberlin.wiwiss.silk.evaluation.Alignment
 import net.liftweb.http.js.JE.JsRaw
 import net.liftweb.http.SHtml
+import net.liftweb.widgets.autocomplete.AutoComplete
 import net.liftweb.util.Helpers._
 
 /**
@@ -21,8 +22,8 @@ class CreateLinkingTaskDialog
   def render(xhtml : NodeSeq) : NodeSeq =
   {
     var name = ""
-    var sourceId = "http://www4.wiwiss.fu-berlin.de/sider/sparql"
-    var targetId = "http://www4.wiwiss.fu-berlin.de/drugbank/sparql"
+    var sourceId = ""
+    var targetId = ""
     var sourceRestriction = "?a rdf:type sider:drugs"
     var targetRestriction = "?b rdf:type drugbank:drugs"
     val prefixes = Map(
@@ -32,32 +33,46 @@ class CreateLinkingTaskDialog
       "sider" -> "http://www4.wiwiss.fu-berlin.de/sider/resource/sider/",
       "drugbank" -> "http://www4.wiwiss.fu-berlin.de/drugbank/resource/drugbank/")
 
+    val sourceNames = User().project.sourceModule.tasks.map(_.name.toString).toList
+
+    def completeSourceId(current : String, limit: Int) =
+    {
+      sourceNames.filter(_.startsWith(current)).take(limit)
+    }
+
     def submit(prefixes : Prefixes) =
     {
-      val linkSpec =
-        LinkSpecification(
-          id = "id",
-          linkType = "http://www.w3.org/2002/07/owl#sameAs",
-          datasets = SourceTargetPair(DatasetSpecification(sourceId, Constants.SourceVariable, sourceRestriction),
-                                      DatasetSpecification(targetId, Constants.TargetVariable, targetRestriction)),
-          condition = LinkCondition(None),
-          filter = LinkFilter(0.95, None),
-          outputs = Nil
-        )
+      try
+      {
+        val linkSpec =
+          LinkSpecification(
+            id = name,
+            linkType = "http://www.w3.org/2002/07/owl#sameAs",
+            datasets = SourceTargetPair(DatasetSpecification(sourceId, Constants.SourceVariable, sourceRestriction),
+                                        DatasetSpecification(targetId, Constants.TargetVariable, targetRestriction)),
+            condition = LinkCondition(None),
+            filter = LinkFilter(0.95, None),
+            outputs = Nil
+          )
 
-      val linkingTask = LinkingTask(name, prefixes, linkSpec, Alignment(), new Cache())
+        val linkingTask = LinkingTask(name, prefixes, linkSpec, Alignment(), new Cache())
 
-      User().project.linkingModule.update(linkingTask)
+        User().project.linkingModule.update(linkingTask)
 
-      JsRaw("$('#createLinkingTaskDialog').dialog('close'); document.forms['toolbarForm'].submit();").cmd
+        JsRaw("$('#createLinkingTaskDialog').dialog('close'); document.forms['toolbarForm'].submit();").cmd
+      }
+      catch
+      {
+        case ex : Exception => JsRaw("alert('" + ex.getMessage.encJs + "');").cmd
+      }
     }
 
     SHtml.ajaxForm(
       bind("entry", xhtml,
          "name" -> SHtml.text(name, name = _, "size" -> "60"),
-         "sourceId" -> SHtml.text(sourceId, sourceId = _, "size" -> "60"),
+         "sourceId" -> AutoComplete(sourceId, completeSourceId _, (id : String) => sourceId = id, "size" -> "60"),
          "sourceRestriction" -> SHtml.text(sourceRestriction, sourceRestriction = _, "size" -> "60"),
-         "targetId" -> SHtml.text(targetId, targetId = _, "size" -> "60"),
+         "targetId" -> AutoComplete(targetId, completeSourceId _, (id : String) => targetId = id, "size" -> "60"),
          "targetRestriction" -> SHtml.text(targetRestriction, targetRestriction = _, "size" -> "60"),
          "prefixes" -> PrefixEditor.prefixEditor(prefixes),
          "submit" -> SHtml.ajaxSubmit("Create", () => PrefixEditor.readPrefixes(submit))))
