deb0a8250449d676040f3c04a59facaad3fee842
a-matteini <a.matteini@mes-info.de>
2011-02-07 15:14:43 +0000

    Workspace.js minor update
    Former-commit-id: 7bb996f0fbce4649fa1ff72c8328d9f6e2a450a0

--- silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/workspace.js
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/workspace.js
@@ -1,3 +1,24 @@
+
+
+// -- callback functions --
+function removeNodeById(nodeId)
+{
+   var node = document.getElementById(nodeId);
+   if (node) node.parentNode.removeChild(node);
+}
+
+// TODO - this implementation would close/refresh the tree anyway -> therefore could be better to update workspaceVar and invoke UpdateWorkspace()
+function newDataSource(jsonDataSource,projectName)
+{
+    var projectNode =  document.getElementById("project_"+projectName);
+    if (projectNode)
+    {
+        //addDataSource(jsonDataSource, projectNode, projectName )
+        addDataSource(jQuery.parseJSON('{"name":"KEGG","url":"http://kegg.us"}'), projectNode, projectName );
+    }
+}
+
+// -- display functions --
 function addLeaf(leaf, parent, desc)
 {
           if (leaf){
@@ -21,17 +42,56 @@ function addAction(name, action, parent)
              parent.appendChild(action_span);
 }
 
-function removeNodeById(nodeId)
+function addDataSource(jsonDataSource,projectNode,projectName)
 {
-   // TODO could be more elegant
-   if (document.getElementById(nodeId))
-      {var toDelete = document.getElementById(nodeId);
-       toDelete.parentNode.removeChild(toDelete);
-      }
+    var ds_name_ul = document.createElement("ul");
+        ds_name_ul.setAttribute('id','datasource_'+projectName+'_'+jsonDataSource.name);
+        projectNode.appendChild(ds_name_ul);
+    var ds_name_li = document.createElement("li");
+        ds_name_li.setAttribute('class', 'closed');
+        ds_name_ul.appendChild(ds_name_li);
+    var ds_name_span = document.createElement("span");
+        ds_name_span.setAttribute('class', 'folder');
+        ds_name_span.innerHTML = 'Data Source: '+jsonDataSource.name;
+        ds_name_li.appendChild(ds_name_span);
+
+    // TODO - missing back-end function
+    addAction('edit',"editDataSource('"+projectName+"','"+ jsonDataSource.name+"')",ds_name_li);
+    // TODO - missing back-end function
+    addAction('remove',"removeNodeById('datasource_"+projectName+"_"+jsonDataSource.name+"')",ds_name_li);
+
+    addLeaf(jsonDataSource.url,ds_name_li, 'url: ');
 }
 
+function addLinkingTask(jsonLinkingTask,projectNode,projectName)
+{
+    var lt_name_ul = document.createElement("ul");
+        // TODO id must be unique
+        lt_name_ul.setAttribute('id','linkingtask_'+projectName+'_'+jsonLinkingTask.name);
+        projectNode.appendChild(lt_name_ul);
+    var lt_name_li = document.createElement("li");
+        lt_name_li.setAttribute('class', 'closed');
+        lt_name_ul.appendChild(lt_name_li);
+    var lt_name_span = document.createElement("span");
+        lt_name_span.setAttribute('class', 'folder');
+        lt_name_span.innerHTML = 'Linking Task: '+jsonLinkingTask.name;
+        lt_name_li.appendChild(lt_name_span);
 
-// display the workspace as treeview
+    // TODO - missing back-end function
+    addAction('edit restriction',"editLinkngRestriction('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_name_li);
+    // TODO - missing back-end function
+    addAction('edit link',"openLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_name_li);
+    addAction('remove',"removeLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_name_li);
+    // TODO using callback functions would be..
+    //addAction('remove',"removeLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"',removeNodeById(linkingtask_"+projectName+"_"+jsonLinkingTask.name+")",lt_name_li);
+
+    addLeaf(jsonLinkingTask.source,lt_name_li, 'source: ');
+    addLeaf(jsonLinkingTask.target,lt_name_li, 'target: ');
+    addLeaf(jsonLinkingTask.sourceDataset,lt_name_li, 'source dataset: ');
+    addLeaf(jsonLinkingTask.targetDataset,lt_name_li, 'target dataset: ');
+}
+
+// -- display the workspace as treeview
 function updateWorkspace(obj){
         //var obj = jQuery.parseJSON('{"workspace": {   "project": [ {"name": "Project_1", "dataSource":[ {"name":"KEGG","url":"http://kegg.us"},{"name":"Wiki","url":"http://aba.com"}  ],  "linkingTask":[ {"name":"Gene","source":"KEGG","target":"Wiki","targetDataset":"rdf:type Wiki:Gene","sourceDataset":"rdf:type KEGG:gene"},{"name":"linkTask2","source":"KEGG","target":"Wiki"} ] }, {"name": "Project_2", "dataSource":[ {"name":"ABA","url":"http://aba.com"} ] }     ] } }');
 
@@ -41,72 +101,61 @@ function updateWorkspace(obj){
         var tree = document.createElement("div");
                 tree.id = "div_tree";
 
-        var root = document.createElement("ul");
+        var root = document.createElement("ul");       function addLeaf(leaf, parent, desc)
+{
+          if (leaf){
+             var leaf_ul = document.createElement("ul");
+                 parent.appendChild(leaf_ul);
+             var leaf_li = document.createElement("li");
+                 leaf_ul.appendChild(leaf_li);
+             var leaf_span = document.createElement("span");
+                 leaf_span.setAttribute('class', 'file');
+                 leaf_span.innerHTML = desc+leaf;
+                 leaf_li.appendChild(leaf_span);
+          }
+}                                           var ds_name_li =
+
+function addAction(name, action, parent)
+{
+         var action_span = document.createElement("span");
+             action_span.setAttribute('class','action');
+             action_span.setAttribute('onclick',action);
+             action_span.innerHTML = name;
+             parent.appendChild(action_span);
+}
             root.id = 'tree';
             root.setAttribute('class','filetree');
             tree.appendChild(root);
 
         // for each project                                  
         for (var p in obj.workspace.project) {
-           var  proj = document.createElement("li");
+            var project = obj.workspace.project[p];
+            var  proj = document.createElement("li");
                 proj.setAttribute('class', 'closed');
+                proj.setAttribute('id','project_'+project.name);
                 root.appendChild(proj);
                 var proj_span = document.createElement("span");
                     proj_span.setAttribute('class', 'folder');
-                    proj_span.innerHTML = obj.workspace.project[p].name;
+                    proj_span.innerHTML = project.name;
                     proj.appendChild(proj_span);
 
-                addAction('add DataSource',"newDataSource('"+obj.workspace.project[p].name+"')",proj);
-                addAction('add LinkingTask',"newLinkingTask('"+obj.workspace.project[p].name+"')",proj);
+                addAction('add DataSource',"newDataSource('','"+project.name+"')",proj);
+                addAction('add LinkingTask',"newLinkingTask('"+project.name+"')",proj);
 
              // display dataSource
-            for (var d in obj.workspace.project[p].dataSource){
-                var ds_name_ul = document.createElement("ul");
-                    ds_name_ul.setAttribute('id','datasource_'+obj.workspace.project[p].name+'_'+obj.workspace.project[p].dataSource[d].name);
-                    proj.appendChild(ds_name_ul);
-                var ds_name_li = document.createElement("li");
-                    ds_name_li.setAttribute('class', 'closed');
-                    ds_name_ul.appendChild(ds_name_li);
-                var ds_name_span = document.createElement("span");
-                    ds_name_span.setAttribute('class', 'folder');
-                    ds_name_span.innerHTML = 'Data Source: '+obj.workspace.project[p].dataSource[d].name;
-                    ds_name_li.appendChild(ds_name_span);
-
-                addAction('edit',"editDataSource('"+obj.workspace.project[p].name+"','"+ obj.workspace.project[p].dataSource[d].name+"')",ds_name_li);
-                addAction('remove',"removeNodeById('datasource_"+obj.workspace.project[p].name+"_"+obj.workspace.project[p].dataSource[d].name+"')",ds_name_li);
-
-                addLeaf(obj.workspace.project[p].dataSource[d].url,ds_name_li, 'url: ');
+            for (var d in obj.workspace.project[p].dataSource)
+            {
+                addDataSource(project.dataSource[d],proj,project.name);
             }
 
             // display linkingTask
-            for (var l in obj.workspace.project[p].linkingTask){
-                var lt_name_ul = document.createElement("ul");
-                    // TODO id must be unique, that's not
-                    lt_name_ul.setAttribute('id','linkingtask_'+obj.workspace.project[p].name+'_'+obj.workspace.project[p].linkingTask[l].name);
-                    proj.appendChild(lt_name_ul);
-                var lt_name_li = document.createElement("li");
-                    lt_name_li.setAttribute('class', 'closed');
-                    lt_name_ul.appendChild(lt_name_li);
-                var lt_name_span = document.createElement("span");
-                    lt_name_span.setAttribute('class', 'folder');
-                    lt_name_span.innerHTML = 'Linking Task: '+obj.workspace.project[p].linkingTask[l].name;
-                    lt_name_li.appendChild(lt_name_span);
-
-                addAction('edit restriction',"editLinkngRestriction('"+obj.workspace.project[p].name+"','"+ obj.workspace.project[p].linkingTask[l].name+"')",lt_name_li);
-                addAction('edit link',"openLinkingTask('"+obj.workspace.project[p].name+"','"+ obj.workspace.project[p].linkingTask[l].name+"')",lt_name_li);
-                addAction('remove',"removeLinkingTask('"+obj.workspace.project[p].name+"','"+ obj.workspace.project[p].linkingTask[l].name+"')",lt_name_li);
-                // TODO using callback functions would be..
-                //addAction('remove',"removeLinkingTask('"+obj.workspace.project[p].name+"','"+ obj.workspace.project[p].linkingTask[l].name+"',removeNodeById(linkingtask_"+obj.workspace.project[p].name+"_"+obj.workspace.project[p].linkingTask[l].name+")",lt_name_li);
-
-                addLeaf(obj.workspace.project[p].linkingTask[l].source,lt_name_li, 'source: ');
-                addLeaf(obj.workspace.project[p].linkingTask[l].target,lt_name_li, 'target: ');
-                addLeaf(obj.workspace.project[p].linkingTask[l].sourceDataset,lt_name_li, 'source dataset: ');
-                addLeaf(obj.workspace.project[p].linkingTask[l].targetDataset,lt_name_li, 'target dataset: ');
-
-                }
+            for (var l in obj.workspace.project[p].linkingTask)
+            {
+                addLinkingTask(project.linkingTask[l],proj,project.name);
+            }
         }
 
         document.getElementById("content").appendChild(tree);
 
-        $("#tree").treeview();
+        $("#tree").treeview();          
     }
