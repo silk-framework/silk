a3d0b4c10a461dd953a0bcea8ab03f1620cc00b5
robertisele <robertisele@googlemail.com>
2011-02-17 13:12:21 +0000

    Showing current project in workspace tab
    Former-commit-id: 954cfc8793224690d2136f641b5e1b25085002d8

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/bootstrap/liftweb/Boot.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/bootstrap/liftweb/Boot.scala
@@ -43,9 +43,12 @@ class Boot
     // Build SiteMap
     val ifLinkingTaskOpen = If(() => User().linkingTaskOpen, () => RedirectResponse("/index"))
 
+    val workspaceText = LinkText[Unit](_ => Text(if(User().projectOpen) "Workspace: " + User().project.name else "Workspace"))
+    val linkSpecText = LinkText[Unit](_ => Text("Link Specification: " + User().linkingTask.name))
+
     val entries =
-        Menu(Loc("Workspace", List("index"), "Workspace")) ::
-        Menu(Loc("Link Specification", List("linkSpec"), LinkText[Unit](_ => Text("Link Specification: " + User().linkingTask.name)), ifLinkingTaskOpen)) ::
+        Menu(Loc("Workspace", List("index"), workspaceText)) ::
+        Menu(Loc("Link Specification", List("linkSpec"), linkSpecText, ifLinkingTaskOpen)) ::
         Menu(Loc("Evaluate", List("evaluate"), "Evaluate", ifLinkingTaskOpen)) ::
         Menu(Loc("Reference Links", List("alignment"), "Reference Links", ifLinkingTaskOpen)) :: Nil
 
