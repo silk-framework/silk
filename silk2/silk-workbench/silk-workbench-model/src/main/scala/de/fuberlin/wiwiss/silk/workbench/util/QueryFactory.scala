936f6b84fdf7404629056870ae787f77c3418abe
a-matteini <a.matteini@mes-info.de>
2011-02-23 14:10:42 +0000

    [workbench] LDE refactor and bug fix

--- silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/util/QueryFactory.scala
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/util/QueryFactory.scala
@@ -5,12 +5,12 @@ object QueryFactory{
 
   val mappingGraph = "smwGraphs:MappingRepository"
   val datasourceGraph = "smwGraphs:DataSourceInformationGraph"
-  val dataSourcesPref = "http://www.example.org/smw-lde/smwDatasources/"
-  val dataSourceLinksPref = "http://www.example.org/smw-lde/smwDatasourceLinks/"
+  val dataSources = "http://www.example.org/smw-lde/smwDatasources/"
+  val dataSourceLinks = "http://www.example.org/smw-lde/smwDatasourceLinks/"
 
   def getPrefixes = Map ( "smwGraphs" -> "http://www.example.org/smw-lde/smwGraphs/",
-                          "smwDatasourceLinks" -> dataSourceLinksPref,
-                          "smwDatasources" -> dataSourcesPref,
+                          "smwDatasourceLinks" -> dataSourceLinks,
+                          "smwDatasources" -> dataSources,
                           "smw-lde" -> "http://www.example.org/smw-lde/smw-lde.owl#",
                           "rdf" -> "http://www.w3.org/1999/02/22-rdf-syntax-ns#"  )
   
@@ -23,17 +23,18 @@ object QueryFactory{
 
   // create a new project
   def iNewProject(projectName : String) = {
-    val projectUri = dataSourceLinksPref + projectName
+    val projectUri = dataSourceLinks + projectName
     "INSERT DATA INTO "+mappingGraph+"  {  <"+projectUri+"> rdf:type  smw-lde:SilkMatchingDescription . <"+projectUri+">	smw-lde:linksTo <http://www.example.org/smw-lde/smwDatasources/Wiki> .  <"+projectUri+">	smw-lde:sourceCode \"<Silk />\" .  <"+projectUri+">	rdf:label \""+projectName+"\" }"
   }
 
   // delete a project by uri
-  def dProject(projectName : String) =  "DELETE DATA FROM  "+mappingGraph+"  { <"+dataSourceLinksPref+projectName+"> ?p ?o } "
+  def dProject(projectName : String) =  "DELETE DATA FROM  "+mappingGraph+"  { <"+dataSourceLinks+projectName+"> ?p ?o } "
 
   //-- LDEProject --
 
-  // retrieve a project by uri
-  def sProject(projectUri : String) = "SELECT ?xml ?from FROM "+mappingGraph+" WHERE  {  <"+projectUri+"> rdf:type  smw-lde:SilkMatchingDescription . OPTIONAL {<"+projectUri+"> smw-lde:sourceCode ?xml } . OPTIONAL { <"+projectUri+"> smw-lde:linksFrom ?from } }"
+  // retrieve a project info by uri
+  def sProjectDataSource(projectUri : String) = "SELECT ?from FROM "+mappingGraph+" WHERE  {   <"+projectUri+"> smw-lde:linksFrom ?from  }"
+  def sProjectSourceCode(projectUri : String) = "SELECT ?xml FROM "+mappingGraph+" WHERE  {  <"+projectUri+"> smw-lde:sourceCode ?xml   }"
 
   // retrieve all datasources
   def sDataSources  =   " SELECT ?uri " +
@@ -48,7 +49,7 @@ object QueryFactory{
   // SMW constraint - every link specification MUST have the WIKI datasource as TARGET source
   def dDataSource(projectUri : String) = "DELETE DATA FROM  "+mappingGraph+"  {<"+projectUri+"> smw-lde:linksFrom ?datasource} "
   // create a SOURCE datasource link
-  def iDataSource(projectUri : String, dataSourceId : String) = "INSERT DATA INTO  "+mappingGraph+" {<"+projectUri+"> smw-lde:linksFrom <"+dataSourcesPref+dataSourceId+"\"> } "
+  def iDataSource(projectUri : String, dataSourceId : String) = "INSERT DATA INTO  "+mappingGraph+" {<"+projectUri+"> smw-lde:linksFrom <"+dataSources+dataSourceId+"> } "
 
   // - Update Link Spec 
   // delete sourceCode mapping property
