e094e91d411a2847f113aa407ed3613e03b3431c
a-matteini <a.matteini@mes-info.de>
2011-02-28 18:05:17 +0000

    [Workbench] Added LDE properties collectortrunk/silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/util/QueryFactory.scala
    Former-commit-id: 552ff4f3945f67ce494f5625c44fde390e5c73fb

---
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/instancespec/LDEPropertiesCollector.scala
@@ -0,0 +1,48 @@
+package de.fuberlin.wiwiss.silk.workbench.instancespec
+
+import de.fuberlin.wiwiss.silk.util.sparql.SparqlEndpoint
+import de.fuberlin.wiwiss.silk.util.Uri
+import java.util.logging.Logger
+import de.fuberlin.wiwiss.silk.instance.{Path, ForwardOperator}
+import de.fuberlin.wiwiss.silk.workbench.util.QueryFactory
+
+/**
+ * Retrieves property paths from the Wiki ontology
+ */
+object LDEPropertiesCollector
+{
+  private val logger = Logger.getLogger(LDEPropertiesCollector.getClass.getName)
+
+  /** The maximum number of properties */
+  private val MaxPropertyCount = 50
+
+  /**
+   * Retrieves a list of properties which are defined on most instances.
+   */
+  def apply(endpoint : SparqlEndpoint, restriction : String) : Traversable[(Path, Double)] =
+  {
+    getAllPaths(endpoint, restriction)
+  }
+
+  private def getAllPaths(endpoint : SparqlEndpoint, restriction : String) : Traversable[(Path, Double)] =
+  {
+    val variable = restriction.dropWhile(_ != '?').drop(1).takeWhile(_ != ' ')
+    val category = restriction.split(' ').last
+
+    val sparql = QueryFactory.sPropertyPaths(category)
+
+    val results = endpoint.query(sparql, MaxPropertyCount).toList
+    if(!results.isEmpty)
+    {
+      for(result <- results) yield
+      {
+        (new Path(variable, ForwardOperator(Uri.fromURI(result("p").value, endpoint.prefixes)) :: Nil), 1.)
+      }
+    }
+    else
+    {
+      Traversable.empty
+    }
+  }
+    
+}
