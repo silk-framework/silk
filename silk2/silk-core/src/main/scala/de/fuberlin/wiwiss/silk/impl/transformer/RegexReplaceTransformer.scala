0f9560ce1e010ab72c290e343faea7126e24bcaa
robertisele <robertisele@googlemail.com>
2011-03-04 17:01:35 +0000

    RegexReplaceTransformer: Improved performance by using a compiled regex pattern

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/RegexReplaceTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/RegexReplaceTransformer.scala
@@ -2,12 +2,18 @@ package de.fuberlin.wiwiss.silk.impl.transformer
 
 import de.fuberlin.wiwiss.silk.linkspec.input.Transformer
 import de.fuberlin.wiwiss.silk.util.strategy.StrategyAnnotation
+import util.matching.Regex
 
-@StrategyAnnotation(id = "regexReplace", label = "Regex replace", description = "Replace all occurrences of a regex \"regex\" with \"replace\" in a string.")
+@StrategyAnnotation(
+  id = "regexReplace",
+  label = "Regex replace",
+  description = "Replace all occurrences of a regex \"regex\" with \"replace\" in a string.")
 class RegexReplaceTransformer(regex : String, replace : String) extends Transformer
 {
+  private val compiledRegex = new Regex(regex)
+
   override def evaluate(strings : Seq[String]) =
   {
-    strings.toList.head.replaceAll(regex, replace)
+    compiledRegex.replaceAllIn(strings.head, replace)
   }
 }
