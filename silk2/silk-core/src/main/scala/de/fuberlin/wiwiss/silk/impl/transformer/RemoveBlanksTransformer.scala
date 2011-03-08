50673b06aa2e93e349b13e28a4f30a5a7a3ac51c
jentzsch <anja@anjeve.de>
2011-03-08 19:09:24 +0000

    - renamed id and label
    Former-commit-id: 60381b8d7a872c26836e13fc3f759d55ab1ce315

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/RemoveBlanksTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/impl/transformer/RemoveBlanksTransformer.scala
@@ -3,5 +3,5 @@ package de.fuberlin.wiwiss.silk.impl.transformer
 import de.fuberlin.wiwiss.silk.linkspec.input.Transformer
 import de.fuberlin.wiwiss.silk.util.strategy.StrategyAnnotation
 
-@StrategyAnnotation(id = "replace", label = "Replace", description = "Remove whitespace from a string.")
+@StrategyAnnotation(id = "removeBlanks", label = "Remove blanks", description = "Remove whitespace from a string.")
 class RemoveBlanksTransformer() extends ReplaceTransformer(" ", "")
