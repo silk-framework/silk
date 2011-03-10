5009e628dd9389cca35c1cb4378bd2e523c6758b
jentzsch <anja@anjeve.de>
2011-03-10 16:36:03 +0000

    - more text for ticket #6 If no property paths found: error dialog in editor.

--- silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/editor.js
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/editor.js
@@ -792,7 +792,7 @@ function getPropertyPaths()
     else if (data.error !== undefined)
     {
       document.getElementById("paths").removeChild(document.getElementById("loading"));
-      alert("Error: " + data.error);
+      alert("Could not load property paths.\nError: " + data.error);
     }
     else
     {
