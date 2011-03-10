95088ae74ab5103c0d36f4cc9f98a1c82fd79239
a-matteini <a.matteini@mes-info.de>
2011-03-10 10:36:50 +0000

    Fixed treeview issues in IE7 and minor ui issue with Firefox - closed #61

--- silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/workspace.js
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/workspace.js
@@ -3,7 +3,6 @@ ws.activeProjectId ="";
 ws.activeTaskId = "";
 ws.activeNodesId = new Array();
 
-
 function saveOpenNodes(activeProject){
     ws.activeNodesId.length=0;
     if (activeProject!=="") ws.activeNodesId.push('project_'+activeProject);
@@ -58,13 +57,13 @@ function addLeaf(leaf, parent, desc)
 {
           if (leaf){
              var leaf_ul = document.createElement("ul");
-                 parent.appendChild(leaf_ul);
+                 $(parent).append(leaf_ul);
              var leaf_li = document.createElement("li");
-                 leaf_ul.appendChild(leaf_li);
+                 $(leaf_ul).append(leaf_li);
              var leaf_span = document.createElement("span");
-                 leaf_span.setAttribute('class', 'file');
-                 leaf_span.innerHTML = desc+leaf;
-                 leaf_li.appendChild(leaf_span);
+                 $(leaf_span).addClass('file')
+                 .text(desc + leaf)
+                 $(leaf_li).append(leaf_span);
           }
 }
 // using jquery.ui icons
@@ -73,43 +72,47 @@ function addAction(type, label, desc, action, parent, activeProject, enabled)
     var icon = getIcon(type);
     var action_button = document.createElement("button");
     if (enabled){
-            action_button.setAttribute('onclick', 'saveOpenNodes(\''+activeProject+'\');'+action);
-            action_button.setAttribute('class','action');
-            $(action_button).button({
+            $(action_button).click(function() {
+                saveOpenNodes(activeProject);
+                eval(action);
+            })
+            .addClass("action")
+            .button({
                 icons: {
                     primary: icon
                 },
                 label: label
             })
+
     } else {
-            action_button.setAttribute('class','ui-icon ui-state-disabled '+icon );
+            $(action_button).addClass('ui-icon ui-state-disabled '+icon);
     }
-    action_button.setAttribute('title', desc);
-    parent.appendChild(action_button);  
+    $(action_button).attr("title",desc);
+    $(parent).append(action_button);
 }
 
 function addDataSource(jsonDataSource,projectNode,projectName)
 {
     var ds_ul = document.createElement("ul");
-        projectNode.appendChild(ds_ul);
+        $(projectNode).append(ds_ul);
     var ds_li = document.createElement("li");
-        ds_li.setAttribute('id','datasource_'+projectName+'_'+jsonDataSource.name);
-        ds_li.setAttribute('class', 'closed');
-        ds_ul.appendChild(ds_li);
+        $(ds_li).attr("id",'datasource_'+projectName+'_'+jsonDataSource.name)
+        .addClass('closed');
+        $(ds_ul).append(ds_li);
     var ds_span = document.createElement("span");
-        ds_span.setAttribute('class', 'source');
-        ds_li.appendChild(ds_span);
+        $(ds_span).addClass('source');
+        $(ds_li).append(ds_span);
 
     var ds_label= document.createElement("span");
-        ds_label.setAttribute('class', 'label');
-        ds_label.innerHTML = jsonDataSource.name;
-        ds_span.appendChild(ds_label);
+        $(ds_label).addClass('label')
+        .text(jsonDataSource.name);
+        $(ds_span).append(ds_label);
 
     var ds_actions = document.createElement("div");
-        ds_actions.setAttribute('class', 'actions');
-        ds_span.appendChild(ds_actions);
-     addAction('ds_edit', "Edit", "Edit data source "+jsonDataSource.name,"editSourceTask('"+projectName+"','"+ jsonDataSource.name+"')",ds_actions,projectName,true);
-     addAction('delete', "Remove", "Remove DataSource "+jsonDataSource.name,"confirmDelete('removeSourceTask','"+projectName+"','"+jsonDataSource.name+"')",ds_actions,projectName,true);
+        $(ds_actions).addClass('actions');
+        $(ds_span).append(ds_actions);
+        addAction('ds_edit', 'Edit', "Edit data source "+jsonDataSource.name,"editSourceTask('"+projectName+"','"+ jsonDataSource.name+"')",ds_actions,projectName,true);
+        addAction('delete',"Remove DataSource "+jsonDataSource.name,"confirmDelete('removeSourceTask','"+projectName+"','"+jsonDataSource.name+"')",ds_actions,projectName,true); 
 
     for(var p in jsonDataSource.params) {
       var param = jsonDataSource.params[p];
@@ -120,26 +123,26 @@ function addDataSource(jsonDataSource,projectNode,projectName)
 function addLinkingTask(jsonLinkingTask,projectNode,projectName)
 {
     var lt_ul = document.createElement("ul");
-        projectNode.appendChild(lt_ul);
+        $(projectNode).append(lt_ul);
     var lt_li = document.createElement("li");
-        lt_li.setAttribute('id','linkingtask_'+projectName+'_'+jsonLinkingTask.name);
-        lt_li.setAttribute('class', 'closed');
-        lt_ul.appendChild(lt_li);
+        $(lt_li).attr("id",'linkingtask_'+projectName+'_'+jsonLinkingTask.name)
+        .addClass('closed');
+        $(lt_ul).append(lt_li);
     var lt_span = document.createElement("span");
-        lt_span.setAttribute('class', 'link');
-        lt_li.appendChild(lt_span);
+        $(lt_span).addClass('link');
+        $(lt_li).append(lt_span);
 
     var lt_label = document.createElement("span");
-        lt_label.setAttribute('class', 'label');
-        lt_label.innerHTML = jsonLinkingTask.name;
-        lt_span.appendChild(lt_label);
+        $(lt_label).addClass('label')
+        .text(jsonLinkingTask.name);
+        $(lt_span).append(lt_label);
 
     var lt_actions = document.createElement("div");
-        lt_actions.setAttribute('class', 'actions');
-        lt_span.appendChild(lt_actions);
-    addAction('link_edit',"Metadata", "Edit metadata","editLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_actions,projectName,true);
-    addAction('link_spec',"Open", "Edit link specification "+jsonLinkingTask.name,"openLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_actions,projectName,true);
-    addAction('delete',"Remove", "Remove task "+jsonLinkingTask.name,"confirmDelete('removeLinkingTask','"+projectName+"','"+ jsonLinkingTask.name+"')",lt_actions,projectName,true);
+        $(lt_actions).addClass('actions');
+        $(lt_span).append(lt_actions);
+    addAction('link_edit', 'Metadata', "Edit metadata","editLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_actions,projectName,true);
+    addAction('link_spec', 'Open', "Edit link specification "+jsonLinkingTask.name,"openLinkingTask('"+projectName+"','"+ jsonLinkingTask.name+"')",lt_actions,projectName,true);
+    addAction('delete', 'Remove',"Remove task "+jsonLinkingTask.name,"confirmDelete('removeLinkingTask','"+projectName+"','"+ jsonLinkingTask.name+"')",lt_actions,projectName,true);
 
     addLeaf(jsonLinkingTask.source,lt_li, 'source: ');
     addLeaf(jsonLinkingTask.target,lt_li, 'target: ');
@@ -157,57 +160,57 @@ function updateWorkspace(obj){
         // root folder
         if (!document.getElementById("root-folder")){
             var rootFolder = document.createElement("div");
-            rootFolder.setAttribute('id','root-folder');  
-            document.getElementById("content").appendChild(rootFolder);
+            $(rootFolder).attr("id",'root-folder');  
+            $("#content").append(rootFolder);
         }
 
         // new project button
         if (!document.getElementById("newproject")) {
             var newProj = document.createElement("div");
-            newProj.setAttribute('id','newproject');
-            addAction('add','Project', 'Create new project',"createProject()",newProj,"",true);
-            document.getElementById("content").appendChild(newProj);
+            $(newProj).attr("id",'newproject');
+            addAction('add', 'Project','Create new project',"createProject()",newProj,"",true);
+            $("#content").append(newProj);
         }
 
         // import project button
         if (!document.getElementById("import")) {
             var importProj = document.createElement("div");
-            importProj.setAttribute('id','import');
-            addAction('import','Import', 'Import a project',"importProject()",importProj,"",true);
-            document.getElementById("content").appendChild(importProj);
-        }
-
+            $(importProj).attr("id",'import');
+            addAction('import','Import','Import a project',"importProject()",importProj,"",true);
+            $("#content").append(importProj);
+         }
+    
         var tree = document.createElement("div");
-                tree.id = "div_tree";
+            tree.id = "div_tree";
 
         var root = document.createElement("ul");      
-            root.id = 'tree';
-            root.setAttribute('class','filetree');
-            tree.appendChild(root);
+            $(root).attr("id", 'tree')
+            .addClass('filetree');
+            $(tree).append(root);
 
         // for each project                                  
         for (var p in obj.workspace.project) {
             var project = obj.workspace.project[p];
             var  proj = document.createElement("li");
-                proj.setAttribute('class', 'closed');
-                proj.setAttribute('id','project_'+project.name);
-                root.appendChild(proj);
+                $(proj).addClass('closed')
+                .attr("id",'project_'+project.name);
+                $(root).append(proj);
                 var proj_span = document.createElement("span");
-                    proj_span.setAttribute('class', 'folder');
-                    proj.appendChild(proj_span);
+                    $(proj_span).addClass('folder');
+                    $(proj).append(proj_span);
 
                 var proj_label = document.createElement("span");
-                    proj_label.setAttribute('class', 'label');
-                    proj_label.innerHTML = project.name;
-                    proj_span.appendChild(proj_label);
+                    $(proj_label).addClass('label')
+                    .text(project.name);
+                    $(proj_span).append(proj_label);
 
                 var proj_actions = document.createElement("div");
-                    proj_actions.setAttribute('class', 'actions');
-                    proj_span.appendChild(proj_actions);
-                addAction('ds_add','Source', 'Add DataSource',"createSourceTask('"+project.name+"')",proj_actions,project.name,true);
-                addAction('link_add','Task', 'Add linking task',"createLinkingTask('"+project.name+"')",proj_actions,project.name,true);
-                addAction('export','Export', 'Export Project '+project.name,"exportProject('"+project.name+"')",proj_actions,project.name,true);
-                addAction('delete','Remove', 'Remove project '+project.name,"confirmDelete('removeProject','"+project.name+"','')",proj_actions,'',true);
+                    $(proj_actions).addClass('actions');
+                    $(proj_span).append(proj_actions);
+                addAction('ds_add', 'Source','Add DataSource',"createSourceTask('"+project.name+"')",proj_actions,project.name,true);
+                addAction('link_add', 'Task','Add linking task',"createLinkingTask('"+project.name+"')",proj_actions,project.name,true);
+                addAction('export', 'Export','Export Project '+project.name,"exportProject('"+project.name+"')",proj_actions,project.name,true);
+                addAction('delete', 'Remove','Remove project '+project.name,"confirmDelete('removeProject','"+project.name+"','')",proj_actions,'',true);
 
 
              // display dataSource
@@ -223,7 +226,7 @@ function updateWorkspace(obj){
             }
         }
 
-        document.getElementById("content").appendChild(tree);
+        $("#content").append(tree);
 
         // uncollapse active project/task
         if(obj.workspace.activeProject){
@@ -255,13 +258,13 @@ function callAction(action,proj,res){
 
 function confirmDelete(action,proj,res){
      var confirmDialog = document.createElement("div");
-         confirmDialog.setAttribute('title','Delete');
-         confirmDialog.setAttribute('id','dialog');
+         $(confirmDialog).attr("title",'Delete')
+         .attr("id",'dialog');
      var dialogText = document.createElement("p");
-         dialogText.innerHTML = "Delete resource: "+proj+" "+res;
-         confirmDialog.appendChild(dialogText);
+         $(dialogText).text("Delete resource: "+proj+" "+res);
+         $(confirmDialog).append(dialogText);
 
-     document.getElementById("content").appendChild(confirmDialog);
+     $("#content").append(confirmDialog);
 
      $("#dialog").dialog({width: 400,
          modal: true,
