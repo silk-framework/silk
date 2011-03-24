0587ffe38f86fdae26e0ad8c691613e23ebdbb34
robertisele <robertisele@googlemail.com>
2011-03-24 16:52:49 +0000

    Evaluation: Using Comet instead of Ajax Polling

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/Evaluation.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/Evaluation.scala
@@ -1,10 +1,8 @@
 package de.fuberlin.wiwiss.silk.workbench.lift.snippet
 
 import xml.NodeSeq
-import net.liftweb.http.js.JsCmds.{Script, OnLoad, SetHtml}
 import net.liftweb.util.Helpers._
 import de.fuberlin.wiwiss.silk.workbench.lift.util.Widgets
-import de.fuberlin.wiwiss.silk.workbench.lift.util.JavaScriptUtils.PeriodicUpdate
 import de.fuberlin.wiwiss.silk.workbench.evaluation.EvaluationServer
 
 class Evaluation
@@ -13,41 +11,5 @@ class Evaluation
   {
     bind("entry", xhtml,
          "control" -> Widgets.taskControl(EvaluationServer.evaluationTask))
-  }
-
-  def content(xhtml : NodeSeq) : NodeSeq =
-  {
-    def createTable() =
-    {
-      <table border="1">
-        <tr>
-          <th>Source</th>
-          <th>Target</th>
-          <th>Confidence</th>
-          <th>Correct?</th>
-        </tr>
-        {
-          for((link, correct) <- EvaluationServer.links) yield
-          {
-            <tr>
-              <td><a href={link.sourceUri}>{link.sourceUri}</a></td>
-              <td><a href={link.targetUri}>{link.targetUri}</a></td>
-              <td>{link.confidence}</td>
-              <td>{correct}</td>
-            </tr>
-          }
-        }
-      </table>
-    }
-
-    //Updates the table message
-    def update() = SetHtml("alignemtTable", createTable())
-
-    bind("entry", xhtml,
-         "table" ->
-           <p>
-             <div id="alignemtTable">Waiting...</div>
-             <div>{Script(OnLoad(PeriodicUpdate(update, 300000)))}</div>
-           </p>)
   }
 }
