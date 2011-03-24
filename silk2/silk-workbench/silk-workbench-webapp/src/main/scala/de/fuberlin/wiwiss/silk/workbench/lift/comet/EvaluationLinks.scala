372f3d1d5d28b019d4eba0168f3a8f1b108933cd
robertisele <robertisele@googlemail.com>
2011-03-24 16:52:49 +0000

    Evaluation: Using Comet instead of Ajax Polling
    Former-commit-id: 0587ffe38f86fdae26e0ad8c691613e23ebdbb34

---
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/EvaluationLinks.scala
@@ -0,0 +1,75 @@
+package de.fuberlin.wiwiss.silk.workbench.lift.comet
+
+import net.liftweb.http.CometActor
+import collection.mutable.{Publisher, Subscriber}
+import de.fuberlin.wiwiss.silk.util.Task
+import de.fuberlin.wiwiss.silk.util.Task.Finished
+import de.fuberlin.wiwiss.silk.workbench.evaluation.EvaluationServer
+
+/**
+* A widget which displays the generated links of the evaluation server.
+*/
+class EvaluationLinks extends CometActor with Subscriber[Task.StatusMessage, Publisher[Task.StatusMessage]]
+{
+  EvaluationServer.evaluationTask.subscribe(this)
+
+  /** Minimum time in milliseconds between two successive updates*/
+  private val minUpdatePeriod = 3000L
+
+  /** The time of the last update */
+  private var lastUpdateTime = 0L
+
+  override def notify(pub : Publisher[Task.StatusMessage], status : Task.StatusMessage)
+  {
+    if(status.isInstanceOf[Finished] || System.currentTimeMillis - lastUpdateTime > minUpdatePeriod )
+    {
+      status match
+      {
+        case Task.StatusChanged(_, _) => reRender()
+        case Task.Finished(_, _) => reRender()
+        case _ =>
+      }
+
+      lastUpdateTime = System.currentTimeMillis
+    }
+  }
+
+  override def render =
+  {
+    if(!EvaluationServer.evaluationTask.isRunning)
+    {
+      <p></p>
+    }
+    else if(EvaluationServer.links.isEmpty)
+    {
+      <p>Waiting...</p>
+    }
+    else
+    {
+      <p>{renderTable}</p>
+    }
+  }
+
+  private def renderTable =
+  {
+    <table border="1">
+      <tr>
+        <th>Source</th>
+        <th>Target</th>
+        <th>Confidence</th>
+        <th>Correct?</th>
+      </tr>
+      {
+        for((link, correct) <- EvaluationServer.links) yield
+        {
+          <tr>
+            <td>{link.sourceUri}</td>
+            <td>{link.targetUri}</td>
+            <td>{link.confidence}</td>
+            <td>{correct}</td>
+          </tr>
+        }
+      }
+    </table>
+  }
+}
