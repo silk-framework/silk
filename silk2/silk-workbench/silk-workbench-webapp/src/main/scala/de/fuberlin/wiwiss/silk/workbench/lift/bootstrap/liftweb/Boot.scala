ae12dd16a150dd8fce5d758301ca7d35fa3e5f2f
robertisele <robertisele@googlemail.com>
2011-04-20 17:07:35 +0200

    -
    Former-commit-id: 8f3d4c54397dd367dbcee768bd2c1d5b88c7751d

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/bootstrap/liftweb/Boot.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/bootstrap/liftweb/Boot.scala
@@ -53,8 +53,7 @@ class Boot
         Menu(Loc("Workspace", List("index"), workspaceText, ifLinkingTaskClosed)) ::
         Menu(Loc("Link Specification", List("linkSpec"), linkSpecText, ifLinkingTaskOpen)) ::
         Menu(Loc("Evaluate", List("evaluate"), "Evaluate", ifLinkingTaskOpen)) ::
-        Menu(Loc("Reference Links", List("alignment"), "Reference Links", ifLinkingTaskOpen)) ::
-        Menu(Loc("Learn", List("learn"), "Learn", ifLinkingTaskOpen)) :: Nil
+        Menu(Loc("Reference Links", List("alignment"), "Reference Links", ifLinkingTaskOpen)) :: Nil
 
     LiftRules.setSiteMap(SiteMap(entries:_*))
 
