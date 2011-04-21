bd655823f86411ff6fbb5ecd045b1ade7395bca2
robertisele <robertisele@googlemail.com>
2011-04-21 14:28:14 +0200

    Implemented per-user evaluation tasks. Fixed #85.
    Former-commit-id: c7d72f747ee0d2940cf9357fb98ef5e6501b60fb

---
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/evaluation/EvaluationTask.scala
@@ -0,0 +1,106 @@
+package de.fuberlin.wiwiss.silk.workbench.evaluation
+
+import de.fuberlin.wiwiss.silk.util.SourceTargetPair
+import de.fuberlin.wiwiss.silk.util.Task
+import de.fuberlin.wiwiss.silk.instance.{MemoryInstanceCache, Instance, InstanceSpecification}
+import collection.mutable.Buffer
+import de.fuberlin.wiwiss.silk.output.Link
+import de.fuberlin.wiwiss.silk.{FilterTask, MatchTask, LoadTask}
+import de.fuberlin.wiwiss.silk.workbench.workspace.User
+import de.fuberlin.wiwiss.silk.evaluation.Alignment
+
+/**
+ * Task which executes the current link specification and allows querying for the generated links.
+ */
+class EvaluationTask(user : User) extends Task[Unit]
+{
+  /** The number of concurrent threads used for matching */
+  private val numThreads = 4
+
+  /** Generated links with detailed information */
+  private val generateDetailedLinks = true
+
+  /** The size of the instance partitions in the cache */
+  private val partitionSize = 300
+
+  private var loadTask : LoadTask = null
+
+  private var matchTask : MatchTask = null
+
+  private var alignment : Alignment = null
+
+  private var filteredLinks : Buffer[Link] = null
+
+  /**
+   * Retrieves the current links.
+   */
+  def links : Traversable[(Link, Int)] =
+  {
+    if(filteredLinks == null)
+    {
+      matchTask.links.view.map(checkLink)
+    }
+    else
+    {
+      filteredLinks.view.map(checkLink)
+    }
+  }
+
+  override protected def execute()
+  {
+    val project = user.project
+    val linkingTask = user.linkingTask
+    val linkSpec = linkingTask.linkSpec
+    val instanceSpecs = InstanceSpecification.retrieve(linkSpec)
+
+    //Retrieve sources
+    val sources = linkSpec.datasets.map(_.sourceId).map(project.sourceModule.task(_).source)
+
+    //Blocking function
+    val blockCount = project.linkingModule.config.blocking.map(_.blocks).getOrElse(1)
+    def blockingFunction(instance : Instance) = linkSpec.condition.index(instance, linkSpec.filter.threshold).map(_ % blockCount)
+
+    //Instance caches
+    val caches = SourceTargetPair(new MemoryInstanceCache(instanceSpecs.source, blockCount, partitionSize),
+                                  new MemoryInstanceCache(instanceSpecs.target, blockCount, partitionSize))
+
+    //Create tasks
+    loadTask = new LoadTask(sources, caches, instanceSpecs, if(blockCount > 0) Some(blockingFunction _) else None)
+    matchTask = new MatchTask(linkingTask.linkSpec, caches, numThreads, generateDetailedLinks)
+    alignment = linkingTask.alignment
+    filteredLinks = null
+
+    //Load instances
+    loadTask.runInBackground()
+
+    //Execute matching
+    executeSubTask(matchTask, 0.95)
+
+    //Filter links
+    val filterTask = new FilterTask(matchTask.links, linkSpec.filter)
+    filteredLinks = executeSubTask(filterTask)
+  }
+
+
+  private def checkLink(link : Link) : (Link, Int) =
+  {
+    if(alignment.positiveLinks.contains(link))
+    {
+      (link, 1)
+    }
+    else if(alignment.negativeLinks.contains(link))
+    {
+      (link, -1)
+    }
+    else
+    {
+      (link, 0)
+    }
+  }
+
+  override def stopExecution()
+  {
+    if(loadTask != null) loadTask.cancel()
+    if(matchTask != null) matchTask.cancel()
+  }
+}
