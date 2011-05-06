2a0a23c66d10d88bca6bdfc7315e1d7f8e5cc585
robertisele <robertisele@googlemail.com>
2011-05-06 12:14:50 +0200

    PathInput: Bugfix

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/input/PathInput.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/input/PathInput.scala
@@ -12,6 +12,8 @@ case class PathInput(path : Path) extends Input
   /** The cached index of this path in the instance specification */
   private var pathIndex = -1
 
+  private var sourcePathCount = -1
+
   /**
    * Retrieves the values of this input for a given instance.
    *
@@ -20,16 +22,18 @@ case class PathInput(path : Path) extends Input
    */
   override def apply(instances : SourceTargetPair[Instance]) =
   {
-    if(pathIndex == -1)
+    if(pathIndex == -1 || sourcePathCount != instances.source.spec.paths.size)
     {
-        if(instances.source.spec.variable == path.variable)
-        {
-          pathIndex = instances.source.spec.pathIndex(path)
-        }
-        else if(instances.target.spec.variable == path.variable)
-        {
-          pathIndex = instances.target.spec.pathIndex(path)
-        }
+      if(instances.source.spec.variable == path.variable)
+      {
+        pathIndex = instances.source.spec.pathIndex(path)
+      }
+      else if(instances.target.spec.variable == path.variable)
+      {
+        pathIndex = instances.target.spec.pathIndex(path)
+      }
+
+      sourcePathCount = instances.source.spec.paths.size
     }
 
     if(instances.source.spec.variable == path.variable)
