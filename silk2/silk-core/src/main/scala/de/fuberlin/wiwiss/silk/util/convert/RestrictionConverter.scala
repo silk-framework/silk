06c8f19b29544b8a43eb997efd451e73c283179b
marco <marco@biggy.(none)>
2011-05-16 17:38:17 +0200

    restrictionConverter update
    Former-commit-id: c4c7abf564ddd549724975cc96aa3ae203ae0d27

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/convert/RestrictionConverter.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/convert/RestrictionConverter.scala
@@ -3,21 +3,21 @@ package de.fuberlin.wiwiss.silk.util.convert
 
 import util.parsing.combinator.RegexParsers
 import de.fuberlin.wiwiss.silk.util.ValidationException
-import de.fuberlin.wiwiss.silk.instance.Restriction.{Or, Condition, And}
 import de.fuberlin.wiwiss.silk.instance.{Path, Restriction, SparqlRestriction}
 import de.fuberlin.wiwiss.silk.config.Prefixes
 import util.parsing.input.CharSequenceReader
 
 import de.fuberlin.wiwiss.silk.instance.{Restriction, SparqlRestriction}
 import de.fuberlin.wiwiss.silk.config.Prefixes
+import de.fuberlin.wiwiss.silk.instance.Restriction.{Operator, Or, Condition, And}
 
 
 /**
  * Converts a SPARQL restriction to a Silk restriction.
  */
-object RestrictionConverter extends RegexParsers
+class RestrictionConverter(implicit prefixes : Prefixes) extends RegexParsers
 {
-  def apply(subjectVar : String, sparqlRestriction : SparqlRestriction)(implicit prefixes : Prefixes) : Restriction =
+  def apply(subjectVar : String, sparqlRestriction : SparqlRestriction) : Restriction =
   {
     parseAll(parser, new CharSequenceReader(sparqlRestriction.toString)) match {
       case Success(parsedPath, _) => parsedPath
@@ -29,24 +29,44 @@ object RestrictionConverter extends RegexParsers
 //    Restriction.empty
   }
 
-    def parser: Parser[Restriction] = triplePattern ^^ { r => Restriction(Some(r)) }
 
-    override val skipWhitespace = false
-
-    def triplePattern = subj ~ predicateObjectFilter ~ predicateObjectFilter ^^
-      { case v ~ p ~ o =>
-        Condition(Path.parse("?" + v + "/" + " " + p),
-          Set(o)) }
-
-    def subj = "?" ~> varAB ^^ { v => v }
-
-    def varAB = "a" | "b"
+  def parser: Parser[Restriction] = tripelPatterns ^^ { r => Restriction(Some(r)) }
 
   //TODO
-//    def predicateObjectFilter = " " ~> wordCharFilter ^^ { case " " ~ name => " "+name }
-//  def predicateObjectFilter = " " ~> wordCharFilter ~ (":" ~> wordCharFilter) ^^ { case prefix ~ name => prefix + ":" + name }
-//  def predicateObjectFilter = " " ~ wordCharFilter ~ ":" ~ wordCharFilter ^^ { case " " ~ prefix ~ ":" ~ name => " " + prefix + ":" + name}
-    def predicateObjectFilter = " " ~> wordCharFilter ~ ":" ~ wordCharFilter ^^ { case prefix ~ ":" ~ name => prefix + ":" + name}
+  /*
+  regex für:
+  beliebig viel whitespace + beliebig viele { + beliebig viel whitespace + triplePatterns + } + belebig viel whitepsace  + Union als String + ...
+   */
+  def unionPattern : Parser[Operator] = rep( anyWhitespace ~> tripelPatterns ~ "UNION" )
 
-    def wordCharFilter = """[a-zA-Z_]\w*""".r
-}                                       //\p{Punct}
+  //one or more whitespace
+  def anyWhitespace = """ \s+ """.r
+
+  // one or more curly brace
+  def brace =
+
+  def tripelPatterns: Parser[Operator] = rep( triplePattern <~ opt(".") ) ^^ { r => r match
+  {
+    case condition :: Nil => condition
+    case head :: tail => And(r)
+  }}
+
+  override val skipWhitespace = false
+
+  def triplePattern = subj ~ predicateObjectFilter ~ predicateObjectFilter  ^^
+    { case v ~ p ~ o => Condition.resolve(Path.parse("?" + v + "/" + " " + p), Set(o)) }
+
+  def subj = "?" ~> varAB ^^ { v => v }
+
+  def varAB = "a" | "b"
+
+  def predicateObjectFilter = " " ~> wordCharFilter ~ ":" ~ wordCharFilter ^^ { case prefix ~ ":" ~ name => prefix + ":" + name}
+
+  def wordCharFilter = """[a-zA-Z_]\w*""".r
+
+  //def end = predicateObjectFilter ~> pointFilter ^^ { e => e }
+
+
+}
+
+
