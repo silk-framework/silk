e6edc4abd82c951e7996c1d452007b6ff9125b11
robertisele <robertisele@googlemail.com>
2011-05-16 15:24:08 +0200

    Added missing prefixes argument to RestrictionConverter
    Former-commit-id: 8b1b9b318d34b6058ee94b95cce30353ce234450

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/convert/RestrictionConverter.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/convert/RestrictionConverter.scala
@@ -1,13 +1,14 @@
 package de.fuberlin.wiwiss.silk.util.convert
 
 import de.fuberlin.wiwiss.silk.instance.{Restriction, SparqlRestriction}
+import de.fuberlin.wiwiss.silk.config.Prefixes
 
 /**
  * Converts a SPARQL restriction to a Silk restriction.
  */
 object RestrictionConverter
 {
-  def apply(subjectVar : String, sparqlRestriction : SparqlRestriction) : Restriction =
+  def apply(subjectVar : String, sparqlRestriction : SparqlRestriction)(implicit prefixes : Prefixes) : Restriction =
   {
     //Dummy
     Restriction.empty
