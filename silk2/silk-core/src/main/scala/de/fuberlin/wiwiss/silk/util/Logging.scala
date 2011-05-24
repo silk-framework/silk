8ddde2d9e785e5fb03f64fe63d2eb48ce515ee51
robertisele <robertisele@googlemail.com>
2011-05-24 12:14:33 +0200

    Added logging util

---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/Logging.scala
@@ -0,0 +1,11 @@
+package de.fuberlin.wiwiss.silk.util
+
+import java.util.logging.{Level, Logger}
+
+object Logging
+{
+  def logQueries_=(enable : Boolean)
+  {
+    Logger.getLogger("de.fuberlin.wiwiss.silk.util.sparql").setLevel(Level.FINE)
+  }
+}
