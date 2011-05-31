208c3e1f3ccc8454da9b7988a9c846b6e92546f6
Anja Jentzsch <anja@anjeve.de>
2011-05-31 20:46:51 +0100

    Fixed #56

--- silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/about.js
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/about.js
@@ -5,7 +5,8 @@ $(document).ready(function() {
     '<p style="font-weight: bold;">Website</p>' +
     '<p style="margin-top:5px;"><a href="http://www4.wiwiss.fu-berlin.de/bizer/silk/">http://www4.wiwiss.fu-berlin.de/bizer/silk/</a></p>' +
     '<p style="font-weight: bold;">Acknowledgements</p>' +
-    '<p style="margin-top:5px;">This work was supported in part by Vulcan Inc. as part of its <a href="http://www.projecthalo.com">Project Halo</a> and by the EU FP7 project <a href="http://lod2.eu/">LOD2 - Creating Knowledge out of Interlinked Data</a> (Grant No. 257943).</p>')
+    '<p style="margin-top:5px;">This work was supported in part by Vulcan Inc. as part of its <a href="http://www.projecthalo.com">Project Halo</a> and by the EU FP7 project <a href="http://lod2.eu/">LOD2 - Creating Knowledge out of Interlinked Data</a> (Grant No. 257943).</p>' +
+    '<p style="margin-top:5px;">The icons were created by Yusuke Kamiyamane and are licensed under the <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 license</a>.</p>')
 		.dialog({
 			autoOpen: false,
 			title: 'About',
