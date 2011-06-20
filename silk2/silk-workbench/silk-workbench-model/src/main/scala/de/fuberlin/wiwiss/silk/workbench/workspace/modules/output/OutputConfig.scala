5ef33ae1e1851b43f6b7cbfa82fa588e3d942ca2
robertisele <robertisele@googlemail.com>
2011-06-20 14:42:39 +0200

    Workbench: Added Output Tasks
    Former-commit-id: 9ca06adfb71630c95396024e7cb9533822b82175

---
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/workspace/modules/output/OutputConfig.scala
@@ -0,0 +1,7 @@
+package de.fuberlin.wiwiss.silk.workbench.workspace.modules.output
+
+import de.fuberlin.wiwiss.silk.workbench.workspace.modules.ModuleConfig
+
+case class OutputConfig() extends ModuleConfig
+{
+}
