9ca06adfb71630c95396024e7cb9533822b82175
robertisele <robertisele@googlemail.com>
2011-06-20 14:42:39 +0200

    Workbench: Added Output Tasks

---
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/workspace/modules/output/OutputModule.scala
@@ -0,0 +1,11 @@
+package de.fuberlin.wiwiss.silk.workbench.workspace.modules.output
+
+import de.fuberlin.wiwiss.silk.workbench.workspace.modules.Module
+
+/**
+ * The source module which encapsulates all data sources.
+ */
+trait OutputModule extends Module[OutputConfig, OutputTask]
+{
+
+}
