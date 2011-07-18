342be30d4612410f20c25c96f1b1b3e529f8d521
a-matteini <a.matteini@mes-info.de>
2011-07-18 15:28:23 +0200

    Links in the About page now open in a new tab/window (target=_blank)
    Former-commit-id: 6f352a7850870d3b67bfff6ff09c1e861809188b

--- silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/about.js
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/webapp/static/js/about.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
 		.html('<p>Silk Workbench, version 2.4.2</p>' +
     '<p>© 2009-2011</p>' +
     '<p style="font-weight: bold;">Website</p>' +
-    '<p style="margin-top:5px;"><a href="http://www4.wiwiss.fu-berlin.de/bizer/silk/">http://www4.wiwiss.fu-berlin.de/bizer/silk/</a></p>' +
+    '<p style="margin-top:5px;"><a target="_blank" href="http://www4.wiwiss.fu-berlin.de/bizer/silk/">http://www4.wiwiss.fu-berlin.de/bizer/silk/</a></p>' +
     '<p style="font-weight: bold;">Acknowledgements</p>' +
-    '<p style="margin-top:5px;">This work was supported in part by Vulcan Inc. as part of its <a href="http://www.projecthalo.com">Project Halo</a> and by the EU FP7 project <a href="http://lod2.eu/">LOD2 - Creating Knowledge out of Interlinked Data</a> (Grant No. 257943).</p>' +
-    '<p style="margin-top:5px;">The icons were created by <a href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a> and are licensed under the <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 license</a>.</p>')
+    '<p style="margin-top:5px;">This work was supported in part by Vulcan Inc. as part of its <a target="_blank" href="http://www.projecthalo.com">Project Halo</a> and by the EU FP7 project <a target="_blank" href="http://lod2.eu/">LOD2 - Creating Knowledge out of Interlinked Data</a> (Grant No. 257943).</p>' +
+    '<p style="margin-top:5px;">The icons were created by <a target="_blank" href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a> and are licensed under the <a target="_blank" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 license</a>.</p>')
 		.dialog({
 			autoOpen: false,
 			title: 'About',
