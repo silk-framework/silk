9143138a7352446d852c4516ee7720a1c8d80cb4
robertisele <robertisele@googlemail.com>
2011-07-18 14:51:55 +0200

    Simplified cache interface

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/CacheLoadingProgress.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/CacheLoadingProgress.scala
@@ -7,4 +7,4 @@ import de.fuberlin.wiwiss.silk.workbench.workspace.User
 /**
  * Shows the progress of the cache loader task.
  */
-class CacheLoadingProgress extends ProgressWidget(User().linkingTask.cache.loader, true)
+class CacheLoadingProgress extends ProgressWidget(User().linkingTask.cache, true)
