36526e5fbafc6295ec6c3cd1b22b4a6536f727f6
robertisele <robertisele@googlemail.com>
2011-07-20 15:16:37 +0200

    Minor updates
    Former-commit-id: e181c706ee318e65d450fb32594974d7b2d24253

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/LinkCondition.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/LinkCondition.scala
@@ -9,8 +9,7 @@ import similarity.SimilarityOperator
 /**
  * A Link Condition specifies the conditions which must hold true so that a link is generated between two instances.
  */
-case class LinkCondition(rootOperator : Option[SimilarityOperator])
-{
+case class LinkCondition(rootOperator: Option[SimilarityOperator]) {
   /**
    * Computes the similarity between two instances.
    *
@@ -21,10 +20,8 @@ case class LinkCondition(rootOperator : Option[SimilarityOperator])
    *         -1.0 for definitive non-matches.
    *         +1.0 for definitive matches.
    */
-  def apply(instances : SourceTargetPair[Instance], limit : Double) : Double =
-  {
-    rootOperator match
-    {
+  def apply(instances: SourceTargetPair[Instance], limit: Double): Double = {
+    rootOperator match {
       case Some(operator) => operator(instances, limit).getOrElse(-1.0)
       case None => -1.0
     }
@@ -38,20 +35,18 @@ case class LinkCondition(rootOperator : Option[SimilarityOperator])
    *
    * @return A set of (multidimensional) indexes. Instances within the threshold will always get the same index.
    */
-  def index(instance : Instance, limit : Double = 0.0) : Set[Int] =
-  {
-    rootOperator match
-    {
-      case Some(operator) =>
-      {
+  def index(instance: Instance, limit: Double = 0.0): Set[Int] = {
+    rootOperator match {
+      case Some(operator) => {
         val indexes = operator.index(instance, limit)
 
         //Convert the index vectors to scalars in the range [0, Int.MaxValue]
-        for(index <- indexes) yield
-        {
-          val flatIndex = (index zip operator.blockCounts(limit)).foldLeft(0){case (iLeft, (iRight, blocks)) => iLeft * blocks + iRight}
+        for (index <- indexes) yield {
+          val flatIndex = (index zip operator.blockCounts(limit)).foldLeft(0) {
+            case (iLeft, (iRight, blocks)) => iLeft * blocks + iRight
+          }
 
-          if(flatIndex == Int.MinValue) 0 else abs(flatIndex)
+          if (flatIndex == Int.MinValue) 0 else abs(flatIndex)
         }
       }
       case None => Set.empty
@@ -62,10 +57,8 @@ case class LinkCondition(rootOperator : Option[SimilarityOperator])
   /**
    * The number of blocks in each dimension of the index.
    */
-  def blockCount(threshold : Double) =
-  {
-    rootOperator match
-    {
+  def blockCount(threshold: Double) = {
+    rootOperator match {
       case Some(operator) => operator.blockCounts(threshold).foldLeft(1)(_ * _)
       case None => 1
     }
@@ -74,10 +67,9 @@ case class LinkCondition(rootOperator : Option[SimilarityOperator])
   /**
    * Serializes this Link Condition as XML.
    */
-  def toXML(implicit prefixes : Prefixes) =
-  {
+  def toXML(implicit prefixes: Prefixes) = {
     <LinkCondition>
-      { rootOperator.toList.map(_.toXML) }
+      {rootOperator.toList.map(_.toXML)}
     </LinkCondition>
   }
 }
--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/Operator.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/Operator.scala
@@ -4,17 +4,28 @@ import xml.Node
 import de.fuberlin.wiwiss.silk.util.Identifier
 import java.util.concurrent.atomic.AtomicInteger
 
-trait Operator
-{
+/**
+ * Base class of all operators in the link condition.
+ */
+trait Operator {
   val id : Identifier
 }
 
-object Operator
-{
+/**
+ * Operator companion object.
+ */
+object Operator {
+  /** Counter used to generate unique identifiers. */
   private val lastId = new AtomicInteger(0)
 
-  def generateId = Identifier("unnamed" + lastId.incrementAndGet())
+  /**
+   * Generates a new operator identifier.
+   */
+  def generateId = Identifier("unnamed_" + lastId.incrementAndGet())
 
+  /**
+   * Reads the operator identifier from an xml element.
+   */
   def readId(xml : Node) : Identifier = {
     (xml \ "@id").headOption.map(_.text).map(Identifier(_)).getOrElse(generateId)
   }
