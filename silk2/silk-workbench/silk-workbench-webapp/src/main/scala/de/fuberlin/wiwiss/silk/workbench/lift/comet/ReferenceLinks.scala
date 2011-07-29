7870d40c2b767389ceae6846d48389d5bdc3e7aa
robertisele <robertisele@googlemail.com>
2011-07-29 13:40:45 +0200

    First version of learning interface based on linklist
    Former-commit-id: ff32588c7c2b01a6e7f02b709e5bf3fd9b8c1f36

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/ReferenceLinks.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/ReferenceLinks.scala
@@ -7,49 +7,39 @@ import de.fuberlin.wiwiss.silk.workbench.workspace.{User, UserData}
 import de.fuberlin.wiwiss.silk.linkspec.evaluation.DetailedEvaluator
 import net.liftweb.http.SHtml
 import xml.NodeSeq
-import net.liftweb.http.js.JsCmds.{OnLoad, SetHtml, Script, JsShowId, JsHideId}
+import net.liftweb.http.js.JsCmds.JsHideId
 import java.util.logging.Logger
 import de.fuberlin.wiwiss.silk.workbench.evaluation.EvalLink._
-import de.fuberlin.wiwiss.silk.util.Timer
 import de.fuberlin.wiwiss.silk.util.task._
 
-class ReferenceLinks extends LinkList
-{
+class ReferenceLinks extends LinkList {
   private implicit val logger = Logger.getLogger(classOf[ReferenceLinks].getName)
 
-  /** Minimum time in milliseconds between two successive updates*/
+  /**Minimum time in milliseconds between two successive updates*/
   private val minUpdatePeriod = 3000L
 
-  /** The time of the last update */
+  /**The time of the last update */
   private var lastUpdateTime = 0L
 
   override protected def registerEvents() {
-    /** Register to updates to the ShowLinks variable */
-    ShowLinks.subscribe(new Subscriber[UserData.ValueUpdated[EvalLink.ReferenceType], Publisher[UserData.ValueUpdated[EvalLink.ReferenceType]]]
-    {
-      def notify(pub : Publisher[UserData.ValueUpdated[EvalLink.ReferenceType]], status : UserData.ValueUpdated[EvalLink.ReferenceType])
-      {
+    /**Register to updates to the ShowLinks variable */
+    ShowLinks.subscribe(new Subscriber[UserData.ValueUpdated[EvalLink.ReferenceType], Publisher[UserData.ValueUpdated[EvalLink.ReferenceType]]] {
+      def notify(pub: Publisher[UserData.ValueUpdated[EvalLink.ReferenceType]], status: UserData.ValueUpdated[EvalLink.ReferenceType]) {
         partialUpdate(updateLinksCmd)
       }
     })
 
-    /** Register to status messages of the cache loader task in order to be notified when new links are available */
-    linkingTask.cache.subscribe(new Subscriber[Status, Publisher[Status]]
-    {
-      def notify(pub : Publisher[Status], status : Status)
-      {
-        status match
-        {
-          case _ : Started =>
-          {
+    /**Register to status messages of the cache loader task in order to be notified when new links are available */
+    linkingTask.cache.subscribe(new Subscriber[Status, Publisher[Status]] {
+      def notify(pub: Publisher[Status], status: Status) {
+        status match {
+          case _: Started => {
           }
-          case _ : Running if System.currentTimeMillis - lastUpdateTime > minUpdatePeriod =>
-          {
+          case _: Running if System.currentTimeMillis - lastUpdateTime > minUpdatePeriod => {
             partialUpdate(updateLinksCmd)
             lastUpdateTime = System.currentTimeMillis
           }
-          case _ : Finished =>
-          {
+          case _: Finished => {
             partialUpdate(updateLinksCmd)
           }
           case _ =>
@@ -58,30 +48,24 @@ class ReferenceLinks extends LinkList
     })
   }
 
-  override protected def links : Seq[EvalLink] =
-  {
+  override protected def links: Seq[EvalLink] = {
     def condition = linkingTask.linkSpec.condition
     def alignment = linkingTask.alignment
     def instances = linkingTask.cache.instances
 
-    ShowLinks() match
-    {
-      case Positive =>
-      {
-        for(link <- alignment.positive.toSeq.view) yield instances.positive.get(link) match
-        {
-          case Some(instances) =>
-          {
+    ShowLinks() match {
+      case Positive => {
+        for (link <- alignment.positive.toSeq.view) yield instances.positive.get(link) match {
+          case Some(instances) => {
             val evaluatedLink = DetailedEvaluator(condition, instances, -1.0).get
 
             new EvalLink(
               link = evaluatedLink,
-              correct = if(evaluatedLink.confidence >= 0.0) Correct else Incorrect,
+              correct = if (evaluatedLink.confidence >= 0.0) Correct else Incorrect,
               linkType = Positive
             )
           }
-          case None =>
-          {
+          case None => {
             val cleanLink = new Link(link.source, link.target)
 
             new EvalLink(
@@ -92,22 +76,18 @@ class ReferenceLinks extends LinkList
           }
         }
       }
-      case Negative =>
-      {
-        for(link <- alignment.negative.toSeq.view) yield instances.negative.get(link) match
-        {
-          case Some(instances) =>
-          {
+      case Negative => {
+        for (link <- alignment.negative.toSeq.view) yield instances.negative.get(link) match {
+          case Some(instances) => {
             val evaluatedLink = DetailedEvaluator(condition, instances, -1.0).get
 
             new EvalLink(
               link = evaluatedLink,
-              correct = if(evaluatedLink.confidence >= 0.0) Incorrect else Correct,
+              correct = if (evaluatedLink.confidence >= 0.0) Incorrect else Correct,
               linkType = Negative
             )
           }
-          case None =>
-          {
+          case None => {
             val cleanLink = new Link(link.source, link.target)
 
             new EvalLink(
@@ -121,10 +101,8 @@ class ReferenceLinks extends LinkList
     }
   }
 
-  override protected def renderStatus(link : EvalLink) : NodeSeq =
-  {
-    ShowLinks() match
-    {
+  override protected def renderStatus(link: EvalLink): NodeSeq = {
+    ShowLinks() match {
       case Positive if link.correct == Correct => <div>correct</div>
       case Positive if link.correct == Incorrect => <div>incorrect</div>
       case Negative if link.correct == Correct => <div>correct</div>
@@ -133,15 +111,13 @@ class ReferenceLinks extends LinkList
     }
   }
 
-  override protected def renderButtons(link : EvalLink) : NodeSeq =
-  {
+  override protected def renderButtons(link: EvalLink): NodeSeq = {
     <div>
       {SHtml.a(() => resetLink(link), <img src="./static/img/delete.png" />)}
     </div>
   }
 
-  private def resetLink(link : Link) =
-  {
+  private def resetLink(link: Link) = {
     val alignment = linkingTask.alignment
     val updatedTask = linkingTask.updateAlignment(alignment.copy(positive = alignment.positive - link, negative = alignment.negative - link), User().project)
 
