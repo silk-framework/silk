92b6c6dc3d28e3b91b25ebaba8d2f0f5ec9c8d66
robertisele <robertisele@googlemail.com>
2011-08-18 11:35:09 +0200

    Improved import dialog error messages. Fixed #183
    Former-commit-id: 86dce2210d1131f599d031ded674a4d252342bc7

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/AddLinkSpecificationDialog.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/AddLinkSpecificationDialog.scala
@@ -1,48 +1,44 @@
 package de.fuberlin.wiwiss.silk.workbench.lift.snippet
 
 import net.liftweb.http.js.JE.JsRaw
-import de.fuberlin.wiwiss.silk.workbench.workspace.{User}
-import xml.{XML, NodeSeq}
+import de.fuberlin.wiwiss.silk.workbench.workspace.User
+import xml.NodeSeq
 import java.io.ByteArrayInputStream
-import net.liftweb.http.{SHtml, FileParamHolder}
 import net.liftweb.http.js.JsCmds.OnLoad
 import net.liftweb.util.Helpers._
-import de.fuberlin.wiwiss.silk.workbench.lift.util.JS
-import net.liftweb.http.js.JsCmd
-import de.fuberlin.wiwiss.silk.workbench.workspace.io.{SilkConfigImporter, ProjectImporter}
+import de.fuberlin.wiwiss.silk.workbench.workspace.io.SilkConfigImporter
 import de.fuberlin.wiwiss.silk.config.SilkConfig
+import net.liftweb.http.{S, SHtml, FileParamHolder}
 
 /**
  * Dialog to add link specifications to a project.
  */
-class AddLinkSpecificationDialog
-{
-  def render(xhtml : NodeSeq) : NodeSeq =
-  {
-    var fileHolder : FileParamHolder = null
+class AddLinkSpecificationDialog {
+  def render(xhtml: NodeSeq): NodeSeq = {
+    var fileHolder: FileParamHolder = null
 
-    def submit()
-    {
-      fileHolder match
-      {
-        case FileParamHolder(_, mime, _, data) =>
-        {
-          val config = SilkConfig.load(new ByteArrayInputStream(data))
+    def submit() {
+      try {
+        fileHolder match {
+          case FileParamHolder(_, mime, _, data) => {
+            val config = SilkConfig.load(new ByteArrayInputStream(data))
 
-          SilkConfigImporter(config, User().project)
+            SilkConfigImporter(config, User().project)
+          }
+          case _ =>
         }
-        case _ =>
+      } catch {
+        case ex: Exception => S.warning("Error loading link specification: " + ex.getMessage)
       }
     }
 
     bind("entry", xhtml,
-         "file" -> SHtml.fileUpload(fileHolder = _),
-         "submit" -> SHtml.submit("Add", submit, "style" -> "float:right;"))
+      "file" -> SHtml.fileUpload(fileHolder = _),
+      "submit" -> SHtml.submit("Add", submit, "style" -> "float:right;"))
   }
 }
 
-object AddLinkSpecificationDialog
-{
+object AddLinkSpecificationDialog {
   def initCmd = OnLoad(JsRaw("$('#addLinkSpecificationDialog').dialog({ autoOpen: false, width: 500, modal: true })").cmd)
 
   def openCmd = JsRaw("$('#addLinkSpecificationDialog').dialog('open')").cmd
