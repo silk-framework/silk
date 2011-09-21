f5f3ece0c91e502b3cf8692b8ee6db3e09efd6a8
Robert Isele <robertisele@googlemail.com>
2011-09-21 17:02:11 +0200

    Show warnings  that have  been raised during link generation
    Former-commit-id: a500963a1718ecb47f8c6bb0f0573b3023ea2436

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/GenerateLinksControl.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/GenerateLinksControl.scala
@@ -6,6 +6,8 @@ import de.fuberlin.wiwiss.silk.workbench.lift.snippet.GenerateLinksDialog
 import de.fuberlin.wiwiss.silk.workbench.workspace.{CurrentTaskStatusListener, User, UserData}
 import de.fuberlin.wiwiss.silk.workbench.evaluation.CurrentGenerateLinksTask
 import de.fuberlin.wiwiss.silk.util.task.{TaskFinished, TaskStarted, TaskStatus}
+import net.liftweb.http.{S, SHtml}
+import net.liftweb.http.js.JsCmds.Alert
 
 class GenerateLinksControl extends DynamicButton {
 
@@ -32,7 +34,11 @@ class GenerateLinksControl extends DynamicButton {
     override def onUpdate(status: TaskStatus) {
       status match {
         case _: TaskStarted => label = "Stop"
-        case _: TaskFinished => label = "Start"
+        case _: TaskFinished => {
+          label = "Start"
+          val warnings = task.warnings
+            partialUpdate(Alert("There have been warnings while generating the links: " + warnings.map(_.getMessage).mkString("\n")))
+        }
         case _ =>
       }
     }
