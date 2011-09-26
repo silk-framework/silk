19c3a1b1b8ce4b49ccab1ef7d319882a66933c61
Robert Isele <robertisele@googlemail.com>
2011-09-26 23:20:30 +0200

    Add registerClasspath and registerJars methods to Factory
    Former-commit-id: b5fe0c0c3c40b4b57c803545a8791313e48ab88c

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/similarity/DistanceMeasure.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/linkspec/similarity/DistanceMeasure.scala
@@ -50,34 +50,4 @@ trait DistanceMeasure extends Strategy {
   }
 }
 
-/**
- * A simple similarity measure which compares pairs of values.
- */
-trait SimpleDistanceMeasure extends DistanceMeasure {
-  override final def apply(values1: Traversable[String], values2: Traversable[String], limit: Double): Double = {
-    var minDistance = Double.MaxValue
-
-    for (str1 <- values1; str2 <- values2) {
-      val distance = evaluate(str1, str2, min(limit, minDistance))
-      minDistance = min(minDistance, distance)
-    }
-
-    minDistance
-  }
-
-  override final def index(values: Set[String], limit: Double): Set[Seq[Int]] =  {
-    values.flatMap(value => indexValue(value, limit))
-  }
-
-  /**
-   * Computes the similarity of a pair of values.
-   */
-  def evaluate(value1: String, value2: String, limit: Double = Double.PositiveInfinity): Double
-
-  /**
-   * Computes the index of a single value.
-   */
-  def indexValue(value: String, limit: Double): Set[Seq[Int]] = Set(Seq(0))
-}
-
 object DistanceMeasure extends Factory[DistanceMeasure]
--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/strategy/Factory.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/strategy/Factory.scala
@@ -1,9 +1,13 @@
 package de.fuberlin.wiwiss.silk.util.strategy
 
+import org.clapper.classutil.ClassFinder
+import java.io.File
+import java.net.{URL, URLClassLoader}
+
 /**
  * An abstract Factory.
  */
-class Factory[T <: Strategy] extends ((String, Map[String, String]) => T) {
+class Factory[T <: Strategy : Manifest] extends ((String, Map[String, String]) => T) {
   private var strategies = Map[String, StrategyDescription[T]]()
 
   override def apply(id: String, params: Map[String, String] = Map.empty): T = {
@@ -21,13 +25,40 @@ class Factory[T <: Strategy] extends ((String, Map[String, String]) => T) {
     Some(t.id, t.parameters)
   }
 
-  def register[U <: T](implementationClass: Class[U]) {
+  def strategy(id: String) = strategies(id)
+
+  def availableStrategies: Traversable[StrategyDescription[T]] = strategies.values
+
+  def register(implementationClass: Class[_ <: T]) {
     val strategyDefinition = StrategyDescription(implementationClass)
 
     strategies += ((strategyDefinition.id, strategyDefinition))
   }
 
-  def strategy(id: String) = strategies(id)
+   def registerClasspath() {
+     val classFinder = ClassFinder()
+     val classes = classFinder.getClasses()
 
-  def availableStrategies: Traversable[StrategyDescription[T]] = strategies.values
+     val pluginClassNames = ClassFinder.concreteSubclasses(manifest[T].erasure.getName, classes).map(_.name)
+     val pluginClasses = pluginClassNames.map(Class.forName)
+
+     for(pluginClass <- pluginClasses)
+       register(pluginClass.asInstanceOf[Class[T]])
+   }
+
+   def registerJars(jarDir: String) {
+    val jarFiles = Option(new File(jarDir).listFiles())
+                   .getOrElse(throw new Exception("Directory " + jarDir + " does not exist"))
+                   .filter(_.getName.endsWith(".jar"))
+
+    val pluginInterface = ClassFinder().getClasses().find(_.name == manifest[T].erasure.getName).get
+    val classes = ClassFinder(jarFiles).getClasses ++ Iterator(pluginInterface)
+    val pluginClassNames = ClassFinder.concreteSubclasses(manifest[T].erasure.getName, classes).map(_.name)
+
+    val classLoader = URLClassLoader.newInstance(jarFiles.map(file => new URL("jar:file:" + file.getName + "!/")))
+    val pluginClasses = pluginClassNames.map(classLoader.loadClass)
+
+    for(pluginClass <- pluginClasses)
+      register(pluginClass.asInstanceOf[Class[T]])
+   }
 }
