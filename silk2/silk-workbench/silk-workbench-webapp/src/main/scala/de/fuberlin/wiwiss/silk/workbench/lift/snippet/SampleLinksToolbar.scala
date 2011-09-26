3151016994ef6c2c0c76a4bbe91f1f2181a83bbb
robertisele <robertisele@googlemail.com>
2011-09-26 19:55:03 +0200

    Removed invalid import
    Former-commit-id: 76498d7ce29b4e92034d72a0ac18377ed3ba57e3

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/SampleLinksToolbar.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/snippet/SampleLinksToolbar.scala
@@ -2,9 +2,6 @@ package de.fuberlin.wiwiss.silk.workbench.lift.snippet
 
 import xml.NodeSeq
 import net.liftweb.util.BindHelpers._
-import net.liftweb.http.SHtml
-import de.fuberlin.wiwiss.silk.workbench.workspace.User
-import de.fuberlin.wiwiss.silk.workbench.learning.{CurrentSampleLinksTask, CurrentLearningTask, SampleLinksTask}
 
 class SampleLinksToolbar {
   def render(xhtml: NodeSeq): NodeSeq = {
