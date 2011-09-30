02d0b7048896ecebbc89c0da4f9efacd30277b23
robertisele <robertisele@googlemail.com>
2011-09-30 18:35:27 +0200

    Update to Jetty 7.5.1 to fix issues with comet support

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/Main.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/Main.scala
@@ -1,28 +1,18 @@
 package de.fuberlin.wiwiss.silk.workbench.lift
 
-import org.mortbay.jetty.Server
-import org.mortbay.jetty.webapp.{WebAppClassLoader, WebAppContext}
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.webapp.WebAppContext
 
 object Main {
   def main(args : Array[String]) {
     val server = new Server(8080)
 
-    val context = new WebAppContext()
-
-    context.setContextPath("/")
-
-    val webAppClassLoader = new WebAppClassLoader(Main.getClass.getClassLoader,context);
-    context.setClassLoader(webAppClassLoader);
-
+    val webapp = new WebAppContext();
+    webapp.setContextPath("/");
     val protectionDomain = Main.getClass.getProtectionDomain
     val location = protectionDomain.getCodeSource.getLocation.toExternalForm
-    context.setWar(location)
-
-    context.setExtractWAR(true)
-
-    server.setHandler(context)
-
-    server.setHandler(context)
+    webapp.setWar(location);
+    server.setHandler(webapp);
 
     server.start()
   }
