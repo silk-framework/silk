c170fc798176b54ead9964e2f8dee156664c997f
robertisele <robertisele@googlemail.com>
2011-10-05 12:41:15 +0200

    LinksHelp: Corrected the icon paths

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/LinksHelp.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/LinksHelp.scala
@@ -6,13 +6,13 @@ trait LinksHelp extends Help {
     <div>
       Based on its correctness, each link can be associated to one of the following 3 categories:
       <br/>
-      <img src="../static/img/confirm.png"></img>
+      <img src="./static/img/confirm.png"></img>
       Confirms the link as correct. Confirmed links are part of the positive reference link set.
       <br/>
-      <img src="../static/img/undecided.png"></img>
+      <img src="./static/img/undecided.png"></img>
       Contains link whose correctness is not decided i.e. which are not contained in the reference link sets.
       <br/>
-      <img src="../static/img/decline.png"></img>
+      <img src="./static/img/decline.png"></img>
       Confirms the link as incorrect. Incorrect links are part of the negative reference link set.
     </div>
   }
