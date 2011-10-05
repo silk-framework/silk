fced342142fc0a18ae3bbc7df6b8bc901b0befa3
robertisele <robertisele@googlemail.com>
2011-10-05 13:56:17 +0200

    Outputs write to {user.dir}/.silk/output by default now

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/writer/FileWriter.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/writer/FileWriter.scala
@@ -1,21 +1,22 @@
 package de.fuberlin.wiwiss.silk.plugins.writer
 
 import de.fuberlin.wiwiss.silk.output.{Formatter, LinkWriter}
-import java.io.{Writer, OutputStreamWriter, FileOutputStream}
 import de.fuberlin.wiwiss.silk.util.plugin.Plugin
 import de.fuberlin.wiwiss.silk.entity.Link
+import java.io.{BufferedWriter, Writer}
 
 /**
  * A file writer.
  */
-@Plugin(id = "file", label = "File")
+@Plugin(id = "file", label = "File", description = "Writes the links to a file. Links are written to {user.dir}/.silk/output/ by default.")
 case class FileWriter(file: String = "output.nt", format: String = "ntriples") extends LinkWriter {
   private val formatter = Formatter(format)
 
   private var out: Writer = null
 
   override def open() {
-    out = new OutputStreamWriter(new FileOutputStream(file), "UTF-8")
+    val filePath = System.getProperty("user.home") + "/.silk/" + file
+    out = new BufferedWriter(new java.io.FileWriter(filePath))
     out.write(formatter.header)
   }
 
