b10b515feec4db1eb445881b31382d800593e142
Robert Isele <robertisele@googlemail.com>
2011-10-19 15:51:18 +0200

    Performance improvements
    Former-commit-id: e50622934225369441b3ed088c4265043a0c664f

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/metric/QGramsMetric.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/metric/QGramsMetric.scala
@@ -3,6 +3,7 @@ package de.fuberlin.wiwiss.silk.plugins.metric
 import de.fuberlin.wiwiss.silk.linkagerule.similarity.SimpleDistanceMeasure
 import de.fuberlin.wiwiss.silk.util.StringUtils._
 import de.fuberlin.wiwiss.silk.util.plugin.Plugin
+import de.fuberlin.wiwiss.silk.entity.Index
 
 /**
  * String similarity based on q-grams.
@@ -11,16 +12,15 @@ import de.fuberlin.wiwiss.silk.util.plugin.Plugin
  * - '''q''' (optional): The size of the sliding window. Default: 2
  */
 @Plugin(id = "qGrams", label = "qGrams", description = "String similarity based on q-grams (by default q=2).")
-//TODO this is actually the Dice’s Coefficient with a qGrams tokenizer, if we add a qGrams tokenizer we could remove it...
 case class QGramsMetric(q: Int = 2) extends SimpleDistanceMeasure {
+  private val diceCoefficient = DiceCoefficient()
+
+  //TODO test with toSet?
   override def evaluate(str1: String, str2: String, threshold: Double) = {
-    val qGrams1 = str1.qGrams(q)
-    val qGrams2 = str2.qGrams(q)
+    diceCoefficient(str1.qGrams(q), str2.qGrams(q), threshold)
+  }
 
-    val matchingQGrams = (qGrams1.toSeq intersect qGrams2.toSeq).size * 2
-    val numQGrams = qGrams1.size + qGrams2.size
-
-    if (numQGrams == 0) 1.0
-    else 1.0 - matchingQGrams.toDouble / numQGrams
+  override def indexValue(value: String, limit: Double): Index = {
+    diceCoefficient.index(value.qGrams(q).toSet, limit)
   }
 }
