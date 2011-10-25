67f9841952413c54503c534fb9b403d42c13e3b2
Robert Isele <robertisele@googlemail.com>
2011-10-25 17:31:01 +0200

    Added logging of workspace loading errors to the workbench.

--- silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/workspace/FileUser.scala
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/workspace/FileUser.scala
@@ -1,18 +1,27 @@
 package de.fuberlin.wiwiss.silk.workbench.workspace
 
 import java.io.{IOException, File}
+import java.util.logging.{Level, Logger}
 
 class FileUser extends User {
   override def workspace = FileUser.workspace
 }
 
 object FileUser {
-  private val workspaceDir = new File(System.getProperty("user.home") + "/.silk/workspace/")
+  private lazy val workspaceDir = new File(System.getProperty("user.home") + "/.silk/workspace/")
 
   val workspace = {
-    if(!workspaceDir.exists && !workspaceDir.mkdirs()) throw new IOException("Could not create workspace directory at: " + workspaceDir.getCanonicalPath)
+    try {
+      if(!workspaceDir.exists && !workspaceDir.mkdirs()) throw new IOException("Could not create workspace directory at: " + workspaceDir.getCanonicalPath)
 
-    new FileWorkspace(workspaceDir)
+      new FileWorkspace(workspaceDir)
+    }
+    catch {
+      case ex: Exception => {
+        Logger.getLogger(FileUser.getClass.getName).log(Level.SEVERE, "Error loading workspace", ex)
+        throw ex
+      }
+    }
   }
 }
 
