72029da442b3fc9ceccd5c5128665981c9ac37c6
robertisele <robertisele@googlemail.com>
2011-11-07 12:30:14 +0100

    Added 'allWords' parameter to CapitalizeTransformation
    Former-commit-id: a5733225145b05d385b717b2800fd9c6f8a01f7a

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/CapitalizeTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/CapitalizeTransformer.scala
@@ -3,9 +3,13 @@ package de.fuberlin.wiwiss.silk.plugins.transformer
 import de.fuberlin.wiwiss.silk.util.plugin.Plugin
 import de.fuberlin.wiwiss.silk.linkagerule.input.SimpleTransformer
 
-@Plugin(id = "capitalize", label = "Capitalize", description = "Capitalizes the string i.e. converts the first character to upper case.")
-class CapitalizeTransformer() extends SimpleTransformer {
+@Plugin(id = "capitalize", label = "Capitalize", description = "Capitalizes the string i.e. converts the first character to upper case. " +
+    "If 'allWords' is set to true, all words are capitalized and not only the first character.")
+case class CapitalizeTransformer(allWords: Boolean = false) extends SimpleTransformer {
   override def evaluate(value: String) = {
-    value.capitalize
+    if(allWords)
+      value.capitalize
+    else
+      value.split("\\s+").map(_.capitalize).mkString
   }
 }
