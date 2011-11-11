ccac6ae8b87b211c65f0021d0a034ef0140c5901
robertisele <robertisele@googlemail.com>
2011-11-11 11:07:58 +0100

    ReproductionTask: Now works with changing reference links

--- silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/individual/Population.scala
+++ silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/individual/Population.scala
@@ -4,7 +4,10 @@ package de.fuberlin.wiwiss.silk.learning.individual
  * Represents a population of candidate solutions.
  */
 case class Population(individuals : Traversable[Individual] = Traversable.empty) {
+
+  /** The individual with the best score */
   lazy val bestIndividual = individuals.maxBy(_.fitness.score)
 
+  /** True, if the population is empty */
   def isEmpty = individuals.isEmpty
 }
