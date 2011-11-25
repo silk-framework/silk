90efb56b1d83abca8207559949df8365edc510fd
robertisele <robertisele@googlemail.com>
2011-11-25 15:18:20 +0100

    Added linkselectors to active learning
    Former-commit-id: 2d38728ccb429391eccb7a51a1ce38e66d1dce19

--- silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/active/SampleFromPopulationTask.scala
+++ silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/active/SampleFromPopulationTask.scala
@@ -17,15 +17,10 @@
 package de.fuberlin.wiwiss.silk.learning.active
 
 import de.fuberlin.wiwiss.silk.util.task.ValueTask
-import de.fuberlin.wiwiss.silk.util.DPair
-import de.fuberlin.wiwiss.silk.learning.individual.{Individual, Population}
-import math.max
-import de.fuberlin.wiwiss.silk.linkagerule.{Operator, LinkageRule}
-import de.fuberlin.wiwiss.silk.entity.{Path, Link}
-import de.fuberlin.wiwiss.silk.linkagerule.similarity.{Comparison, Aggregation}
-import de.fuberlin.wiwiss.silk.linkagerule.input.{PathInput, TransformInput}
-import de.fuberlin.wiwiss.silk.evaluation.{ReferenceEntities, ReferenceLinks}
-import math.{pow, sqrt, min, max}
+import de.fuberlin.wiwiss.silk.learning.individual.Population
+import linkselector.{WeightedLinkageRule, EntropySelector}
+import de.fuberlin.wiwiss.silk.entity.Link
+import de.fuberlin.wiwiss.silk.evaluation.ReferenceEntities
 
 private class SampleFromPopulationTask(population: Population, unlabeledLinks: Seq[Link], referenceEntities: ReferenceEntities) extends ValueTask[Seq[Link]](Seq.empty) {
 
@@ -41,68 +36,11 @@ private class SampleFromPopulationTask(population: Population, unlabeledLinks: S
     }
   }
 
-  val positivePoints = {
-    for((link, entityPair) <- referenceEntities.positive) yield {
-      weightedRules.map(_.apply(entityPair))
-    }
-  }
-
-  val negativePoints = {
-    for((link, entityPair) <- referenceEntities.negative) yield {
-      weightedRules.map(_.apply(entityPair))
-    }
-  }
-
   override protected def execute(): Seq[Link] = {
-    //println("AGREEMENT: " + links.map(entropy).minBy(_.abs))
-    //println("MAXDIST: " + links.map(distance2).max)
-
-    val valLinks = for(link <- unlabeledLinks) yield link.update(confidence = Some(rate(link)))
-    valLinks.sortBy(_.confidence.get.abs).take(3)
+    val selector = new EntropySelector(weightedRules, unlabeledLinks)
+    //val selector = new UncertaintySelector(weightedRules, unlabeledLinks, referenceEntities)
+    //val selector = new KullbackLeiblerDivergenceSelector(weightedRules, unlabeledLinks, referenceEntities)
+    selector()
   }
 
-  def withAgreement(link: Link) = {
-    val confidenceSum = weightedRules.map(_.apply(link.entities.get)).sum
-    val confidence = confidenceSum / weightedRules.size
-
-    link.update(confidence = Some(confidence))
-  }
-
-  def rate(link: Link) = {
-    //entropy(link)
-    certainty(link)
-  }
-
-  def entropy(link: Link) = {
-    val fulfilledRules = weightedRules.filter(rule => rule(link.entities.get) > 0.0)
-    val confidence = fulfilledRules.map(_.weight).sum / weightedRules.map(_.weight).sum
-
-    confidence * 2.0 - 1.0
-  }
-
-  def certainty(link: Link) = {
-    val c = weightedRules.map(_.apply(link.entities.get))
-
-    val posDist = positivePoints.map(distance(_, c)).min
-    val negDist = negativePoints.map(distance(_, c)).min
-
-    (negDist - posDist) / (posDist + negDist)
-  }
-
-  def distance2(link: Link) = {
-    val c = weightedRules.map(_.apply(link.entities.get))
-
-    val minDist = (positivePoints ++ negativePoints).map(distance(_, c)).min
-
-    minDist
-  }
-
-  def distance(v1: Seq[Double], v2: Seq[Double]) = {
-    sqrt((v1 zip v2).map(p => pow(p._1 - p._2, 2.0)).sum) / (2.0 * weightedRules.size)
-  }
-
-  class WeightedLinkageRule(individual: Individual) extends LinkageRule(individual.node.build.operator) {
-    /** The weight of this linkage rule. Never smaller than 0.0001 */
-    val weight = max(0.0001, individual.fitness)
-  }
 }
---
+++ silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/active/linkselector/KullbackLeiblerDivergenceSelector.scala
@@ -0,0 +1,120 @@
+/*
+ * Copyright 2009-2011 Freie Universität Berlin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.learning.active.linkselector
+
+import de.fuberlin.wiwiss.silk.linkagerule.LinkageRule
+import de.fuberlin.wiwiss.silk.entity.Link
+import math.{pow, sqrt, log}
+import java.io.{FileWriter, BufferedWriter}
+import de.fuberlin.wiwiss.silk.evaluation.ReferenceEntities
+
+class KullbackLeiblerDivergenceSelector(rules: Seq[WeightedLinkageRule], unlabeledLinks: Seq[Link], referenceEntities: ReferenceEntities) {
+
+  /** Each positive link defines a point in the space spanned by the linkage rules. */
+  val positivePoints: Traversable[Seq[Double]] = {
+    for((link, entityPair) <- referenceEntities.positive) yield {
+      kullbackLeiblerDivergenceVector(link.update(entities = Some(entityPair)))
+    }
+  }
+
+  /** Each negative link defines a point in the space spanned by the linkage rules. */
+  val negativePoints: Traversable[Seq[Double]] = {
+    for((link, entityPair) <- referenceEntities.negative) yield {
+      kullbackLeiblerDivergenceVector(link.update(entities = Some(entityPair)))
+    }
+  }
+
+  def apply(): Seq[Link] = {
+    val valLinks = for(link <- unlabeledLinks) yield link.update(confidence = Some(informationGain(link)))
+    valLinks.sortBy(-_.confidence.get).take(3)
+  }
+
+  private def informationGain(link: Link) = {
+    val pl = kullbackLeiblerDivergenceVector(link)
+    (positivePoints ++ negativePoints).map(p => distance(p, pl)).min
+  }
+
+  write("space_plain.csv", plainVector)
+  write("space_kl.csv", kullbackLeiblerDivergenceVector)
+
+  private def write(file: String,  proj: (Link => Seq[Double])) {
+    val writer = new BufferedWriter(new FileWriter(file))
+
+    for(link <- unlabeledLinks) {
+      val entities = link.entities.get
+      val l0 = entities.source.evaluate(0) == entities.target.evaluate(0)
+      val l1 = entities.source.evaluate(1) == entities.target.evaluate(1)
+      writer.write((link.source + link.target).replace(",", "%2C") + "," + l0 + "_"  + l1 + "," + proj(link).mkString(",") + "\n")
+    }
+
+    val posLinks = for((link, entities) <- referenceEntities.positive) yield link.update(entities = Some(entities))
+    for(link <- posLinks) {
+      writer.write((link.source + link.target).replace(",", "%2C") + ",pos," + proj(link).mkString(",") + "\n")
+    }
+
+    val negLinks = for((link, entities) <- referenceEntities.negative) yield link.update(entities = Some(entities))
+    for(link <- negLinks) {
+      writer.write((link.source + link.target).replace(",", "%2C") + ",neg," + proj(link).mkString(",") + "\n")
+    }
+
+    writer.close()
+  }
+
+  def plainVector(link: Link): Seq[Double] = {
+    rules.map(rule => rule(link.entities.get, 0.0) * 0.5 + 0.5)
+  }
+
+  def kullbackLeiblerDivergenceVector(link: Link): Seq[Double] = {
+    /** The consensus probability that this link is correct */
+    val q = rules.map(probability(_, link)).sum / rules.size
+    //val q = probability(weightedRules.maxBy(_.weight), link)
+
+    if(q == 0.0 || q == 1.0)
+      rules.map(rule => 0.0)
+    else
+      rules.map(rule => kullbackLeiblerDivergence(q, rule, link))
+  }
+
+  def kullbackLeiblerDivergence(q: Double, rule: LinkageRule, link: Link) = {
+    /** The probability that this link is true based on the given linkage rule */
+    val p = probability(rule, link)
+
+    val p1 = if(p <= 0.0) 0.0 else p * log(p / q)
+    val p2 = if(p >= 1.0) 0.0 else (1 - p) * log((1 - p) / (1 - q))
+
+    (p1 + p2) / log(2)
+  }
+
+  //TODO include fitness
+  private def probability(rule: LinkageRule, link: Link) = {
+    //rule(link.entities.get, 0.0) * 0.5 + 0.5
+    if(rule(link.entities.get, 0.0) > 0.0) 1.0 else 0.0
+  }
+
+  def distance(v1: Seq[Double], v2: Seq[Double]) = {
+    sqrt((v1 zip v2).map(p => pow(p._1 - p._2, 2.0)).sum) / (2.0 * rules.size)
+  }
+}
+
+
+
+
+
+
+
+
+
