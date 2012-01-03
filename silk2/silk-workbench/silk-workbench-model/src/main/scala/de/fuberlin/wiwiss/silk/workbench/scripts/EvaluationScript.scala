eac2eb073a333b1edd67bb6676854020b13b9aa5
robertisele <robertisele@googlemail.com>
2012-01-03 19:27:32 +0100

    Added linkselectors to active learning
    Former-commit-id: 93d9bade342a3d60fc7d0514d95b8068ae7e6cb4

--- silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/scripts/EvaluationScript.scala
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/scripts/EvaluationScript.scala
@@ -12,21 +12,16 @@
  * limitations under the License.
  */
 
-package de.fuberlin.wiwiss.silk.learning
+package de.fuberlin.wiwiss.silk.workbench.scripts
 
-case class ResultTable(header: Seq[String], values: Seq[Seq[String]]) {
+import de.fuberlin.wiwiss.silk.plugins.Plugins
+import de.fuberlin.wiwiss.silk.plugins.jena.JenaPlugins
 
-  def toCsv = {
-    header.mkString("", ",", "\n") + values.map(_.mkString("", ",", "\n")).mkString
-  }
+trait EvaluationScript extends App {
 
-  def toLatex = {
-    "\\begin{tabular}{| l | l | c | c | c | c |}\n" +
-    "\\hline\n" +
-    header.mkString("", " & ", "\\\\\n") +
-    "\\hline\n" +
-    values.map(_.mkString("", " & ", "\\\\\n")).mkString +
-    "\\hline\n" +
-    "\\end{tabular}\n"
-  }
+  Plugins.register()
+  JenaPlugins.register()
+  run()
+
+  protected def run()
 }
