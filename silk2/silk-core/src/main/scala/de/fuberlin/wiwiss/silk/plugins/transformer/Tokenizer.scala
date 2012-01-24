c72ff909862d01abc9ebb475aa86cb6688e2da4d
robertisele <robertisele@googlemail.com>
2012-01-24 16:46:43 +0100

    Changed all transformers to case classes
    Former-commit-id: 33f4f505298bd9e8b0cec541ebd019de8943f211

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/Tokenizer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/Tokenizer.scala
@@ -18,7 +18,7 @@ import de.fuberlin.wiwiss.silk.util.plugin.Plugin
 import de.fuberlin.wiwiss.silk.linkagerule.input.Transformer
 
 @Plugin(id = "tokenize", label = "Tokenize", description = "Tokenizes all input values.")
-class Tokenizer(regex: String = "\\s") extends Transformer {
+case class Tokenizer(regex: String = "\\s") extends Transformer {
   override def apply(values: Seq[Set[String]]): Set[String] = {
     values.head.flatMap(_.split(regex))
   }
