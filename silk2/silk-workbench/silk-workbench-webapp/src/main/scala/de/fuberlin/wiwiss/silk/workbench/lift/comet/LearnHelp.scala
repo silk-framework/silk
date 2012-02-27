fca3478e3886912ed9eb658642a632e9420e612b
robertisele <robertisele@googlemail.com>
2012-02-27 17:20:04 +0100

    Updated learning help for passive and active learning

--- silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/LearnHelp.scala
+++ silk2/silk-workbench/silk-workbench-webapp/src/main/scala/de/fuberlin/wiwiss/silk/workbench/lift/comet/LearnHelp.scala
@@ -14,39 +14,18 @@
 
 package de.fuberlin.wiwiss.silk.workbench.lift.comet
 
-import de.fuberlin.wiwiss.silk.learning.individual.Population
-import de.fuberlin.wiwiss.silk.workbench.learning.{CurrentActiveLearningTask, CurrentPopulation}
-import de.fuberlin.wiwiss.silk.workbench.workspace.{CurrentTaskStatusListener, TaskDataListener, User}
-import de.fuberlin.wiwiss.silk.util.task.{TaskStarted, TaskStatus}
+import de.fuberlin.wiwiss.silk.workbench.learning.CurrentValidationLinks
+import de.fuberlin.wiwiss.silk.workbench.workspace.{TaskDataListener, User}
+import de.fuberlin.wiwiss.silk.entity.Link
 
 class LearnHelp extends LinksHelp {
 
   /**
-   * Re-renders the widget if the current linking task has been changed.
+   * Listens to changes of the current validation links.
    */
-  private val linkingTaskListener = User().onUpdate {
-    case _: User.CurrentTaskChanged => reRender()
-    case _ =>
-  }
-
-  /**
-   * Re-renders the widget whenever the population has been updated.
-   */
-  private val populationListener = new TaskDataListener(CurrentPopulation) {
-    override def onUpdate(population: Population) {
+  private val validationListener = new TaskDataListener(CurrentValidationLinks) {
+    override def onUpdate(status: Seq[Link]) {
       reRender()
-    }
-  }
-
-  /**
-   * Listens to changes of the current active learning task.
-   */
-  private val activeLearningTaskListener = new CurrentTaskStatusListener(CurrentActiveLearningTask) {
-    override def onUpdate(status: TaskStatus) {
-      status match {
-        case _: TaskStarted => reRender()
-        case _ =>
-      }
     }
   }
 
@@ -57,21 +36,23 @@ class LearnHelp extends LinksHelp {
   }
 
   override def actions = {
-    if(!CurrentActiveLearningTask().status.isRunning && CurrentPopulation().isEmpty) {
-      <div>Start the learning by pressing the <em>Start</em> button.</div>
-    }
-    else if(User().linkingTask.referenceLinks.positive.size + User().linkingTask.referenceLinks.negative.size < 5) {
-      <div>
-        Rate the links for which the learning algorithm is uncertain:
-        { howToRateLinks }
-      </div>
+    if(CurrentValidationLinks().isEmpty) {
+      <div>Wait until you are happy with the result and press the <em>Done</em> button</div>
     }
     else {
-      <div>
-        Press the <em>Done</em> button if you are happy with the current linkage rule.
-        You can also rate additional links in order to further improve the result:
-        { howToRateLinks }
-      </div>
+      if(User().linkingTask.referenceLinks.positive.size + User().linkingTask.referenceLinks.negative.size < 5) {
+        <div>
+          Rate the links for which the learning algorithm is uncertain:
+          { howToRateLinks }
+        </div>
+      }
+      else {
+        <div>
+          Press the <em>Done</em> button if you are happy with the current linkage rule.
+          You can also rate additional links in order to further improve the result:
+          { howToRateLinks }
+        </div>
+      }
     }
   }
 }
