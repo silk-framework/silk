f4b18408c8d4dbb7a1bb418039de21a70867de29
robertisele <robertisele@googlemail.com>
2012-03-20 11:50:42 +0100

    Minor changes

--- silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/util/PrefixRegistry.scala
+++ silk2/silk-workbench/silk-workbench-model/src/main/scala/de/fuberlin/wiwiss/silk/workbench/util/PrefixRegistry.scala
@@ -1,5 +1,3 @@
-package de.fuberlin.wiwiss.silk.workbench.util
-
 /*
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +11,8 @@ package de.fuberlin.wiwiss.silk.workbench.util
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package de.fuberlin.wiwiss.silk.workbench.util
 
 import io.Source
 import java.util.logging.{Level, Logger}
