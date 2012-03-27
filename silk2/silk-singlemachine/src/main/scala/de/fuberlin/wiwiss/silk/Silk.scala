a6ae343366921909bdb53b07171e4f63a64ca1b3
robertisele <robertisele@googlemail.com>
2012-03-27 11:58:37 +0200

    Incremented version to 2.5.4
    Former-commit-id: c3768188810583d47e14eae0c637957883bd74bf

--- silk2/silk-singlemachine/src/main/scala/de/fuberlin/wiwiss/silk/Silk.scala
+++ silk2/silk-singlemachine/src/main/scala/de/fuberlin/wiwiss/silk/Silk.scala
@@ -35,7 +35,7 @@ object Silk {
   private val logger = Logger.getLogger(Silk.getClass.getName)
 
   //Print welcome message on start-up
-  println("Silk Link Discovery Framework - Version 2.5.3")
+  println("Silk Link Discovery Framework - Version 2.5.4")
 
   //Register all available plugins
   Plugins.register()
