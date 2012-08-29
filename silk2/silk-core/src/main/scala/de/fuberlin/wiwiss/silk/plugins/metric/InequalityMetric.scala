9ac56d9b423dc4db220a33d12221161edec75df0
Robert Isele <robertisele@googlemail.com>
2012-08-29 11:28:57 +0300

    Fixed misleading comments

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/metric/InequalityMetric.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/metric/InequalityMetric.scala
@@ -17,7 +17,7 @@ package de.fuberlin.wiwiss.silk.plugins.metric
 import de.fuberlin.wiwiss.silk.linkagerule.similarity.SimpleDistanceMeasure
 import de.fuberlin.wiwiss.silk.util.plugin.Plugin
 
-@Plugin(id = "inequality", label = "Inequality", description = "Return 0 if strings are equal, 1 otherwise.")
+@Plugin(id = "inequality", label = "Inequality", description = "Return 1 if strings are equal, 0 otherwise.")
 case class InequalityMetric() extends SimpleDistanceMeasure {
   override def evaluate(str1: String, str2: String, threshold: Double) = if (str1 == str2) 1.0 else 0.0
 }
