c8f71a23e0f65fb8f8979db0878b1453356f0aa3
robertisele <robertisele@googlemail.com>
2012-09-04 15:28:13 +0200

    Updated crossover operators
    Former-commit-id: 364ffbd2fe5f6df40b3fdc64799a9f1886e9759c

--- silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/reproduction/DistanceMeasureCrossover.scala
+++ silk2/silk-learning/src/main/scala/de/fuberlin/wiwiss/silk/learning/reproduction/DistanceMeasureCrossover.scala
@@ -14,14 +14,14 @@
 
 package de.fuberlin.wiwiss.silk.learning.reproduction
 
+import de.fuberlin.wiwiss.silk.learning.individual.ComparisonNode
 import de.fuberlin.wiwiss.silk.util.DPair
-import de.fuberlin.wiwiss.silk.learning.individual.AggregationNode
 
 /**
  * A crossover operator which interchanges the aggregation strategies.
  */
-case class AggregationStrategyCrossover() extends NodePairCrossoverOperator[AggregationNode] {
-  override protected def crossover(nodes: DPair[AggregationNode]) = {
-    nodes.source.copy(aggregation = nodes.target.aggregation)
+case class DistanceMeasureCrossover() extends NodePairCrossoverOperator[ComparisonNode] {
+  override protected def crossover(nodes: DPair[ComparisonNode]) = {
+    nodes.source.copy(metric = nodes.target.metric)
   }
 }
