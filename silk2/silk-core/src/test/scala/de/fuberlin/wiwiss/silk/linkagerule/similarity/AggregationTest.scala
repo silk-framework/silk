f6d4d53f8d68fdc4d0a67c9a9af9f0a85f54fc4c
Robert Isele <robertisele@googlemail.com>
2012-10-17 11:23:04 +0200

    Fixed index aggregation

--- silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/linkagerule/similarity/AggregationTest.scala
+++ silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/linkagerule/similarity/AggregationTest.scala
@@ -32,40 +32,51 @@ class AggregationTest extends FlatSpec with ShouldMatchers {
   val aggregator = new AverageAggregator()
 
   "Aggregation with average aggregator" should "return the average if both operators return a value" in {
-    aggregation(
-      operator(1, false, Some(0.8)) ::
-      operator(1, false, Some(0.6)) :: Nil
+    eval(
+      operator(_required = false, value = Some(0.8)) ::
+      operator(_required = false, value = Some(0.6)) :: Nil
     ) should be(approximatelyEqualToOption(Some(0.7)))
   }
 
   "Aggregation with average aggregator" should "return the average of all values if the operators not returning a value are not required" in {
-    aggregation(
-      operator(1, false, Some(0.8)) ::
-      operator(1, false, Some(0.6)) ::
-      operator(1, false, None) :: Nil
+    eval(
+      operator(_required = false, value = Some(0.8)) ::
+      operator(_required = false, value = Some(0.6)) ::
+      operator(_required = false, value = None) :: Nil
     ) should be(approximatelyEqualToOption(Some(0.7)))
   }
 
   "Aggregation with average aggregator" should "return nothing if a required operator does not return a value" in {
-    aggregation(
-      operator(1, false, Some(0.8)) ::
-      operator(1, false, Some(0.6)) ::
-      operator(1, true, None) :: Nil
+    eval(
+      operator(_required = false, value = Some(0.8)) ::
+      operator(_required = false, value = Some(0.6)) ::
+      operator(_required = true, value = None) :: Nil
     ) should be(approximatelyEqualToOption(None))
   }
 
-  private def aggregation(ops: Seq[SimilarityOperator]) = {
+  "Aggregation with average aggregator" should "aggregate the indices" in {
+    index(
+      operator(_required = false, indices = Index.default) ::
+      operator(_required = false, indices = Index.empty) :: Nil
+    ) should equal (Index.default)
+  }
+
+  private def eval(ops: Seq[SimilarityOperator]) = {
     Aggregation(operators = ops, aggregator = aggregator).apply(null)
   }
 
-  private def operator(w: Int, r: Boolean, value: Option[Double]) = {
+  private def index(ops: Seq[SimilarityOperator]) = {
+    Aggregation(operators = ops, aggregator = aggregator).index(null, 0.0)
+  }
+
+  private def operator(_weight: Int = 1, _required: Boolean = false, value: Option[Double] = None, indices: Index = Index.default) = {
     new SimilarityOperator {
-      val id: Identifier = null
-      val weight: Int = w
-      val required: Boolean = r
-      val indexing: Boolean = false
+      val id: Identifier = Identifier.random
+      val weight: Int = _weight
+      val required: Boolean = _required
+      val indexing: Boolean = true
       def apply(entities: DPair[Entity], limit: Double): Option[Double] = value
-      def index(entity: Entity, limit: Double): Index = null
+      def index(entity: Entity, limit: Double): Index = indices
       def toXML(implicit prefixes: Prefixes): Node = null
     }
   }
