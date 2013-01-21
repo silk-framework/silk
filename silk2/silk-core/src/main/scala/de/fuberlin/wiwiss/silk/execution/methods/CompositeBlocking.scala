6dd3f028ff745ed5532ba00183ee75e2b6f82ae0
Robert Isele <robertisele@googlemail.com>
2013-01-21 14:58:57 +0100

    Moved execution methods to separate package

---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/execution/methods/CompositeBlocking.scala
@@ -0,0 +1,21 @@
+package de.fuberlin.wiwiss.silk.execution.methods
+
+import de.fuberlin.wiwiss.silk.entity.{Index, Entity, Path}
+import de.fuberlin.wiwiss.silk.linkagerule.LinkageRule
+import de.fuberlin.wiwiss.silk.execution.ExecutionMethod
+
+/**
+  * Blocking using a composite key built from two single keys.
+  */
+class CompositeBlocking(blockingKey1: Path, blockingKey2: Path) extends ExecutionMethod {
+
+   override def indexEntity(entity: Entity, rule: LinkageRule): Index = {
+     Index.oneDim(
+       for(v1 <- entity.evaluate(blockingKey1);
+           v2 <- entity.evaluate(blockingKey2)) yield {
+         (v1 + v2).hashCode
+       }
+     )
+   }
+
+ }
