a0a0c3fd8e543db7023151beca469f438b4e4919
Robert Isele <robertisele@googlemail.com>
2013-01-23 14:23:26 +0100

    Added initial version of CsvDataSource
    Former-commit-id: 8d20a5305571a7224e519edc8d1bd276a64258cf

---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/datasource/CsvDataSource.scala
@@ -0,0 +1,54 @@
+package de.fuberlin.wiwiss.silk.plugins.datasource
+
+import de.fuberlin.wiwiss.silk.datasource.DataSource
+import de.fuberlin.wiwiss.silk.entity._
+import io.Source
+import java.io.File
+
+case class CsvDataSource(url: String, properties: String, separator: Char = ',', prefix: String = "") extends DataSource {
+
+  private val propertyList: Seq[String] = properties.split(separator)
+
+  override def retrievePaths(restriction: SparqlRestriction, depth: Int, limit: Option[Int]): Traversable[(Path, Double)] = {
+    for(property <- propertyList) yield {
+      (Path.parse("?" + restriction.variable + "/<" + prefix + property + ">"), 1.0)
+    }
+  }
+
+  override def retrieve(entityDesc: EntityDescription, entities: Seq[String] = Seq.empty): Traversable[Entity] = {
+    // Retrieve the indices of the request paths
+    val indices =
+      for(path <- entityDesc.paths) yield {
+        val property = path.operators.head.asInstanceOf[ForwardOperator].property.uri.stripPrefix(prefix)
+        val propertyIndex = propertyList.indexOf(property)
+        propertyIndex
+      }
+
+    // Return new Traversable that generates an entity for each line
+    new Traversable[Entity] {
+      def foreach[U](f: Entity => U) {
+        val source = Source.fromURL(url)
+        try {
+          // Iterate through all lines of the source file.
+          for ((line, number) <- source.getLines.zipWithIndex) {
+            //Split the line into values
+            val allValues = line.split(separator)
+            assert(propertyList.size == allValues.size, "Invalid line with " + allValues.size + " elements. Expected numer of elements " + propertyList.size + ".")
+            //Extract requested values
+            val values = indices.map(allValues(_))
+            //Build entity
+            f(new Entity(
+              uri = prefix + number,
+              values = values.map(Set(_)),
+              desc = entityDesc
+            ))
+          }
+        } finally {
+          source.close()
+        }
+      }
+    }
+
+
+  }
+}
