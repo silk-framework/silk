15f94233831a255ccaff7cd78bbddec4a7fcb7cd
Robert Isele <robertisele@googlemail.com>
2013-02-13 22:04:12 +0100

    Fixed bug in NormalizeCharsTransformer and added a test case
    Former-commit-id: 56a7d29653e45b04b8b6c905b3ed24b8192c7586

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/NormalizeCharsTransformer.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/NormalizeCharsTransformer.scala
@@ -26,22 +26,22 @@ class NormalizeCharsTransformer extends SimpleTransformer {
   */
   private val DIACRITICS_AND_FRIENDS = Pattern.compile("[\\p{InCombiningDiacriticalMarks}\\p{IsLm}\\p{IsSk}]+");
 
-  private val NONDIACRITICS = List(
+  private val NONDIACRITICS = Map(
     //replace non-diacritics as their equivalent chars
-    ("\u0141", "l"), // BiaLystock
-    ("\u0142", "l"), // Bialystock
-    ("ß", "ss"),
-    ("æ", "ae"),
-    ("ø", "o"),
-    ("©", "c"),
-    ("\u00D0", "d"), // all Ð ð from http://de.wikipedia.org/wiki/%C3%90
-    ("\u00F0", "d"),
-    ("\u0110", "d"),
-    ("\u0111", "d"),
-    ("\u0189", "d"),
-    ("\u0256", "d"),
-    ("\u00DE", "th"), // thorn Þ
-    ("\u00FE", "th")).toMap // thorn þ
+    ('\u0141' -> "l"), // BiaLystock
+    ('\u0142' -> "l"), // Bialystock
+    ('ß' -> "ss"),
+    ('æ' -> "ae"),
+    ('ø' -> "o"),
+    ('©' -> "c"),
+    ('\u00D0' -> "d"), // all Ð ð from http://de.wikipedia.org/wiki/%C3%90
+    ('\u00F0' -> "d"),
+    ('\u0110' -> "d"),
+    ('\u0111' -> "d"),
+    ('\u0189' -> "d"),
+    ('\u0256' -> "d"),
+    ('\u00DE' -> "th"), // thorn Þ
+    ('\u00FE' -> "th")) // thorn þ
 
 
   def evaluate(value: String) = {
@@ -50,26 +50,25 @@ class NormalizeCharsTransformer extends SimpleTransformer {
 
   private def simplifyString(str: String): String = {
     if (str == null) {
-      null;
+      null
     } else {
-      stripNonDiacritics(stripDiacritics(str));
+      stripNonDiacritics(stripDiacritics(str))
     }
   }
 
   private def stripNonDiacritics(orig: String): String = {
-    val ret = new StringBuffer();
+    val ret = new StringBuffer()
     for (i <- 0 until orig.length()) {
-      val source = orig.substring(i, i + 1);
-      val replace = NONDIACRITICS.get(source);
-      val toReplace = if (replace == null) String.valueOf(source) else replace;
-      ret.append(toReplace);
+      val source = orig.charAt(i)
+      val replace = NONDIACRITICS.get(source).getOrElse(source)
+      ret.append(replace)
     }
-    return ret.toString();
+    ret.toString
   }
 
   private def stripDiacritics(str: String): String = {
-    val normalized = Normalizer.normalize(str, Normalizer.Form.NFD);
-    DIACRITICS_AND_FRIENDS.matcher(normalized).replaceAll("");
+    val normalized = Normalizer.normalize(str, Normalizer.Form.NFD)
+    DIACRITICS_AND_FRIENDS.matcher(normalized).replaceAll("")
   }
 
 
