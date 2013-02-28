73316db26327767e939879a4c0e16a5392b4d1b8
Robert Isele <robertisele@googlemail.com>
2013-02-28 23:55:04 +0100

    More work in GenerateLinksTaskTest
    Former-commit-id: dca44c0538820e0c20f40c45bc600c5bcd44bc02

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/execution/methods/Blocking.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/execution/methods/Blocking.scala
@@ -9,13 +9,14 @@ import de.fuberlin.wiwiss.silk.execution.ExecutionMethod
   *
   * @param sourceKey The source blocking key, e.g., rdfs:label
   * @param targetKey The target blocking key, e.g., rdfs:label
+  * @param q The maximum number of characters that are used for the indexing
   */
-case class Blocking(sourceKey: Path, targetKey: Path) extends ExecutionMethod {
+case class Blocking(sourceKey: Path, targetKey: Path, q: Int = 2) extends ExecutionMethod {
 
    override def indexEntity(entity: Entity, rule: LinkageRule): Index = {
      val key = if(sourceKey.variable == entity.desc.variable) sourceKey else targetKey
      val values = entity.evaluate(key)
-     Index.blocks(values.map(_.hashCode))
+     Index.blocks(values.map(_.take(q).hashCode))
    }
 
  }
