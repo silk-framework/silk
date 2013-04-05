407fd2e780244fd18a5553e3570b463601bb1b45
Robert Isele <robertisele@googlemail.com>
2013-04-05 16:36:40 +0200

    Added missing matcher for tests
    Former-commit-id: ea9819dd0c43a5d94e0e55214375790e21110a0f

---
+++ silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/testutil/EqualIgnoringWhitespace.scala
@@ -0,0 +1,21 @@
+package de.fuberlin.wiwiss.silk.testutil
+
+import org.scalatest.matchers.{MatchResult, BeMatcher}
+
+/**
+ * Matcher to test if 2 strings are equal when ignoring whitespaces.
+ */
+case class equalIgnoringWhitespace(r: String) extends BeMatcher[String] {
+
+  def apply(l: String) = {
+    //Strip both strings from whitespace characters
+    val ls = l.filterNot(_.isWhitespace)
+    val rs = r.filterNot(_.isWhitespace)
+    //Match strings
+    MatchResult(
+      ls == rs,
+      ls + " is not equal to " + rs,
+      ls + " is equal to " + rs
+    )
+  }
+}
