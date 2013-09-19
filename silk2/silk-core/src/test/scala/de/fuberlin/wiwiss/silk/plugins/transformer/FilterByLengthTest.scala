d43bbaf0adfd958b465e602d799372c0eda32197
Robert Isele <robertisele@gmail.com>
2013-09-19 17:26:05 +0200

    Added Tests for FilterByLength and FilterByRegex transformations and fixed small bug
    Former-commit-id: 1f3d5710004c6488195d14398f068ab75a39d588

---
+++ silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/plugins/transformer/FilterByLengthTest.scala
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.plugins.transformer
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import de.fuberlin.wiwiss.silk.plugins.Plugins
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class FilterByLengthTest extends FlatSpec with ShouldMatchers {
+  Plugins.register()
+
+  val transformer = new FilterByLength(min = 3, max = 5)
+
+  "FilterByLength" should "retain strings inside the range" in {
+    transformer(Seq(Set("abc"))) should equal(Set("abc"))
+    transformer(Seq(Set("abcd"))) should equal(Set("abcd"))
+    transformer(Seq(Set("abcde"))) should equal(Set("abcde"))
+  }
+
+  "FilterByLength" should "remove strings outside the range" in {
+    transformer(Seq(Set("ab"))) should equal(Set())
+    transformer(Seq(Set("abc", "ab", "abd", "abcdef"))) should equal(Set("abc", "abd"))
+  }
+}
