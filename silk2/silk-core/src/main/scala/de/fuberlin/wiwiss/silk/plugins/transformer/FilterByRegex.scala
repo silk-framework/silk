06c8377fe54620dd777120df8fa2d6a5bc0acf05
Robert Isele <robertisele@gmail.com>
2013-09-19 17:26:05 +0200

    Added Tests for FilterByLength and FilterByRegex transformations and fixed small bug
    Former-commit-id: ea44627242b7f0fd76c1cd3f4f9690c1d596d864

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/FilterByRegex.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/FilterByRegex.scala
@@ -9,14 +9,14 @@ import de.fuberlin.wiwiss.silk.linkagerule.input.Transformer
 @Plugin(
   id = "filterByRegex",
   label = "filterByRegex",
-  description = "Removes all strings that do NOT match a regex. If 'negate' is true, only strings will be retained that match the regex."
+  description = "Removes all strings that do NOT match a regex. If 'negate' is true, only strings will be removed that match the regex."
 )
 case class FilterByRegex(regex: String, negate: Boolean = false) extends Transformer {
 
   override def apply(values: Seq[Set[String]]) = {
     if(!negate)
-      values.head.filterNot(str => str.matches(regex))
-    else
       values.head.filter(str => str.matches(regex))
+    else
+      values.head.filterNot(str => str.matches(regex))
   }
 }
