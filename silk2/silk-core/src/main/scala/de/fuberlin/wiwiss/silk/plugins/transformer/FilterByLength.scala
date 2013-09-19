88b5809304115e00c7be07df11a67a2d02f036c6
Robert Isele <robertisele@gmail.com>
2013-09-19 17:04:23 +0200

    Added FilterByLength and FilterByRegex transformations
    Former-commit-id: d8aef243087147809f4f33c0a555e12df708060f

---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/FilterByLength.scala
@@ -0,0 +1,19 @@
+package de.fuberlin.wiwiss.silk.plugins.transformer
+
+import de.fuberlin.wiwiss.silk.util.plugin.Plugin
+import de.fuberlin.wiwiss.silk.linkagerule.input.{Transformer, SimpleTransformer}
+
+/**
+ * Removes all strings that are shorter than 'min' characters and longer than 'max' characters.
+ */
+@Plugin(
+  id = "filterByLength",
+  label = "filterByLength",
+  description = "Removes all strings that are shorter than 'min' characters and longer than 'max' characters."
+)
+case class FilterByLength(min: Int = 0, max: Int = Int.MaxValue) extends Transformer {
+
+  override def apply(values: Seq[Set[String]]) = {
+    values.head.filterNot(str => str.length < min || str.length > max)
+  }
+}
