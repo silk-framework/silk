3dc33028efd5bcca24f51d45166c5b09b971aae0
Robert Isele <robertisele@gmail.com>
2013-09-24 16:30:41 +0200

    Added new transformations to default plugins
    Former-commit-id: 2fd773c7d725993ab4b0d42539a31b42561dfd08

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/FilterByLength.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/transformer/FilterByLength.scala
@@ -8,7 +8,7 @@ import de.fuberlin.wiwiss.silk.linkagerule.input.{Transformer, SimpleTransformer
  */
 @Plugin(
   id = "filterByLength",
-  label = "filterByLength",
+  label = "filter by length",
   description = "Removes all strings that are shorter than 'min' characters and longer than 'max' characters."
 )
 case class FilterByLength(min: Int = 0, max: Int = Int.MaxValue) extends Transformer {
