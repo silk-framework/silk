06cb7c9129f647d9d19e02fa673ef7fc6c352c86
Robert Isele <robertisele@gmail.com>
2013-10-07 17:01:53 +0200

    Made Workbench more configurable
    Former-commit-id: 695a38ab05ce41963eb78f6551efb04b1ff3972a

---
+++ silk2/silk-workbench/app/models/WorkbenchConfig.scala
@@ -0,0 +1,35 @@
+package models
+
+import java.io.File
+import scala.xml.{Elem, XML}
+
+/**
+ * Workbench configuration.
+ *
+ * @param title
+ */
+case class WorkbenchConfig(title: String = "Silk Workbench") {
+}
+
+object WorkbenchConfig {
+  /**
+   * Retrieves the Workbench configuration.
+   */
+  lazy val get = {
+    val configFile = new File("config.xml")
+    if(configFile.exists) {
+      fromXml(XML.loadFile(configFile))
+    } else {
+      new WorkbenchConfig()
+    }
+  }
+
+  /**
+   * Loads the Workbench configuration from xml.
+   */
+  private def fromXml(xml: Elem) = {
+    new WorkbenchConfig(
+      title = (xml \ "Branding" \ "Title").text
+    )
+  }
+}
