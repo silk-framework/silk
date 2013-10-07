e918c07ea727112eeb82cd417107b795126ecaf7
RobertIsele <robertisele@gmail.com>
2013-10-07 18:21:15 +0200

    Made logo configurable

---
+++ silk2/silk-workbench/app/controllers/util/Branding.scala
@@ -0,0 +1,19 @@
+package controllers.util
+
+import play.api.mvc.{Action, Controller}
+import models.WorkbenchConfig
+import java.io.{BufferedInputStream, FileInputStream}
+
+object Branding extends Controller {
+
+  def logo = Action {
+    val imgStream = new BufferedInputStream(new FileInputStream(WorkbenchConfig.get.logo))
+    val bytes = scala.Stream.continually(imgStream.read).takeWhile(-1 !=).map(_.toByte).toArray
+    Ok(bytes).as("image/png")
+  }
+
+  def aboutDialog = Action {
+    Ok(views.html.aboutDialog())
+  }
+
+}
