27c91c4e24b7d68142e37ca05098b6f7f9aea5f6
robertisele <robertisele@gmail.com>
2013-10-21 14:58:42 +0200

    Workbench is now configured using the Play Configuration file

--- silk2/silk-workbench/app/models/WorkbenchConfig.scala
+++ silk2/silk-workbench/app/models/WorkbenchConfig.scala
@@ -1,15 +1,17 @@
 package models
 
-import java.io.File
+import java.io.{FileNotFoundException, File}
 import scala.xml.{Elem, XML}
+import play.api.Play
+import play.api.Play.current
 
 /**
  * Workbench configuration.
  *
- * @param title The application title
- * @param logo The application logo
+ * @param title The application title.
+ * @param logo The application logo. Must point to a file in the conf directory.
  */
-case class WorkbenchConfig(title: String = "Silk Workbench", logo: String = "/logo.png") {
+case class WorkbenchConfig(title: String = "Silk Workbench", logo: File = new File("logo.png")) {
 }
 
 object WorkbenchConfig {
@@ -17,21 +19,27 @@ object WorkbenchConfig {
    * Retrieves the Workbench configuration.
    */
   lazy val get = {
-    val configFile = new File("config.xml")
-    if(configFile.exists) {
-      fromXml(XML.loadFile(configFile))
-    } else {
-      new WorkbenchConfig()
-    }
-  }
+    val config = Play.configuration
 
-  /**
-   * Loads the Workbench configuration from xml.
-   */
-  private def fromXml(xml: Elem) = {
-    new WorkbenchConfig(
-      title = (xml \ "Branding" \ "Title").text,
-      logo  = (xml \ "Branding" \ "Logo").text
+    WorkbenchConfig(
+      title = config.getString("workbench.title").getOrElse("Silk Workbench"),
+      logo = {
+        // Reading the file name of the logo
+        val logoFile = config.getString("workbench.logo").getOrElse("logo.png")
+
+        //Depending on the distribution method, the configuration directory may be located at different paths
+        val paths = "conf/" + logoFile :: "conf/_/" + logoFile ::
+                    "../conf/" + logoFile :: "../conf/_/" + logoFile :: Nil
+
+        //Trying all paths and using the first one that works
+        val files = paths.map(Play.getFile)
+        files.find(_.exists) match {
+          case Some(f) => f
+          case None => {
+            throw new FileNotFoundException("Logo file not found. Tried paths: " + files.map(_.getAbsolutePath).mkString(", "))
+          }
+        }
+      }
     )
   }
 }
