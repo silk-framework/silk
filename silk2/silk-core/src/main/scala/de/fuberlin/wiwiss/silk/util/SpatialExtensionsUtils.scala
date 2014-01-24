f46bf9548d32cb73f42c796196c2349997c0cb8a
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-01-24 20:32:48 +0200

    Added dependency to JTS Topology Suite.
    Former-commit-id: 669920b2a78c8af0c5449a6480c49af0bcdec8a4

---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SpatialExtensionsUtils.scala
@@ -0,0 +1,36 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.util
+
+import com.vividsolutions.jts.geom.Geometry
+import com.vividsolutions.jts.io.ParseException
+import com.vividsolutions.jts.io.WKTReader
+import com.vividsolutions.jts.io.WKTWriter
+import com.vividsolutions.jts.io.gml2.GMLReader
+
+
+import java.util.logging.Logger
+
+/**
+ * Useful Utils
+ * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
+ */
+
+object SpatialExtensionsUtils {
+  
+
+  
+
+}
