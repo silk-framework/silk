06a1f40269e04698c7fde8af6246ff8d11dfa432
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-01-27 20:59:35 +0200

    Added some constants.
    Former-commit-id: bc1af908a0caf3f698ad46d9245abc604b7596e8

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SpatialExtensionsUtils.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SpatialExtensionsUtils.scala
@@ -17,7 +17,6 @@ package de.fuberlin.wiwiss.silk.util
 import com.vividsolutions.jts.geom.Geometry
 import com.vividsolutions.jts.io.ParseException
 import com.vividsolutions.jts.io.WKTReader
-import com.vividsolutions.jts.io.WKTWriter
 import com.vividsolutions.jts.io.gml2.GMLReader
 
 
@@ -28,9 +27,75 @@ import java.util.logging.Logger
  * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
  */
 
+
 object SpatialExtensionsUtils {
-  
+	
 
-  
+ 
+	val strdf =	  Constants.stRDF
 
+	
+	
+	def convertToDefaultSRID(){}  
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	private object Constants{
+    
+		/**
+		 * The namespace for stRDF data model
+		 */
+		val stRDF					= "http://strdf.di.uoa.gr/ontology#"
+	
+		/**
+		 * The namespace for GeoSPARQL ontology
+		 */
+		val GEO						= "http://www.opengis.net/ont/geosparql#";
+	
+		/**
+		 * The namespace of GML
+		 */
+		val GML_OGC					= "http://www.opengis.net/gml";  
+
+		/**
+		 * The URI for the datatype Well-Known Text (WKT)
+		 */
+		val WKT 					= stRDF + "WKT";
+	
+		/**
+		 * The URI for the datatype Geography Markup Language (GML)
+		 */
+		val GML						= stRDF + "GML";
+		
+		/**
+		 * The URI for the datatype wktLiteral
+		 */
+		val WKTLITERAL				=  GEO + "wktLiteral";
+		
+		/**
+		 * The URI for the datatype gmlLiteral
+		 */
+		val GMLLITERAL				=  GEO + "gmlLiteral";			
+	}
+  
+	private object Parser{
+
+		val wktr = new WKTReader
+		val gmlr = new GMLReader
+  
+		def getGeometryfromLiteral(){
+		  
+		  
+		}
+
+	}
 }
