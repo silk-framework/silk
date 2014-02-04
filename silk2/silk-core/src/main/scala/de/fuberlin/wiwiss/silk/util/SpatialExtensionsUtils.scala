34bf331bc1f9ab26f2a96a432b333f9175f3c885
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-02-04 15:31:46 +0200

    Finished parser for geometries.
    Former-commit-id: 5535dcf977d5d3011c2f0370b27f8e5060d43d52

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SpatialExtensionsUtils.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/SpatialExtensionsUtils.scala
@@ -18,157 +18,156 @@ import com.vividsolutions.jts.geom.Geometry
 import com.vividsolutions.jts.io.ParseException
 import com.vividsolutions.jts.io.WKTReader
 import com.vividsolutions.jts.io.gml2.GMLReader
-
-
-import java.util.logging.Logger
+import java.util.logging.{ Level, Logger }
 
 /**
  * Useful utils for the spatial extensions of Silk.
  * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
  */
 
-
 object SpatialExtensionsUtils {
-	
 
+  private val logger = Logger.getLogger(Parser.getClass.getName)
+  logger.setLevel(Level.OFF)
 
+  def convertToDefaultSRID(literal: String) {
 
-	
-	
-	def convertToDefaultSRID(literal : String){
-	  
-	  var (geometry, fromSRID) = Parser.separeteGeomeryFromSRID(literal)
-	  println(geometry, fromSRID)
-	  
-	}  
+    var (geometry, fromSRID) = Parser.separateGeometryFromSRID(literal)
+    if (fromSRID != Constants.DEFAULT_SRID) {
+      println("Convert from " + fromSRID)
+    }
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
+  }
 
-	/**
-  	 * A parser that separates the geometry literal from the SRID.
-  	 */	
-	private object Parser{
+  /**
+   * An stRDF/stSPARQL and GeoSPARQL parser.
+   */
+  private object Parser {
 
-	  	/**
-	  	 * Reader for WKT
-	  	 */
-		private val wktr = new WKTReader
+    /**
+     * Reader for WKT
+     */
+    private val wktr = new WKTReader
 
-	  	/**
-	  	 * Reader for GML
-	  	 */
-		private val gmlr = new GMLReader
-  
-		def separeteGeomeryFromSRID(literal : String) : (String, Int) = {
-			val trimmedLiteral=literal.trim
-			
-			var geometry : String = ""
-			var srid : Int = -1
-			
-			// TODO: error handling
-			
-			if(trimmedLiteral.length() != 0)
-			{
-				val index = trimmedLiteral.lastIndexOf(Constants.STRDF_SRID_DELIM)
-			
-				if(index > 0) {	// strdf:WKT with SRID
-					geometry = trimmedLiteral.substring(0, index)		
-					srid = augmentString(trimmedLiteral.substring((trimmedLiteral.lastIndexOf("/")+1))).toInt
-				}
-				else{
-					if (trimmedLiteral.startsWith("<")){ // starts with a URI => assume EPSG URI, geo:wktLiteral
-						val index = trimmedLiteral.indexOf(">")
-						if (index > 0) {
-							geometry = trimmedLiteral.substring(index+1).trim
-							srid = augmentString(trimmedLiteral.substring(trimmedLiteral.indexOf("/")+1, index-1)).toInt
-						}
-					}
-					else{ // cannot guess, only WKT representation was given => assume strdf:WKT
-						geometry = trimmedLiteral
-						srid = Constants.DEFAULT_SRID
-					}
-				}
-			}
-			else{ // empty geometry => assume strdf:WKT
-				geometry = Constants.EMPTY_GEOM
-				srid  = Constants.DEFAULT_SRID
-			}
+    /**
+     * Reader for GML
+     */
+    private val gmlr = new GMLReader
 
-			(geometry, srid)
-		}		
-	}
-	
-  	/**
-  	 * An object that contains all needed constants (namespaces, URIs, prefixes).
-  	 */	
-	private object Constants{
-    
-		/**
-		 * The namespace for stRDF data model.
-		 */
-		val stRDF = "http://strdf.di.uoa.gr/ontology#"
-	
-		/**
-		 * The namespace for GeoSPARQL ontology.
-		 */
-		val GEO	= "http://www.opengis.net/ont/geosparql#"
-	
-		/**
-		 * The namespace for GML.
-		 */
-		val GML_OGC	= "http://www.opengis.net/gml"
+    /**
+     * This function parses a geometry literal according to the specifications of stRDF/stSPARQL and GeoSPARQL.
+     *
+     * If the literal is an strdf:WKT, strdf:GML, geo:wktLiteral or geo:gmlLiteral literal concatenated with the SRID URI it returns the tuple (geometry, srid).
+     * If the literal is a plain WKT or GML Literal it returns the tuple (geometry, @link Constants.DEFAULT_SRID).
+     * In any other case it returns (literal, @link Constants.DEFAULT_SRID).
+     *
+     * @param literal
+     * @return (geometry, srid)
+     */
+    def separateGeometryFromSRID(literal: String): (String, Int) = {
+      val trimmedLiteral = literal.trim
+      var geometry: String = ""
+      var srid: Int = -1
 
-		/**
-		 * The URI for the datatype Well-Known Text (WKT).
-		 */
-		val WKT	= stRDF + "WKT"
-	
-		/**
-		 * The URI for the datatype Geography Markup Language (GML).
-		 */
-		val GML	= stRDF + "GML"
-		
-		/**
-		 * The URI for the datatype wktLiteral.
-		 */
-		val WKTLITERAL =  GEO + "wktLiteral"
-		
-		/**
-		 * The URI for the datatype gmlLiteral.
-		 */
-		val GMLLITERAL = GEO + "gmlLiteral"
+      try {
+        if (trimmedLiteral.length() != 0) {
+          val index = trimmedLiteral.lastIndexOf(Constants.STRDF_SRID_DELIM)
+          if (index > 0) {
+            // strdf:WKT or strdf:GML with SRID (assumes EPSG URI for SRID)
+            geometry = trimmedLiteral.substring(0, index)
+            srid = augmentString(trimmedLiteral.substring((trimmedLiteral.lastIndexOf('/') + 1))).toInt
+          } else {
+            if (trimmedLiteral.startsWith("<http://")) {
+              // starts with a URI => geo:wktLiteral or geo:gmlLiteral (assumes EPSG URI for SRID)
+              val index = trimmedLiteral.indexOf('>')
+              val URI = trimmedLiteral.substring(0, index)
+              geometry = trimmedLiteral.substring(index + 1).trim
+              srid = augmentString(URI.substring(URI.lastIndexOf('/') + 1)).toInt
+            } else {
+              // cannot guess the datatype, only plain literal was given
+              geometry = trimmedLiteral
+              srid = Constants.DEFAULT_SRID
+            }
+          }
+        } else {
+          // empty geometry
+          geometry = Constants.EMPTY_GEOM
+          srid = Constants.DEFAULT_SRID
+        }
+      } catch {
+        case e: Exception =>
+          // in case of parse exception the literal is returned as it is
+          geometry = trimmedLiteral
+          srid = Constants.DEFAULT_SRID
+      }
+      logger.info("\nGeometry: " + geometry + "\nSRID: " + srid)
+      (geometry, srid)
+    }
+  }
 
-		/**
-		 * WKT representation for an empty geometry.
-		 */
-		val EMPTY_GEOM = "MULTIPOLYGON EMPTY"
+  /**
+   * An object that contains all needed constants (namespaces, URIs, prefixes).
+   */
+  object Constants {
 
-		/**
-		 * EPSG:4326 (default for stSPARQL).
-		 */
-		val WGS84_LAT_LON_SRID = 4326
-		
-		/**
-		 * EPSG:3857 (default for GeoSPARQL).
-		 */
-		val WGS84_LON_LAT_SRID = 3857
+    /**
+     * The namespace for stRDF data model.
+     */
+    val stRDF = "http://strdf.di.uoa.gr/ontology#"
 
-		/**
-		 * Default SRID
-		 */
-		val DEFAULT_SRID 	= WGS84_LAT_LON_SRID;
-		
-		/**
-		 * stRDF SRID delimiter.
-		 */		  
-		val STRDF_SRID_DELIM 	= ";"  
-	}	
+    /**
+     * The namespace for GeoSPARQL ontology.
+     */
+    val GEO = "http://www.opengis.net/ont/geosparql#"
+
+    /**
+     * The namespace for GML.
+     */
+    val GML_OGC = "http://www.opengis.net/gml"
+
+    /**
+     * The URI for the datatype Well-Known Text (WKT).
+     */
+    val WKT = stRDF + "WKT"
+
+    /**
+     * The URI for the datatype Geography Markup Language (GML).
+     */
+    val GML = stRDF + "GML"
+
+    /**
+     * The URI for the datatype wktLiteral.
+     */
+    val WKTLITERAL = GEO + "wktLiteral"
+
+    /**
+     * The URI for the datatype gmlLiteral.
+     */
+    val GMLLITERAL = GEO + "gmlLiteral"
+
+    /**
+     * WKT representation for an empty geometry.
+     */
+    val EMPTY_GEOM = "MULTIPOLYGON EMPTY"
+
+    /**
+     * EPSG:4326 (default for stSPARQL).
+     */
+    val WGS84_LAT_LON_SRID = 4326
+
+    /**
+     * EPSG:3857 (default for GeoSPARQL).
+     */
+    val WGS84_LON_LAT_SRID = 3857
+
+    /**
+     * Default SRID
+     */
+    val DEFAULT_SRID = WGS84_LAT_LON_SRID;
+
+    /**
+     * stRDF SRID delimiter.
+     */
+    val STRDF_SRID_DELIM = ";"
+  }
 }
