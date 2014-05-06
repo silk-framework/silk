61935e85a377d8d4f622b8cb2c29c50af271acf9
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-05-06 19:34:34 +0300

    Added some tests.
    Former-commit-id: 7f61f6cc77342a5b61b50f567cd7f3e244f1ab25

---
+++ silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/plugins/distance/spatial/CentroidDinstanceMetricTest.scala
@@ -0,0 +1,56 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.plugins.distance.spatial
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import de.fuberlin.wiwiss.silk.plugins.Plugins
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import de.fuberlin.wiwiss.silk.entity.Index
+
+/**
+ * Tests the CentroidDinstance Metric.
+ * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
+ */
+
+@RunWith(classOf[JUnitRunner])
+class CentroidDinstanceMetricTest extends FlatSpec with ShouldMatchers {
+  Plugins.register()
+
+  val metric = new CentroidDistanceMetric()
+
+  //Same point.
+  "CentroidDinstanceMetric test 1" should "return '0.0'" in {
+    metric.evaluate("POINT (0 0)", "POINT (0 0)") should equal(0.0)
+  }
+
+  //Same centroid.
+  "CentroidDinstanceMetric test 2" should "return '0.0'" in {
+    metric.evaluate("POLYGON ((0 0, 0 2, 2 2, 2 0, 0 0))", "POINT (1 1)") should equal(0.0)
+
+  }
+
+  //Distance 1.
+  "CentroidDinstanceMetric test 3" should "return '1.0'" in {
+    metric.evaluate("POINT (0 0)", "POINT (0 1)") should equal(1.0)
+
+  }
+  
+  //Indexing.
+  "CentroidDinstanceMetric test 4" should "return 'Index.multiDim(Set(Seq(0, 0)), 2)'" in {
+    metric.indexValue("POINT (-180 -90)", 0.0) should equal(Index.multiDim(Set(Seq(0, 0)), 2))
+  }  
+}
