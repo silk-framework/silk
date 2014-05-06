7df64012810c857a5153235d0273ceda23eaf174
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-05-06 20:40:33 +0300

    Spatial Relations Tests.
    Former-commit-id: 023f81a79cbcc8d0c65740d2afefebee782b7bc7

---
+++ silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/plugins/distance/spatial/EqualsMetricTest.scala
@@ -0,0 +1,39 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.plugins.distance.spatial
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import de.fuberlin.wiwiss.silk.plugins.Plugins
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * Tests the Equals Metric.
+ * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
+ */
+
+@RunWith(classOf[JUnitRunner])
+class EqualsMetricTest extends FlatSpec with ShouldMatchers {
+  Plugins.register()
+
+  val metric = new EqualsMetric()
+
+  //Equals evaluation.
+  "EqualsMetric test 1" should "return '0.0'" in {
+    metric.evaluate("POINT (0 0)", "POINT (0 0)", 0.0) should equal(0.0)
+  }
+
+}
