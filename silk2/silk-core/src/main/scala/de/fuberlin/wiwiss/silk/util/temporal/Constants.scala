de6b7ac09c637c3ef20d032e19154f738f4aac8e
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-05-13 19:10:17 +0300

    Added functionality for the temporal extensions.
    Former-commit-id: 384ac10a8032d37f41c9033e43c2f6e755dd9940

---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/temporal/Constants.scala
@@ -0,0 +1,88 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.util.temporal
+
+import java.util.Date
+
+/**
+ * An object that contains all needed constants.
+ * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
+ */
+
+object Constants {
+
+  /**
+   * period delimiter.
+   */
+  val PERIOD_DELIM = ","
+
+  /**
+   * Simple Date Format
+   */
+  val SIMPLE_DATE_FORMAT = "yyyy-MM-DD'T'hh:mm:ss"
+
+  /**
+   * Max Distance
+   */
+  val MAX_DISTANCE = -1.0
+
+  /**
+   * Min Time - "the epoch" (January 1, 1970, 00:00:00 GMT)
+   */
+  val MIN_TIME = new Date(0l).getDate()
+
+  /**
+   * Max Time - "NOW"
+   */
+  val MAX_TIME = new Date().getDate()
+
+  /**
+   * Convert Milliseconds
+   */
+  val MILLISECS_PER_SEC = 1000.0
+  val MILLISECS_PER_MIN = 60000.0
+  val MILLISECS_PER_HOUR = 3600000.0
+  val MILLISECS_PER_DAY = 86400000.0
+  val MILLISECS_PER_MONTH = 2629743830.0
+  val MILLISECS_PER_YEAR = 31556926000.0
+
+  /**
+   * Time Distances
+   */
+  val MILLISECS_DISTANCE = "millisecsDistance"
+  val SECS_DISTANCE = "secsDistance"
+  val MIN_DISTANCE = "minDistance"
+  val HOUR_DISTANCE = "hourDistance"
+  val DAY_DISTANCE = "dayDistance"
+  val MONTH_DISTANCE = "monthDistance"
+  val YEAR_DISTANCE = "yearDistance"
+
+  /**
+   * Allen's Relations
+   */
+  val BEFORE = "before"
+  val AFTER = "after"
+  val MEETS = "meets"
+  val IS_MET_BY = "isMetBy"
+  val OVERLAPS = "overlaps"
+  val IS_OVERLAPPED_BY = "isOverlappedBy"
+  val FINISHES = "finishes"
+  val IS_FINISHED_BY = "isFinishesBy"
+  val CONTAINS = "contains"
+  val DURRING = "durring"
+  val STARTS = "starts"
+  val IS_STARTED_BY = "isStartedBy"
+  val EQUALS = "equals"
+}
---
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/util/temporal/TemporalExtensionsUtils.scala
@@ -0,0 +1,162 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.util.temporal
+
+import java.util.Date
+import java.util.logging.Logger
+
+import de.fuberlin.wiwiss.silk.entity.Index
+import de.fuberlin.wiwiss.silk.util.temporal.Constants._
+import de.fuberlin.wiwiss.silk.util.temporal.Parser.parseTime
+
+/**
+ * Useful utils for the temporal extensions of Silk.
+ * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
+ */
+
+object TemporalExtensionsUtils {
+
+  private val logger = Logger.getLogger(this.getClass.getName)
+
+  /**
+   * This function indexes times. Assumes that all the times are between "the epoch" (January 1, 1970, 00:00:00 GMT) and "NOW".
+   *
+   * @param timeString : String
+   * @param distance: Double
+   * @return Index
+   */
+  def indexGeometries(timeString: String, distance: Double): Index = {
+    try {
+      val period = parseTime(timeString)
+
+      //Ensure that period is well-defined.
+      if (!period.isDefined)
+        return Index.empty
+
+      val (start, end) = period.get
+
+      //Case that we want the relations between all the times.
+      var newDistance = distance
+      if (distance == MAX_DISTANCE)
+        newDistance = MAX_TIME - MIN_TIME
+
+      Index.continuous((start.getDate() + end.getDate()) / 2.0, MIN_TIME, MAX_TIME, newDistance)
+
+    } catch {
+      case e: Exception =>
+        Index.empty
+    }
+  }
+
+  /**
+   * This function evaluates a distance between two time periods or instants (for periods, it evaluates the distance between their centres).
+   *
+   * @param timeString1 : String
+   * @param timeString2 : String
+   * @param limit: Double
+   * @param distanceType: String
+   * @return Double
+   */
+  def evaluateDistance(timeString1: String, timeString2: String, limit: Double, distanceType: String): Double = {
+    try {
+      val period1 = parseTime(timeString1)
+      val period2 = parseTime(timeString2)
+
+      //Ensure that periods are well-defined.
+      if (!period1.isDefined || !period2.isDefined)
+        return Double.PositiveInfinity
+
+      val (start1, end1) = period1.get
+      val (start2, end2) = period2.get
+      val diffInMillisecs = Math.abs(((start1.getDate() + end1.getDate()) / 2.0) - ((start2.getDate() + end2.getDate()) / 2.0))
+
+      distanceType match {
+        case MILLISECS_DISTANCE => diffInMillisecs
+        case SECS_DISTANCE => diffInMillisecs / MILLISECS_PER_SEC
+        case MIN_DISTANCE => diffInMillisecs / MILLISECS_PER_MIN
+        case HOUR_DISTANCE => diffInMillisecs / MILLISECS_PER_HOUR
+        case DAY_DISTANCE => diffInMillisecs / MILLISECS_PER_DAY
+        case MONTH_DISTANCE => diffInMillisecs / MILLISECS_PER_MONTH
+        case YEAR_DISTANCE => diffInMillisecs / MILLISECS_PER_YEAR
+        case _ => Double.PositiveInfinity
+      }
+
+    } catch {
+      case e: Exception =>
+        Double.PositiveInfinity
+    }
+  }
+
+  /**
+   * This function evaluates a relation between two time periods or instants.
+   *
+   * @param timeString1 : String
+   * @param timeString2 : String
+   * @param limit: Double
+   * @param relation: String
+   * @return Double
+   */
+  def evaluateRelation(timeString1: String, timeString2: String, limit: Double, relation: String): Double = {
+    try {
+      val period1 = parseTime(timeString1)
+      val period2 = parseTime(timeString2)
+
+      //Ensure that periods are well-defined.
+      if (!period1.isDefined || !period2.isDefined)
+        return Double.PositiveInfinity
+
+      //Compute the temporal relation.
+      if (relate(period1.get, period2.get, relation))
+        return limit
+      else
+        return Double.PositiveInfinity
+
+    } catch {
+      case e: Exception =>
+        Double.PositiveInfinity
+    }
+  }
+
+  /**
+   * This function returns true if the given relation holds between two periods.
+   *
+   * @param period1: (Date, Date)
+   * @param period2: (Date, Date)
+   * @param relation: String
+   * @return Boolean
+   */
+  def relate(period1: (Date, Date), period2: (Date, Date), relation: String): Boolean = {
+    val (start1, end1) = period1
+    val (start2, end2) = period2
+
+    relation match {
+      case BEFORE => end1.before(start2)
+      case AFTER => start1.after(end2)
+      case MEETS => end1.equals(start2)
+      case IS_MET_BY => start1.equals(end2)
+      case OVERLAPS => end1.after(start2) && end1.before(end2)
+      case IS_OVERLAPPED_BY => start1.after(start2) && start1.before(end2)
+      case FINISHES => start1.after(start2) && end1.equals(end2)
+      case IS_FINISHED_BY => start1.before(start2) && end1.equals(end2)
+      case CONTAINS => start1.before(start2) && end1.after(end2)
+      case DURRING => start1.after(start2) && end1.before(end2)
+      case STARTS => start1.equals(start2) && end1.before(end2)
+      case IS_STARTED_BY => start1.equals(start2) && end1.after(end2)
+      case EQUALS => start1.equals(start2) && end1.equals(end2)
+      case _ => false
+    }
+  }
+
+}
