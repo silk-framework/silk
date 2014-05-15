89845af1d04269ca360f3a62d53ee21ee6d46d30
Panayiotis Smeros <psmeros@di.uoa.gr>
2014-05-15 13:39:26 +0300

    Added tests for Allen's relations.
    Former-commit-id: db49c0c06b3d139d5bc461aa23f19bdabe57436c

---
+++ silk2/silk-core/src/test/scala/de/fuberlin/wiwiss/silk/plugins/distance/temporal/StartsMetricTest.scala
@@ -0,0 +1,38 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.fuberlin.wiwiss.silk.plugins.distance.temporal
+
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FlatSpec
+import de.fuberlin.wiwiss.silk.plugins.Plugins
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * Tests the Starts Metric.
+ * @author Panayiotis Smeros (Department of Informatics & Telecommunications, National & Kapodistrian University of Athens)
+ */
+
+@RunWith(classOf[JUnitRunner])
+class StartsMetricTest extends FlatSpec with ShouldMatchers {
+  Plugins.register()
+
+  val metric = new StartsMetric()
+
+  //Starts evaluation.
+  "StartsMetric test 1" should "return '1.0'" in {
+    metric.evaluate("[2000-01-01T00:00:00, 2000-01-01T00:00:01)", "[2000-01-01T00:00:00, 2000-01-01T00:00:02)", 1.0) should equal(1.0)
+  }
+}
