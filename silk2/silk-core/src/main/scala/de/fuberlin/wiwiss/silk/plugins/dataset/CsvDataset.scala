e25d76ac06e826057102c7934bf10d2ef44992f4
robertisele <mail@robertisele.com>
2014-08-12 17:26:06 +0200

    Fixed routing for tomcat context paths

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/dataset/CsvDataset.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/plugins/dataset/CsvDataset.scala
@@ -16,6 +16,7 @@ Parameters:
              Regexes, such as '\t' for specifying tab-separated values, are also supported.
   prefix: The prefix that is used to generate URIs for each line.
   uri: A pattern used to construct the entity URI. If not provided the prefix + the line number is used.
+       An example of such a pattern is 'urn:zyx:{id}' where *id* is a name of a property.
   regexFilter: A regex filter used to match rows from the CSV file. If not set all the rows are used."""
 )
 case class CsvDataset(file: Resource, properties: String, separator: String = ",", prefix: String = "", uri: String = "", regexFilter: String = "") extends DatasetPlugin {
--- silk2/silk-workbench/silk-workbench-core/app/WorkbenchGlobal.scala
+++ silk2/silk-workbench/silk-workbench-core/app/WorkbenchGlobal.scala
@@ -1,29 +1,34 @@
 
 import play.api.mvc.Results._
 import play.api.mvc.{Handler, RequestHeader}
-import play.api.{Application, GlobalSettings}
+import play.api.{Logger, Application, GlobalSettings}
 import play.core.Router.Routes
 import plugins.WorkbenchPlugins
 import scala.concurrent.Future
 
 trait WorkbenchGlobal extends GlobalSettings {
 
-  private var pluginRoutes = Map[String, Routes]()
+  /** The context path of this application, i.e. the URI prefix */
+  protected var context: String = _
+
+  private var pluginRoutes: Map[String, Routes] = _
 
   override def beforeStart(app: Application) {
+    // Set context path
+    context = app.configuration.getString("application.context").getOrElse("/")
     // Collect plugin routes
     pluginRoutes = WorkbenchPlugins().map(_.routes).reduce(_ ++ _)
     for((prefix, routes) <- pluginRoutes)
-      routes.setPrefix("/" + prefix + "/")
+      routes.setPrefix(context + prefix + "/")
   }
 
   override def onRouteRequest(request: RequestHeader): Option[Handler] = {
     // Route to start page
-    if(request.path == "/")
-      return core.Routes.handlerFor(request.copy(path = "/core"))
+    if(request.path == context)
+      return core.Routes.handlerFor(request.copy(path = context + "core"))
 
     // Route to registered modules
-    val prefix = request.path.stripPrefix("/").takeWhile(_ != '/')
+    val prefix = request.path.stripPrefix(context).takeWhile(_ != '/')
     pluginRoutes.get(prefix) match {
       case Some(routes) => routes.handlerFor(request)
       case None => super.onRouteRequest(request)
