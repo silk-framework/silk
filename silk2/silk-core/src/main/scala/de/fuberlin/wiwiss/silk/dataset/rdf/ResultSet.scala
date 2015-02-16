8702168f251772cf0f84f8a1733201fcdb60742c
Robert Isele <mail@robertisele.com>
2015-02-16 18:53:25 +0100

    Fixed ordering of sparql result bindings

--- silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/dataset/rdf/ResultSet.scala
+++ silk2/silk-core/src/main/scala/de/fuberlin/wiwiss/silk/dataset/rdf/ResultSet.scala
@@ -2,4 +2,9 @@ package de.fuberlin.wiwiss.silk.dataset.rdf
 
 import scala.collection.immutable.SortedMap
 
-case class ResultSet(bindings: Traversable[SortedMap[String, RdfNode]])
+case class ResultSet(bindings: Traversable[SortedMap[String, RdfNode]]) {
+
+  def variables: Seq[String] =
+    if(bindings.isEmpty) Seq.empty
+    else bindings.head.keys.toSeq
+}
