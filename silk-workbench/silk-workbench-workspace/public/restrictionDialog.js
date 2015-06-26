be91dc8e28f5b1c7e456b3594ec5075ab126bc3d
Robert Isele <mail@robertisele.com>
2015-06-26 13:57:41 +0200

    Added restriction dialog to transformtion dialog

--- silk-workbench/silk-workbench-workspace/public/restrictionDialog.js
+++ silk-workbench/silk-workbench-workspace/public/restrictionDialog.js
@@ -4,7 +4,7 @@ $(function() {
   $("button").button();
 });
 
-function commitSimpleRestriction(sourceVar, targetVar) {
+function commitSimpleRestriction(varName) {
   var sparql =
       $(".type.selected").map(function() {
         // Retrieve type name
@@ -13,14 +13,8 @@ function commitSimpleRestriction(sourceVar, targetVar) {
         // Enclose full URIs with brackets
         if(name.indexOf("/") != -1)
           name = "<" + name + ">";
-        // Determine variable
-        var variable;
-        if(sourceOrTarget == "source")
-          variable = sourceVar;
-        else
-          variable = targetVar;
         // Return SPARQL pattern
-        return "{ ?" + variable + " a " + name + " }";
+        return "{ ?" + varName + " a " + name + " }";
       }).toArray().join("\n UNION \n");
 
   closeRestrictionDialog(sparql);
