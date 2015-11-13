29aafa9d9211e87c73a705fd1d7614cdb72154c9
Robert Isele <mail@robertisele.com>
2015-11-13 11:09:11 +0100

    EntityTest: Added test for non-ASCII chars

--- silk-core/src/test/scala/de/fuberlin/wiwiss/silk/entity/EntityTest.scala
+++ silk-core/src/test/scala/de/fuberlin/wiwiss/silk/entity/EntityTest.scala
@@ -14,8 +14,14 @@ class EntityTest extends FlatSpec with Matchers {
 
   val entity1 = new Entity("http://silk-framework.com/example", IndexedSeq(Set("value1", "value2"), Set("value3")), schema)
 
-  "EntityTest" should "be serializable" in {
+  val entity2 = new Entity("http://silk-framework.com/example", IndexedSeq(Set("dÃ©clarÃ©s", "v2"), Set("v3")), schema)
+
+  "Entity" should "be serializable" in {
     serialized(entity1) should be (entity1)
+  }
+
+  "Entity" should "be serializable if it contains non-ASCII characters" in {
+    serialized(entity2) should be (entity2)
   }
 
   def serialized(entity: Entity): Entity = {
