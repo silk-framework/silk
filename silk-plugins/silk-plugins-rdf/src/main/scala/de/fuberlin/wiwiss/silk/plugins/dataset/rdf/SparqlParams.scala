14e392d2dd9e60742cd22e54130069444ddd813c
Robert Isele <mail@robertisele.com>
2015-11-13 10:30:55 +0100

    Reverted accidental commit

--- silk-plugins/silk-plugins-rdf/src/main/scala/de/fuberlin/wiwiss/silk/plugins/dataset/rdf/SparqlParams.scala
+++ silk-plugins/silk-plugins-rdf/src/main/scala/de/fuberlin/wiwiss/silk/plugins/dataset/rdf/SparqlParams.scala
@@ -12,7 +12,7 @@ package de.fuberlin.wiwiss.silk.plugins.dataset.rdf
  * @param queryParameters Additional parameters to be appended to every request e.g. &soft-limit=1
  * @param parallel True, if multiple queries should be executed in parallel for faster retrieval.
 */
-case class SparqlParams(uri: String, user: String = null, password: String = null,
+case class SparqlParams(uri: String = "", user: String = null, password: String = null,
                         graph: String = null, pageSize: Int = 1000, entityList: String = null,
                         pauseTime: Int = 0, retryCount: Int = 3, retryPause: Int = 1000,
                         queryParameters: String = "", parallel: Boolean = true, useOrderBy: Boolean = true) {
