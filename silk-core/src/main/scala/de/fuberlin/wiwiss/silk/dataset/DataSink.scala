76c39995535f36f5e96205f1a54f8491a6c9a497
Andreas Schultz <a.schultz@fu-berlin.de>
2015-11-16 13:27:37 +0100

    Split DataSink into EntitySink and LinkSink

--- silk-core/src/main/scala/de/fuberlin/wiwiss/silk/dataset/DataSink.scala
+++ silk-core/src/main/scala/de/fuberlin/wiwiss/silk/dataset/DataSink.scala
@@ -7,39 +7,16 @@ import de.fuberlin.wiwiss.silk.entity.Link
  *
  * Implementing classes of this trait must override the write methods.
  */
-trait DataSink {
+trait DataSink extends EntitySink with LinkSink {
   /**
    * Initializes this writer.
    *
    * @param properties The list of properties of the entities to be written.
    */
-  def open(properties: Seq[String] = Seq.empty) {}
-
-  /**
-   * Writes a new entity.
-   *
-   * @param subject The subject URI of the entity.
-   * @param values The list of values of the entity. For each property that has been provided
-   *               when opening this writer, it must contain a set of values.
-   */
-  def writeEntity(subject: String, values: Seq[Set[String]])
-
-  /**
-   * Writes a new link to this writer.
-   */
-  def writeLink(link: Link, predicateUri: String)
-
-  /**
-   * Writes a set of links.
-   */
-  def writeLinks(links: Traversable[Link], predicateUri: String) {
-    open()
-    for (link <- links) writeLink(link, predicateUri)
-    close()
-  }
+  override def open(properties: Seq[String] = Seq.empty) { init() }
 
   /**
    * Closes this writer.
    */
-  def close() {}
+  override def close() {}
 }
