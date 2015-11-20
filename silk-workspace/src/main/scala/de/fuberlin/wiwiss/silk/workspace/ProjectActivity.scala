b01ae8bc170f10f44f021a680a4bfb28c3de77f1
Robert Isele <mail@robertisele.com>
2015-11-20 14:35:53 +0100

    Added project activity class

---
+++ silk-workspace/src/main/scala/de/fuberlin/wiwiss/silk/workspace/ProjectActivity.scala
@@ -0,0 +1,23 @@
+package de.fuberlin.wiwiss.silk.workspace
+
+import de.fuberlin.wiwiss.silk.runtime.activity.{Activity, ActivityContext}
+
+/**
+  * An activity that belongs to a project.
+  *
+  * @param project The project this activity belongs to
+  * @param activityGenerator A function that generates a new activity for a project.
+  */
+class ProjectActivity(project: Project, activityGenerator: Project => Activity[Unit]) extends Activity[Unit] {
+
+  /**
+    * Executes this activity.
+    *
+    * @param context Holds the context in which the activity is executed.
+    */
+  override def run(context: ActivityContext[Unit]): Unit = {
+    val activity = activityGenerator(project)
+    activity.run(context)
+  }
+
+}
