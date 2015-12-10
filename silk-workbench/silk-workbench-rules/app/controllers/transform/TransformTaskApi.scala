4379092e7aa4fff6d3c209858df6afb046c8e2ed
Andreas Schultz <a.schultz@fu-berlin.de>
2015-12-10 12:13:39 +0100

    Added REST API endpoint for transform tasks to transform POST data, which returns N-Triples or can write to any registed data sink

--- silk-workbench/silk-workbench-rules/app/controllers/transform/TransformTaskApi.scala
+++ silk-workbench/silk-workbench-rules/app/controllers/transform/TransformTaskApi.scala
@@ -1,19 +1,24 @@
 package controllers.transform
 
+import java.io.StringWriter
 import java.util.logging.{Level, Logger}
 
+import com.hp.hpl.jena.rdf.model.ModelFactory
 import org.silkframework.config.{DatasetSelection, TransformSpecification}
 import org.silkframework.dataset.Dataset
 import org.silkframework.entity.rdf.SparqlRestriction
 import org.silkframework.execution.ExecuteTransform
+import org.silkframework.plugins.dataset.rdf.endpoint.JenaModelEndpoint
+import org.silkframework.plugins.dataset.rdf.{SparqlParams, SparqlSink}
 import org.silkframework.rule.TransformRule
+import org.silkframework.runtime.activity.Activity
+import org.silkframework.runtime.resource.InMemoryResourceManager
 import org.silkframework.runtime.serialization.{Serialization, ValidationException}
-import org.silkframework.util.{Identifier, CollectLogs}
-import org.silkframework.workspace.Constants
+import org.silkframework.util.{CollectLogs, Identifier}
+import org.silkframework.workspace.{Constants, User}
 import org.silkframework.workspace.activity.transform.TransformPathsCache
-import org.silkframework.workspace.User
 import play.api.libs.json.{JsArray, JsObject, JsString}
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Action, AnyContentAsXml, Controller}
 
 object TransformTaskApi extends Controller {
 
@@ -192,4 +197,33 @@ object TransformTaskApi extends Controller {
     // Convert to JSON and return
     Ok(JsArray(matches.map(JsString)))
   }
+
+  def postTransformInput(projectName: String, taskName: String) = Action { request =>
+    val project = User().workspace.project(projectName)
+    val task = project.task[TransformSpecification](taskName)
+    request.body match {
+      case AnyContentAsXml(xmlBody) =>
+        implicit val resourceManager = InMemoryResourceManager()
+        val resource = xmlBody \ "resource"
+        val resourceId = resource \ "@name"
+        resourceManager.
+            get(resourceId.text).
+            write(resource.text)
+
+//        val dataSink = xmlBody \ "dataSink"
+        val model = ModelFactory.createDefaultModel()
+        val dataSink = new SparqlSink(SparqlParams(parallel = false), new JenaModelEndpoint(model))
+        val dataSource = xmlBody \ "DataSources"
+        val dataset = Serialization.fromXml[Dataset]((dataSource \ "_").head)
+        val transform = new ExecuteTransform(dataset.source, DatasetSelection.empty, task.data.rules, Seq(dataSink))
+        Activity(transform).startBlocking()
+        val writer = new StringWriter()
+        val result = model.write(writer, "N-Triples")
+        Ok(writer.toString).as("application/n-triples")
+      case _ =>
+        UnsupportedMediaType("Only XML supported")
+    }
+  }
 }
+
+
