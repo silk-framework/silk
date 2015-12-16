93f77a860b09eaf3df4d25cf3759bbdcc62286de
Robert Isele <mail@robertisele.com>
2015-12-16 15:53:51 +0100

    Added plugin documentation generation

--- silk-core/src/main/scala/org/silkframework/plugins/PluginDocumentation.scala
+++ silk-core/src/main/scala/org/silkframework/plugins/PluginDocumentation.scala
@@ -1,39 +1,64 @@
 package org.silkframework.plugins
 
 import java.io.{OutputStreamWriter, FileOutputStream}
+import org.silkframework.dataset.{DatasetPlugin, Dataset}
 import org.silkframework.rule.input.Transformer
-import org.silkframework.rule.similarity.DistanceMeasure
+import org.silkframework.rule.similarity.{Aggregator, Aggregation, DistanceMeasure}
 import org.silkframework.runtime.plugin._
 import org.silkframework.util.Table
 
 import scala.reflect.ClassTag
 
 /**
- * Script for generating markdown documentation for all registered plugins.
+ * Generates markdown documentation for all registered plugins.
  */
-object PluginDocumentation extends App {
+object PluginDocumentation {
 
-  val sb = new StringBuilder
+  // Currently we add category descriptions manually here
+  val categoryDescriptions: Map[String, String] =
+    Map(
+      "Characterbased" -> "Character-based distance measures compare strings on the character level. They are well suited for\nhandling typographical errors.",
+      "Tokenbased" -> "While character-based distance measures work well for typographical errors, there are a number of tasks where token-base distance measures are better suited:\n- Strings where parts are reordered e.g. &ldquo;John Doe&rdquo; and &ldquo;Doe, John&rdquo;\n- Texts consisting of multiple words"
+    )
 
-  printPlugins[DistanceMeasure](
-    title = "Similarity Measures",
-    description = "The following similarity measures are included:"
-  )
+  def apply(): String = {
+    implicit val sb = new StringBuilder
 
-  printPlugins[Transformer](
-    title = "Transformations",
-    description = "The following transform and normalization functions are included:"
-  )
+    sb ++= "# Plugin Reference\n\n"
 
-  val writer = new OutputStreamWriter(new FileOutputStream("doc/plugins2.md"))
-  writer.write(sb.toString())
-  writer.close()
+    printPlugins[DatasetPlugin](
+      title = "Dataset Plugins",
+      description = "The following dataset plugins are available:"
+    )
 
-  def printPlugins[T: ClassTag](title: String, description: String) = {
-    sb ++= "# " + title + "\n\n"
+    printPlugins[DistanceMeasure](
+      title = "Similarity Measures",
+      description = "The following similarity measures are available:"
+    )
+
+    printPlugins[Transformer](
+      title = "Transformations",
+      description = "The following transform and normalization functions are available:"
+    )
+
+    printPlugins[Aggregator](
+      title = "Aggregations",
+      description = "The following aggregation functions are available:"
+    )
+
+    sb.toString
+  }
+
+  def printPlugins[T: ClassTag](title: String, description: String)(implicit sb: StringBuilder) = {
+    sb ++= "## " + title + "\n\n"
     sb ++= description + "\n\n"
-    for(category <- PluginRegistry.availablePlugins[T].flatMap(_.categories).distinct if category != "Recommended") {
-      sb ++= "## " + category + "\n"
+    val categories = PluginRegistry.availablePlugins[T].flatMap(_.categories).filter(_ != "Recommended").distinct.sorted
+    for(category <- categories) {
+      if(categories.size > 1)
+        sb ++= "### " + category + "\n\n"
+      for(categoryDescription <- categoryDescriptions.get(category)) {
+        sb ++= categoryDescription + "\n\n"
+      }
       sb ++= pluginTable[T](title, category).toMarkdown
       sb ++= "\n"
     }
