18ef32ee7745126ef168d47af4019b15478c574a
Andreas Schultz <a.schultz@fu-berlin.de>
2016-01-28 18:33:46 +0100

    Fixed GraphStoreTrait.postDataToGraph, added chunking mode and enable by default

--- silk-core/src/main/scala/org/silkframework/dataset/rdf/GraphStoreTrait.scala
+++ silk-core/src/main/scala/org/silkframework/dataset/rdf/GraphStoreTrait.scala
@@ -2,6 +2,9 @@ package org.silkframework.dataset.rdf
 
 import java.io.OutputStream
 import java.net.{HttpURLConnection, URL}
+import java.util.logging.Logger
+
+import scala.util.Try
 
 /**
  * Created by andreas on 1/28/16.
@@ -15,15 +18,44 @@ trait GraphStoreTrait {
    * @param contentType
    * @return
    */
-  def postDataToGraph(graph: String, contentType: String = "application/n-triples"): OutputStream = {
+  def postDataToGraph(graph: String,
+                      contentType: String = "application/n-triples",
+                      chunkedStreamingMode: Option[Int] = Some(10000)): OutputStream = {
     val updateUrl = graphStoreEndpoint(graph)
     val url = new URL(updateUrl)
     val connection = url.openConnection().asInstanceOf[HttpURLConnection]
-    connection.setRequestMethod( "POST" )
-    connection.setDoInput( true )
-    connection.setDoOutput( true )
-    connection.setUseCaches( false )
-    connection.setRequestProperty( "Content-Type", contentType )
+    connection.setRequestMethod("POST")
+    connection.setDoInput(true)
+    connection.setDoOutput(true)
+    chunkedStreamingMode foreach { connection.setChunkedStreamingMode(_) }
+    connection.setUseCaches(false)
+    connection.setRequestProperty("Content-Type", contentType)
+    ConnectionClosingOutputStream(connection)
+  }
+}
+
+/**
+ * Handles the sending of the request and the closing of the connection on closing the [[OutputStream]].
+ */
+case class ConnectionClosingOutputStream(connection: HttpURLConnection) extends OutputStream {
+  private val log: Logger = Logger.getLogger(this.getClass.getName)
+
+  private lazy val outputStream = {
+    connection.connect()
     connection.getOutputStream()
+  }
+
+  override def write(i: Int): Unit = {
+    outputStream.write(i)
+  }
+
+  override def close(): Unit = {
+    try {
+      outputStream.flush()
+      outputStream.close()
+      log.info("Finished with code " + connection.getResponseCode)
+    } finally {
+      connection.disconnect()
+    }
   }
 }
