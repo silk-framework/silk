c6a60b530ce0de27426fbc63c5a5f9f2bd670d76
Robert Isele <mail@robertisele.com>
2016-01-28 18:19:17 +0100

    Added missing parrameter documentation for dataset plugins

--- silk-plugins/silk-plugins-json/src/main/scala/org/silkframework/plugins/dataset/json/JsonDataset.scala
+++ silk-plugins/silk-plugins-json/src/main/scala/org/silkframework/plugins/dataset/json/JsonDataset.scala
@@ -1,7 +1,7 @@
 package org.silkframework.plugins.dataset.json
 
 import org.silkframework.dataset._
-import org.silkframework.runtime.plugin.Plugin
+import org.silkframework.runtime.plugin.{Param, Plugin}
 import org.silkframework.runtime.resource.Resource
 
 import scala.io.Codec
@@ -10,17 +10,17 @@ import scala.io.Codec
   id = "json",
   label = "JSON",
   description =
-"""Retrieves all entities from an JSON file.
-
-Parameters:
-
-- file:  File name inside the resources directory. In the Workbench, this is the '(projectDir)/resources' directory.
-- basePath: The path to the elements to be read, starting from the root element, e.g., '/Persons/Person'.
-            If left empty, all direct children of the root element will be read.
-- uriPrefix: A URI pattern, e.g., http://namespace.org/{ID}, where {path} may contain relative paths to elements
-"""
+"""Retrieves all entities from an JSON file."""
 )
-case class JsonDataset(file: Resource, basePath: String = "", uriPattern: String = "", charset: String = "UTF8") extends DatasetPlugin {
+case class JsonDataset(
+  @Param("File name inside the resources directory. In the Workbench, this is the '(projectDir)/resources' directory.")
+  file: Resource,
+  @Param("The path to the elements to be read, starting from the root element, e.g., '/Persons/Person'. If left empty, all direct children of the root element will be read.")
+  basePath: String = "",
+  @Param("A URI pattern, e.g., http://namespace.org/{ID}, where {path} may contain relative paths to elements")
+  uriPattern: String = "",
+  @Param("The file encoding, e.g., UTF8, ISO-8859-1")
+  charset: String = "UTF8") extends DatasetPlugin {
 
   private val codec = Codec(charset)
 
