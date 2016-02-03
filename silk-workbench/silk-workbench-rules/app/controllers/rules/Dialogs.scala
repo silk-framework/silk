0afe0f1bbc923384f2fada99c8e5e88fa5db317c
Robert Isele <mail@robertisele.com>
2016-02-03 18:55:44 +0100

    Restriction dialog: Removed old types selection

--- silk-workbench/silk-workbench-rules/app/controllers/rules/Dialogs.scala
+++ silk-workbench/silk-workbench-rules/app/controllers/rules/Dialogs.scala
@@ -1,14 +1,8 @@
 package controllers.rules
 
-import controllers.workspace.Workspace._
 import org.silkframework.dataset.Dataset
-import org.silkframework.entity.{ForwardOperator, Restriction}
-import org.silkframework.entity.Restriction.{Or, Condition, Operator}
-import org.silkframework.entity.rdf.{SparqlRestriction, SparqlRestrictionParser}
-import org.silkframework.runtime.serialization.ValidationException
 import org.silkframework.workspace.User
 import org.silkframework.workspace.activity.dataset.TypesCache
-import play.Logger
 import play.api.mvc.{Action, Controller}
 
 object Dialogs extends Controller {
@@ -18,32 +12,7 @@ object Dialogs extends Controller {
     val typesCache = project.task[Dataset](sourceName).activity[TypesCache].value.typesByFrequency
     implicit val prefixes = project.config.prefixes
 
-    // Try to parse the SPARQL restriction
-    val restrictionParser = new SparqlRestrictionParser()
-    val restrictionTree =
-      try {
-        restrictionParser(SparqlRestriction.fromSparql(varName, restriction))
-      } catch {
-        case ex: ValidationException =>
-          Logger.info(s"Could not parse SPARQL restriction '$restriction'.")
-          Restriction.empty
-      }
-
-    // Collect all type statements from the restriction
-    def collectTypes(op: Operator): Set[String] = op match {
-      case Condition(path, value) => path.operators match {
-        case ForwardOperator(uri) :: Nil if uri.uri == "http://www.w3.org/1999/02/22-rdf-syntax-ns#type" => Set(value)
-        case _ => Set.empty
-      }
-      case Or(ops) => ops.toSet.flatMap(collectTypes)
-      case _ => Set.empty
-    }
-    val types = restrictionTree.operator match {
-      case Some(op) => collectTypes(op)
-      case None => Set.empty[String]
-    }
-
-    Ok(views.html.dialogs.restrictionDialog(project, varName, restriction, types, typesCache))
+    Ok(views.html.dialogs.restrictionDialog(project, varName, restriction))
   }
 
 }
