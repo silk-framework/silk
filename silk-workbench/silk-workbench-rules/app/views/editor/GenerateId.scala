adaeebe2bedafcf2ea0bc303e92c4ceea4403e89
Robert Isele <mail@robertisele.com>
2016-02-08 11:37:49 +0100

    GenerateId: Added doc

--- silk-workbench/silk-workbench-rules/app/views/editor/GenerateId.scala
+++ silk-workbench/silk-workbench-rules/app/views/editor/GenerateId.scala
@@ -1,12 +1,20 @@
 package views.editor
 
+/**
+  * Generates a unique id for a rule operator to be used in the editor html.
+  */
 object GenerateId {
 
-  def apply(elementId: String, displayed: Boolean): String = {
+  /**
+    * Generates a unique id.
+    * @param operatorId The id of the rule operator, which is unique inside the rule.
+    * @param displayed Whether the rule is displayed, or hidden in the toolbox.
+    */
+  def apply(operatorId: String, displayed: Boolean): String = {
     if(displayed)
-      "operator_" + elementId
+      "operator_" + operatorId
     else
-      "toolboxOperator_" + elementId
+      "toolboxOperator_" + operatorId
   }
 
 }
