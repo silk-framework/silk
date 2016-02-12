1aab54440833aa5664ec87e6a0c9237fab072294
Robert Isele <mail@robertisele.com>
2016-02-12 11:11:46 +0100

    Simplified logging

--- silk-workbench/app/Global.scala
+++ silk-workbench/app/Global.scala
@@ -1,6 +1,7 @@
 import java.io.File
 import java.util.logging.{ConsoleHandler, FileHandler, SimpleFormatter}
 
+import org.silkframework.config.Config
 import org.silkframework.runtime.activity.Activity
 import org.silkframework.workspace.{FileUser, User}
 import play.api.Play.current
@@ -9,12 +10,12 @@ import plugins.WorkbenchPlugins
 
 object Global extends WorkbenchGlobal {
 
+  // Forward the silk.home property, so it can be read in other configurations such as the logging configuration
+  System.setProperty("silk.home", Config().getString("silk.home"))
+
   override def beforeStart(app: Application) {
     // Use Play execution context for running activities
     Activity.executionContext = play.api.libs.concurrent.Execution.defaultContext
-
-    // Configure logging
-    configureLogging(app)
 
     // Load Workbench plugins
     WorkbenchPlugins.register(DatasetPlugin())
@@ -23,34 +24,5 @@ object Global extends WorkbenchGlobal {
     // WorkbenchPlugins.register(WorkflowPlugin())
 
     super.beforeStart(app)
-  }
-
-  private def configureLogging(app: Application) {
-    // If the ELDS_HOME variable is set, Silk is run inside the eccenca Linked Data Suite
-    val elds_home = System.getenv("ELDS_HOME")
-
-    // Configure workbench logging
-    // The 'workbench.logDir' variable will be read in application.logger.xml
-    if (elds_home != null)
-      System.setProperty("workbench.logDir", elds_home + "/var/log/dataintegration")
-    else
-      System.setProperty("workbench.logDir", Play.configuration.getString("application.home") + "logs")
-
-    // Configure engine logging
-    val logFile =
-      if (elds_home != null)
-        new File(elds_home + "/var/log/dataintegration/engine.log")
-      else if(app.getFile("/logs/").exists)
-        app.getFile("/logs/engine.log")
-      else
-        app.getFile("../logs/engine.log")
-
-    logFile.getParentFile.mkdirs()
-    val fileHandler = new FileHandler(logFile.getAbsolutePath)
-    fileHandler.setFormatter(new SimpleFormatter())
-    java.util.logging.Logger.getLogger("").addHandler(fileHandler)
-
-    val consoleHandler = new ConsoleHandler()
-    java.util.logging.Logger.getLogger("").addHandler(consoleHandler)
   }
 }
