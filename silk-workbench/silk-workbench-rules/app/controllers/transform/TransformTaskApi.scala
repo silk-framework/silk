cbdae874b8f7ca83a4bbb17e9fdb59ca0b637c7a
Robert Isele <mail@robertisele.com>
2016-02-12 13:06:45 +0100

    Fix in Transform dialog: Persist source type

--- silk-workbench/silk-workbench-rules/app/controllers/transform/TransformTaskApi.scala
+++ silk-workbench/silk-workbench-rules/app/controllers/transform/TransformTaskApi.scala
@@ -26,7 +26,7 @@ object TransformTaskApi extends Controller {
     val proj = User().workspace.project(project)
     implicit val prefixes = proj.config.prefixes
 
-    val input = DatasetSelection(values("source"), Uri(values.getOrElse("type", "")), Restriction.custom(values("restriction")))
+    val input = DatasetSelection(values("source"), Uri(values.getOrElse("sourceType", "")), Restriction.custom(values("restriction")))
     val outputs = values.get("output").filter(_.nonEmpty).map(Identifier(_)).toSeq
 
     proj.tasks[TransformSpecification].find(_.name == task) match {
