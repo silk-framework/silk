b676315b090d26d2040194c059a26323e00d265d
Robert Isele <mail@robertisele.com>
2016-03-14 16:26:04 +0100

    CSV Sink: Now works with resources other than files

--- silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvEntitySink.scala
+++ silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvEntitySink.scala
@@ -1,12 +1,12 @@
 package org.silkframework.plugins.dataset.csv
 
 import org.silkframework.dataset.EntitySink
-import org.silkframework.runtime.resource.Resource
+import org.silkframework.runtime.resource.{WritableResource, Resource}
 
 /**
  * Created by andreas on 12/11/15.
  */
-class CsvEntitySink(file: Resource, settings: CsvSettings) extends CsvSink(file, settings) with EntitySink {
+class CsvEntitySink(file: WritableResource, settings: CsvSettings) extends CsvSink(file, settings) with EntitySink {
 
   override def writeEntity(subject: String, values: Seq[Seq[String]]) {
     write(values.map(_.mkString(settings.arraySeparator.getOrElse(' ').toString)).mkString(settings.separator.toString) + "\n")
--- silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvLinkSink.scala
+++ silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvLinkSink.scala
@@ -2,12 +2,12 @@ package org.silkframework.plugins.dataset.csv
 
 import org.silkframework.dataset.LinkSink
 import org.silkframework.entity.Link
-import org.silkframework.runtime.resource.Resource
+import org.silkframework.runtime.resource.{WritableResource, Resource}
 
 /**
  * Created by andreas on 12/11/15.
  */
-class CsvLinkSink(file: Resource, settings: CsvSettings) extends CsvSink(file, settings) with LinkSink {
+class CsvLinkSink(file: WritableResource, settings: CsvSettings) extends CsvSink(file, settings) with LinkSink {
 
   override def writeLink(link: Link, predicateUri: String) {
     write(link.source + settings.separator + link.target + "\n")
