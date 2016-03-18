25b5529aa0de47bc2e580ad36ffeaaf242083653
Robert Isele <mail@robertisele.com>
2016-03-18 11:27:37 +0100

    LinkingPathsCache: Simplified path retrieval

--- silk-workspace/src/main/scala/org/silkframework/workspace/activity/linking/LinkingPathsCache.scala
+++ silk-workspace/src/main/scala/org/silkframework/workspace/activity/linking/LinkingPathsCache.scala
@@ -1,11 +1,11 @@
 package org.silkframework.workspace.activity.linking
 
-import org.silkframework.config.{DatasetSelection, LinkSpecification, TransformSpecification}
-import org.silkframework.dataset.Dataset
-import org.silkframework.entity.{Path, EntitySchema}
+import org.silkframework.config.{DatasetSelection, LinkSpecification}
+import org.silkframework.entity.EntitySchema
 import org.silkframework.runtime.activity.{Activity, ActivityContext}
 import org.silkframework.util.DPair
 import org.silkframework.workspace.Task
+import org.silkframework.workspace.activity.linking.LinkingTaskUtils._
 
 /**
  * Holds the most frequent paths.
@@ -45,14 +45,9 @@ class LinkingPathsCache(task: Task[LinkSpecification]) extends Activity[DPair[En
   }
 
   private def retrievePaths(datasetSelection: DatasetSelection) = {
-    task.project.taskOption[TransformSpecification](datasetSelection.inputId) match {
-      case Some(transformTask) =>
-        transformTask.data.rules.flatMap(_.target).map(Path(_)).distinct.toIndexedSeq
-      case None =>
-        // Retrieve the data source
-        val source = task.project.task[Dataset](datasetSelection.inputId).data.source
-        //Retrieve most frequent paths
-        source.retrievePaths(datasetSelection.typeUri, 1, Some(50))
-    }
+    // Retrieve the data source
+    val source = task.dataSource(datasetSelection.inputId)
+    //Retrieve most frequent paths
+    source.retrievePaths(datasetSelection.typeUri, 1, Some(50))
   }
 }
