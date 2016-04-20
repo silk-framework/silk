13cc9e86b0d7320b09146e3339752003e9adbecc
Knud Möller <dunken69@gmail.com>
2016-04-20 15:16:22 +0200

    jqueryui buttonset styling removed

--- silk-workbench/silk-workbench-rules/public/js/links.js
+++ silk-workbench/silk-workbench-rules/public/js/links.js
@@ -151,7 +151,7 @@ function updateResultsWidth() {
 }
 
 $(function() {
-  $("#selectLinks").buttonset();
+  //$("#selectLinks").buttonset();
 
   $(document).on('click', ".link-header", function(e) {
     var link_id = $(this).parent().attr('id');
