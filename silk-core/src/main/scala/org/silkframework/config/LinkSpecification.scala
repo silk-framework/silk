2e81a69fec1d5f4173b96313332400ef6cae57ca
Robert Isele <mail@robertisele.com>
2016-06-07 13:07:14 +0200

    Don't require EntitySchema to contain paths that are referenced by filter conditions

--- silk-core/src/main/scala/org/silkframework/config/LinkSpecification.scala
+++ silk-core/src/main/scala/org/silkframework/config/LinkSpecification.scala
@@ -61,8 +61,8 @@ case class LinkSpecification(id: Identifier = Identifier.random,
       case None => Set[Path]()
     }
 
-    val sourceEntityDesc = EntitySchema(dataSelections.source.typeUri, (sourcePaths ++ sourceRestriction.paths).toIndexedSeq.distinct, sourceRestriction)
-    val targetEntityDesc = EntitySchema(dataSelections.target.typeUri, (targetPaths ++ targetRestriction.paths).toIndexedSeq.distinct, targetRestriction)
+    val sourceEntityDesc = EntitySchema(dataSelections.source.typeUri, sourcePaths.toIndexedSeq.distinct, sourceRestriction)
+    val targetEntityDesc = EntitySchema(dataSelections.target.typeUri, targetPaths.toIndexedSeq.distinct, targetRestriction)
 
     DPair(sourceEntityDesc, targetEntityDesc)
   }
