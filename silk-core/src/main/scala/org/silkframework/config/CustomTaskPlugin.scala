7b3afc153db4265f6ab1d885fea14207ffd95a4a
Robert Isele <mail@robertisele.com>
2016-07-01 14:44:49 +0200

    CustomTaskPlugin: Added companion object factory

--- silk-core/src/main/scala/org/silkframework/config/CustomTaskPlugin.scala
+++ silk-core/src/main/scala/org/silkframework/config/CustomTaskPlugin.scala
@@ -1,8 +1,10 @@
 package org.silkframework.config
 
-import org.silkframework.runtime.plugin.AnyPlugin
+import org.silkframework.runtime.plugin.{AnyPlugin, PluginFactory}
 
 /**
   * A custom task provided by a plugin.
   */
 trait CustomTaskPlugin extends AnyPlugin
+
+object CustomTaskPlugin extends PluginFactory[CustomTaskPlugin]
