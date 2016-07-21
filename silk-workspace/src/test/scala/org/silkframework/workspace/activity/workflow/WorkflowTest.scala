dfc067635a42d2246f40261ebc472b4d2326957d
Andreas Schultz <a.schultz@fu-berlin.de>
2016-07-21 16:21:29 +0200

    Add workflow test

---
+++ silk-workspace/src/test/scala/org/silkframework/workspace/activity/workflow/WorkflowTest.scala
@@ -0,0 +1,50 @@
+package org.silkframework.workspace.activity.workflow
+
+import org.scalatest.{Matchers, FlatSpec}
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.silkframework.dataset.Dataset
+import org.silkframework.util.Identifier
+import org.silkframework.workspace.{Project, Task}
+
+/**
+  * Created on 7/21/16.
+  */
+class WorkflowTest extends FlatSpec with Matchers with MockitoSugar {
+  behavior of "Workflow"
+
+  it should "support sorting its workflow operators topologically" in {
+    val project = mock[Project]
+    val workflow = Workflow(
+      Identifier("workflow"),
+      operators = Seq(
+        operator(task = "transform1", inputs = Seq("dsA1"), outputs = Seq("dsB1")),
+        operator(task = "transform2", inputs = Seq("dsA2"), outputs = Seq("dsB1")),
+        operator(task = "linking", inputs = Seq("dsB1", "dsB1"), outputs = Seq("links")),
+        operator(task = "generateOutput", inputs = Seq("links"), outputs = Seq("output"))
+      ),
+      datasets = Seq(
+        dataset("dsA1"),
+        dataset("dsA2"),
+        dataset("dsB1"),
+        dataset("links"),
+        dataset("output")
+      ))
+    for(dataset <- workflow.datasets) {
+      val id = Identifier(dataset.task)
+      val datasetTask = mock[Task[Dataset]]
+      when(datasetTask.name).thenReturn(id)
+      when(project.taskOption[Dataset](id)).thenReturn(Some(datasetTask))
+    }
+    val sortedOperators = workflow.topologicalSortedOperators(project).map(_.task)
+    sortedOperators shouldBe Seq("transform1", "transform2", "linking", "generateOutput")
+  }
+
+  def operator(task: String, inputs: Seq[String], outputs: Seq[String]): WorkflowOperator = {
+    WorkflowOperator(inputs = inputs, task = task, outputs = outputs, Seq(), (0,0), task)
+  }
+
+  def dataset(task: String): WorkflowDataset = {
+    WorkflowDataset(Seq(), task, Seq(), (0, 0))
+  }
+}
