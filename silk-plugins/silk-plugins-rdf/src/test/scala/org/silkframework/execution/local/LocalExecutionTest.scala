8557e58ea2e189dd492dad44443038dad623861f
Andreas Schultz <a.schultz@fu-berlin.de>
2016-10-04 15:59:06 +0200

    Add test dependency on core of rdf plugin

--- silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/execution/local/LocalExecutionTest.scala
+++ silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/execution/local/LocalExecutionTest.scala
@@ -1,13 +1,13 @@
 package org.silkframework.execution.local
 
 import org.scalatest.{FlatSpec, ShouldMatchers}
+import org.silkframework.util.ConfigTestTrait
 
 /**
   * Created on 9/2/16.
   */
-class LocalExecutionTest extends FlatSpec with ShouldMatchers {
+class LocalExecutionTest extends FlatSpec with ShouldMatchers with ConfigTestTrait {
   behavior of "Local Execution"
-  System.setProperty("dataset.internal.plugin", "inMemory")
 
   private val ID: String = "id"
 
@@ -37,4 +37,12 @@ class LocalExecutionTest extends FlatSpec with ShouldMatchers {
     execution1.createInternalDataset(None) should not be theSameInstanceAs (execution2.createInternalDataset(None))
     execution1.createInternalDataset(Some(ID)) should not be theSameInstanceAs (execution2.createInternalDataset(Some(ID)))
   }
+
+  /** The properties that should be changed.
+    * If the value is [[None]] then the property value is removed,
+    * else it is set to the new value.
+    */
+  override def propertyMap: Map[String, Option[String]] = Map(
+    "dataset.internal.plugin" -> Some("inMemory")
+  )
 }
