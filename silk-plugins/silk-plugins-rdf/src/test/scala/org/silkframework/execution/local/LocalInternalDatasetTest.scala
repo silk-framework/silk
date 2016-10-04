c49e4e0cbb5c95cdac7401017f526309336f802e
Andreas Schultz <a.schultz@fu-berlin.de>
2016-10-04 17:32:38 +0200

    Use ConfigTestTrait to make LocalInternalDatasetTest stable

--- silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/execution/local/LocalInternalDatasetTest.scala
+++ silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/execution/local/LocalInternalDatasetTest.scala
@@ -2,12 +2,12 @@ package org.silkframework.execution.local
 
 import org.scalatest.{FlatSpec, MustMatchers}
 import org.silkframework.plugins.dataset.InternalDatasetTrait
+import org.silkframework.util.ConfigTestTrait
 
 /**
   * Tests the internal dataset. This test is located in this module as the default is to use the RDF-based one from this module.
   */
-class LocalInternalDatasetTest extends FlatSpec with MustMatchers {
-  System.setProperty("dataset.internal.plugin", "inMemory")
+class LocalInternalDatasetTest extends FlatSpec with MustMatchers with ConfigTestTrait {
   "LocalInternalDataset" should "store and retrieve data" in {
     val exec = LocalExecution(useLocalInternalDatasets = true)
     for(id <- Seq(None, Some("id"), Some("id2"))) {
@@ -23,4 +23,12 @@ class LocalInternalDatasetTest extends FlatSpec with MustMatchers {
       ds.sparqlEndpoint.select("SELECT ?s WHERE {?s ?p ?o}").bindings.size mustBe 1
     }
   }
+
+  /** The properties that should be changed.
+    * If the value is [[None]] then the property value is removed,
+    * else it is set to the new value.
+    */
+  override def propertyMap: Map[String, Option[String]] = Map(
+    "dataset.internal.plugin" -> Some("inMemory")
+  )
 }
