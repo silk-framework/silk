a0bc1e1ce62a39d0cbb3ecea553c7edf648915d4
Robert Isele <mail@robertisele.com>
2016-10-07 16:42:41 +0200

    XmlParserTask: Need to use constants for annotations

--- silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XmlParserTask.scala
+++ silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XmlParserTask.scala
@@ -11,18 +11,15 @@ import org.silkframework.util.Uri
 @Plugin(
   id = "XmlParserOperator",
   label = "XML Parser Operator",
-  description = "Takes exactly one input and reads either the defined inputPath or the first value of the first entity as " +
-      "XML document. Then executes the given output entity schema similar to the XML dataset to construct the result entities."
+  description =
+"""Takes exactly one input and reads either the defined inputPath or the first value of the first entity as
+XML document. Then executes the given output entity schema similar to the XML dataset to construct the result entities."""
 )
-case class XmlParserTask(@Param("The Silk path expression of the input entity that contains the XML document. If not set, " +
-    "the value of the first defined property will be taken.")
+case class XmlParserTask(@Param("The Silk path expression of the input entity that contains the XML document. If not set, the value of the first defined property will be taken.")
                          inputPath: String = "",
-                         @Param("The path to the elements to be read, starting from the root element, e.g., '/Persons/Person'. " +
-                             "If left empty, all direct children of the root element will be read.")
+                         @Param("The path to the elements to be read, starting from the root element, e.g., '/Persons/Person'. If left empty, all direct children of the root element will be read.")
                          basePath: String = "",
-                         @Param("A URI pattern that is relative to the base URI of the input entity, e.g., /{ID}, where {path} " +
-                             "may contain relative paths to elements. This relative part is appended to the input entity URI to " +
-                             "construct the full URI pattern.")
+                         @Param("A URI pattern that is relative to the base URI of the input entity, e.g., /{ID}, where {path} may contain relative paths to elements. This relative part is appended to the input entity URI to construct the full URI pattern.")
                          uriSuffixPattern: String = "") extends CustomTask {
   val parsedInputPath = {
     if (inputPath != "") {
