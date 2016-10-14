fbe0c99293248c217495031c09aa441e381bedc7
Robert Isele <mail@robertisele.com>
2016-10-14 18:11:54 +0200

    Added TokenizerTest

---
+++ silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/tokenization/TokenizerTest.scala
@@ -0,0 +1,8 @@
+package org.silkframework.rule.plugins.transformer.tokenization
+
+import org.silkframework.test.PluginTest
+
+class TokenizerTest extends PluginTest {
+
+  override protected def pluginObject = Tokenizer()
+}
