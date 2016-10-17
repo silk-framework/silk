94cedd3ebc63c2cf3f30a1dd4e37a7984d243313
Robert Isele <mail@robertisele.com>
2016-10-17 10:54:09 +0200

    Tokenizer: Added test

--- silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/tokenization/TokenizerTest.scala
+++ silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/tokenization/TokenizerTest.scala
@@ -5,4 +5,9 @@ import org.silkframework.test.PluginTest
 class TokenizerTest extends PluginTest {
 
   override protected def pluginObject = Tokenizer()
+
+  it should "split words" in {
+    val tokenizer = Tokenizer("\\s")
+    tokenizer(Seq(Seq("Hello World"))) shouldBe Seq("Hello", "World")
+  }
 }
