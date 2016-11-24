7b8ddcb187b697e0b69c1cab17e87381fba4e217
Knud Möller <dunken69@gmail.com>
2016-11-24 17:02:24 +0100

    all editor functionality now in editor object, incl. deserializeWorkflow

--- silk-workbench/silk-workbench-workflow/public/editor/editor.js
+++ silk-workbench/silk-workbench-workflow/public/editor/editor.js
@@ -1,7 +1,5 @@
 function WorkflowEditor() {
 
-  this.handler = new DynamicEndpointHandler();
-
   this.styles = {};
   this.styles.connectors = {};
   this.styles.endpoints = {};
@@ -38,58 +36,20 @@ function WorkflowEditor() {
     },
     connectorStyle: this.styles.connectors.plain,
     isTarget: true,
-    maxConnections: -1
+    maxConnections: 1
   }
-}
 
+  this.handler = new DynamicEndpointHandler();
+  this.handler.styles = editor.styles.endpoints;
 
-var connectorStyle = {
-  lineWidth: 4,
-  strokeStyle: "#61B7CF",
-  joinstyle: "round"
-};
-
-var connectorHoverStyle = {
-  strokeStyle: "#216477"
-};
-
-var endpointSource = {
-  anchor: "RightMiddle",
-  endpoint: "Dot",
-  paintStyle: {
-    fillStyle: "#3187CF",
-    radius: 4
-  },
-  connectorStyle: connectorStyle,
-  connectorHoverStyle: connectorHoverStyle,
-  connectorOverlays: [ [ "Arrow", { location: 1, width: 15, length: 15 } ] ],
-  connector: [ "Flowchart", { stub: 10, cornerRadius: 5 } ],
-  isSource: true,
-  maxConnections: -1
-};
-
-var endpointTarget = {
-  anchor: "LeftMiddle",
-  endpoint: "Dot",
-  paintStyle: {
-    fillStyle: "#3187CF",
-    radius: 4
-  },
-  connectorStyle: connectorStyle,
-  isTarget: true,
-  maxConnections: -1
-};
-
-$(function () {
+  var _this = this;
 
   // Set jsPlumb default values
   jsPlumb.setContainer("editorContent");
 
-  // Load workflow from backend
-  loadWorkflow();
-
-  // Make operators draggable
-  $('.toolboxOperator').draggable({
+  this.bindEvents() {
+    // Make operators draggable
+    $('.toolboxOperator').draggable({
       init: function() {
 
         this.helper = function() {
@@ -121,52 +81,134 @@ $(function () {
         }
         return this;
       }
-    }.init()
-  );
+      }.init()
+    );
 
-  // Handle dropped operators
-  $("#editorContent").droppable({
-    drop: function (ev, ui) {
-      // Check if we still need to add endpoints to the dropped element
-      if(jsPlumb.getEndpoints(ui.helper) === undefined) {
-        var id = ui.helper.attr('id');
-        // Hide operator in toolbox
-//        if($(ui.helper).hasClass('dataset')) {
-//          ui.draggable.hide();
-//        }
+    // Handle dropped operators
+    $("#editorContent").droppable({
+      drop: function (ev, ui) {
+        // Check if we still need to add endpoints to the dropped element
+        if(jsPlumb.getEndpoints(ui.helper) === undefined) {
+          var id = ui.helper.attr('id');
+          // Hide operator in toolbox
+          // if($(ui.helper).hasClass('dataset')) {
+          //   ui.draggable.hide();
+          // }
 
-        // Add operator to editor contents
-        $.ui.ddmanager.current.cancelHelperRemoval = true;
-        ui.helper.appendTo(this);
+          // Add operator to editor contents
+          $.ui.ddmanager.current.cancelHelperRemoval = true;
+          ui.helper.appendTo(this);
+
+          // Make operator draggable
+          jsPlumb.draggable(ui.helper);
+
+          // Add endpoints
+          jsPlumb.addEndpoint(id, _this.styles.endpoints.source);
+          jsPlumb.addEndpoint(id, _this.styles.endpoints.target);
+        }
+      }
+    });
+
+    // Delete connections on clicking them
+    jsPlumb.bind("click", function(conn, originalEvent) {
+      jsPlumb.detach(conn);
+    });
+
+  };
+
+  this.loadWorkflow = function() {
+    $.get(apiUrl, function(data) {
+      this.deserializeWorkflow($(data));
+    })
+    .fail(function(msg) {
+      alert( "Error loading workflow from backend: " + msg);
+    })
+  };
+
+  this.deserializeWorkflow = function(xml) {
+    // Retrieve the xml root element
+    var xmlRoot = xml.children('Workflow');
+    // Find the editor contents to put the operators into
+    var editorContent = $("#editorContent");
+    // Remember generated endpoints
+    var sourceEndpoints = {};
+    var targetEndpoints = {};
+
+    deserializeWorkflowOperator('Operator', 'operator');
+    deserializeWorkflowOperator('Dataset', 'dataset');
+
+    function deserializeWorkflowOperator(elementName, childClass) {
+      xmlRoot.find(elementName).each(function() {
+        var xml = $(this);
+        var taskId = xml.attr('task');
+        var opId = xml.attr('id');
+        var outputPriority = xml.attr('outputPriority')
+        if(opId === undefined) {
+          opId = taskId
+        }
+
+        var toolbox = $("#toolbox_" + taskId);
+        // Don't hide, workflow operators can be used multiple times
+        //    toolbox.hide();
+
+        var box = toolbox.children('.' + childClass).clone(false);
+        if(outputPriority) {
+          box.attr('output_priority', outputPriority);
+        }
+        box.attr('taskid', taskId);
+        box.attr('id', opId)
+        box.show();
+        box.css({top: xml.attr('posY') + 'px', left: xml.attr('posX') + 'px', position: 'absolute'});
+        box.appendTo(editorContent);
 
         // Make operator draggable
-        jsPlumb.draggable(ui.helper);
+        jsPlumb.draggable(box);
 
         // Add endpoints
-        jsPlumb.addEndpoint(id, endpointSource);
-        jsPlumb.addEndpoint(id, endpointTarget);
-      }
+        sourceEndpoints[opId] = jsPlumb.addEndpoint(box, _this.styles.endpoints.source);
+        targetEndpoints[opId] = jsPlumb.addEndpoint(box, _this.styles.endpoints.target);
+      });
     }
-  });
 
-  // Delete connections on clicking them
-  jsPlumb.bind("click", function(conn, originalEvent) {
-    jsPlumb.detach(conn);
-  });
+    connectEndpoints('Operator');
+    connectEndpoints('Dataset');
+
+    function connectEndpoints(elementName) {
+      // Connect endpoints
+      // Since operators are connected in both directions we only need to look at one direction, i.e. inputs.
+      xmlRoot.find(elementName).each(function() {
+        var xml = $(this);
+
+        var taskId = xml.attr('id');
+        // Connect inputs
+        $.each(xml.attr('inputs').split(','), function() {
+          if(this != "") {
+            jsPlumb.connect({source: sourceEndpoints[this], target: targetEndpoints[taskId]});
+          }
+        });
+      });
+    }
+  };
+
+  this.removeElement = function(elementId) {
+    //We need to set a time-out here as a element should not remove its own parent in its event handler
+    setTimeout(function() {
+      // Remove the elemenet from the workflow
+      jsPlumb.removeAllEndpoints(elementId);
+      $('#' + elementId).remove();
+      // Show the corresponding element in the toolbox again
+      $('#toolbox' + elementId.substring(elementId.indexOf("_"))).show();
+    }, 100);
+  };
+}
+
+
+
+$(function () {
 
   editor = new WorkflowEditor();
-  editor.handler.styles = editor.styles.endpoints;
-  editor.handler.bindEvents();
+  // Load workflow from backend
+  editor.loadWorkflow();
 
 });
 
-function removeElement(elementId) {
-  //We need to set a time-out here as a element should not remove its own parent in its event handler
-  setTimeout(function() {
-    // Remove the elemenet from the workflow
-    jsPlumb.removeAllEndpoints(elementId);
-    $('#' + elementId).remove();
-    // Show the corresponding element in the toolbox again
-    $('#toolbox' + elementId.substring(elementId.indexOf("_"))).show();
-  }, 100);
-}
