d0545181c8418b3c25252b2508281ec167ca5235
Andreas Schultz <a.schultz@fu-berlin.de>
2016-11-25 14:58:26 +0100

    Add test trait to configure in-memory workspace

---
+++ silk-workbench/silk-workbench-core/test/browser/workbench/InMemoryWorkspaceTestTrait.scala
@@ -0,0 +1,18 @@
+package browser.workbench
+
+import org.scalatest.Suite
+import org.silkframework.util.ConfigTestTrait
+
+/**
+  * A trait that will configure the workspace to be in-memory.
+  */
+trait InMemoryWorkspaceTestTrait extends ConfigTestTrait {
+  this: Suite =>
+  /** The properties that should be changed.
+    * If the value is [[None]] then the property value is removed,
+    * else it is set to the new value.
+    */
+  override def propertyMap: Map[String, Option[String]] = Map(
+    "workspace.provider.plugin" -> Some("inMemoryRdfWorkspace")
+  )
+}
