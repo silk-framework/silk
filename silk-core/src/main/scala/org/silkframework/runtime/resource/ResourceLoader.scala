8186965b39f4fd1177e69170b845e41037459c55
cwartner <cwartner@brox.de>
2016-12-02 18:13:50 +0100

    Add method to recursivly list files in subdirectories to ResourceLoader trait

--- silk-core/src/main/scala/org/silkframework/runtime/resource/ResourceLoader.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/resource/ResourceLoader.scala
@@ -1,28 +1,38 @@
 package org.silkframework.runtime.resource
 
 /**
- * Loads external resources that are required by a data set, such as files.
- */
+  * Loads external resources that are required by a data set, such as files.
+  */
 trait ResourceLoader {
 
   /**
-   * The full path of this resource loader.
-   */
+    * The full path of this resource loader.
+    */
   def basePath: String
 
   /**
-   * Lists all available resources.
-   */
+    * Lists all available resources. Only direct child resources are
+    * returned.
+    */
   def list: List[String]
 
   /**
-   * Retrieves a named resource.
-   *
-   * @param name The name of the resource.
-   * @param mustExist If true, an ResourceNotFoundException is thrown if the resource does not exist
-   * @return The resource.
-   * @throws ResourceNotFoundException If no resource with the given name has been found and mustExist is set to true.
-   */
+    * Lists all available resources in the base path.
+    * All resources in all subdirectories of the base path will be returned.
+    */
+  def listRecursive: List[String] = {
+    val children = for (child <- this.listChildren) yield this.child(child).listRecursive
+    this.list ++ children.flatten
+  }
+
+  /**
+    * Retrieves a named resource.
+    *
+    * @param name      The name of the resource.
+    * @param mustExist If true, an ResourceNotFoundException is thrown if the resource does not exist
+    * @return The resource.
+    * @throws ResourceNotFoundException If no resource with the given name has been found and mustExist is set to true.
+    */
   def get(name: String, mustExist: Boolean = false): Resource
 
   def listChildren: List[String]
