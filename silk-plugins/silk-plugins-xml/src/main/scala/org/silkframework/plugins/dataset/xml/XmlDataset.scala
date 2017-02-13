f5a57aaef547646e460e34b83142fb68c8a2f554
Robert Isele <mail@robertisele.com>
2017-02-13 16:24:04 +0100

    Updated XML doc

--- silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XmlDataset.scala
+++ silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XmlDataset.scala
@@ -10,7 +10,7 @@ import org.silkframework.runtime.resource.{WritableResource, Resource}
   description =
 """Retrieves all entities from an xml file.""",
   documentation =
-"""Typically, this dataset is used to transform an XML file to another format, e.g., to RDF. Currently only reading XML files is supported.
+"""Typically, this dataset is used to transform an XML file to another format, e.g., to RDF. Currently, writing XML files is not supported.
 When this dataset is used as an input for another task (e.g., a transformation task), the input type of the consuming task selects the path where the entities to be read are located.
 
 Example:
@@ -26,8 +26,23 @@ Example:
       </Person>
     </Persons>
 
-A transformation for reading all persons of the above XML would set the input type to "/Persons/Person".
+A transformation for reading all persons of the above XML would set the input type to `/Persons/Person`.
 The transformation iterates all entities matching the given input path.
+In the above example the first entity to be read is:
+
+    <Person>
+      <Name>John Doe</Name>
+      <Year>1970</Year>
+    </Person>
+
+All paths used in the consuming task are relative to this, e.g., the person name can be addressed with the path `/Name`.
+
+Path examples:
+
+- `/Persons/Person` selects all persons.
+- `/Persons/Person[Year = "1970"]` selects all persons which are born in 1970.
+- `/#` Is a special syntax for generating an id for a selected element. It can be used in URI patterns for entities which do not provide an identifier. Examples: `http://example.org/{#}` or `http://example.org/{/pathToEntity/#}`.
+- The backslash can be used to navigate to the parent XML node, e.g., `\Persons/SomeHeader`.
 """
 )
 case class XmlDataset(
