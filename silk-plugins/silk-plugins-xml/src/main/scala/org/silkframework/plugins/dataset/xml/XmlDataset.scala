36f6c9f7ef1f597cf6573dee6e40933113d22c3d
Robert Isele <mail@robertisele.com>
2017-02-13 15:41:00 +0100

    XMLDataset: Added documentation

--- silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XmlDataset.scala
+++ silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XmlDataset.scala
@@ -8,14 +8,34 @@ import org.silkframework.runtime.resource.{WritableResource, Resource}
   id = "xml",
   label = "XML",
   description =
-"""Retrieves all entities from an xml file."""
+"""Retrieves all entities from an xml file.""",
+  documentation =
+"""Typically, this dataset is used to transform an XML file to another format, e.g., to RDF. Currently only reading XML files is supported.
+When this dataset is used as an input for another task (e.g., a transformation task), the input type of the consuming task selects the path where the entities to be read are located.
+
+Example:
+
+    <Persons>
+      <Person>
+        <Name>John Doe</Name>
+        <Year>1970</Year>
+      </Person>
+      <Person>
+        <Name>Max Power</Name>
+        <Year>1980</Year>
+      </Person>
+    </Persons>
+
+A transformation for reading all persons of the above XML would set the input type to "/Persons/Person".
+The transformation iterates all entities matching the given input path.
+"""
 )
 case class XmlDataset(
   @Param("File name inside the resources directory. In the Workbench, this is the '(projectDir)/resources' directory.")
   file: Resource,
-  @Param("The path to the elements to be read, starting from the root element, e.g., '/Persons/Person'. If left empty, all direct children of the root element will be read.")
+  @Param(value = "The path to the elements to be read, starting from the root element, e.g., '/Persons/Person'. If left empty, all direct children of the root element will be read.", advanced = true)
   basePath: String = "",
-  @Param("A URI pattern, e.g., http://namespace.org/{ID}, where {path} may contain relative paths to elements")
+  @Param(value = "A URI pattern, e.g., http://namespace.org/{ID}, where {path} may contain relative paths to elements", advanced = true)
   uriPattern: String = "") extends Dataset {
 
   override def source: DataSource = new XmlSource(file, basePath, uriPattern)
