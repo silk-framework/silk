55d98c9c76fb269cd964515181056c7cbc15f299
Andreas Schultz <a.schultz@fu-berlin.de>
2017-03-09 18:12:43 +0100

    Remove Workflow id

--- silk-workspace/src/test/scala/org/silkframework/workspace/activity/workflow/WorkflowTest.scala
+++ silk-workspace/src/test/scala/org/silkframework/workspace/activity/workflow/WorkflowTest.scala
@@ -123,7 +123,6 @@ class WorkflowTest extends FlatSpec with MockitoSugar with MustMatchers {
 
   val testWorkflow: Workflow = {
     Workflow(
-      Identifier(WORKFLOW),
       operators = Seq(
         operator(task = TRANSFORM_1, inputs = Seq(DS_A1), outputs = Seq(DS_B1), TRANSFORM_1),
         operator(task = TRANSFORM_2, inputs = Seq(DS_A2), outputs = Seq(DS_B1), TRANSFORM_2),
@@ -142,7 +141,6 @@ class WorkflowTest extends FlatSpec with MockitoSugar with MustMatchers {
 
   val testWorkflowEndingInOperator: Workflow = {
     Workflow(
-      Identifier(WORKFLOW),
       operators = Seq(
         operator(task = TRANSFORM, inputs = Seq(DS_A, DS_B), outputs = Seq(), TRANSFORM)
       ),
@@ -154,7 +152,6 @@ class WorkflowTest extends FlatSpec with MockitoSugar with MustMatchers {
 
   val testWorkflowWithMultipleEndNodesAndDisjunctDataFlows: Workflow = {
     Workflow(
-      Identifier(WORKFLOW),
       operators = Seq(
         operator(task = TRANSFORM, inputs = Seq(DS_A, DS_B), outputs = Seq(), TRANSFORM, outputPriority = Some(1.5)),
         operator(task = OP_1, inputs = Seq(DS_C), outputs = Seq(), OP_1),
@@ -169,7 +166,6 @@ class WorkflowTest extends FlatSpec with MockitoSugar with MustMatchers {
 
   val singleNodeWorkflow: Workflow = {
     Workflow(
-      Identifier(WORKFLOW),
       operators = Seq(
         operator(task = OP_1, inputs = Seq(), outputs = Seq(), OP_1)
       ),
@@ -179,7 +175,6 @@ class WorkflowTest extends FlatSpec with MockitoSugar with MustMatchers {
 
   val circularWorkflow: Workflow = {
     Workflow(
-      Identifier("circularWorkflow"),
       operators = Seq(
         operator(task = TRANSFORM_1, inputs = Seq(TRANSFORM_2), outputs = Seq(TRANSFORM_2), TRANSFORM_1),
         operator(task = TRANSFORM_2, inputs = Seq(TRANSFORM_1), outputs = Seq(TRANSFORM_1), TRANSFORM_2)
