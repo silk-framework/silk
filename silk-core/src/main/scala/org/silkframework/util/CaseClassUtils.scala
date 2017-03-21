3ddc84417c23e7327270ef7c3df7890777ac3b76
Robert Isele <mail@robertisele.com>
2017-03-21 14:43:45 +0100

    Improvements to CaseClassUtils

--- silk-core/src/main/scala/org/silkframework/util/CaseClassUtils.scala
+++ silk-core/src/main/scala/org/silkframework/util/CaseClassUtils.scala
@@ -11,13 +11,12 @@ object CaseClassUtils {
     * Pretty prints case classes.
     */
   def prettyPrint(a: Any, indentation: Int = 0): String = {
-    // Recursively get all the fields; this will grab vals declared in parents of case classes.
-    def getFields(cls: Class[_]): List[Field] =
-      Option(cls.getSuperclass).map(getFields).getOrElse(Nil) ++
-        cls.getDeclaredFields.toList.filterNot(f =>
-          f.isSynthetic || java.lang.reflect.Modifier.isStatic(f.getModifiers))
-
     val indent = " " * indentation
+
+    def formatField(p: Product, field: Field) = {
+      field.setAccessible(true)
+      s"\n$indent  ${field.getName} = ${prettyPrint(field.get(p), indentation + 4)}"
+    }
 
     a match {
       // Make Strings look similar to their literal form.
@@ -30,16 +29,24 @@ object CaseClassUtils {
         s"Array(${xs.map(e => prettyPrint(e, indentation)) mkString ", "})"
       // This covers case classes.
       case p: Product =>
-        s"\n$indent${p.productPrefix}(\n${
-          (getFields(p.getClass) map { f =>
-            f setAccessible true
-            s"$indent  ${f.getName} = ${prettyPrint(f.get(p), indentation + 4)}"
-          }) mkString ",\n"
-        }\n$indent)"
+        val fields = getFields(p.getClass)
+        if(fields.isEmpty) {
+          s"${p.productPrefix}"
+        } else {
+          s"\n$indent${p.productPrefix}(${fields.map(formatField(p, _)).mkString(",")}\n$indent)"
+        }
       // General objects and primitives end up here.
       case q =>
         Option(q).map(_.toString).getOrElse("¡null!")
     }
   }
+
+  /**
+    *   Gets all fields of a case class
+    */
+  def getFields(cls: Class[_]): List[Field] =
+    Option(cls.getSuperclass).map(getFields).getOrElse(Nil) ++
+      cls.getDeclaredFields.toList.filterNot(f =>
+        f.isSynthetic || java.lang.reflect.Modifier.isStatic(f.getModifiers))
 
 }
