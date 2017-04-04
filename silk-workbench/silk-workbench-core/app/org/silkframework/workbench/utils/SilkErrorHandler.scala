331e74eeb479a77023e001b72f8f580cfe5cea34
Robert Isele <mail@robertisele.com>
2017-04-04 16:12:28 +0200

    Improved error page for client errors

--- silk-workbench/silk-workbench-core/app/org/silkframework/workbench/utils/SilkErrorHandler.scala
+++ silk-workbench/silk-workbench-core/app/org/silkframework/workbench/utils/SilkErrorHandler.scala
@@ -6,8 +6,8 @@ import javax.inject.Provider
 import org.silkframework.workspace.{ProjectNotFoundException, TaskNotFoundException}
 import play.api.PlayException.ExceptionSource
 import play.api.http.{DefaultHttpErrorHandler, MimeTypes}
-import play.api.mvc.Results.{InternalServerError, NotFound, Status}
-import play.api.mvc.{AcceptExtractors, RequestHeader, Result}
+import play.api.mvc.Results.{BadRequest, Forbidden, InternalServerError, NotFound, Status}
+import play.api.mvc.{AcceptExtractors, RequestHeader, Result, Results}
 import play.api.routing.Router
 import play.api.{Configuration, Environment, OptionalSourceMapper, UsefulException}
 
@@ -21,6 +21,13 @@ class SilkErrorHandler (env: Environment,
 
   private val log = Logger.getLogger(classOf[SilkErrorHandler].getName)
 
+  /**
+    * Invoked when a client error occurs, that is, an error in the 4xx series.
+    *
+    * @param request The request that caused the client error.
+    * @param statusCode The error status code.  Must be greater or equal to 400, and less than 500.
+    * @param message The error message.
+    */
   override def onClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
     if(prefersHtml(request)) {
       super.onClientError(request, statusCode, message)
@@ -31,6 +38,12 @@ class SilkErrorHandler (env: Environment,
     }
   }
 
+  /**
+    * Invoked when a server error occurs.
+    *
+    * @param request The request that triggered the server error.
+    * @param exception The server error.
+    */
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     if(prefersHtml(request)) {
       super.onServerError(request, exception)
@@ -39,12 +52,55 @@ class SilkErrorHandler (env: Environment,
     }
   }
 
-  override protected def onDevServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
-    Future.successful(InternalServerError(views.html.error(exception)(request.session)))
+  /**
+    * Invoked when a client makes a bad request.
+    *
+    * @param request The request that was bad.
+    * @param message The error message.
+    */
+  override protected def onBadRequest(request: RequestHeader, message: String): Future[Result] =
+    Future.successful(BadRequest(views.html.clientError(message)(request.session)))
+
+  /**
+    * Invoked when a client makes a request that was forbidden.
+    *
+    * @param request The forbidden request.
+    * @param message The error message.
+    */
+  override protected def onForbidden(request: RequestHeader, message: String): Future[Result] = {
+    Future.successful(Forbidden(views.html.clientError(message)(request.session)))
   }
 
+  /**
+    * Invoked when a client error occurs, that is, an error in the 4xx series, which is not handled by any of
+    * the other methods.
+    *
+    * @param request The request that caused the client error.
+    * @param statusCode The error status code.  Must be greater or equal to 400, and less than 500.
+    * @param message The error message.
+    */
+  override protected def onOtherClientError(request: RequestHeader, statusCode: Int, message: String): Future[Result] = {
+    Future.successful(Results.Status(statusCode)(views.html.clientError(message)(request.session)))
+  }
+
+  /**
+    * Invoked in dev mode when a server error occurs.
+    *
+    * @param request The request that triggered the error.
+    * @param exception The exception.
+    */
+  override protected def onDevServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
+    Future.successful(InternalServerError(views.html.serverError(exception)(request.session)))
+  }
+
+  /**
+    * Invoked in prod mode when a server error occurs.
+    *
+    * @param request The request that triggered the error.
+    * @param exception The exception.
+    */
   override protected def onProdServerError(request: RequestHeader, exception: UsefulException): Future[Result] = {
-    Future.successful(InternalServerError(views.html.error(exception)(request.session)))
+    Future.successful(InternalServerError(views.html.serverError(exception)(request.session)))
   }
 
   private def handleError(requestPath: String, ex: Throwable): Result = {
@@ -66,7 +122,7 @@ class SilkErrorHandler (env: Environment,
     }
   }
 
-  private def prefersHtml(request: RequestHeader): Boolean = {
+  protected def prefersHtml(request: RequestHeader): Boolean = {
     val htmlIndex = request.acceptedTypes.indexWhere(_.accepts(MimeTypes.HTML))
     val jsonIndex = request.acceptedTypes.indexWhere(_.accepts(MimeTypes.JSON))
 
