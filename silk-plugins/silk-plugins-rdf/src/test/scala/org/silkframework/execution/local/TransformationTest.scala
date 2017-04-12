fc51596ea4b86008aae0edcce6dbb1c9fb88ab1c
Robert Isele <mail@robertisele.com>
2017-04-12 15:41:17 +0200

    TransformationTest improvements

--- silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/execution/local/TransformationTest.scala
+++ silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/execution/local/TransformationTest.scala
@@ -1,9 +1,12 @@
 package org.silkframework.execution.local
 
-import java.io.File
+import java.io.{File, StringWriter}
 
+import com.hp.hpl.jena.rdf.model.ModelFactory
+import org.silkframework.dataset.rdf.SparqlParams
 import org.silkframework.entity.{BackwardOperator, Path}
-import org.silkframework.plugins.dataset.rdf.FileDataset
+import org.silkframework.plugins.dataset.rdf.{FileDataset, SparqlSink}
+import org.silkframework.plugins.dataset.rdf.endpoint.JenaModelEndpoint
 import org.silkframework.rule._
 import org.silkframework.rule.execution.ExecuteTransform
 import org.silkframework.rule.input.{PathInput, TransformInput}
@@ -68,16 +71,19 @@ object TransformationTest extends App {
 
 
   private def execute(resourceName: String, transform: TransformSpec): Unit = {
-    resources.get("transformOutput.nt").write("")
-
     val source = FileDataset(resources.get(resourceName), "Turtle")
 
-    val target = FileDataset(resources.get("transformOutput.nt"), "N-Triples")
+    val model = ModelFactory.createDefaultModel()
+    model.setNsPrefix("", "https://silkframework.org/ex/")
+    val endpoint = new JenaModelEndpoint(model)
+    val dataSink = new SparqlSink(SparqlParams(), endpoint)
 
-    val executor = new ExecuteTransform(source.source, transform, Seq(target.entitySink))
+    val executor = new ExecuteTransform(source.source, transform, Seq(dataSink))
     Activity(executor).startBlocking()
 
-    println("Output:\n" + resources.get("transformOutput.nt").loadAsString)
+    val stringWriter = new StringWriter()
+    model.write(stringWriter, "TURTLE")
+    println(stringWriter.toString)
   }
 
   private def uri(name: String) = "https://silkframework.org/ex/" + name
