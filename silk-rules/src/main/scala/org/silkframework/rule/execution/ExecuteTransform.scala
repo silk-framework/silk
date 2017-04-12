fc4f2972d29085bdc16361c95d55811dfef1d88d
Robert Isele <mail@robertisele.com>
2017-04-12 14:25:58 +0200

    ExecuteTransform refactoring

--- silk-rules/src/main/scala/org/silkframework/rule/execution/ExecuteTransform.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/execution/ExecuteTransform.scala
@@ -3,7 +3,7 @@ package org.silkframework.rule.execution
 import org.silkframework.dataset.{DataSource, EntitySink, TypedProperty}
 import org.silkframework.entity._
 import org.silkframework.execution.ExecutionReport
-import org.silkframework.rule.{HierarchicalMapping, TransformSpec, TypeMapping}
+import org.silkframework.rule.{HierarchicalMapping, TransformRule, TransformSpec, TypeMapping}
 import org.silkframework.rule.execution.local.TransformedEntities
 import org.silkframework.runtime.activity.{Activity, ActivityContext}
 import org.silkframework.runtime.validation.ValidationException
@@ -35,14 +35,34 @@ class ExecuteTransform(input: DataSource, transform: TransformSpec, outputs: Seq
   def run(context: ActivityContext[TransformReport]): Unit = {
     isCanceled = false
 
+    transformEntities(transform.inputSchema, transform.rules, transform.outputSchema, context)
+
+    for(HierarchicalMapping(_, relativePath, _, childRules) <- transform.rules) {
+      val inputSchema =
+        EntitySchema(
+          typeUri = transform.inputSchema.typeUri,
+          typedPaths = childRules.flatMap(_.paths).map(p => TypedPath(p, StringValueType)).distinct.toIndexedSeq,
+          subPath = relativePath
+        )
+      val outputSchema =
+        EntitySchema(
+          typeUri = childRules.collect { case tm: TypeMapping => tm.typeUri }.headOption.getOrElse(""),
+          typedPaths = childRules.flatMap(_.target).map(mt => TypedPath(Path(mt.propertyUri), mt.valueType)).toIndexedSeq
+        )
+
+      transformEntities(inputSchema, childRules, outputSchema, context)
+    }
+  }
+
+  private def transformEntities(inputSchema: EntitySchema, rules: Seq[TransformRule], outputSchema: EntitySchema, context: ActivityContext[TransformReport]): Unit = {
     try {
       for (output <- outputs) {
-        output.open(transform.outputSchema.typedPaths.map(_.property.get))
+        output.open(outputSchema.typedPaths.map(_.property.get))
       }
 
-      val entities = input.retrieve(transform.inputSchema)
+      val entities = input.retrieve(inputSchema)
 
-      val transformedEntities = new TransformedEntities(entities, transform.rules, transform.outputSchema, context.asInstanceOf[ActivityContext[ExecutionReport]])
+      val transformedEntities = new TransformedEntities(entities, rules, outputSchema, context.asInstanceOf[ActivityContext[ExecutionReport]])
       for (entity <- transformedEntities) {
         for (output <- outputs) {
           output.writeEntity(entity.uri, entity.values)
@@ -57,46 +77,6 @@ class ExecuteTransform(input: DataSource, transform: TransformSpec, outputs: Seq
         output.close()
       }
     }
-
-    for(rule @ HierarchicalMapping(_, _, _, _) <- transform.rules) {
-      val childPaths = rule.childRules.flatMap(_.target).map(p => TypedProperty(p.propertyUri.toString, p.valueType))
-
-      for(output <- outputs) {
-        output.open(childPaths)
-      }
-
-      val childSchema =
-        EntitySchema(
-          typeUri = transform.inputSchema.typeUri,
-          typedPaths = rule.childRules.flatMap(_.paths).map(p => TypedPath(p, StringValueType)).distinct.toIndexedSeq,
-          subPath = rule.relativePath
-        )
-
-      val entities = input.retrieve(childSchema)
-
-      val childOutputSchema =
-        EntitySchema(
-          typeUri = rule.childRules.collect { case tm: TypeMapping => tm.typeUri }.headOption.getOrElse(""),
-          typedPaths = rule.childRules.flatMap(_.target).map(mt => TypedPath(Path(mt.propertyUri), mt.valueType)).toIndexedSeq
-        )
-
-      val transformedEntities = new TransformedEntities(entities, rule.childRules, childOutputSchema, context.asInstanceOf[ActivityContext[ExecutionReport]])
-
-      for (entity <- transformedEntities) {
-        for (output <- outputs) {
-          output.writeEntity(entity.uri, entity.values)
-        }
-        if (isCanceled) {
-          return
-        }
-      }
-
-    }
-
-    for (output <- outputs) {
-      output.close()
-    }
-
   }
 
   override def cancelExecution(): Unit = {
