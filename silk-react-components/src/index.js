a726a35bb9dba8add10a4ac5857ccfe2e13198d9
Lukas Eipert <git@leipert.io>
2017-05-09 17:12:09 +0200

    expand minimal example to render transform task metadata

--- silk-react-components/src/index.js
+++ silk-react-components/src/index.js
@@ -1,9 +1,61 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import superagent from 'superagent';
 
+const Table = ({data, title}) => {
+    return (
+    <div>
+        <strong>{title}</strong>
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+    )
+}
 
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('react')
-);
+class TransformMetadata extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            inputSchemata: {},
+            outputSchema: {},
+            dependentTasks: [],
+        }
+        this.getData()
+    }
+
+    getData = () => {
+        superagent.get(this.props.api)
+            .end((err, result) => {
+                console.warn(result.body)
+
+                this.setState(
+                    result.body
+                )
+            })
+    }
+
+    render() {
+
+        const {inputSchemata, outputSchema, dependentTasks} = this.state;
+
+        return (
+            <div>
+                <h3>Task Metadata</h3>
+                <Table title="Input Schemata" data={inputSchemata} />
+                <Table title="Output Schema" data={outputSchema} />
+                <Table title="Dependent Tasks" data={dependentTasks} />
+                <button onClick={this.getData}>Retrieve Metadata</button>
+            </div>
+        );
+    }
+}
+
+const hierarchicalMapping = (containerId, apiUrl) => {
+    ReactDOM.render(
+      <TransformMetadata api={apiUrl}/>,
+      document.getElementById(containerId)
+    );
+}
+
+window.HierarchicalMapping = hierarchicalMapping;
