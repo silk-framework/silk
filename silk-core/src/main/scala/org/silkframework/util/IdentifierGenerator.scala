d46e4aa407ce4ecced605c91a4f663766368987c
Robert Isele <mail@robertisele.com>
2017-05-22 13:41:31 +0200

    IdentifierGenerator: Added some tests and fixed bugs

--- silk-core/src/main/scala/org/silkframework/util/IdentifierGenerator.scala
+++ silk-core/src/main/scala/org/silkframework/util/IdentifierGenerator.scala
@@ -13,17 +13,41 @@ class IdentifierGenerator(prefix: String = "") {
   /**
     * Generates a new unique identifier.
     */
-  def generate(name: String): Identifier = synchronized {
-    val identifier = Identifier.fromAllowed(prefix + name)
-
-    identifiers.get(identifier.toString) match {
+  def generate(identifier: String): Identifier = synchronized {
+    val (name, num) = split(Identifier.fromAllowed(prefix + identifier))
+    identifiers.get(name) match {
       case Some(count) =>
-        identifiers += ((identifier.toString, count + 1))
-        identifier + count.toString
+        identifiers += ((name, count + 1))
+        name + count.toString
       case None =>
-        identifiers += ((identifier.toString, 1))
-        identifier
+        identifiers += ((name, 1))
+        name
     }
+  }
+
+  /**
+    * Adds an existing identifier without changing it.
+    */
+  def add(identifier: Identifier): Unit = synchronized {
+    val (name, num) = split(identifier)
+    identifiers.get(name.toString) match {
+      case Some(count) =>
+        identifiers += ((name, math.max(count, num + 1)))
+      case None =>
+        identifiers += ((name, num + 1))
+    }
+  }
+
+  /**
+    * Splits an identifier into a name and a number.
+    * For instance 'name3' is split into (name, 3)
+    */
+  private def split(identifier: Identifier): (String, Int) = {
+    val str = identifier.toString
+    val splitIndex = str.length - str.reverse.indexWhere(!_.isDigit)
+    val (name, numStr) = str.splitAt(splitIndex)
+    val num = if(numStr.isEmpty) 0 else numStr.toInt
+    (name, num)
   }
 
 }
