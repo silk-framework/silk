3081f6c404112048f60cf0865b8308eb9d3a063b
Lukas Eipert <lukas.eipert@eccenca.com>
2017-06-07 14:18:59 +0200

    remove dead code

--- silk-react-components/src/index.js
+++ silk-react-components/src/index.js
@@ -1,56 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import superagent from 'superagent';
 
 import HierarchicalMappingComponent from './HierarchicalMapping/HierarchicalMapping.jsx';
-
-/*const Table = ({data, title}) => {
-    return (
-        <div>
-            <strong>{title}</strong>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-    );
-}
-
-class TransformMetadata extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            inputSchemata: {},
-            outputSchema: {},
-            dependentTasks: [],
-        }
-        this.getData()
-    }
-
-    getData = () => {
-        superagent.get(this.props.api)
-            .end((err, result) => {
-                console.warn(result.body)
-
-                this.setState(
-                    result.body
-                )
-            })
-    }
-
-    render() {
-
-        const {inputSchemata, outputSchema, dependentTasks} = this.state;
-
-        return (
-            <div>
-                <h3>Task Metadata</h3>
-                <Table title="Input Schemata" data={inputSchemata}/>
-                <Table title="Output Schema" data={outputSchema}/>
-                <Table title="Dependent Tasks" data={dependentTasks}/>
-                <button onClick={this.getData}>Retrieve Metadata</button>
-            </div>
-        );
-    }
-}*/
 
 const hierarchicalMapping = (containerId, apiUrl) => {
     ReactDOM.render(
