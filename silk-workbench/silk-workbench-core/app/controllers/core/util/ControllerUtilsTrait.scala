5ad0d99ebaa769811f663bcc7ab84fe9453d9db3
Robert Isele <mail@robertisele.com>
2017-06-23 15:54:37 +0200

    Readded method lost while merging

--- silk-workbench/silk-workbench-core/app/controllers/core/util/ControllerUtilsTrait.scala
+++ silk-workbench/silk-workbench-core/app/controllers/core/util/ControllerUtilsTrait.scala
@@ -33,6 +33,8 @@ trait ControllerUtilsTrait {
     (project, task)
   }
 
+  def getProject(projectName: String): Project = User().workspace.project(projectName)
+
   def task[T <: TaskSpec : ClassTag](projectName: String, taskName: String): ProjectTask[T] = {
     val project = User().workspace.project(projectName)
     val task = project.task[T](taskName)
