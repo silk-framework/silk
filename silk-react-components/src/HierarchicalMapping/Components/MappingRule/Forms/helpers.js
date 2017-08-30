7999c44b008e6d955912956e8e24700c78e46360
Lukas Eipert <lukas.eipert@eccenca.com>
2017-08-30 17:17:18 +0200

    catch thrown URI.js errors in hierarchical mapping view

--- silk-react-components/src/HierarchicalMapping/Components/MappingRule/Forms/helpers.js
+++ silk-react-components/src/HierarchicalMapping/Components/MappingRule/Forms/helpers.js
@@ -5,8 +5,11 @@ export const wasTouched = (initialValues, currentState) =>
     _.some(initialValues, (value, key) => value !== currentState[key]);
 
 export const newValueIsIRI = ({label}) => {
-    if (_.isString(label)) {
+    try {
         return new URI(label.replace(/^<|>$/g, '')).is('resourceURI');
+    } catch (e) {
+        // If the URI constructor throws an Error,
+        // we can be sure that the entered string is not an URI
     }
 
     return false;
