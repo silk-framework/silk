a87c92e542b0a986c1b78178632e984fff7539f2
Robert Isele <mail@robertisele.com>
2017-08-31 17:46:26 +0200

    Added stringMustBeInRange parameter to SubstringTransformer. Added tests.

---
+++ silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/substring/SubstringTransformerTest.scala
@@ -0,0 +1,5 @@
+package org.silkframework.rule.plugins.transformer.substring
+
+import org.silkframework.rule.test.TransformerTest
+
+class SubstringTransformerTest extends TransformerTest[SubstringTransformer]
