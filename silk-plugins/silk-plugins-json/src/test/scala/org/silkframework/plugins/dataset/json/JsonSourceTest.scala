ec1bfa35dd3764cf1ab07d8cda4c3694b2d0afe7
Andreas Schultz <a.schultz@fu-berlin.de>
2017-10-26 17:19:01 +0200

    Add test for JSON object URI generation

--- silk-plugins/silk-plugins-json/src/test/scala/org/silkframework/plugins/dataset/json/JsonSourceTest.scala
+++ silk-plugins/silk-plugins-json/src/test/scala/org/silkframework/plugins/dataset/json/JsonSourceTest.scala
@@ -62,4 +62,13 @@ class JsonSourceTest extends FlatSpec with MustMatchers {
     paths.size mustBe 9
     paths must contain allOf(Path.parse("/persons"), Path.parse("/persons/phoneNumbers"))
   }
+
+  it should "return valid URIs for resource paths" in {
+    val result = jsonSource.retrieve(EntitySchema(Uri(""), typedPaths = IndexedSeq(Path.parse("/persons").asStringTypedPath)))
+    val uris = result.flatMap(_.values.flatten).toSeq
+    for(uri <- uris) {
+      assert(Uri(uri).isValidUri, s"URI $uri was not valid!")
+    }
+    uris.distinct.size mustBe uris.size
+  }
 }
