d0cbcbcbdea467b4d3045e606e7f08996b3fb0a3
Andreas Schultz <a.schultz@fu-berlin.de>
2017-11-20 18:38:36 +0100

    Allow to change vocabulary plugin at runtime

--- silk-rules/src/main/scala/org/silkframework/rule/vocab/VocabularyManager.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/vocab/VocabularyManager.scala
@@ -1,5 +1,6 @@
 package org.silkframework.rule.vocab
 
+import org.silkframework.config.DefaultConfig
 import org.silkframework.runtime.plugin.PluginRegistry
 import org.silkframework.util.Identifier
 
@@ -10,11 +11,19 @@ trait VocabularyManager {
 }
 
 object VocabularyManager {
+  private var lastPlugin: String = ""
+  private var vocabularyManager: Option[VocabularyManager] = None
 
-  private lazy val instance = {
-    PluginRegistry.createFromConfig[VocabularyManager]("vocabulary.manager")
+  private def instance = this.synchronized {
+    val plugin = DefaultConfig.instance().getString("vocabulary.manager.plugin")
+    if(plugin != lastPlugin || vocabularyManager.isEmpty) {
+      vocabularyManager = Some(PluginRegistry.createFromConfig[VocabularyManager]("vocabulary.manager"))
+      lastPlugin = plugin
+    }
+    vocabularyManager.get
+
   }
 
-  def apply() = instance
+  def apply(): VocabularyManager = instance
 
 }
