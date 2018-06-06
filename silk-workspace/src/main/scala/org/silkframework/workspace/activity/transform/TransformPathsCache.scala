02257d852f1d2295946d5367e31948ef5572d776
Andreas Schultz <a.schultz@fu-berlin.de>
2018-06-06 10:22:28 +0200

    Move CachedEntitySchemata to own file

--- silk-workspace/src/main/scala/org/silkframework/workspace/activity/transform/TransformPathsCache.scala
+++ silk-workspace/src/main/scala/org/silkframework/workspace/activity/transform/TransformPathsCache.scala
@@ -1,18 +1,14 @@
 package org.silkframework.workspace.activity.transform
 
-import org.silkframework.dataset.DatasetSpec.GenericDatasetSpec
-import org.silkframework.dataset.{Dataset, DatasetSpec}
 import org.silkframework.dataset.rdf.RdfDataset
-import org.silkframework.entity.{EntitySchema, PathOperator, TypedPath}
+import org.silkframework.dataset.{Dataset, DatasetSpec}
+import org.silkframework.entity.EntitySchema
 import org.silkframework.rule.TransformSpec
 import org.silkframework.runtime.activity.ActivityContext
 import org.silkframework.runtime.resource.WritableResource
-import org.silkframework.runtime.serialization.{ReadContext, WriteContext, XmlFormat, XmlSerialization}
 import org.silkframework.util.Uri
 import org.silkframework.workspace.ProjectTask
 import org.silkframework.workspace.activity.{CachedActivity, PathsCacheTrait}
-
-import scala.xml.{Node, Null}
 
 /**
  * Holds the most frequent paths.
@@ -60,56 +56,4 @@ class TransformPathsCache(transformTask: ProjectTask[TransformSpec]) extends Cac
   override def resource: WritableResource = transformTask.project.cacheResources.child("transform").child(transformTask.id).get(s"pathsCache.xml")
 
   override protected val wrappedXmlFormat = WrappedXmlFormat()
-}
-
-/**
-  * The cached schemata of the input tasks
-  * @param configuredSchema The schema of the input as configured in this transform spec
-  * @param untypedSchema    The optional schema of the input without type. This is stored for some datasets, currently
-  *                         only RDF datasets, in order to make services like auto-completion work in hierarchical mappings.
-  */
-case class CachedEntitySchemata(configuredSchema: EntitySchema, untypedSchema: Option[EntitySchema]) {
-  /**
-    * Returns the cached paths. Depending on the provided context either the configured or the untyped
-    * cached paths are returned.
-    * @param task       The transform task for which the paths are requested
-    * @param sourcePath The complete source path at which position the paths are requested, e.g. for auto-completion
-    * @return
-    */
-  def fetchCachedPaths(task: ProjectTask[TransformSpec], sourcePath: List[PathOperator]): IndexedSeq[TypedPath] = {
-    if(task.selection.typeUri.uri.nonEmpty && sourcePath.nonEmpty && isRdfInput(task) && untypedSchema.isDefined) {
-      untypedSchema.get.typedPaths
-    } else {
-      configuredSchema.typedPaths
-    }
-  }
-
-  def isRdfInput(task: ProjectTask[TransformSpec]): Boolean = task.project.taskOption[GenericDatasetSpec](task.selection.inputId).exists(_.data.plugin.isInstanceOf[RdfDataset])
-}
-
-object CachedEntitySchemata {
-  implicit object CachedEntitySchemaXmlFormat extends XmlFormat[CachedEntitySchemata] {
-    override def read(value: Node)(implicit readContext: ReadContext): CachedEntitySchemata = {
-      val configured = XmlSerialization.fromXml[EntitySchema]((value \ "ConfiguredEntitySchema" \ "EntityDescription").head)
-      val untyped = (value \ "UnTypedEntitySchema" \ "EntityDescription").headOption.map(XmlSerialization.fromXml[EntitySchema])
-      CachedEntitySchemata(configured, untyped)
-    }
-
-    override def write(value: CachedEntitySchemata)(implicit writeContext: WriteContext[Node]): Node = {
-      <CachedEntitySchemata>
-        <ConfiguredEntitySchema>
-          {XmlSerialization.toXml(value.configuredSchema)}
-        </ConfiguredEntitySchema>
-        { value.untypedSchema match {
-            case Some(schema) =>
-              <UnTypedEntitySchema>
-                {XmlSerialization.toXml(schema)}
-              </UnTypedEntitySchema>
-            case None =>
-              Null
-          }
-        }
-      </CachedEntitySchemata>
-    }
-  }
 }
