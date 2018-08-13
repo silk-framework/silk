af0e7cdf777b17d21f01d6a9e4fd5b1d9bddf499
chile12 <markus.freudenberg@gmail.com>
2018-08-13 09:39:34 +0200

    Initial Commit (bugfix/RepairingTaskPropagation)

--- silk-core/src/main/scala/org/silkframework/execution/DatasetExecutor.scala
+++ silk-core/src/main/scala/org/silkframework/execution/DatasetExecutor.scala
@@ -36,12 +36,12 @@ trait DatasetExecutor[DatasetType <: Dataset, ExecType <: ExecutionType] extends
     * @return
     */
   final override def execute(
-                              task: Task[DatasetSpec[DatasetType]],
-                              inputs: Seq[ExecType#DataType],
-                              outputSchema: Option[EntitySchema],
-                              execution: ExecType,
-                              context: ActivityContext[ExecutionReport]
-                            ): Option[ExecType#DataType] = {
+    task: Task[DatasetSpec[DatasetType]],
+    inputs: Seq[ExecType#DataType],
+    outputSchema: Option[EntitySchema],
+    execution: ExecType,
+    context: ActivityContext[ExecutionReport]
+  ): Option[ExecType#DataType] = {
 
     for (input <- inputs) {
       write(input, task, execution)
