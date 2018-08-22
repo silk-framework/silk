77ae28a9e39335d28dcd5ae168aeda14c9a9c68f
Robert Isele <mail@robertisele.com>
2018-08-22 14:47:08 +0200

    Added linkLimit parameter to limit the number of links that are generated

--- silk-workspace/src/main/scala/org/silkframework/workspace/activity/linking/GenerateLinksFactory.scala
+++ silk-workspace/src/main/scala/org/silkframework/workspace/activity/linking/GenerateLinksFactory.scala
@@ -7,6 +7,7 @@ import org.silkframework.runtime.plugin.{Param, Plugin}
 import org.silkframework.workspace.ProjectTask
 import org.silkframework.workspace.activity.TaskActivityFactory
 import org.silkframework.workspace.activity.linking.LinkingTaskUtils._
+import GenerateLinksFactory._
 
 @Plugin(
   id = "GenerateLinks",
@@ -20,11 +21,14 @@ case class GenerateLinksFactory(
   @Param("Use a file cache. This avoids memory overflows for big files.")
   useFileCache: Boolean = false,
   @Param("The number of entities in a single partition in the cache.")
-  partitionSize: Int = 300,
+  partitionSize: Int = DEFAULT_PARTITION_SIZE,
   @Param("Generate detailed information about the matched entities. If set to false, the generated links won't be shown in the Workbench.")
   generateLinksWithEntities: Boolean = true,
   @Param("Write the generated links to the configured output of this task.")
-  writeOutputs: Boolean = true) extends TaskActivityFactory[LinkSpec, GenerateLinks] {
+  writeOutputs: Boolean = true,
+  @Param("If defined, the execution will stop after the configured number of links is reached.\nThis is just a hint and the execution may produce slightly fewer or more links.")
+  linkLimit: Int = DEFAULT_LINK_LIMIT
+  ) extends TaskActivityFactory[LinkSpec, GenerateLinks] {
 
   def apply(task: ProjectTask[LinkSpec]): Activity[Linking] = {
     val runtimeConfig =
@@ -32,7 +36,8 @@ case class GenerateLinksFactory(
         includeReferenceLinks = includeReferenceLinks,
         useFileCache = useFileCache,
         partitionSize = partitionSize,
-        generateLinksWithEntities = generateLinksWithEntities
+        generateLinksWithEntities = generateLinksWithEntities,
+        linkLimit = Some(linkLimit)
       )
     new GenerateLinksActivity(task, runtimeConfig, writeOutputs)
   }
@@ -43,9 +48,9 @@ class GenerateLinksActivity(task: ProjectTask[LinkSpec], runtimeConfig: RuntimeL
   @volatile
   private var generateLinks: Option[GenerateLinks] = None
 
-  override def name = "GenerateLinks"
+  override def name: String = "GenerateLinks"
 
-  override def initialValue = Some(Linking())
+  override def initialValue: Option[Linking] = Some(Linking())
 
   /**
     * Executes this activity.
@@ -57,9 +62,7 @@ class GenerateLinksActivity(task: ProjectTask[LinkSpec], runtimeConfig: RuntimeL
 
     val inputs = task.dataSources
 
-    val outputs =
-      if (writeOutputs) task.linkSinks
-      else Nil
+    val outputs = if (writeOutputs) task.linkSinks else Nil
 
     generateLinks = Some(
       new GenerateLinks(
@@ -74,6 +77,14 @@ class GenerateLinksActivity(task: ProjectTask[LinkSpec], runtimeConfig: RuntimeL
     generateLinks = None
   }
 
-  override def cancelExecution() = generateLinks.foreach(_.cancelExecution())
+  override def cancelExecution(): Unit = generateLinks.foreach(_.cancelExecution())
+
+}
+
+object GenerateLinksFactory {
+
+  val DEFAULT_PARTITION_SIZE = 100
+
+  val DEFAULT_LINK_LIMIT = 10000
 
 }
