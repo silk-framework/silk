717e72b1afc06e87cb66d541bcd8ea4b158a8153
Robert Isele <mail@robertisele.com>
2018-08-30 14:54:24 +0200

    Moved code to retrieve the current execution to ExecutorRegistry

--- silk-core/src/main/scala/org/silkframework/execution/ExecutionManager.scala
+++ silk-core/src/main/scala/org/silkframework/execution/ExecutionManager.scala
@@ -2,8 +2,14 @@ package org.silkframework.execution
 
 import org.silkframework.runtime.plugin.AnyPlugin
 
+/**
+  * Plugin to manage the current execution.
+  */
 trait ExecutionManager extends AnyPlugin {
 
+  /**
+    * Returns the current execution. Usually this is a cached instance.
+    */
   def current(): ExecutionType
 
 }
