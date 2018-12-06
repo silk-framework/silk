f56566062082739eb2031c2f145c035b78367e70
Robert Isele <mail@robertisele.com>
2018-12-06 14:43:15 +0100

    Clean eccenca Corporate Memory identifiers

--- silk-core/src/test/scala/org/silkframework/config/MetaDataTest.scala
+++ silk-core/src/test/scala/org/silkframework/config/MetaDataTest.scala
@@ -15,4 +15,11 @@ class MetaDataTest extends FlatSpec with MustMatchers {
     labelFromId("myABC") mustBe "my ABC"
   }
 
+  it should "clean eccenca Corporate Memory identifiers" in {
+    labelFromId("datasetresource_1544099996423_customers_csv") mustBe "customers csv"
+    labelFromId("transform_datasetresource_1544099996423_customers_csv") mustBe "transform customers csv"
+    labelFromId("linking_transform_datasetresource_1544100171360_loans_csv_transform_datasetresource_1544099996423_customers_csv") mustBe
+      "linking transform loans csv transform customers csv"
+  }
+
 }
