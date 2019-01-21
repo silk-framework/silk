dd47932149e34ed0adb93fdd82332f242c746d36
Christian Wartner <cwartner@brox.de>
2019-01-21 11:37:14 +0100

    Add message hleper function/constants, clean up

--- silk-core/src/main/scala/org/silkframework/entity/metadata/ExecutionFailure.scala
+++ silk-core/src/main/scala/org/silkframework/entity/metadata/ExecutionFailure.scala
@@ -11,6 +11,9 @@ package org.silkframework.entity.metadata
   * The exceptions are therefore wrapped in this easily serializable case class. This reduces the serialization problem
   * to a mapping of exceptions to this class. This class is not a subclass of throwable and should not be thrown anywhere.
   *
+  * However, since the serializers are called from various places and expect a Throwable an asThrowable method is provided.
+  * Like wise this object can be instantiated with a given Throwable.
+  *
   * A message does not necessarily exist. In the ExecutionFailure it is mandatory. It should always at least have a message
   * saying that no information could be found.
   *
@@ -28,26 +31,27 @@ case class ExecutionFailure( message: String,
                              className: Option[String]
                            ) {
 
-  def apply(t: Throwable) = {
-    if (t.getCause != null) {
-      ExecutionFailure(t.getMessage, Some(ExecutionFailure.fromThrowable(t.getCause)), Some(t.getClass.getSimpleName))
-    }
-    else {
-      ExecutionFailure(t.getMessage, None, Some(t.getClass.getSimpleName))
-    }
+  def apply(t: Throwable): ExecutionFailure = {
+    ExecutionFailure.fromThrowable(t)
   }
 
   var stackTrace: Option[Array[StackTraceElement]] = None
-
-  def setStackTrace(ste: Array[StackTraceElement]): Unit = {
-    stackTrace = Some(ste)
+  def setStackTrace(st: Array[StackTraceElement]): Unit = {
+    stackTrace = Some(st)
   }
 
   def getStackTrace: Array[StackTraceElement] = stackTrace.getOrElse(Array.empty)
 
-  def getMessage: String = message
+  def getMessage: String = {
+    if (message == null || message.trim.isEmpty) {
+      ExecutionFailure.NOMSG + " " + ExecutionFailure.messageOriginClass(className)
+    }
+    else {
+      message
+    }
+  }
 
-  def getExceptionClass: String = className.getOrElse("unknown class")
+  def getExceptionClass: String = className.getOrElse(ExecutionFailure.NOCLA)
 
   def getCause: Option[ExecutionFailure] = cause
 }
@@ -56,6 +60,14 @@ case class ExecutionFailure( message: String,
   * Some helper methods for JSON and XML serialization.
   */
 object ExecutionFailure {
+
+
+  /* messages */
+  final val NOMSG: String = "The exception message was empty or incorrectly de/derialized."
+  final val NOCLA: String = "The exception class does not exist or was incorrectly de/derialized."
+  def messageOriginClass(className: Option[String]): String = {
+    s"The origin class was: ${className.getOrElse("unknown")}"
+  }
 
   /**
     * Basic ExecutionFailure instance with only an optional message
