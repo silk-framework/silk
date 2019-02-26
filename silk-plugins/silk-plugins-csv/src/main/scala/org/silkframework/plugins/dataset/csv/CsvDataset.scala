45c32173e1ae79b1da8781d8b5b288da6c9b9c45
Christian Wartner <cwartner@brox.de>
2019-02-26 11:38:33 +0100

    Add bulk load supporting trait for implementing in datasets

--- silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvDataset.scala
+++ silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvDataset.scala
@@ -38,7 +38,9 @@ case class CsvDataset
     ignoreBadLines: Boolean = false,
   @Param(label = "Quote escape character",
     value = "Escape character to be used inside quotes, used to escape the quote character. It must also be used to escape itself, e.g. by doubling it, e.g. \"\". If left empty, it defaults to quote.")
-  quoteEscapeCharacter: String = "\"") extends Dataset with DatasetPluginAutoConfigurable[CsvDataset] with WritableResourceDataset with CsvDatasetTrait with ResourceBasedDataset {
+  quoteEscapeCharacter: String = "\"") extends Dataset with DatasetPluginAutoConfigurable[CsvDataset] with WritableResourceDataset with CsvDatasetTrait with ResourceBasedDataset with BulkResourceSupport {
+
+  checkForBulkResource()
 
   override def source(implicit userContext: UserContext): DataSource = csvSource()
 
