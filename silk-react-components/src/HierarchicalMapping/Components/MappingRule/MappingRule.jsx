3254f6481a696e6a2257d56c65243b3aff87ee53
Alberto Martínez Pizarro <alberto.martinezpizarro@eccenca.com>
2019-03-01 09:17:41 +0100

    add animation of copied Mapping Rules

--- silk-react-components/src/HierarchicalMapping/Components/MappingRule/MappingRule.jsx
+++ silk-react-components/src/HierarchicalMapping/Components/MappingRule/MappingRule.jsx
@@ -47,8 +47,12 @@ const MappingRule = React.createClass({
 
     // initilize state
     getInitialState() {
+        const isPasted = sessionStorage.getItem('pastedId') &&
+            (sessionStorage.getItem('pastedId') === this.props.id) &&
+            !sessionStorage.removeItem('pastedId');
         return {
-            expanded: false,
+            isPasted : isPasted,
+            expanded: isPasted || false,
             editing: false,
             askForDiscard: false,
             loading: false,
@@ -331,6 +335,7 @@ const MappingRule = React.createClass({
                             'ecc-silk-mapping__ruleitem--literal':
                                 type !== 'object',
                             'ecc-silk-mapping__ruleitem--defect': errorInfo,
+                            'mdl-layout_item--background-flash': this.state.isPasted,
                         })}>
                         <div
                             className={'ecc-silk-mapping__ruleitem--dnd'}
