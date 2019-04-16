757bbabc90db0466862a68492bb01e663aeb5084
Robert Isele <mail@robertisele.com>
2019-04-16 13:40:42 +0200

    Syntax fixes

--- silk-core/src/main/scala/org/silkframework/runtime/resource/EmptyResourceManager.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/resource/EmptyResourceManager.scala
@@ -5,38 +5,46 @@ import java.time.Instant
 /**
  * A resource manager that does not provide any resources.
  */
-case class EmptyResourceManager(name: String = "", parent: Option[ResourceManager] = None) extends ResourceManager {
+object EmptyResourceManager {
 
-  def basePath: String = parent.map(_.basePath).getOrElse("") + "/" + name
+  private lazy val rootInstance = new EmptyResourceManagerImpl("", None)
 
-  override def list: Nil.type = Nil
+  /** Retrieves the empty resource manager */
+  def apply(): ResourceManager = rootInstance
 
-  override def get(name: String, mustExist: Boolean): WritableResource = {
-    if(mustExist) {
-      throw new ResourceNotFoundException(s"Cannot retrieve resource $name as no resource manager is available.")
-    } else {
-      new NonExistentResource(name)
+  private class EmptyResourceManagerImpl(val name: String, val parent: Option[ResourceManager]) extends ResourceManager {
+
+    def basePath: String = parent.map(_.basePath).getOrElse("") + "/" + name
+
+    override def list: Nil.type = Nil
+
+    override def get(name: String, mustExist: Boolean): WritableResource = {
+      if(mustExist) {
+        throw new ResourceNotFoundException(s"Cannot retrieve resource $name as no resource manager is available.")
+      } else {
+        new NonExistentResource(basePath, name)
+      }
     }
-  }
 
-  override def delete(name: String) {
-    throw new UnsupportedOperationException("Cannot delete resource.")
-  }
+    override def delete(name: String) {
+      throw new UnsupportedOperationException("Cannot delete resource.")
+    }
 
-  override def listChildren: Nil.type = Nil
+    override def listChildren: Nil.type = Nil
 
-  /**
-    * Returns an empty resource manager.
-    */
-  override def child(name: String): ResourceManager = {
-    EmptyResourceManager(name, Some(this))
+    /**
+      * Returns an empty resource manager.
+      */
+    override def child(name: String): ResourceManager = {
+      new EmptyResourceManagerImpl(name, Some(this))
+    }
   }
 
   /**
     * If the user calls get(mustExist = false), he gets a resource that actually does not exist.
     * We need this to be consistent with the semantics of the other resource managers.
     */
-  private class NonExistentResource(val name: String) extends WritableResource {
+  private class NonExistentResource(val basePath: String, val name: String) extends WritableResource {
 
     override def delete(): Unit = {}
 
@@ -56,4 +64,5 @@ case class EmptyResourceManager(name: String = "", parent: Option[ResourceManage
       throw new ResourceNotFoundException(s"Cannot write to resource $path as no resource manager is available.")
     }
   }
+
 }
