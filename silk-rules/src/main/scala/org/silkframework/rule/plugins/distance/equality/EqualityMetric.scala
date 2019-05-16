9fe6f732dca01ba52ecdef608ad8483c518ed6cb
Andreas Schultz <aschultz@brox.de>
2019-05-16 09:07:17 +0200

    Better describe the string equality and numeric equality operators

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/distance/equality/EqualityMetric.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/distance/equality/EqualityMetric.scala
@@ -22,7 +22,8 @@ import org.silkframework.runtime.plugin.Plugin
   id = "equality",
   categories = Array("Equality", "Recommended"),
   label = "String Equality",
-  description = "Returns success if string values are equal, failure otherwise."
+  description = "Checks for equality of the string representation of the given values. Returns success if string values are equal, failure otherwise. For" +
+      " a numeric comparison of values use the 'Numeric Equality' comparator."
 )
 case class EqualityMetric() extends SimpleDistanceMeasure {
 
