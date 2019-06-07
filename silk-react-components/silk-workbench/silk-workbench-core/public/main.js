793f5035377db90dc754894989f01068cdee7914
Robert Isele <mail@robertisele.com>
2019-06-07 16:06:17 +0200

    Improvements to Activity REST Api and Status Polling

--- silk-react-components/silk-workbench/silk-workbench-core/public/main.js
+++ silk-react-components/silk-workbench/silk-workbench-core/public/main.js
@@ -210,3 +210,30 @@ function generateNewIdsForTooltips($parent) {
         }
     });
 }
+
+/**
+ * Connects to a WebSocket on window load and calls a provided function on every update.
+ *
+ * param websocketUrl Address of a WebSocket endpoint that supplies updates via JSON objects.
+ * param pollingUrl Address of a polling endpoint for fallback.
+ * param updateFunc Function to be called on every update. Receives a single argument, which is the received JSON object.
+ */
+function connectWebSocket(websocketUrl, pollingUrl, updateFunc) {
+    const websocket = new WebSocket(websocketUrl);
+    websocket.onmessage = function(evt) { updateFunc(JSON.parse(evt.data)) };
+    websocket.onerror = function(evt) {
+        console.log("Connecting to WebSocket at '" + websocketUrl + "' failed. Falling back to polling...");
+        let lastUpdate = 0;
+        setInterval(function() {
+            let currentTime = new Date().getTime();
+            $.getJSON(pollingUrl + "&timestamp=" + lastUpdate, function(data) {
+                lastUpdate = currentTime;
+                if(Array.isArray(data)) {
+                    data.forEach(updateFunc)
+                } else {
+                    updateFunc(data);
+                }
+            });
+        }, 500);
+    };
+}
--- silk-workbench/silk-workbench-workspace/app/controllers/workspace/ActivityApi.scala
+++ silk-workbench/silk-workbench-workspace/app/controllers/workspace/ActivityApi.scala
@@ -5,20 +5,18 @@ import java.util.logging.{LogRecord, Logger}
 import akka.actor.ActorSystem
 import akka.stream.Materializer
 import akka.stream.scaladsl.{Flow, Merge, Sink, Source}
-import controllers.core.{RequestUserContextAction, Stream, UserContextAction, Widgets}
+import controllers.core.{RequestUserContextAction, UserContextAction}
 import controllers.util.{AkkaUtils, SerializationUtils}
 import javax.inject.Inject
 import org.silkframework.config.TaskSpec
-import org.silkframework.runtime.activity.{Activity, ActivityControl, UserContext, _}
+import org.silkframework.runtime.activity.{Activity, UserContext, _}
 import org.silkframework.runtime.serialization.WriteContext
-import org.silkframework.runtime.validation.{BadUserInputException, NotFoundException, ValidationException}
+import org.silkframework.runtime.validation.BadUserInputException
 import org.silkframework.serialization.json.ActivitySerializers.ExtendedStatusJsonFormat
 import org.silkframework.util.Identifier
 import org.silkframework.workbench.utils.ErrorResult
 import org.silkframework.workspace.activity.WorkspaceActivity
 import org.silkframework.workspace.{Project, ProjectTask, WorkspaceFactory}
-import play.api.http.ContentTypes
-import play.api.libs.iteratee.Enumerator
 import play.api.libs.json.{JsArray, JsValue, Json}
 import play.api.mvc._
 
@@ -65,7 +63,7 @@ class ActivityApi @Inject() (implicit system: ActorSystem, mat: Materializer) ex
   }
 
   def cancelActivity(projectName: String, taskName: String, activityName: String): Action[AnyContent] = UserContextAction { implicit userContext: UserContext =>
-    val activity = activityControl(projectName, taskName, activityName)
+    val activity = singleActivity(projectName, taskName, activityName).control
     activity.cancel()
     Ok
   }
@@ -74,7 +72,7 @@ class ActivityApi @Inject() (implicit system: ActorSystem, mat: Materializer) ex
                       taskName: String,
                       activityName: String,
                       blocking: Boolean): Action[AnyContent] = UserContextAction { implicit userContext: UserContext =>
-    val activity = activityControl(projectName, taskName, activityName)
+    val activity = singleActivity(projectName, taskName, activityName).control
     activity.reset()
     if(blocking) {
       activity.startBlocking()
@@ -116,16 +114,17 @@ class ActivityApi @Inject() (implicit system: ActorSystem, mat: Materializer) ex
   }
 
   def getActivityStatus(projectName: String, taskName: String, activityName: String): Action[AnyContent] = UserContextAction { implicit userContext: UserContext =>
-    val activity = activityControl(projectName, taskName, activityName)
+    val activity = singleActivity(projectName, taskName, activityName)
     implicit val writeContext = WriteContext[JsValue]()
-    Ok(new ExtendedStatusJsonFormat(projectName, taskName, activityName, activity.startTime).write(activity.status()))
+
+    Ok(new ExtendedStatusJsonFormat(activity).write(activity.status))
   }
 
   def getActivityValue(projectName: String,
                        taskName: String,
                        activityName: String): Action[AnyContent] = RequestUserContextAction { implicit request => implicit userContext: UserContext =>
     implicit val project: Project = WorkspaceFactory().workspace.project(projectName)
-    val activity = activityControl(projectName, taskName, activityName)
+    val activity = singleActivity(projectName, taskName, activityName).control
     val value = activity.value()
     SerializationUtils.serializeRuntime(value)
   }
@@ -153,73 +152,107 @@ class ActivityApi @Inject() (implicit system: ActorSystem, mat: Materializer) ex
     Ok(JsonSerializer.logRecords(ActivityLog.records))
   }
 
-  def activityUpdates(projectName: String,
+  def getActivityStatusUpdates(projectName: String,
+                               taskName: String,
+                               activityName: String,
+                               timestamp: Long): Action[AnyContent] = UserContextAction { implicit userContext: UserContext =>
+    val activities = allActivities(projectName, taskName, activityName)
+    implicit val writeContext = WriteContext[JsValue]()
+
+    Ok(
+      JsArray(
+        for(activity <- activities if activity.status.timestamp >= timestamp) yield {
+          new ExtendedStatusJsonFormat(activity).write(activity.status)
+        }
+      )
+    )
+  }
+
+  def activityStatusUpdatesWebsocket(projectName: String,
                       taskName: String,
                       activityName: String): WebSocket = {
 
     implicit val userContext = UserContext.Empty
     implicit val writeContext = WriteContext[JsValue]()
 
-    val projects =
-      if (projectName.nonEmpty) WorkspaceFactory().workspace.project(projectName) :: Nil
-      else WorkspaceFactory().workspace.projects
-
-    def tasks(project: Project) =
-      if (taskName.nonEmpty) project.anyTask(taskName) :: Nil
-      else project.allTasks
-
-    def projectActivities(project: Project) =
-      if (taskName.nonEmpty) Nil
-      else project.activities
-
-    def taskActivities(task: ProjectTask[_ <: TaskSpec]) =
-      if (activityName.nonEmpty) task.activity(activityName) :: Nil
-      else task.activities
-
-    val projectActivitySources =
-      for (project <- projects; activity <- projectActivities(project)) yield {
-        implicit val format = new ExtendedStatusJsonFormat(project.name, "", activity.name, activity.startTime)
-        AkkaUtils.createSource(activity.control.status).map(format.write)
-      }
-
-    val taskActivitySources =
-      for (project <- projects;
-           task <- tasks(project);
-           activity <- taskActivities(task)) yield {
-        implicit val format = new ExtendedStatusJsonFormat(project.name, task.id, activity.name, activity.startTime)
+    val activities = allActivities(projectName, taskName, activityName)
+    val sources =
+      for(activity <- activities) yield {
+        implicit val format = new ExtendedStatusJsonFormat(activity)
         AkkaUtils.createSource(activity.control.status).map(format.write)
       }
 
     // Combine all sources into a single flow
-    val allSources = projectActivitySources ++ taskActivitySources
-    val combinedSources = Source.combine(Source.empty, Source.empty, allSources :_*)(Merge(_))
+    val combinedSources = Source.combine(Source.empty, Source.empty, sources :_*)(Merge(_))
     val flow = Flow.fromSinkAndSource(Sink.ignore, combinedSources)
 
     AkkaUtils.createWebSocket(flow)
   }
 
-  private def activityControl(projectName: String, taskName: String, activityName: String)
-                             (implicit userContext: UserContext): ActivityControl[_] = {
+  /**
+    * Retrieves a single workspace activity.
+    */
+  private def singleActivity(projectName: String, taskName: String, activityName: String)
+                      (implicit userContext: UserContext): WorkspaceActivity[_] = {
     val project = WorkspaceFactory().workspace.project(projectName)
-    val activityId: Identifier = activityName
     if (taskName.nonEmpty) {
       val task = project.anyTask(taskName)
-      val activities = task.activities.flatMap(_.allInstances.get(activityId).asInstanceOf[Option[ActivityControl[_]]].toSeq)
-      activities match {
-        case Seq(activity) => activity
-        case Seq() => throw new NotFoundException(s"Activity with id $activityName not found")
-        case _ => throw new ValidationException(s"Multiple activities with id $activityName found")
-      }
+      task.activity(activityName)
     } else {
-      project.activity(activityName).control
+      project.activity(activityName)
     }
+  }
+
+  /**
+    * Retrieves all workspace activities that satisfy the filter conditions.
+    */
+  private def allActivities(projectName: String, taskName: String, activityName: String)
+                           (implicit userContext: UserContext): Seq[WorkspaceActivity[_]] = {
+    val projects: Seq[Project] =
+      if (projectName.nonEmpty) {
+        Seq(WorkspaceFactory().workspace.project(projectName))
+      } else {
+        WorkspaceFactory().workspace.projects
+      }
+
+    def tasks(project: Project): Seq[ProjectTask[_ <: TaskSpec]] =
+      if(taskName.nonEmpty) {
+        Seq(project.anyTask(taskName))
+      } else {
+        project.allTasks
+      }
+
+    def activities(task: ProjectTask[_ <: TaskSpec]): Seq[WorkspaceActivity[_]] = {
+      if (activityName.nonEmpty) {
+        Seq(task.activity(activityName))
+      } else {
+        task.activities
+      }
+    }
+
+    val projectActivities: Seq[WorkspaceActivity[_]] = {
+      if (taskName.nonEmpty) {
+        Seq.empty
+      } else {
+        for (project <- projects;
+             activity <- project.activities) yield activity
+      }
+    }
+
+    val taskActivities: Seq[WorkspaceActivity[_]] = {
+      for(project <- projects;
+          task <- tasks(project);
+          activity <- activities(task)) yield activity
+    }
+
+    projectActivities ++ taskActivities
   }
 
   /** Only affects activities with singleton==false setting, removes activity control instance */
   def removeActivityControl(projectName: String,
                             taskName: String,
                             activityName: String): Action[AnyContent] = UserContextAction { implicit userContext: UserContext =>
-    val activity = activityControl(projectName, taskName, activityName)
+    val activity = singleActivity(projectName, taskName, activityName).control
     activity.cancel()
     Ok
   }
