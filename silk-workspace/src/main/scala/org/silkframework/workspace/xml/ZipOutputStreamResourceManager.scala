214aecac986d34acf5acf47b39874fe94d7aacf3
Markus Freudenberg <markus.freudenberg@eccenca.com>
2019-08-20 15:02:03 +0200

    Extend ZipOutputStreamResourceManager

--- silk-workspace/src/main/scala/org/silkframework/workspace/xml/ZipOutputStreamResourceManager.scala
+++ silk-workspace/src/main/scala/org/silkframework/workspace/xml/ZipOutputStreamResourceManager.scala
@@ -2,7 +2,9 @@ package org.silkframework.workspace.xml
 
 import java.io.{InputStream, OutputStream}
 import java.time.Instant
+import java.util.concurrent.ConcurrentHashMap
 import java.util.zip.{ZipEntry, ZipOutputStream}
+import scala.collection.JavaConverters._
 
 import org.silkframework.runtime.resource.{ResourceManager, WritableResource}
 
@@ -10,31 +12,43 @@ import org.silkframework.runtime.resource.{ResourceManager, WritableResource}
   * A resource manager that writes all data to a ZIP output stream.
   * Does not support reading data or any modification to already written data.
   */
-case class ZipOutputStreamResourceManager(zip: ZipOutputStream, basePath: String = "") extends ResourceManager {
+case class ZipOutputStreamResourceManager(zip: ZipOutputStream, basePath: String = "", closeEntriesAutomatically: Boolean = true) extends ResourceManager {
 
-  override def child(name: String): ResourceManager = ZipOutputStreamResourceManager(zip, basePath + "/" + name)
+  private val children = new ConcurrentHashMap[String, ZipOutputStreamResourceManager]()
+  private val resources = new ConcurrentHashMap[String, WritableResource]()
 
-  override def get(name: String, mustExist: Boolean): WritableResource = {
-    ZipWritableResource(name)
+  override def child(name: String): ResourceManager = synchronized {
+    val rm = ZipOutputStreamResourceManager(zip, basePath + "/" + name)
+    children.put(name, rm)
+    rm
+  }
+
+  override def get(name: String, mustExist: Boolean): WritableResource = synchronized {
+    val res = ZipWritableResource(name, closeEntriesAutomatically)
+    resources.put(name, res)
+    res
   }
 
   override def parent: Option[ResourceManager] = throw ZipReadException()
 
-  override def list: List[String] = throw ZipReadException()
+  override def list: List[String] = resources.keys.asScala.toList
 
-  override def listChildren: List[String] = throw ZipReadException()
+  override def listChildren: List[String] = children.keys.asScala.toList
 
   override def delete(name: String): Unit = throw ZipDeleteException()
 
   /**
     * Writes a single entry to the zip output stream.
+    * @param name - the name of the entry
+    * @param closeEntriesAutomatically - if true, each write call will create a new entry, else the caller has to manage entries
     */
-  case class ZipWritableResource(name: String) extends WritableResource {
+  case class ZipWritableResource(name: String, closeEntriesAutomatically: Boolean) extends WritableResource {
 
     override def path: String = basePath.stripPrefix("/") + "/" + name
 
     override def write(append: Boolean)(write: OutputStream => Unit): Unit = {
-      zip.putNextEntry(new ZipEntry(path))
+      if(closeEntriesAutomatically)
+        zip.putNextEntry(new ZipEntry(path))
       write(zip)
     }
 
@@ -47,6 +61,7 @@ case class ZipOutputStreamResourceManager(zip: ZipOutputStream, basePath: String
     override def delete(): Unit = throw ZipDeleteException()
 
     override def inputStream: InputStream = throw ZipReadException()
+
   }
 
   case class ZipReadException() extends UnsupportedOperationException(s"${classOf[ZipOutputStreamResourceManager]} does not support any read operations.")
