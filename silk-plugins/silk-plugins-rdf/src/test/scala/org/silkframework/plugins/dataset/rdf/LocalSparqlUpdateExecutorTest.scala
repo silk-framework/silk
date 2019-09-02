b5e1b7d916e714b0bd7231fbdd859520b3c7435d
Andreas Schultz <a.schultz@fu-berlin.de>
2019-09-02 09:55:14 +0200

    Adapt test to changes

--- silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/plugins/dataset/rdf/LocalSparqlUpdateExecutorTest.scala
+++ silk-plugins/silk-plugins-rdf/src/test/scala/org/silkframework/plugins/dataset/rdf/LocalSparqlUpdateExecutorTest.scala
@@ -2,6 +2,7 @@ package org.silkframework.plugins.dataset.rdf
 
 import org.scalatest.{FlatSpec, MustMatchers}
 import org.scalatestplus.mockito.MockitoSugar
+import org.mockito.Mockito._
 import org.silkframework.config.{PlainTask, Prefixes}
 import org.silkframework.entity._
 import org.silkframework.entity.paths.{TypedPath, UntypedPath}
@@ -18,6 +19,7 @@ class LocalSparqlUpdateExecutorTest extends FlatSpec with MustMatchers with Mock
   behavior of "Local SPARQL Update Executor"
 
   implicit val uc: UserContext = UserContext.Empty
+  implicit val prefixes: Prefixes = Prefixes.empty
 
   // execution data
   private val executor = LocalSparqlUpdateExecutor()
@@ -31,9 +33,12 @@ class LocalSparqlUpdateExecutorTest extends FlatSpec with MustMatchers with Mock
     Entity("http://example.org/entity/3", IndexedSeq(Seq(), Seq(notIncluded)), schema),
     Entity("http://example.org/entity/4", IndexedSeq(Seq("http://s2a", "http://s2b"), Seq("s2a", "s2b", "s2c")), schema)
   )
-  private val inputTask = mock[PlainTask[TransformSpec]]
+  private val inputTaskMock = mock[PlainTask[TransformSpec]]
+  private val transformSpecMock = mock[TransformSpec]
+  when(inputTaskMock.data).thenReturn(transformSpecMock)
+  when(transformSpecMock.properties).thenReturn(Seq.empty)
   private val context = TestMocks.activityContextMock()
-  private val input = Seq(GenericEntityTable(inputEntities, schema, inputTask))
+  private val input = Seq(GenericEntityTable(inputEntities, schema, inputTaskMock))
 
   it should "generate the correct batches" in {
     val result = executor.execute(task, input, ExecutorOutput.empty, LocalExecution(true), context)
@@ -56,7 +61,7 @@ class LocalSparqlUpdateExecutorTest extends FlatSpec with MustMatchers with Mock
 
   it should "throw validation exception if an invalid input schema is found" in {
     val invalidSchema = EntitySchema("", typedPaths = IndexedSeq("s", "wrong").map(UntypedPath(_).asUntypedValueType))
-    val input = Seq(GenericEntityTable(inputEntities, invalidSchema, inputTask))
+    val input = Seq(GenericEntityTable(inputEntities, invalidSchema, inputTaskMock))
     intercept[ValidationException] {
       executor.execute(task, input, ExecutorOutput.empty, LocalExecution(true), context).get.entities.head
     }
