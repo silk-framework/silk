48b790b6e6adcffe49c3e93386d0c9b9296d15f6
Robert Isele <mail@robertisele.com>
2019-09-06 11:22:26 +0200

    MultiEntitySchema.typedPaths should only return the paths of the pivot schema to be symmetric to the other methods and the actual representation of the data in individual tables.

--- silk-core/src/main/scala/org/silkframework/entity/MultiEntitySchema.scala
+++ silk-core/src/main/scala/org/silkframework/entity/MultiEntitySchema.scala
@@ -17,9 +17,8 @@ class MultiEntitySchema(private val pivot: EntitySchema, private val subs: Index
   lazy val subSchemata: IndexedSeq[EntitySchema] = getNonPivotSchemata(this)
   //NOTE: ----------------------------------------------------------------------------
 
-  // TypedPaths of pivot and typedPaths of subSchemata (prepended with their sub-paths)
-  override val typedPaths: IndexedSeq[TypedPath] = pivotSchema.typedPaths ++
-    subSchemata.flatMap(ses => ses.typedPaths.map(tp => TypedPath(ses.subPath.operators ++ tp.operators, tp.valueType, tp.isAttribute)))
+  // TypedPaths of pivot table
+  override val typedPaths: IndexedSeq[TypedPath] = pivotSchema.typedPaths
 
   /**
     * Will replace the property uris of selects paths of a given EntitySchema, using a Map[oldUri, newUri].
