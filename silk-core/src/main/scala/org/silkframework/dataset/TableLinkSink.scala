486baee4cdc21d791c35f34e1cf970c71bf8ed22
Robert Isele <mail@robertisele.com>
2019-09-17 12:00:48 +0200

    Added new generic link sink that can be used for all table based datasets.

---
+++ silk-core/src/main/scala/org/silkframework/dataset/TableLinkSink.scala
@@ -0,0 +1,30 @@
+package org.silkframework.dataset
+import org.silkframework.entity.{Link, StringValueType}
+import org.silkframework.runtime.activity.UserContext
+
+/**
+  * Generic link sink that write links to a table with two columns.
+  * The columns are named ''source'' and ''target''.
+  */
+class TableLinkSink(entitySink: EntitySink) extends LinkSink {
+
+  override def init()(implicit userContext: UserContext): Unit = {
+    entitySink.openTable("", Seq(
+      TypedProperty("source", StringValueType, isBackwardProperty = false),
+      TypedProperty("target", StringValueType, isBackwardProperty = false)))
+  }
+
+  override def writeLink(link: Link, predicateUri: String)
+                        (implicit userContext: UserContext){
+    entitySink.writeEntity(link.source, Seq(Seq(link.source), Seq(link.target)))
+  }
+
+  override def close()(implicit userContext: UserContext): Unit = {
+    entitySink.closeTable()
+    entitySink.close()
+  }
+
+  override def clear()(implicit userContext: UserContext): Unit = {
+    entitySink.clear()
+  }
+}
