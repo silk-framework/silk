4b35242d519cceec5f88653509585ed624c5d092
extern.rkeyan <robert.keyan@eccenca.com>
2019-09-19 15:33:32 +0400

    Add istanbul coverage reporter and add coverage and report folder in gitingore

--- silk-react-components/jest.config.js
+++ silk-react-components/jest.config.js
@@ -28,6 +28,12 @@ module.exports = {
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/file.js',
     },
+    collectCoverageFrom: [
+        "**/*.{js,jsx}",
+        "!**/node_modules/**",
+        "!**/vendor/**"
+    ],
+    coverageDirectory: './test/reports/coverage',
     reporters: [
         'default',
         [
