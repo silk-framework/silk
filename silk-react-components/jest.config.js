6efa6ca877360ce1651674d9f5da9e91dde8b5e4
extern.rkeyan <robert.keyan@eccenca.com>
2019-09-19 15:36:36 +0400

    Fix istanbul configuration

--- silk-react-components/jest.config.js
+++ silk-react-components/jest.config.js
@@ -29,7 +29,7 @@ module.exports = {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/test/file.js',
     },
     collectCoverageFrom: [
-        "**/*.{js,jsx}",
+        "src/HierarchicalMapping/**/*.{js,jsx}",
         "!**/node_modules/**",
         "!**/vendor/**"
     ],
