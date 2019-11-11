433af6ec7107b371ab69bcda109531e2ba5c2eda
Andreas Schultz <a.schultz@fu-berlin.de>
2019-11-11 11:48:53 +0100

    Add route for new workspace for testing

--- silk-workbench/app/controllers/Workbench.scala
+++ silk-workbench/app/controllers/Workbench.scala
@@ -3,14 +3,15 @@ package controllers
 import config.WorkbenchConfig
 import controllers.core.RequestUserContextAction
 import javax.inject.Inject
-import play.api.mvc.{InjectedController, Action, AnyContent, ControllerComponents}
+import play.api.mvc.{Action, AnyContent, InjectedController}
 import play.twirl.api.Html
 
-class Workbench @Inject() () extends InjectedController {
+class Workbench @Inject() (assets: Assets) extends InjectedController {
 
   def index: Action[AnyContent] = RequestUserContextAction { implicit request =>implicit userContext =>
     val welcome = Html(WorkbenchConfig.get.welcome.loadAsString)
     Ok(views.html.start(welcome))
   }
 
+  def reactWorkbench: Action[AnyContent] = assets.at("/public", "index.html", aggressiveCaching = false)
 }
