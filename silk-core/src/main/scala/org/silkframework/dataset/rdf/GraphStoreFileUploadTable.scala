a9c955686cdef988e67cf93d5ac4082eeb207f8c
Andreas Schultz <a.schultz@fu-berlin.de>
2019-12-13 08:59:17 +0100

    Fix GraphStoreFileUploadTable.updateEntities

--- silk-core/src/main/scala/org/silkframework/dataset/rdf/GraphStoreFileUploadTable.scala
+++ silk-core/src/main/scala/org/silkframework/dataset/rdf/GraphStoreFileUploadTable.scala
@@ -15,5 +15,5 @@ case class LocalGraphStoreFileUploadTable(files: Traversable[FileResource], task
 
   override def entities: Traversable[Entity] = Traversable.empty
 
-  override def updateEntities(newEntities: Traversable[Entity]): LocalEntities = EmptyEntityTable(task)
+  override def updateEntities(newEntities: Traversable[Entity]): LocalEntities = this // Changing entities has no effect
 }
