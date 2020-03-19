92e8544000850a11e73aad8fbeaae1c9b9dab777
Michael Haschke <mhaschke@brox.de>
2020-03-19 16:46:53 +0100

    improve OverviewItemList element
    Former-commit-id: 68795b49a5a09d9bd913aceab590cce369a9ff1c

--- workspace/src/app/wrappers/src/components/OverviewItem/OverviewItem.tsx
+++ workspace/src/app/wrappers/src/components/OverviewItem/OverviewItem.tsx
@@ -3,10 +3,22 @@ import React from "react";
 const OverviewItem = ({
     children,
     className = '',
+    densityHigh = false,
+    hasDivider = false,
+    hasSpacing = false,
     ...restProps
 }: any) => {
     return (
-        <div {...restProps} className={'ecc-overviewitem__item '+className}>
+        <div
+            {...restProps}
+            className={
+                'ecc-overviewitem__item ' +
+                (densityHigh ? 'ecc-overviewitem__item--highdensity ' : '') +
+                (hasDivider ? 'ecc-overviewitem__item--hasdivider ' : '') +
+                (hasSpacing ? 'ecc-overviewitem__item--hasspacing ' : '') +
+                className
+            }
+        >
             {children}
         </div>
     )
--- workspace/src/app/wrappers/src/components/OverviewItem/OverviewItemList.tsx
+++ workspace/src/app/wrappers/src/components/OverviewItem/OverviewItemList.tsx
@@ -3,12 +3,28 @@ import React from "react";
 const OverviewItemList = ({
     children,
     className = '',
+    densityHigh = false,
+    hasDivider = false,
+    hasSpacing = false,
     ...restProps
 }: any) => {
     return (
-        <div {...restProps} className={'ecc-overviewitem__list '+className}>
-            {children}
-        </div>
+        <ol
+            {...restProps}
+            className={
+                'ecc-overviewitem__list ' +
+                (densityHigh ? 'ecc-overviewitem__list--highdensity ' : '') +
+                (hasDivider ? 'ecc-overviewitem__list--hasdivider ' : '') +
+                (hasSpacing ? 'ecc-overviewitem__list--hasspacing ' : '') +
+                className
+            }
+        >
+            {
+                React.Children.map(children, (child, i) => {
+                    return <li>{ child }</li>
+                })
+            }
+        </ol>
     )
 }
 
