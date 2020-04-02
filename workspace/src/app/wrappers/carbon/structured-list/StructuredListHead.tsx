3d89468c96e8b369be6836bf0849ced2b68786b6
Michael Haschke <mhaschke@brox.de>
2020-04-02 15:53:41 +0200

    remove structured list wrapper
    Former-commit-id: 1feb4b1a8535fb5454d9f0a5ee9407e251da2da8

--- workspace/src/app/wrappers/carbon/structured-list/StructuredListHead.tsx
+++
@@ -1,4 +0,0 @@
-import React, { memo } from 'react';
-import { StructuredListHead } from "carbon-components-react";
-
-export default memo(StructuredListHead);
--- workspace/src/app/wrappers/carbon/structured-list/StructuredListWrapper.tsx
+++
@@ -1,4 +0,0 @@
-import React, { memo } from 'react';
-import { StructuredListWrapper } from "carbon-components-react";
-
-export default memo(StructuredListWrapper);
