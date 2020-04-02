339e0a50795b024fcaa5a990645e9e79d0f88b3a
Robert Isele <mail@robertisele.com>
2020-04-02 08:58:24 +0200

    Fixed import

--- silk-react-components/src/HierarchicalMapping/EvaluateMapping.jsx
+++ silk-react-components/src/HierarchicalMapping/EvaluateMapping.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import _ from 'lodash';
 
 import PropTypes from 'prop-types';
-import setApiDetails from './store';
+import {setApiDetails} from './store';
 import MappingsTree from './containers/MappingsTree';
 
 /**
