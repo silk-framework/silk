e1600e8edbb08ccc0992d1bd9a0c63ea3ca4a597
Michael Haschke <mhaschke@brox.de>
2020-04-06 20:10:22 +0200

    add option to align grid column vertical centered
    Former-commit-id: 183d2a56bef850da838f7012b62d5fd485c5ed1e

--- workspace/src/app/wrappers/src/components/Grid/GridColumn.tsx
+++ workspace/src/app/wrappers/src/components/Grid/GridColumn.tsx
@@ -8,13 +8,22 @@ function GridColumn({
     small = false,
     medium = false,
     full = true,
-    ...restProps
+    verticalAlign = '',
+    ...otherProps
 }: any) {
     let sizeConfig = {};
     if (small) sizeConfig = { md:2, lg:3 };
     if (medium) sizeConfig = { md:3, lg:5 };
     return (
-        <CarbonColumn {...restProps} {...sizeConfig} className={'ecc-grid__column '+className}>
+        <CarbonColumn
+            {...otherProps}
+            {...sizeConfig}
+            className={
+                'ecc-grid__column' +
+                (verticalAlign ? ' ecc-grid__column--vertical-'+verticalAlign : '') +
+                (className ? ' '+className : '')
+            }
+        >
             { children }
         </CarbonColumn>
     )
