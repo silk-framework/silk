fbb8c27ab930f7a85e2ff3290a06c6ed20bdcdb7
Robert Isele <mail@robertisele.com>
2020-04-08 10:45:56 +0200

    Added rule operator for generating UUIDs

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/value/GenerateUUID.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/value/GenerateUUID.scala
@@ -11,22 +11,34 @@ import GenerateUUID._
   id = "generateUUID",
   categories = Array("Value"),
   label = "Generate UUID",
-  description = "Generates UUIDs from input values."
+  description =
+""" Generates UUIDs.
+If no input value is provided, a random UUID is generated using a cryptographically strong pseudo random number generator.
+If input values are provided, a name-based UUID is generated for each input value.
+"""
 )
 @TransformExamples(Array(
   new TransformExample(
     input1 = Array("input value"),
     output = Array("cee963a2-8f70-3e97-b51a-85ef732e66dd")
+  ),
+  new TransformExample(
+    input1 = Array("üöä!", "êéè"),
+    output = Array("690802dd-a317-335f-807c-e4e1e32b7b5b", "925cbd7f-377b-3fbd-8f4c-ca41529b74ad")
   )
 ))
 case class GenerateUUID() extends Transformer {
 
   override def apply(values: Seq[Seq[String]]): Seq[String] = {
-    values.flatten.map(generateUUID)
+    if(values.isEmpty) {
+      Seq(UUID.randomUUID.toString)
+    } else {
+      values.flatten.map(generateUUIDfromName)
+    }
   }
 
   @inline
-  private def generateUUID(str: String): String = {
+  private def generateUUIDfromName(str: String): String = {
     UUID.nameUUIDFromBytes(str.getBytes(charset)).toString
   }
 
