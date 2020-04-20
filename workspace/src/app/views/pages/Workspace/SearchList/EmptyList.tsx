ac66bff80bf88b1f545f934153450819f4e378cd
Michael Haschke <mhaschke@brox.de>
2020-04-20 14:33:11 +0200

    improve layout of call to action for empty result lists
    Former-commit-id: 7d766e1afbb328602eab6233743552b2b431f844

--- workspace/src/app/views/pages/Workspace/SearchList/EmptyList.tsx
+++ workspace/src/app/views/pages/Workspace/SearchList/EmptyList.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import {
+    Card,
+    OverviewItem,
+    OverviewItemDepiction,
+    OverviewItemDescription,
+    OverviewItemLine,
+    OverviewItemActions,
+    HelperClasses,
+} from '@wrappers/index';
 
-function EmptyList({actions}) {
-    return <div>
-        {actions}
-    </div>
+function EmptyList({
+    depiction, // use large icon here
+    textInfo,
+    textCallout,
+    actionButtons
+}) {
+    return (
+        <Card
+            isOnlyLayout
+            className={HelperClasses.Intent.INFO}
+        >
+            <OverviewItem hasSpacing>
+                {
+                    depiction && <OverviewItemDepiction>{depiction}</OverviewItemDepiction>
+                }
+                <OverviewItemDescription>
+                    {
+                        textInfo && <OverviewItemLine>{textInfo}</OverviewItemLine>
+                    }
+                    {
+                        textCallout && <OverviewItemLine>{textCallout}</OverviewItemLine>
+                    }
+                </OverviewItemDescription>
+                {
+                    actionButtons && <OverviewItemActions>{actionButtons}</OverviewItemActions>
+                }
+            </OverviewItem>
+        </Card>
+    );
 }
 
 export default EmptyList;
