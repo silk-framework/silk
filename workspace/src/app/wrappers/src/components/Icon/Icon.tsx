3f9d2c5ae3ef237bd7626651a29a4605f14d3c9b
Michael Haschke <mhaschke@brox.de>
2020-04-29 14:36:46 +0200

    enable managed tooltips on IconButtons
    Former-commit-id: 24b549ef6b0dfe26413c18bc29d8d9afae2d9443

--- workspace/src/app/wrappers/src/components/Icon/Icon.tsx
+++ workspace/src/app/wrappers/src/components/Icon/Icon.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-// import PropTypes from 'prop-types';
-import canonicalIconNames from './canonicalIconNames.json';
+import Tooltip from "./../Tooltip/Tooltip";
+import canonicalIconNames from "./canonicalIconNames.json";
 
 /*
     Properties from us:
@@ -29,24 +29,31 @@ for more see https://www.npmjs.com/package/@carbon/icons-react
 // TODO: add list of canonical icon names and their identifier in carbon icons
 // TODO: add properties for intention/state (e.g. success, info, earning, error)
 
-function Icon ({
-    className = '',
-    name = 'undefined',
+function Icon({
+    className = "",
+    name = "undefined",
     large = false,
     small = false,
+    tooltipText,
+    tooltipOpenDelay,
     ...restProps
 }: any) {
     let sizeConfig = { height: 20, width: 20 };
     if (small) sizeConfig = { height: 16, width: 16 };
     if (large) sizeConfig = { height: 32, width: 32 };
-    let iconImportName = 'Undefined'+sizeConfig.width;
-    if (typeof canonicalIconNames[name] !== 'undefined') {
-        iconImportName = canonicalIconNames[name]+sizeConfig.width;
+    let iconImportName = "Undefined" + sizeConfig.width;
+    if (typeof canonicalIconNames[name] !== "undefined") {
+        iconImportName = canonicalIconNames[name] + sizeConfig.width;
     }
-    const CarbonIcon = require('@carbon/icons-react')[iconImportName];
-    return (
-        <CarbonIcon {...restProps} {...sizeConfig} className={'ecc-icon '+className} />
-    )
+    const CarbonIcon = require("@carbon/icons-react")[iconImportName];
+    const icon = <CarbonIcon {...restProps} {...sizeConfig} className={"ecc-icon " + className} />;
+    return tooltipText ? (
+        <Tooltip content={tooltipText} hoverOpenDelay={tooltipOpenDelay}>
+            {icon}
+        </Tooltip>
+    ) : (
+        icon
+    );
 }
 
 export default Icon;
--- workspace/src/app/wrappers/src/components/Icon/IconButton.tsx
+++ workspace/src/app/wrappers/src/components/Icon/IconButton.tsx
@@ -1,27 +1,25 @@
-import React from 'react';
-import Button from '../Button/Button';
-import Icon from './Icon';
+import React from "react";
+import Button from "../Button/Button";
+import Icon from "./Icon";
 
-function IconButton({
-    className='',
-    name = 'undefined',
-    text,
-    ...restProps
-}: any) {
-
+function IconButton({ className = "", name = "undefined", text, description, ...restProps }: any) {
     return (
         <Button
             {...restProps}
-            icon={<Icon
-                name={name}
-                small={restProps.small}
-                large={restProps.large}
-                description={text}
-            />}
-            className={'ecc-button--icon ' + className}
+            icon={
+                <Icon
+                    name={name}
+                    small={restProps.small}
+                    large={restProps.large}
+                    tooltipText={text}
+                    tooltipOpenDelay={1000}
+                    description={description ? description : text}
+                />
+            }
+            className={"ecc-button--icon " + className}
             minimal
         />
     );
-};
+}
 
 export default IconButton;
