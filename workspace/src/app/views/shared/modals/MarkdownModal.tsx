a7963f3dc21eeea7b5f7fa039cfe5c0c20c2c9b0
Michael Haschke <mhaschke@brox.de>
2020-04-30 18:00:31 +0200

    put error log in html content block element to add improved typography formatting
    Former-commit-id: 778024355c83b7582142fa9eafbe9e6d9f9164c8

--- workspace/src/app/views/shared/modals/MarkdownModal.tsx
+++ workspace/src/app/views/shared/modals/MarkdownModal.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import { Button, SimpleDialog, } from '@wrappers/index';
+import { Button, SimpleDialog, HtmlContentBlock } from "@wrappers/index";
 
-const MarkdownModal = ({onDiscard, isOpen, markdown, title = 'Report'}) => {
-
+const MarkdownModal = ({ onDiscard, isOpen, markdown, title = "Error report" }) => {
     const handleDownload = () => {
-        const element = document.createElement('a');
-        element.href = window.URL.createObjectURL(new Blob([markdown], {type: 'text/markdown'}));
+        const element = document.createElement("a");
+        element.href = window.URL.createObjectURL(new Blob([markdown], { type: "text/markdown" }));
         element.download = `${title}.md`;
         //the above code is equivalent to
         document.body.appendChild(element);
@@ -21,19 +20,19 @@ const MarkdownModal = ({onDiscard, isOpen, markdown, title = 'Report'}) => {
             isOpen={isOpen}
             onClose={onDiscard}
             actions={[
-                <Button
-                    affirmative
-                    onClick={handleDownload}
-                    key='download'
-                >
+                <Button affirmative onClick={handleDownload} key="download">
                     Download
                 </Button>,
-                <Button key='close' onClick={onDiscard}>Close</Button>
+                <Button key="close" onClick={onDiscard}>
+                    Close
+                </Button>,
             ]}
         >
-            <ReactMarkdown source={markdown}/>
+            <HtmlContentBlock>
+                <ReactMarkdown source={markdown} />
+            </HtmlContentBlock>
         </SimpleDialog>
-    )
+    );
 };
 
 export default MarkdownModal;
