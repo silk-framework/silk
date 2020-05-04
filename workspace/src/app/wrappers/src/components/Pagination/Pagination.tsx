e802d022f9eec16361046d145361af1f0169eea4
Michael Haschke <mhaschke@brox.de>
2020-05-04 17:41:22 +0200

    improve Pagination layout and add option to hide sub parts of it
    Former-commit-id: e941a4b9156ca09c4c182513fd15356391049b3d

--- workspace/src/app/wrappers/src/components/Pagination/Pagination.tsx
+++ workspace/src/app/wrappers/src/components/Pagination/Pagination.tsx
@@ -1,4 +1,27 @@
-import { memo } from 'react';
-import { Pagination } from "carbon-components-react";
+import React from "react";
+import { Pagination as CarbonPagination } from "carbon-components-react";
 
-export default memo(Pagination);
+function Pagination({
+    className,
+    hidePageSizeConfiguration = false,
+    hideInfoText = false,
+    hidePageSelect = false,
+    hideNavigationArrows = false,
+    ...otherProps
+}: any) {
+    return (
+        <CarbonPagination
+            {...otherProps}
+            className={
+                "ecc-pagination" +
+                (className ? " " + className : "") +
+                (hidePageSizeConfiguration ? " ecc-pagination--hidepagesize" : "") +
+                (hideInfoText ? " ecc-pagination--hideinfotext" : "") +
+                (hidePageSelect ? " ecc-pagination--hidepageselect" : "") +
+                (hideNavigationArrows ? " ecc-pagination--hidenavigation" : "")
+            }
+        />
+    );
+}
+
+export default Pagination;
