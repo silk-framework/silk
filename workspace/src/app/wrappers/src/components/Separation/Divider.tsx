35c62755012af577bd526bf38dbcb6bc5040c883
Michael Haschke <mhaschke@brox.de>
2020-05-13 17:41:59 +0200

    improve code regarding linter
    Former-commit-id: 7e41778fe9ae5bc5af899685a6d4a184ce939b6b

--- workspace/src/app/wrappers/src/components/Separation/Divider.tsx
+++ workspace/src/app/wrappers/src/components/Separation/Divider.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Divider({ addSpacing = "none" }: any) {
-    return <hr className={"ecc-separation__divider-horizontal " + "ecc-separation__spacing--" + addSpacing} />;
+    return <hr className={"ecc-separation__divider-horizontal ecc-separation__spacing--" + addSpacing} />;
 }
 
 export default Divider;
