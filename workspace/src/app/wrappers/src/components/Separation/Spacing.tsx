5aafadd75774dddbfab5357c11199502716555da
Michael Haschke <mhaschke@brox.de>
2020-05-13 12:43:09 +0200

    add vertical Spacing element
    Former-commit-id: 84d9b7e87cf33ef8fae214140d59ed15b6ed2516

--- workspace/src/app/wrappers/src/components/Separation/Spacing.tsx
+++ workspace/src/app/wrappers/src/components/Separation/Spacing.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React from "react";
 
-function Spacing({
-    size="medium"
-}: any) {
+function Spacing({ size = "medium", hasDivider = false, vertical = false }: any) {
+    const direction = vertical ? "vertical" : "horizontal";
     return (
         <div
             className={
-                'ecc-separation__spacing-horizontal ' +
-                'ecc-separation__spacing-horizontal--' + size
+                "ecc-separation__spacing-" +
+                direction +
+                " ecc-separation__spacing--" +
+                size +
+                (hasDivider ? " ecc-separation__spacing--hasdivider" : "")
             }
         />
     );
-};
+}
 
 export default Spacing;
