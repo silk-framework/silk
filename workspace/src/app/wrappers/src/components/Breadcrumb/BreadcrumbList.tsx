074cb1dd276315202ef3484a8d6c792a1581f038
Andreas Schultz <aschultz@brox.de>
2020-05-28 11:51:38 +0200

    Fix breadcrumb to use routing
    Former-commit-id: 33893607a49207d694fed808183464e74f250ed5

--- workspace/src/app/wrappers/src/components/Breadcrumb/BreadcrumbList.tsx
+++ workspace/src/app/wrappers/src/components/Breadcrumb/BreadcrumbList.tsx
@@ -1,39 +1,36 @@
 import React from "react";
 // import PropTypes from 'prop-types';
 import { Breadcrumbs as BlueprintBreadcrumbList } from "@blueprintjs/core";
-import BreadcrumbItem from './BreadcrumbItem';
+import BreadcrumbItem from "./BreadcrumbItem";
+import { routerOp } from "@ducks/router";
+import { useDispatch } from "react-redux";
 
-function BreadcrumbList ({
-    className='',
-    itemDivider='/',
-    ...otherProps
-}: any) {
+function BreadcrumbList({ className = "", itemDivider = "/", ...otherProps }: any) {
+    const dispatch = useDispatch();
+
+    const gotoPage = (page) => (e) => {
+        e.preventDefault();
+        if (page) {
+            dispatch(routerOp.goToPage(page, {}));
+        }
+    };
 
     const renderBreadcrumb = (propsBreadcrumb) => {
-        return <BreadcrumbItem
-            {...propsBreadcrumb}
-            itemDivider="/"
-        />;
+        return <BreadcrumbItem {...propsBreadcrumb} itemDivider="/" onClick={gotoPage(propsBreadcrumb.href)} />;
     };
 
     const renderCurrentBreadcrumb = (propsBreadcrumb) => {
-        return <BreadcrumbItem
-            {...propsBreadcrumb}
-            current={true}
-            href={null}
-            onClick={null}
-            itemDivider={itemDivider}
-        />;
+        return <BreadcrumbItem {...propsBreadcrumb} current={true} href={null} itemDivider={itemDivider} />;
     };
 
     return (
         <BlueprintBreadcrumbList
             {...otherProps}
-            className={'ecc-breadcrumb__list '+className}
+            className={"ecc-breadcrumb__list " + className}
             breadcrumbRenderer={renderBreadcrumb}
             currentBreadcrumbRenderer={renderCurrentBreadcrumb}
         />
     );
-};
+}
 
 export default BreadcrumbList;
