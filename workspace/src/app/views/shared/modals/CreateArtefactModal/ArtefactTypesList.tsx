65a6fdd8e74890d6fce2b04f55ce07aa8d1eca86
Andreas Schultz <aschultz@brox.de>
2020-06-09 14:37:15 +0200

    Don't render type list if it's not available
    Former-commit-id: 19ea35cae1bf60471d263d63dbfd4540d88e9c42

--- workspace/src/app/views/shared/modals/CreateArtefactModal/ArtefactTypesList.tsx
+++ workspace/src/app/views/shared/modals/CreateArtefactModal/ArtefactTypesList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { commonSel } from "@ducks/common";
 import { Menu, MenuItem, TitleSubsection } from "@wrappers/index";
@@ -12,14 +12,15 @@ function ArtefactTypesList({ onSelect }) {
             <TitleSubsection>Item type</TitleSubsection>
             <Menu>
                 <MenuItem text={"All"} key="all" onClick={() => onSelect("all")} active={selectedDType === "all"} />
-                {typeModifier.options.map((type) => (
-                    <MenuItem
-                        text={type.label}
-                        key={type.id}
-                        onClick={() => onSelect(type.id)}
-                        active={selectedDType === type.id}
-                    />
-                ))}
+                {typeModifier &&
+                    typeModifier.options.map((type) => (
+                        <MenuItem
+                            text={type.label}
+                            key={type.id}
+                            onClick={() => onSelect(type.id)}
+                            active={selectedDType === type.id}
+                        />
+                    ))}
             </Menu>
         </>
     );
