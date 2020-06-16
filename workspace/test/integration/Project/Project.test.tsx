f3b5b069165d94f2ab14b8b76bc7cee4e5d21542
Andreas Schultz <aschultz@brox.de>
2020-06-16 13:17:24 +0200

    Update jest-mock-axios version to 4.2.0, more test refactorings
    Former-commit-id: 6fbe0ebb1c61a8f1120bcb787a0e64d0c3d10bbb

---
+++ workspace/test/integration/Project/Project.test.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "@testing-library/jest-dom";
+import { createBrowserHistory } from "history";
+import mockAxios from "../../__mocks__/axios";
+import { logRequests, testWrapper } from "../TestHelper";
+
+describe("Project page", () => {
+    let hostPath = process.env.HOST;
+    afterEach(() => {
+        mockAxios.reset();
+    });
+
+    const loadProjectPage = () => {
+        const history = createBrowserHistory();
+        history.location.pathname = "/dataintegration/workspace-beta/projects/cmem";
+
+        testWrapper({}, history);
+    };
+
+    it("should get common data types or for specific project", async () => {
+        loadProjectPage();
+        logRequests(mockAxios);
+        const reqInfo = mockAxios.getReqMatching({
+            url: hostPath + "/api/workspace/searchConfig/types?projectId=cmem",
+        });
+        expect(reqInfo).toBeTruthy();
+    });
+
+    it("should request meta data", async () => {
+        loadProjectPage();
+        const reqInfo = mockAxios.getReqMatching({
+            url: hostPath + "/api/workspace/projects/cmem/metaData",
+        });
+        expect(reqInfo).toBeTruthy();
+    });
+
+    xit("should send the right projectId to backend", async () => {
+        loadProjectPage();
+        const reqInfo = mockAxios.getReqMatching({
+            url: hostPath + "/api/workspace/searchItems",
+        });
+        expect(reqInfo.data).toEqual({
+            limit: 10,
+            offset: 0,
+            project: "cmem",
+            textQuery: "",
+            facets: [],
+        });
+    });
+});
--- workspace/test/integration/TestHelper.tsx
+++ workspace/test/integration/TestHelper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { createBrowserHistory, History } from "history";
+import { History } from "history";
 import { mount } from "enzyme";
 import { Provider } from "react-redux";
 import { AppLayout } from "../../src/app/views/layout/AppLayout/AppLayout";
@@ -7,9 +7,10 @@ import { Workspace } from "../../src/app/views/pages/Workspace/Workspace";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../../src/app/store/reducers";
 import { ConnectedRouter } from "connected-react-router";
+import { AxiosMockType } from "jest-mock-axios/dist/lib/mock-axios-types";
 
 /** Creates the Redux store. */
-export const createStore = (history = createBrowserHistory()) =>
+export const createStore = (history: History<{}>) =>
     configureStore({
         reducer: rootReducer(history),
     });
@@ -27,4 +28,10 @@ export const testWrapper = (props: any, history: History<{}>) => {
             </ConnectedRouter>
         </Provider>
     );
+};
+
+export const logRequests = (mockAxios: AxiosMockType) => {
+    mockAxios.queue().forEach((request) => {
+        console.log(request);
+    });
 };
