cb880032dcdee0b674d5ca7fe1fbbba5bbb2798e
Michael Haschke <mhaschke@brox.de>
2020-09-08 11:57:16 +0200

    add description to properties
    Former-commit-id: c0677f3f573895ca00a7becb8b8a08356866b9ba

--- workspace/src/app/views/shared/ContentBlobToggler/ContentBlobToggler.tsx
+++ workspace/src/app/views/shared/ContentBlobToggler/ContentBlobToggler.tsx
@@ -2,17 +2,43 @@ import React, { useState } from "react";
 import { HtmlContentBlock } from "@gui-elements/index";
 
 interface IContentBlobTogglerProps {
+    /**
+        space-delimited list of class names
+    */
     className?: string;
+    /**
+        when the preview content is a string then it will be cut to this length
+    */
     previewMaxLength?: number;
     // currently not supported, when necessary we need to move some parts to the GUI elements library to include style rules there
     previewForceSingleLine?: boolean;
+    /**
+        text label used for toggler when preview is displayed
+    */
     textToggleExtend?: string;
+    /**
+        text label used for toggler when full view is displayed
+    */
     textToggleReduce?: string;
+    /**
+        content that is displayed as preview
+    */
     contentPreview: JSX.Element | string;
+    /**
+        content that is displayed as extended full view
+    */
     contentFullview: JSX.Element | string;
+    /**
+        render function that could alter full view content, e.g. processing markdown content
+    */
     renderContentFullview?(contenFullview: JSX.Element | string): JSX.Element | string;
+    /**
+        show extended full view initially
+    */
     startExtended?: boolean;
-    // show toggler even if maximum preview content is equal to full content
+    /**
+        show toggler even if maximum preview content is equal to full content
+    */
     showAlwaysToggler?: boolean;
     [otherProps: string]: any;
 }
