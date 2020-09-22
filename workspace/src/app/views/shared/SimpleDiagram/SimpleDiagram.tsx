a28cb3b37eb4b3e96f57a3c869820f52f8e2a860
Michael Haschke <mhaschke@brox.de>
2020-09-22 23:00:02 +0200

    provide simple diagram element

---
+++ workspace/src/app/views/shared/SimpleDiagram/SimpleDiagram.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Sparklines, SparklinesBars } from "react-sparklines";
+
+export function SimpleDiagram({ data, titlePrefix = "Data", ...restProps }: any) {
+    console.log(Sparklines);
+
+    if (data.length < 1) {
+        return <></>;
+    }
+
+    return (
+        <div className="app-di-simplediagram" title={titlePrefix + ": " + data.join(", ")}>
+            <Sparklines data={data} {...restProps}>
+                <SparklinesBars
+                    barWidth={data.length > 1 ? 0 : { ...restProps }.width}
+                    style={{
+                        stroke: "transparent",
+                        strokeWidth: "0",
+                        fill: "inherit",
+                        opacity: "0.61",
+                    }}
+                />
+            </Sparklines>
+        </div>
+    );
+}
