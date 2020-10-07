ff5ff1d92a3d517894c5b2fbd37f8559197c4921
Michael Haschke <mhaschke@brox.de>
2020-10-07 17:51:04 +0200

    add stubs for file widget search tests

--- workspace/test/integration/Project/Project.test.tsx
+++ workspace/test/integration/Project/Project.test.tsx
@@ -1,17 +1,30 @@
 import React from "react";
 import mockAxios from "../../__mocks__/axios";
-import { apiUrl, checkRequestMade, legacyApiUrl, testWrapper, withMount, workspacePath } from "../TestHelper";
+import {
+    apiUrl,
+    checkRequestMade,
+    legacyApiUrl,
+    testWrapper,
+    withMount,
+    workspacePath,
+    findAll,
+    byTestId,
+} from "../TestHelper";
 import { createBrowserHistory } from "history";
 import Project from "../../../src/app/views/pages/Project";
 import qs from "qs";
 
 describe("Project page", () => {
+    const testProjectId = "Hierarchical_Person_Example";
     let hostPath = process.env.HOST;
+    let projectPageWrapper: ReactWrapper<any, any> = null;
     beforeEach(() => {
         const history = createBrowserHistory();
-        history.location.pathname = workspacePath("/projects/cmem");
+        history.location.pathname = workspacePath("/projects/" + testProjectId);
 
-        return withMount(testWrapper(<Project />, history));
+        projectPageWrapper = withMount(testWrapper(<Project />, history));
+
+        return projectPageWrapper;
     });
 
     afterEach(() => {
@@ -19,19 +32,19 @@ describe("Project page", () => {
     });
 
     it("should get common data types or for specific project", async () => {
-        checkRequestMade(apiUrl("/workspace/searchConfig/types?projectId=cmem"));
+        checkRequestMade(apiUrl("/workspace/searchConfig/types?projectId=" + testProjectId));
     });
 
     it("should request meta data", async () => {
-        checkRequestMade(apiUrl("/workspace/projects/cmem/metaData"));
+        checkRequestMade(apiUrl("/workspace/projects/" + testProjectId + "/metaData"));
     });
 
     it("should get available resources for file widget", () => {
-        checkRequestMade(legacyApiUrl("/workspace/projects/cmem/resources"));
+        checkRequestMade(legacyApiUrl("/workspace/projects/" + testProjectId + "/resources"));
     });
 
     it("should get prefixes for configuration widget", () => {
-        checkRequestMade(apiUrl("/workspace/projects/cmem/prefixes"));
+        checkRequestMade(apiUrl("/workspace/projects/" + testProjectId + "/prefixes"));
     });
 
     it("should filter items, by given criteria from URL search params", async () => {
@@ -49,7 +62,7 @@ describe("Project page", () => {
         );
 
         let history = createBrowserHistory();
-        history.location.pathname = workspacePath("/projects/cmem");
+        history.location.pathname = workspacePath("/projects/" + testProjectId);
         history.location.search = filteredQueryParams;
 
         withMount(testWrapper(<Project />, history));
@@ -59,7 +72,7 @@ describe("Project page", () => {
             itemType: "dataset",
             limit: 15,
             offset: 10,
-            project: "cmem",
+            project: testProjectId,
             facets: [
                 { facetId: "facetId1", type: "keyword", keywordIds: ["facet1Key1", "facet1Key2"] },
                 { facetId: "facetId2", type: "keyword", keywordIds: ["facet2Key"] },
@@ -67,5 +80,23 @@ describe("Project page", () => {
         };
 
         checkRequestMade(apiUrl("/workspace/searchItems"), "POST", expectedSearchResponse);
+    });
+
+    // TODO
+    xit("file search bar is not shown when there are no files", () => {
+        const filesearchinput = findAll(projectPageWrapper, byTestId(`file-search-bar`));
+        expect(filesearchinput).toHaveLength(0);
+    });
+
+    // TODO
+    xit("file search bar never disappears when no results are shown", async (done) => {
+        const filesearchinput = screen.queryByTestId(`file-search-bar`);
+        fireEvent.change(filesearchinput, { target: { value: "this-is-not-a-known-file-teststring" } });
+        fireEvent.keyDown(filesearchinput, { key: "Enter", code: "Enter" });
+        await waitFor(() => {
+            const filesearchinputTest = screen.findAllByTestId(`file-search-bar`);
+            expect(filesearchinputTest).toHaveLength(1);
+            done();
+        });
     });
 });
