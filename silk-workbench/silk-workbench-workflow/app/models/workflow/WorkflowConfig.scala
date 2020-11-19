b704cd77753dbbf26a7cc08826b9d0667cb8c788
Robert Isele <mail@robertisele.com>
2020-11-19 14:15:11 +0100

    Workflow editor links: If the new workspace is configured, clicking on the wrench icon opens the details page in a new tab.

--- silk-workbench/silk-workbench-workflow/app/models/workflow/WorkflowConfig.scala
+++ silk-workbench/silk-workbench-workflow/app/models/workflow/WorkflowConfig.scala
@@ -1,6 +1,11 @@
 package models.workflow
 
-import org.silkframework.config.DefaultConfig
+import org.silkframework.config.{DefaultConfig, ProductionConfig}
+import org.silkframework.dataset.DatasetSpec.GenericDatasetSpec
+import org.silkframework.rule.{LinkSpec, TransformSpec}
+import org.silkframework.util.Identifier
+import org.silkframework.workbench.WorkbenchPlugin.TaskActions
+import org.silkframework.workspace.activity.workflow.Workflow
 
 object WorkflowConfig {
 
@@ -13,6 +18,28 @@ object WorkflowConfig {
       case "SparkExecutionManager" => "ExecuteSparkWorkflow"
       case manager: String =>
         throw new RuntimeException("Unknown execution manager: " + manager)
+    }
+  }
+
+  /**
+    * Given a task in the workflow, returns the URI of the corresponding details page.
+    */
+  def editorLink(taskActions: TaskActions, workflowId: Identifier): Option[String] = {
+    if(ProductionConfig.betaWorkspaceSwitchEnabled) {
+      val projectId = taskActions.task.project.name
+      val taskId = taskActions.task.id
+      val taskType = taskActions.task.data match {
+        case _: GenericDatasetSpec => "dataset"
+        case _: TransformSpec => "transform"
+        case _: LinkSpec => "linking"
+        case _: Workflow => "workflow"
+        case _ => "task"
+      }
+      Some(s"${config.baseUrl}/workbench/projects/$projectId/$taskType/$taskId")
+    } else {
+      for(path <- taskActions.openPath(Some(workflowId), Some(taskActions.task.id.toString))) yield {
+        s"${config.baseUrl}/$path"
+      }
     }
   }
 
