6e40e0e0a22e827b1fd6e60c550deb0710b69f6b
Andreas Schultz <aschultz@brox.de>
2020-11-26 11:07:48 +0100

    Add more tests for simple variable workflow execution

--- silk-workbench/silk-workbench-workflow/test/controllers/workflowApi/SimpleVariableWorkflowApiTest.scala
+++ silk-workbench/silk-workbench-workflow/test/controllers/workflowApi/SimpleVariableWorkflowApiTest.scala
@@ -2,14 +2,15 @@ package controllers.workflowApi
 
 import controllers.workflowApi.variableWorkflow.VariableWorkflowRequestUtils
 import helper.IntegrationTestTrait
-import org.scalatest.FlatSpec
+import org.scalatest.{FlatSpec, MustMatchers}
 import org.silkframework.workspace.SingleProjectWorkspaceProviderTestTrait
 import play.api.libs.ws.WSResponse
 import play.api.routing.Router
 
 import scala.concurrent.Future
 
-class SimpleVariableWorkflowApiTest extends FlatSpec with SingleProjectWorkspaceProviderTestTrait with IntegrationTestTrait {
+class SimpleVariableWorkflowApiTest extends FlatSpec
+    with SingleProjectWorkspaceProviderTestTrait with IntegrationTestTrait with MustMatchers {
   behavior of "Simple variable workflow API"
 
   private val validVariableWorkflows = Seq(
@@ -20,6 +21,11 @@ class SimpleVariableWorkflowApiTest extends FlatSpec with SingleProjectWorkspace
     "283e0d90-514f-4f32-9a6f-81340d592b8f_Workflowtoomanysources",
     "293470bf-a1ff-42a3-a16a-3b0c5d8e3468_Workflowtoomanysinks"
   )
+
+  private val sourceProperty1 = "inputProp1"
+  private val sourceProperty2 = "inputProp2"
+
+  private def targetProp(nr: Int) = s"targetProp$nr"
 
   override def routes: Option[Class[_ <: Router]] = Some(classOf[workflowApi.Routes])
 
@@ -49,7 +55,36 @@ class SimpleVariableWorkflowApiTest extends FlatSpec with SingleProjectWorkspace
     // FIXME CMEM-3051: Enable JSON in test as soon as writing to it is supported
     for(mimeType <- VariableWorkflowRequestUtils.acceptedMimeType.filter(mimeType => mimeType != "application/json"
         && mimeType != "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")) {
-      checkResponseExactStatusCode(executeVariableWorkflow(validVariableWorkflows.head, Map(), mimeType = mimeType))
+      for(IndexedSeq(param1Values, param2Values) <- Seq(
+        IndexedSeq(Seq(), Seq()),
+        IndexedSeq(Seq("val 1"), Seq("val 2")))) {
+        val parameterMap = Map(
+          sourceProperty1 -> param1Values,
+          sourceProperty2 -> param2Values
+        ).filter(_._2.nonEmpty)
+        val response = checkResponseExactStatusCode(executeVariableWorkflow(validVariableWorkflows.head, parameterMap, mimeType = mimeType))
+        mimeType match {
+          case "application/xml" =>
+            response.body must startWith("<?xml")
+            response.body must include("<Entity")
+          case "application/n-triples" =>
+            if(param1Values.isEmpty && param2Values.isEmpty) {
+              response.body mustBe ""
+            } else {
+              response.body must include("<urn:instance:variable_workflow_json_input")
+            }
+          case "text/comma-separated-values" =>
+            response.body must include(s"${targetProp(1)},${targetProp(2)}")
+        }
+        for(value <- param1Values) {
+          response.body must include(targetProp(1))
+          response.body must include(value)
+        }
+        for(value <- param2Values) {
+          response.body must include(targetProp(2))
+          response.body must include(value)
+        }
+      }
     }
   }
 
