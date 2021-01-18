c6c9e07a1f5e17415933c3ab4e4be39f609b9892
Michael Haschke <michael.haschke@eccenca.com>
2021-01-18 18:08:36 +0100

    upgrade blueprintjs and carbon elements libraries

--- silk-react-components/src/HierarchicalMapping/containers/SuggestionNew/SuggestionHeader.tsx
+++ silk-react-components/src/HierarchicalMapping/containers/SuggestionNew/SuggestionHeader.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
+// Todo: use our gui elements wrappers instead of native carbon elements
+
 import {
     TableToolbar,
     TableToolbarAction,
     TableToolbarContent,
     TableToolbarMenu,
     TableToolbarSearch
-} from 'carbon-components-react';
+} from 'carbon-components-react/lib/components/DataTable';
 
 export default function SuggestionHeader({ onSearch }) {
     return (
