b38b84377538793f05533a63ac0b24bd35868ed8
Andreas <andreas.schultz@eccenca.com>
2021-03-04 15:29:27 +0100

    Fix mouse click on custom element of auto-complete widget

--- workspace/src/app/views/shared/Autocomplete/autoCompletionParameterUtils.tsx
+++ workspace/src/app/views/shared/Autocomplete/autoCompletionParameterUtils.tsx
@@ -1,22 +1,22 @@
-// Displays the 'search in workspace' option in the list.
-import { OverviewItem, OverviewItemDescription, OverviewItemLine } from "@gui-elements/src/components/OverviewItem";
-import { CLASSPREFIX as eccguiprefix } from "@gui-elements/src/configuration/constants";
-import { Icon } from "@gui-elements/index";
+import { MenuItem } from "@gui-elements/index";
 import React from "react";
 
-/** Returns a function to be used in an AutoComplete widget */
-export const createNewItemRendererFactory = (itemTextRenderer: (query: string) => string, iconName?: string) => (
-    query: string,
-    active: boolean
-) => {
-    return (
-        <OverviewItem className={active ? `${eccguiprefix}-overviewitem__item--active` : ""} key={query} densityHigh>
-            <OverviewItemDescription>
-                <OverviewItemLine>
-                    {iconName && <Icon name={iconName} small={true} />}
-                    <span> {itemTextRenderer(query)}</span>
-                </OverviewItemLine>
-            </OverviewItemDescription>
-        </OverviewItem>
-    );
+/** Returns a function to be used in an AutoComplete widget for rendering custom elements based on the query string.
+ *
+ * @param itemTextRenderer The text that should be displayed for the new custom item suggestion.
+ * @param iconName Optional icon to show left to the text.
+ */
+export const createNewItemRendererFactory = (itemTextRenderer: (query: string) => string, iconName?: string) => {
+    // Return custom render function
+    return (query: string, active: boolean, handleClick: React.MouseEventHandler<HTMLElement>) => {
+        return (
+            <MenuItem
+                icon={iconName}
+                active={active}
+                key={query}
+                onClick={handleClick}
+                text={itemTextRenderer(query)}
+            />
+        );
+    };
 };
