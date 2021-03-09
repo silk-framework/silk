48a82f4db6676fc01c16cc090fcbcfbba7edc97f
Robert Isele <mail@robertisele.com>
2021-03-09 14:53:45 +0100

    If the workspace is reloaded, all activities, including task activities need to be cancelled.

--- silk-workspace/src/main/scala/org/silkframework/workspace/Workspace.scala
+++ silk-workspace/src/main/scala/org/silkframework/workspace/Workspace.scala
@@ -239,14 +239,12 @@ class Workspace(val provider: WorkspaceProvider, val repository: ResourceReposit
   /**
     * Reloads this workspace.
     */
-  def reload()(implicit userContext: UserContext): Unit = {
+  def reload()(implicit userContext: UserContext): Unit = synchronized {
     loadUserProjects()
 
-    // TODO: Should global workspace activities also be reloaded?
     // Stop all activities
-    for{ project <- projects // Should not work directly on the cached projects
-         activity <- project.activities } {
-      activity.control.cancel()
+    for(project <- projects) { // Should not work directly on the cached projects
+      project.cancelActivities()
     }
     for(workspaceActivity <- activities) {
       workspaceActivity.control.cancel()
