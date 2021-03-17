dd7ae636ccf9494a67b59275f892b6560d204908
Michael Haschke <michael.haschke@eccenca.com>
2021-03-17 13:23:58 +0100

    add page header to project pages

--- workspace/src/app/views/pages/Workspace/Artefacts.tsx
+++ workspace/src/app/views/pages/Workspace/Artefacts.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { workspaceOp, workspaceSel } from "@ducks/workspace";
+import { useTranslation } from "react-i18next";
 import {
     Button,
     Divider,
@@ -15,11 +15,11 @@ import {
     WorkspaceMain,
     WorkspaceSide,
 } from "@gui-elements/index";
-import Filterbar from "./Filterbar";
+import { workspaceOp, workspaceSel } from "@ducks/workspace";
 import SearchList from "../../shared/SearchList";
 import SearchBar from "../../shared/SearchBar";
-import { useTranslation } from "react-i18next";
 import { usePageHeader } from "../../shared/PageHeader/PageHeader";
+import Filterbar from "./Filterbar";
 
 const Artefacts = () => {
     const dispatch = useDispatch();
