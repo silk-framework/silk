7020f1194c2ea5101f53887478388b5ce795c597
Robert Isele <mail@robertisele.com>
2021-04-13 15:54:34 +0200

    CleanPopulationTask bugfix and added missing Scaladoc

--- silk-learning/src/main/scala/org/silkframework/learning/cleaning/CleanPopulationTask.scala
+++ silk-learning/src/main/scala/org/silkframework/learning/cleaning/CleanPopulationTask.scala
@@ -67,7 +67,7 @@ class CleanPopulationTask(population: Population, fitnessFunction: (LinkageRule 
       agg1 == agg2 &&
           ops1.forall(op1 => ops2.exists(op2 => compareOperators(op1, op2)))
     }
-    case (ComparisonNode(inputs1, limit1, weight1, metric1), ComparisonNode(inputs2, limit2, weight2, metric2)) => {
+    case (ComparisonNode(inputs1, limit1, weight1, required1, metric1), ComparisonNode(inputs2, limit2, weight2, required2, metric2)) => {
       metric1 == metric2 &&
           compareInputs(inputs1.source, inputs2.source) &&
           compareInputs(inputs1.target, inputs2.target)
--- silk-rules/src/main/scala/org/silkframework/rule/similarity/SimilarityScore.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/similarity/SimilarityScore.scala
@@ -1,5 +1,13 @@
 package org.silkframework.rule.similarity
 
+/**
+  * A similarity score.
+  *
+  * @param score A value between -1 (inclusive) and 1 (inclusive). Higher values imply a better match:
+  *              0 is the minimum score to consider two values a match.
+  *              1 denotes a perfect match.
+  *              May be [[None]], if no similarity score could be computed (e.g., because of missing values)
+  */
 case class SimilarityScore(score: Option[Double])
 
 object SimilarityScore {
@@ -14,8 +22,14 @@ object SimilarityScore {
 
 }
 
+/**
+  * A [[SimilarityScore]] that is annotated with a weight.
+  */
 case class WeightedSimilarityScore(score: Option[Double], weight: Int = 1) {
 
+  /**
+    * Converts this weighted score to a plain similarity score.
+    */
   def unweighted: SimilarityScore = {
     SimilarityScore(score)
   }
