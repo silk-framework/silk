7fa0e50acdc39b5126c8341fa3649644744a4344
Andreas Schultz <a.schultz@fu-berlin.de>
2021-06-01 11:46:24 +0200

    Add coalesce transform operator

---
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/selection/CoalesceTransformer.scala
@@ -0,0 +1,51 @@
+package org.silkframework.rule.plugins.transformer.selection
+
+import org.silkframework.rule.input.Transformer
+import org.silkframework.runtime.plugin.PluginCategories
+import org.silkframework.runtime.plugin.annotations.{Plugin, TransformExample, TransformExamples}
+
+@TransformExamples(Array(
+  new TransformExample(
+    input1 = Array(),
+    input2 = Array(""),
+    input3 = Array(),
+    output = Array("")
+  ),
+  new TransformExample(
+    input1 = Array(),
+    input2 = Array(),
+    output = Array()
+  ),
+  new TransformExample(
+    output = Array()
+  ),
+  new TransformExample(
+    input1 = Array(),
+    input2 = Array("first"),
+    input3 = Array("second"),
+    output = Array("first")
+  ),
+  new TransformExample(
+    input1 = Array(),
+    input2 = Array("first A", "first B"),
+    input3 = Array("second"),
+    output = Array("first A", "first B")
+  ),
+  new TransformExample(
+    input1 = Array("first"),
+    input2 = Array("second"),
+    output = Array("first")
+  ),
+))
+@Plugin(
+  id = "coalesce",
+  label = "Coalesce (first non-empty input)",
+  categories = Array("Selection", PluginCategories.recommended),
+  description = "Forwards the first non-empty input, i.e. for which any value(s) exist. A single empty string is considered a value."
+)
+case class CoalesceTransformer() extends Transformer {
+
+  override def apply(values: Seq[Seq[String]]): Seq[String] = {
+    values.find(_.nonEmpty).getOrElse(Seq.empty)
+  }
+}
---
+++ silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/selection/CoalesceTransformerTest.scala
@@ -0,0 +1,6 @@
+package org.silkframework.rule.plugins.transformer.selection
+
+import org.silkframework.rule.test.TransformerTest
+
+
+class CoalesceTransformerTest extends TransformerTest[CoalesceTransformer]
