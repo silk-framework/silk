a5626e18410754c1946b901d6a3b66a03c7b9a83
Robert Isele <mail@robertisele.com>
2021-06-03 18:44:52 +0200

    Extended workflow report UI to display nested workflow reports

--- silk-react-components/src/ExecutionReport/WorkflowExecutionReport.jsx
+++ silk-react-components/src/ExecutionReport/WorkflowExecutionReport.jsx
@@ -13,53 +13,15 @@ export default class WorkflowExecutionReport extends React.Component {
     super(props);
     this.displayName = 'WorkflowExecutionReport';
     this.state = {
-      executionMetaData: null,
-      executionReport: {
-        summary: [],
-        warnings: [],
-        task: {
-          id: "workflow"
-        },
-        taskReports: []
-      },
       selectedIndex: -1 // the index of the selected task report or -1 for the workflow itself
     };
   }
 
-  componentDidMount() {
-    this.loadExecutionReport();
-  }
-
-  componentDidUpdate(prevProps) {
-    if (this.props.project !== prevProps.project ||
-        this.props.task !== prevProps.task ||
-        this.props.time !== prevProps.time) {
-      this.loadExecutionReport();
-    }
-  }
-
-  loadExecutionReport() {
-    this.props.diStore.retrieveExecutionReport(
-        this.props.baseUrl,
-        this.props.project,
-        this.props.task,
-        this.props.time)
-        .then((report) => {
-          this.setState({
-            executionReport: report.value,
-            executionMetaData: report.metaData
-          });
-        })
-        .catch((error) => {
-          console.log("Loading execution report failed! " + error); // FIXME: Handle error and give user feedback. Currently this is done via the activity status widget
-        });
-  }
-
   render() {
       let executionWarnings = [];
-      if(this.state.executionMetaData != null && this.state.executionMetaData.finishStatus.cancelled) {
+      if(this.props.executionMetaData != null && this.props.executionMetaData.finishStatus.cancelled) {
         executionWarnings = [ "Executed cancelled" ]
-      } else if(this.state.executionMetaData != null && this.state.executionMetaData.finishStatus.failed) {
+      } else if(this.props.executionMetaData != null && this.props.executionMetaData.finishStatus.failed) {
         executionWarnings = [ "Executed failed" ]
       }
 
@@ -71,7 +33,7 @@ export default class WorkflowExecutionReport extends React.Component {
                     </CardTitle>
                     <CardContent>
                       <ul className="mdl-list">
-                        { this.renderTaskItem(this.state.executionReport, -1, executionWarnings) }
+                        { this.renderTaskItem(this.props.executionReport, -1, executionWarnings) }
                       </ul>
                     </CardContent>
                   </Card>
@@ -81,19 +43,24 @@ export default class WorkflowExecutionReport extends React.Component {
                     </CardTitle>
                     <CardContent className="silk-report-sidebar-tasks-content">
                       <ul className="mdl-list">
-                        { this.state.executionReport.taskReports.map((report, index) => this.renderTaskItem(report, index, report.warnings)) }
+                        { this.props.executionReport.taskReports.map((report, index) => this.renderTaskItem(report, index, report.warnings)) }
                       </ul>
                     </CardContent>
                   </Card>
                 </div>
                 <div className="mdl-cell mdl-cell--10-col">
-                  { this.renderReport(this.state.executionReport.nodeId) }
+                  { this.renderReport(this.props.executionReport.nodeId) }
                 </div>
               </div>
   }
 
   renderTaskItem(report, index, warnings) {
-    return <li key={"report-" + index} className="mdl-list__item mdl-list__item--two-line silk-report-list-item" onClick={() => this.setState({selectedIndex: index})} >
+    let classNames = "mdl-list__item mdl-list__item--two-line silk-report-list-item"
+    if(index === this.state.selectedIndex) {
+      classNames += " silk-report-list-item-icon-selected"
+    }
+
+    return <li key={"report-" + index} className={classNames} onClick={() => this.setState({selectedIndex: index})} >
              <span className="mdl-list__item-primary-content">
                { report.label } { (report.operation != null) ? '(' + report.operation + ')' : ''}
                { this.renderTaskDescription(warnings) }
@@ -122,18 +89,26 @@ export default class WorkflowExecutionReport extends React.Component {
 
   renderReport(nodeId) {
     if(this.state.selectedIndex >= 0) {
-      // Render the report of the selected task
-      return <ExecutionReport baseUrl={this.props.baseUrl}
-                              project={this.props.project}
-                              nodeId={nodeId}
-                              executionReport={this.state.executionReport.taskReports[this.state.selectedIndex]}/>
+      const taskReport = this.props.executionReport.taskReports[this.state.selectedIndex];
+      if ('taskReports' in taskReport) {
+        // This is a nested workflow execution report
+        return <WorkflowExecutionReport baseUrl={this.props.baseUrl}
+                                        project={this.props.project}
+                                        executionReport={taskReport} />
+      } else {
+        // Render the report of the selected task
+        return <ExecutionReport baseUrl={this.props.baseUrl}
+                                project={this.props.project}
+                                nodeId={nodeId}
+                                executionReport={taskReport}/>
+    }
     } else {
       // Render the report of the workflow itself
       return <ExecutionReport baseUrl={this.props.baseUrl}
                               project={this.props.project}
-                              nodeId={this.state.executionReport.task.id}
-                              executionReport={this.state.executionReport}
-                              executionMetaData={this.state.executionMetaData}/>
+                              nodeId={this.props.executionReport.task.id}
+                              executionReport={this.props.executionReport}
+                              executionMetaData={this.props.executionMetaData}/>
     }
   }
 }
@@ -141,8 +116,8 @@ export default class WorkflowExecutionReport extends React.Component {
 WorkflowExecutionReport.propTypes = {
   baseUrl: PropTypes.string.isRequired, // Base URL of the DI service
   project: PropTypes.string.isRequired, // project ID
-  task: PropTypes.string.isRequired, // task ID
-  time: PropTypes.string.isRequired, // timestamp of the current report
+  executionMetaData: PropTypes.object,
+  executionReport: PropTypes.object.isRequired,
   diStore: PropTypes.shape({
     retrieveExecutionReport: PropTypes.func,
   }) // DI store object that provides the business layer API to DI related services
