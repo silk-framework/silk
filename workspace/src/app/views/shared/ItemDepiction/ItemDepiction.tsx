c056be9e2cfbf14a9c6f3563b62863c77e881fe1
Andreas <andreas.schultz@eccenca.com>
2021-07-01 19:32:48 +0200

    Fix icons of workflow nodes

--- workspace/src/app/views/shared/ItemDepiction/ItemDepiction.tsx
+++ workspace/src/app/views/shared/ItemDepiction/ItemDepiction.tsx
@@ -14,21 +14,16 @@ interface IProps {
 
 /** Creates possible icon names ordered by priority. This can be used directly with the Icon component. */
 export const createIconNameStack = (itemType: string, pluginId: string): string[] => {
-    return [].concat([(itemType ? itemType + "-" : "") + pluginId]).concat(itemType ? [itemType] : []);
+    const generatedIconNames = []
+        .concat([(itemType ? itemType + "-" : "") + pluginId])
+        .concat(itemType ? [itemType] : []);
+    const prefixedGeneratedIconNames = generatedIconNames.map((type) => {
+        return "artefact-" + type.toLowerCase();
+    });
+    return prefixedGeneratedIconNames.filter((x, i, a) => a.indexOf(x) === i);
 };
 
 /** Item icon derived from the item type and optionally the plugin ID. */
 export const ItemDepiction = ({ itemType, pluginId, size = { large: true } }: IProps) => {
-    const iconNameStack = createIconNameStack(itemType, pluginId);
-
-    return (
-        <Icon
-            name={iconNameStack
-                .map((type) => {
-                    return "artefact-" + type.toLowerCase();
-                })
-                .filter((x, i, a) => a.indexOf(x) === i)}
-            {...size}
-        />
-    );
+    return <Icon name={createIconNameStack(itemType, pluginId)} {...size} />;
 };
