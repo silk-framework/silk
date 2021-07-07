60789e107d6e4a41592f654a56a64ce856cc5fe6
darausi <daniel.arausi@eccenca.com>
2021-07-07 08:36:34 +0100

    refactored errorId generation to come from hook instead

--- workspace/src/app/hooks/useErrorHandler.tsx
+++ workspace/src/app/hooks/useErrorHandler.tsx
@@ -1,11 +1,12 @@
 import React from "react";
+import shortId from "shortid";
 import { DIErrorFormat, ERROR_HANDLED_SECTIONS } from "@ducks/error/typings";
 import { Notification } from "@gui-elements/index";
 import { useDispatch, useSelector } from "react-redux";
 import errorSelector from "@ducks/error/selectors";
 import { registerNewError } from "@ducks/error/errorSlice";
 
-type ErrorHandlerRegisterFuncType = (errMsg: string, errorId: string, cause: Error) => JSX.Element;
+type ErrorHandlerRegisterFuncType = (errMsg: string, cause: Error) => JSX.Element;
 
 interface ErrorHandlerDict {
     register: ErrorHandlerRegisterFuncType;
@@ -16,12 +17,13 @@ const useErrorHandler = (groupId: ERROR_HANDLED_SECTIONS): ErrorHandlerDict => {
     const error = useSelector(errorSelector);
     const dispatch = useDispatch();
 
-    const register: ErrorHandlerRegisterFuncType = (errMsg, errorId, cause) => {
+    const register: ErrorHandlerRegisterFuncType = (errMsg, cause) => {
         const error: DIErrorFormat = {
             message: errMsg,
             timestamp: Date.now(),
             cause,
         };
+        const errorId: string = shortId.generate();
         //push error to state
         dispatch(registerNewError({ groupId, errorId, error }));
         return <Notification message={errMsg} warning />;
@@ -30,8 +32,6 @@ const useErrorHandler = (groupId: ERROR_HANDLED_SECTIONS): ErrorHandlerDict => {
     const getAllErrors = () => {
         return Object.values(error[groupId]);
     };
-
-    console.log("ERROR!!!!!!!!", { error });
 
     return {
         register,
