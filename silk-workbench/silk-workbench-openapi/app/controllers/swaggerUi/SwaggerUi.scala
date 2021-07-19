02e6d8413a6383469aa08422aae02e601fa3b26b
Robert Isele <mail@robertisele.com>
2021-07-19 18:53:19 +0200

    Swagger UI: Added default parameters to reference.conf and documented how "try it out" can be disabled.

--- silk-workbench/silk-workbench-openapi/app/controllers/swaggerUi/SwaggerUi.scala
+++ silk-workbench/silk-workbench-openapi/app/controllers/swaggerUi/SwaggerUi.scala
@@ -1,22 +1,27 @@
 package controllers.swaggerUi
 
+import com.typesafe.config.{Config, ConfigRenderOptions}
 import config.WorkbenchConfig
 import controllers.openapi.routes.OpenApi
 import controllers.swaggerUi.routes.SwaggerUi
+import org.silkframework.config.{ConfigValue, DefaultConfig}
 import org.silkframework.openapi.OpenApiValidator
+import org.silkframework.runtime.plugin.PluginRegistry.Config.PLUGINS_CONFIG_PATH
 import org.silkframework.runtime.validation.BadUserInputException
 import play.api.libs.json.Json
 import play.api.mvc.{Action, _}
 
 import javax.inject.Inject
 import scala.concurrent.ExecutionContext
+import scala.jdk.CollectionConverters.collectionAsScalaIterableConverter
 
 class SwaggerUi @Inject()(cc: ControllerComponents)(implicit executionContext: ExecutionContext) extends AbstractController(cc) {
 
   def ui: Action[AnyContent] = Action { implicit request =>
     Ok(views.html.openapi.swaggerUi(
       url = OpenApi.openApiJson.absoluteURL(WorkbenchConfig().useHttps),
-      validatorUrl = SwaggerUi.validator(None).absoluteURL(WorkbenchConfig().useHttps)
+      validatorUrl = SwaggerUi.validator(None).absoluteURL(WorkbenchConfig().useHttps),
+      config = swaggerUiConfigJson()
     ))
   }
 
@@ -41,5 +46,11 @@ class SwaggerUi @Inject()(cc: ControllerComponents)(implicit executionContext: E
       case None =>
         throw new BadUserInputException("Parameter 'url' is missing.")
     }
+  }
+
+  private val swaggerUiConfigJson: ConfigValue[String] = (config: Config) => {
+    val allPluginsConf = config.getObject("swagger.ui")
+    val json = allPluginsConf.render(ConfigRenderOptions.concise())
+    json
   }
 }
