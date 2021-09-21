a5fe77526dded2fa9f7733a83c9512190c057605
Andreas Schultz <a.schultz@fu-berlin.de>
2021-09-21 15:10:48 +0200

    Update object rule example view when URI pattern is valid and looses focus

--- silk-react-components/src/HierarchicalMapping/containers/MappingRule/ExampleView.tsx
+++ silk-react-components/src/HierarchicalMapping/containers/MappingRule/ExampleView.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 
 import { Chip, Spinner } from '@eccenca/gui-elements';
@@ -9,135 +9,129 @@ import { childExampleAsync, ruleExampleAsync } from '../../store';
 import { InfoBox } from '../../components/InfoBox';
 import { isDebugMode } from '../../utils/isDebugMode';
 
+interface IProps {
+    id: string
+    rawRule?: object
+    ruleType: string
+}
 /** Shows example input and output values for a mapping rule. */
-export class ExampleView extends React.Component {
-    static propTypes = {
-        id: PropTypes.string,
-        rawRule: PropTypes.object,
-        ruleType: PropTypes.string,
-    };
+export const ExampleView = ({id, rawRule, ruleType}: IProps) => {
+    const [example, setExample] = useState<any>(undefined)
+    const [error, setError] = useState<any>(undefined)
 
-    state = {
-        example: undefined,
-        errorExpanded: false,
-    };
-
-    componentDidMount() {
-        const ruleExampleFunc = this.props.rawRule ? childExampleAsync : ruleExampleAsync;
+    useEffect(() => {
+        const ruleExampleFunc = rawRule ? childExampleAsync : ruleExampleAsync;
         ruleExampleFunc({
-            id: this.props.id,
-            rawRule: this.props.rawRule,
-            ruleType: this.props.ruleType,
+            id: id,
+            rawRule: rawRule,
+            ruleType: ruleType,
         }).subscribe(
             ({ example }) => {
-                this.setState({ example });
+                setExample(example);
             },
             error => {
                 isDebugMode('err MappingRuleOverview: rule.example');
-                this.setState({ error });
+                setError(error);
             }
         );
+    }, [])
+
+    if (error) {
+        return <ErrorView {...error} titlePrefix={"There has been an error loading the examples: "}/>;
     }
 
-    // template rendering
-    render() {
-        if (this.state.error) {
-            return <ErrorView {...this.state.error} titlePrefix={"There has been an error loading the examples: "} />;
-        }
+    if (_.isUndefined(example)) {
+        return <div/>;
+    }
 
-        if (_.isUndefined(this.state.example)) {
-            return <div />;
-        }
+    const pathsCount = _.size(example.sourcePaths);
+    const resultsCount = _.size(example.results);
 
-        const pathsCount = _.size(this.state.example.sourcePaths);
-        const resultsCount = _.size(this.state.example.results);
+    if (pathsCount === 0 && resultsCount === 0) {
+        return null;
+    }
 
-        if (pathsCount === 0 && resultsCount === 0) {
-            return false;
-        }
+    const sourcePaths =
+        pathsCount === 0 ? [''] : example.sourcePaths;
 
-        const sourcePaths =
-            pathsCount === 0 ? [''] : this.state.example.sourcePaths;
-
-        return (
-            <InfoBox>
-                <table className="mdl-data-table ecc-silk-mapping__rulesviewer__examples-table">
-                    <thead>
-                        <tr>
-                            <th className="ecc-silk-mapping__rulesviewer__examples-table__path">
-                            Value path
-                            </th>
-                            <th className="ecc-silk-mapping__rulesviewer__examples-table__value">
-                            Value
-                            </th>
-                            <th className="ecc-silk-mapping__rulesviewer__examples-table__result">
-                            Transformed value
-                            </th>
-                        </tr>
-                    </thead>
-                    {_.map(this.state.example.results, (result, index) => (
-                        <tbody key={`tbody_${index}`}>
-                            {_.map(sourcePaths, (sourcePath, i) => (
-                                <tr
-                                    key={`${index}_${sourcePath}_${i}`}
-                                    id={`${index}_${sourcePath}_${i}`}
-                                >
-                                    <td
-                                        key="path"
-                                        className="ecc-silk-mapping__rulesviewer__examples-table__path"
-                                    >
-                                        {sourcePath ? (
-                                            <Chip>&lrm;{sourcePath}</Chip>
-                                        ) : (
-                                            false
-                                        )}
-                                    </td>
-                                    <td
-                                        key="value"
-                                        className="ecc-silk-mapping__rulesviewer__examples-table__value"
-                                    >
-                                        {_.map(
-                                            result.sourceValues[i],
-                                            (value, valueIndex) => (
-                                                <Chip
-                                                    key={`${index}_${sourcePath}_${i}_${valueIndex}`}
-                                                >
-                                                    {value}
-                                                </Chip>
-                                            )
-                                        )}
-                                    </td>
-                                    {i > 0 ? (
-                                        false
-                                    ) : (
-                                        <td
-                                            key="result"
-                                            className="ecc-silk-mapping__rulesviewer__examples-table__result"
-                                            rowSpan={pathsCount}
+    return (
+        <InfoBox>
+            <table className="mdl-data-table ecc-silk-mapping__rulesviewer__examples-table">
+                <thead>
+                <tr>
+                    <th className="ecc-silk-mapping__rulesviewer__examples-table__path">
+                        Value path
+                    </th>
+                    <th className="ecc-silk-mapping__rulesviewer__examples-table__value">
+                        Value
+                    </th>
+                    <th className="ecc-silk-mapping__rulesviewer__examples-table__result">
+                        Transformed value
+                    </th>
+                </tr>
+                </thead>
+                {_.map(example.results, (result, index) => (
+                    <tbody key={`tbody_${index}`}>
+                    {sourcePaths.map((sourcePath, i) => (
+                        <tr
+                            key={`${index}_${sourcePath}_${i}`}
+                            id={`${index}_${sourcePath}_${i}`}
+                        >
+                            <td
+                                key="path"
+                                className="ecc-silk-mapping__rulesviewer__examples-table__path"
+                            >
+                                {sourcePath ? (
+                                    <Chip>&lrm;{sourcePath}</Chip>
+                                ) : (
+                                    false
+                                )}
+                            </td>
+                            <td
+                                key="value"
+                                className="ecc-silk-mapping__rulesviewer__examples-table__value"
+                            >
+                                {_.map(
+                                    result.sourceValues[i],
+                                    (value, valueIndex) => (
+                                        <Chip
+                                            key={`${index}_${sourcePath}_${i}_${valueIndex}`}
                                         >
-                                            {_.map(
-                                                this.state.example.results[
-                                                    index
-                                                ].transformedValues,
-                                                (transformedValue, row) => (
-                                                    <Chip
-                                                        key={`value_${index}_${i}_${row}`}
-                                                        id={`value_${index}_${i}_${row}`}
-                                                    >
-                                                        {transformedValue}
-                                                    </Chip>
-                                                )
-                                            )}
-                                        </td>
+                                            {value}
+                                        </Chip>
+                                    )
+                                )}
+                            </td>
+                            {i > 0 ? (
+                                false
+                            ) : (
+                                <td
+                                    key="result"
+                                    className="ecc-silk-mapping__rulesviewer__examples-table__result"
+                                    rowSpan={pathsCount}
+                                >
+                                    {_.map(
+                                        example.results[
+                                            index
+                                            ].transformedValues,
+                                        (transformedValue, row) => (
+                                            <Chip
+                                                key={`value_${index}_${i}_${row}`}
+                                                id={`value_${index}_${i}_${row}`}
+                                            >
+                                                {transformedValue}
+                                            </Chip>
+                                        )
                                     )}
-                                </tr>
-                            ))}
-                        </tbody>
+                                </td>
+                            )}
+                        </tr>
                     ))}
-                </table>
-            </InfoBox>
-        );
-    }
+                    </tbody>
+                ))}
+            </table>
+        </InfoBox>
+    );
 }
 
 export default ExampleView;
