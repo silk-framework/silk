917c75070449e6b3ac32bcbd24eecf31e2620389
Robert Isele <mail@robertisele.com>
2021-11-16 16:01:50 +0100

    Test Resource listRecursive

--- silk-workspace/src/test/scala/org/silkframework/workspace/resources/ResourceRepositoryTest.scala
+++ silk-workspace/src/test/scala/org/silkframework/workspace/resources/ResourceRepositoryTest.scala
@@ -30,18 +30,29 @@ abstract class ResourceRepositoryTest extends FlatSpec with MustMatchers  {
   }
 
   it should "allow reading and writing nested resources" in {
+    // Write some resources
     val testResource = project.child("child1").child("child2").get("resource")
     val testContent = "my resource"
     testResource.writeString(testContent)
 
+    val testResource2 = project.get("rootResource")
+    testResource2.writeString("rootResource")
+
+    val testResource3 = project.child("child1").get("resource2")
+    testResource3.writeString("resource2")
+
+    // Test list children methods
     project.listChildren mustBe Seq("child1")
     project.child("child1").listChildren mustBe Seq("child2")
+    project.listRecursive must contain theSameElementsAs Seq("rootResource", "child1/resource2", "child1/child2/resource")
 
     // Retrieve the resource manager again and do some navigation
     val testResourceRead = project.child("child1").parent.get.child("child1").child("child2").get("resource")
     testResourceRead.loadAsString mustBe testContent
     testResource.delete()
     testResource.exists mustBe false
+    testResource2.delete()
+    testResource3.delete()
   }
 
   it should "not allow to open resources that do not exist" in {
