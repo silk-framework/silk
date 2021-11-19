fae1887142b3488e8aac4c12446fd478d0bdbfbd
Andreas Schultz <andreas.schultz@eccenca.com>
2021-11-19 14:39:33 +0100

    Set local storage flag, so DataManager knows that the language has been changed in DI

--- workspace/src/language.ts
+++ workspace/src/language.ts
@@ -3,8 +3,21 @@ import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import en from "./locales/en.json";
 import de from "./locales/de.json";
-import { isTestEnv, isDevelopment } from "./app/constants/path";
+import { isDevelopment, isTestEnv } from "./app/constants/path";
 import Store from "store";
+
+// Fetches the configured language from local storage
+export const fetchStoredLang: () => string = () => {
+    return Store.get("i18nextLng", "en");
+};
+
+// Sets the language in local storage
+export const setStoredLang: (lang: string, userChoice?: boolean) => void = (lang, userChoice = false) => {
+    Store.set("i18nextLng", lang);
+    if (userChoice) {
+        Store.set("i18nUserChoice", true);
+    }
+};
 
 i18n.use(initReactI18next)
     .use(LanguageDetector)
@@ -18,7 +31,7 @@ i18n.use(initReactI18next)
         },
         debug: isDevelopment && !isTestEnv,
         interpolation: { escapeValue: false },
-        lng: Store.get("locale"),
+        lng: fetchStoredLang(),
     });
 
 export default i18n;
