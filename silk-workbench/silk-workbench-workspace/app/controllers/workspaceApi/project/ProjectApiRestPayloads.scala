8b3f0888dea589f86523a339361a2dd7e3d9ef95
Robert Isele <mail@robertisele.com>
2022-01-25 12:58:55 +0100

    Fixed OpenAPI spec

--- silk-workbench/silk-workbench-workspace/app/controllers/workspaceApi/project/ProjectApiRestPayloads.scala
+++ silk-workbench/silk-workbench-workspace/app/controllers/workspaceApi/project/ProjectApiRestPayloads.scala
@@ -1,5 +1,6 @@
 package controllers.workspaceApi.project
 
+import io.swagger.v3.oas.annotations.media.{ArraySchema, Schema}
 import org.silkframework.config.MetaData
 import org.silkframework.util.Uri
 import play.api.libs.json.{Format, Json}
@@ -15,7 +16,9 @@ object ProjectApiRestPayloads {
     * @param label       label of the item
     * @param description optional description of the item
     */
-  case class ItemMetaData(label: String, description: Option[String] = None, tags: Option[Set[String]] = None) {
+  case class ItemMetaData(label: String, description: Option[String] = None,
+                          @ArraySchema(schema = new Schema(implementation = classOf[String], required = false, nullable = true))
+                          tags: Option[Set[String]] = None) {
     def asMetaData: MetaData = MetaData(Some(label), description, tags = tags.getOrElse(Set.empty).map(new Uri(_)))
   }
 
