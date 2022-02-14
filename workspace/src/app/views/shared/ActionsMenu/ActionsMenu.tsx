273a7e6142c7f1a921622fcfe305b0f2b5affdba
Andreas Schultz <a.schultz@fu-berlin.de>
2022-02-14 12:26:01 +0100

    Import Carbon icons explicitly and type icon names

--- workspace/src/app/views/shared/ActionsMenu/ActionsMenu.tsx
+++ workspace/src/app/views/shared/ActionsMenu/ActionsMenu.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Button, ContextMenu, IconButton, MenuItem, OverflowText } from "gui-elements";
+import { ValidIconName } from "gui-elements/src/components/Icon/canonicalIconNames";
 
 interface IActionBasicProps {
     text: string;
-    icon?: string;
+    icon?: ValidIconName;
     affirmative?: boolean;
     disruptive?: boolean;
     disabled?: boolean;
