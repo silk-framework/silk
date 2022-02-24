a8ecc76fae2ba003964956a72dc1695fe53a9632
Andreas Schultz <a.schultz@fu-berlin.de>
2022-02-24 14:30:53 +0100

    Small changes after resolving merge conflicts

--- workspace/src/app/views/shared/RuleEditor/view/ruleNode/RuleNodeParameter.typings.ts
+++ workspace/src/app/views/shared/RuleEditor/view/ruleNode/RuleNodeParameter.typings.ts
@@ -1,10 +1,8 @@
-import { IParameterSpecification, RuleParameterType } from "../../RuleEditor.typings";
+import { IParameterSpecification } from "../../RuleEditor.typings";
 
 export interface IRuleNodeParameter {
     /** The ID of the parameter. */
     parameterId: string;
-    /** The type of the parameter, i.e. determines which UI widget to use. */
-    parameterType: RuleParameterType;
     /** The update function for the value. */
     update: (value: string) => void;
     /** Fetches the current value for this parameter. */
