3f8496d0dcec3a563f39f3ff012740a5e54b5d35
Robert Isele <mail@robertisele.com>
2022-05-31 18:03:16 +0200

    Print full thread dump instead of only the first 8 frames per thread

--- silk-workbench/silk-workbench-core/app/controllers/workspaceApi/coreApi/SystemApi.scala
+++ silk-workbench/silk-workbench-core/app/controllers/workspaceApi/coreApi/SystemApi.scala
@@ -6,8 +6,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import play.api.mvc._
 
-import java.lang.management.ManagementFactory
+import java.lang.management.{LockInfo, ManagementFactory, ThreadInfo}
 import javax.inject.Inject
+import scala.collection.mutable
 
 @Tag(name = "Workbench")
 class SystemApi @Inject()() extends InjectedController {
@@ -25,12 +26,71 @@ class SystemApi @Inject()() extends InjectedController {
       )
     ))
   def threadDump(): Action[AnyContent] = Action { implicit request => {
-    val threadDump = new StringBuilder()
+    val threadDump = new mutable.StringBuilder()
     val threadMXBean = ManagementFactory.getThreadMXBean
     for (threadInfo <- threadMXBean.dumpAllThreads(true, true)) {
-      threadDump.append(threadInfo.toString)
+      threadInfoToString(threadInfo, threadDump)
     }
     Ok(threadDump.toString)
   }}
+
+  /**
+    * Prints a thread info.
+    * Based on ThreadInfo.toString(), but does print all frames instead of the first 8.
+    */
+  private def threadInfoToString(threadInfo: ThreadInfo, sb: mutable.StringBuilder) = {
+    sb.append("\"" + threadInfo.getThreadName + "\"" + (if (threadInfo.isDaemon) " daemon"
+    else "") + " prio=" + threadInfo.getPriority + " Id=" + threadInfo.getThreadId + " " + threadInfo.getThreadState)
+    if (threadInfo.getLockName != null) sb.append(" on " + threadInfo.getLockName)
+    if (threadInfo.getLockOwnerName != null) sb.append(" owned by \"" + threadInfo.getLockOwnerName + "\" Id=" + threadInfo.getLockOwnerId)
+    if (threadInfo.isSuspended) sb.append(" (suspended)")
+    if (threadInfo.isInNative) sb.append(" (in native)")
+    sb.append('\n')
+
+    val stackTrace = threadInfo.getStackTrace
+    var i: Int = 0
+    while (i < stackTrace.length) {
+      val ste: StackTraceElement = stackTrace(i)
+      sb.append("\tat " + ste.toString)
+      sb.append('\n')
+      if (i == 0 && threadInfo.getLockInfo != null) {
+        val ts: Thread.State = threadInfo.getThreadState
+        ts match {
+          case Thread.State.BLOCKED =>
+            sb.append("\t-  blocked on " + threadInfo.getLockInfo)
+            sb.append('\n')
+
+          case Thread.State.WAITING =>
+            sb.append("\t-  waiting on " + threadInfo.getLockInfo)
+            sb.append('\n')
+
+          case Thread.State.TIMED_WAITING =>
+            sb.append("\t-  waiting on " + threadInfo.getLockInfo)
+            sb.append('\n')
+
+          case _ =>
+        }
+      }
+      for (mi <- threadInfo.getLockedMonitors) {
+        if (mi.getLockedStackDepth == i) {
+          sb.append("\t-  locked " + mi)
+          sb.append('\n')
+        }
+      }
+
+      i += 1
+    }
+    val locks: Array[LockInfo] = threadInfo.getLockedSynchronizers
+    if (locks.length > 0) {
+      sb.append("\n\tNumber of locked synchronizers = " + locks.length)
+      sb.append('\n')
+      for (li <- locks) {
+        sb.append("\t- " + li)
+        sb.append('\n')
+      }
+    }
+    sb.append('\n')
+    sb.toString
+  }
 }
 
