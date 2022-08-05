b1de6fda9e9f82c7ae3e04f2809f2555f67d28f3
Robert Isele <mail@robertisele.com>
2022-08-05 13:51:32 +0200

    Added endpoint for retrieving current comparison pairs. Added Swagger doc.

--- silk-workbench/silk-workbench-rules/app/controllers/linking/ActiveLearningApi.scala
+++ silk-workbench/silk-workbench-rules/app/controllers/linking/ActiveLearningApi.scala
@@ -4,7 +4,13 @@ import akka.stream.Materializer
 import controllers.core.UserContextActions
 import controllers.core.util.JsonUtils
 import controllers.linking.activeLearning.ActiveLearningIterator
-import controllers.linking.activeLearning.JsonFormats.ComparisonPairFormat
+import controllers.linking.activeLearning.JsonFormats._
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.{Operation, Parameter}
+import io.swagger.v3.oas.annotations.media.{Content, Schema}
+import io.swagger.v3.oas.annotations.parameters.RequestBody
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.silkframework.learning.active.ActiveLearning
 import org.silkframework.learning.active.comparisons.{ComparisonPair, ComparisonPairGenerator}
 import org.silkframework.rule.LinkSpec
@@ -14,22 +20,145 @@ import org.silkframework.serialization.json.JsonSerializers.LinkageRuleJsonForma
 import org.silkframework.serialization.json.LinkingSerializers.LinkJsonFormat
 import org.silkframework.workbench.Context
 import org.silkframework.workspace.WorkspaceFactory
-import play.api.libs.json.JsValue
+import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, InjectedController}
 
 import javax.inject.Inject
 
+@Tag(name = "Linking - Active learning", description = "Active learning API.")
 class ActiveLearningApi @Inject() (implicit mat: Materializer) extends InjectedController with UserContextActions {
 
-  def addComparisonPair(project: String, task: String): Action[AnyContent] = RequestUserContextAction { implicit request => implicit userContext =>
+  @Operation(
+    summary = "Comparison pairs",
+    description = "Retrieves all comparison pairs.",
+    responses = Array(
+      new ApiResponse(
+        responseCode = "200",
+        description = "Success"
+      ),
+      new ApiResponse(
+        responseCode = "404",
+        description = "If the specified project or task has not been found."
+      )
+    )
+  )
+  @RequestBody(
+    required = true,
+    content = Array(
+      new Content(
+        mediaType = "application/json",
+        schema = new Schema(implementation = classOf[ComparisonPairsFormat]),
+      )
+    )
+  )
+  def comparisonPairs(@Parameter(
+                        name = "project",
+                        description = "The project identifier",
+                        required = true,
+                        in = ParameterIn.PATH,
+                        schema = new Schema(implementation = classOf[String])
+                      )
+                      projectId: String,
+                      @Parameter(
+                        name = "task",
+                        description = "The task identifier",
+                        required = true,
+                        in = ParameterIn.PATH,
+                        schema = new Schema(implementation = classOf[String])
+                      )
+                      taskId: String): Action[AnyContent] = RequestUserContextAction { implicit request => implicit userContext =>
+    val project = WorkspaceFactory().workspace.project(projectId)
+    val task = project.task[LinkSpec](taskId)
+    val activity = task.activity[ComparisonPairGenerator]
+    val json = Json.toJson(ComparisonPairsFormat(activity.value()))
+    Ok(json)
+  }
+
+  @Operation(
+    summary = "Add a comparison pair",
+    description = "Adds a comparison pair to the list of selected pairs.",
+    responses = Array(
+      new ApiResponse(
+        responseCode = "200",
+        description = "Success"
+      ),
+      new ApiResponse(
+        responseCode = "404",
+        description = "If the specified project or task has not been found."
+      )
+    )
+  )
+  @RequestBody(
+    required = true,
+    content = Array(
+      new Content(
+        mediaType = "application/json",
+        schema = new Schema(implementation = classOf[ComparisonPairFormat]),
+      )
+    )
+  )
+  def addComparisonPair(@Parameter(
+                          name = "project",
+                          description = "The project identifier",
+                          required = true,
+                          in = ParameterIn.PATH,
+                          schema = new Schema(implementation = classOf[String])
+                        )
+                        projectId: String,
+                        @Parameter(
+                          name = "task",
+                          description = "The task identifier",
+                          required = true,
+                          in = ParameterIn.PATH,
+                          schema = new Schema(implementation = classOf[String])
+                        )
+                        taskId: String): Action[AnyContent] = RequestUserContextAction { implicit request => implicit userContext =>
     val newComparisonPair = JsonUtils.validateJsonFromRequest[ComparisonPairFormat](request).toComparisonPair
-    updateSelectedComparisonPairs(project, task)(_ :+ newComparisonPair)
+    updateSelectedComparisonPairs(projectId, taskId)(_ :+ newComparisonPair)
     Ok
   }
 
-  def removeComparisonPair(project: String, task: String): Action[AnyContent] = RequestUserContextAction { implicit request => implicit userContext =>
+  @Operation(
+    summary = "Remove a comparison pair",
+    description = "Removes a comparison pair from the list of selected pairs.",
+    responses = Array(
+      new ApiResponse(
+        responseCode = "200",
+        description = "Success"
+      ),
+      new ApiResponse(
+        responseCode = "404",
+        description = "If the specified project or task has not been found."
+      )
+    )
+  )
+  @RequestBody(
+    required = true,
+    content = Array(
+      new Content(
+        mediaType = "application/json",
+        schema = new Schema(implementation = classOf[ComparisonPairFormat]),
+      )
+    )
+  )
+  def removeComparisonPair(@Parameter(
+                             name = "project",
+                             description = "The project identifier",
+                             required = true,
+                             in = ParameterIn.PATH,
+                             schema = new Schema(implementation = classOf[String])
+                           )
+                           projectId: String,
+                           @Parameter(
+                             name = "task",
+                             description = "The task identifier",
+                             required = true,
+                             in = ParameterIn.PATH,
+                             schema = new Schema(implementation = classOf[String])
+                           )
+                           taskId: String): Action[AnyContent] = RequestUserContextAction { implicit request => implicit userContext =>
     val removeComparisonPair = JsonUtils.validateJsonFromRequest[ComparisonPairFormat](request).toComparisonPair
-    updateSelectedComparisonPairs(project, task)(_.filterNot(_ == removeComparisonPair))
+    updateSelectedComparisonPairs(projectId, taskId)(_.filterNot(_ == removeComparisonPair))
     Ok
   }
 
