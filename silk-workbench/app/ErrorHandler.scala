72788b3b1246038c0347b5f86715e10767bb7db8
Robert Isele <mail@robertisele.com>
2022-10-13 14:35:54 +0200

    Fix error handler

--- silk-workbench/app/ErrorHandler.scala
+++ silk-workbench/app/ErrorHandler.scala
@@ -5,9 +5,12 @@ import org.silkframework.workbench.utils.SilkErrorHandler
 import play.api.routing.Router
 import play.api.{Configuration, Environment, OptionalSourceMapper}
 
+import scala.concurrent.ExecutionContext
+
 @Singleton
 class ErrorHandler @Inject() (env: Environment,
                               config: Configuration,
                               sourceMapper: OptionalSourceMapper,
                               router: Provider[Router],
-                              workspaceReact: WorkspaceReact) extends SilkErrorHandler(env, config, sourceMapper, router, workspaceReact)
+                              executionContext: ExecutionContext,
+                              workspaceReact: WorkspaceReact) extends SilkErrorHandler(env, config, sourceMapper, router, executionContext, workspaceReact)
