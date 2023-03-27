fd214a4a26c41ffd8f2344596774255034a17dc4
Andreas Schultz <a.schultz@fu-berlin.de>
2023-03-27 14:27:40 +0200

    Support hiding transform examples (again)

--- workspace/src/app/views/taskViews/transform/evalution/TransformRuleEvaluation.tsx
+++ workspace/src/app/views/taskViews/transform/evalution/TransformRuleEvaluation.tsx
@@ -35,6 +35,7 @@ export const TransformRuleEvaluation: React.FC<TransformRuleEvaluationProps> = (
     const [evaluationRunning, setEvaluationRunning] = React.useState<boolean>(false);
     const [evaluationResult, setEvaluationResult] = React.useState<EvaluatedTransformEntity[]>([]);
     const [evaluationResultMap] = React.useState<Map<string, EvaluationResultType>>(new Map());
+    const [evaluationResultsShown, setEvaluationResultsShown] = React.useState<boolean>(false);
     const [nodeUpdateCallbacks] = React.useState(
         new Map<string, (evaluationValues: EvaluationResultType | undefined) => any>()
     );
@@ -73,6 +74,7 @@ export const TransformRuleEvaluation: React.FC<TransformRuleEvaluationProps> = (
                 updateCallback(undefined);
             });
         }
+        setEvaluationResultsShown(show);
     };
 
     const fetchReferenceLinksEvaluation: (
@@ -171,7 +173,7 @@ export const TransformRuleEvaluation: React.FC<TransformRuleEvaluationProps> = (
                 toggleEvaluationResults,
                 evaluationScore: undefined,
                 // There is no evaluation result for mapping rules
-                evaluationResultsShown: false,
+                evaluationResultsShown: evaluationResultsShown,
                 ruleValidationError,
                 clearRuleValidationError,
             }}
