11b8b1ca3628eaee46954434a766f31730e8240d
Robert Isele <mail@robertisele.com>
2023-04-03 14:26:14 +0200

    UriFixTransformer: Added example descriptions.

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/normalize/UriFixTransformer.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/normalize/UriFixTransformer.scala
@@ -17,39 +17,48 @@ import java.net.{MalformedURLException, URI, URISyntaxException, URL, URLDecoder
 )
 @TransformExamples(Array(
   new TransformExample(
+    description = "Non-absolute URIs are prefixed with the configured URI prefix.",
     input1 = Array("ab"),
     output = Array("urn:url-encoded-value:ab")
   ),
   new TransformExample(
+    description = "URI reserved characters are encoded.",
     input1 = Array("a&b"),
     output = Array("urn:url-encoded-value:a%26b")
   ),
   new TransformExample(
+    description = "Valid absolute URIs are forwarded unchanged.",
     input1 = Array("http://example.org/some/path"),
     output = Array("http://example.org/some/path")
   ),
   new TransformExample(
+    description = "Query parameters and fragments are left unchanged.",
     input1 = Array("http://example.org/path?query=some+stuff#hashtag"),
     output = Array("http://example.org/path?query=some+stuff#hashtag")
   ),
   new TransformExample(
+    description = "Valid URNs are forwarded unchanged.",
     input1 = Array("urn:valid:uri"),
     output = Array("urn:valid:uri")
   ),
   new TransformExample(
+    description = "Special characters are encoded.",
     input1 = Array("http://www.broken domain.com/broken weird path äöü/nice/path/andNowSomeFragment#fragmentäöü"),
     output = Array("http://www.broken%20domain.com/broken%20weird%20path%20%C3%A4%C3%B6%C3%BC/nice/path/andNowSomeFragment#fragment%C3%A4%C3%B6%C3%BC")
   ),
   new TransformExample(
+    description = "Hash signs are only encoded if they don't denote a fragment.",
     input1 = Array("http://domain/##path#"),
     output = Array("http://domain/#%23path%23")
   ),
   new TransformExample(
+    description = "Invalid URIs are fully encoded.",
     input1 = Array("http : invalid URI"),
     output = Array("urn:url-encoded-value:http+%3A+invalid+URI")
   ),
   new TransformExample(
-    input1 = Array("  http://domain.com/[squareBrackets]"),
+    description = "Leading and trailing spaces are removed.",
+    input1 = Array("  http://domain.com/[squareBrackets] "),
     output = Array("http://domain.com/%5BsquareBrackets%5D")
   )
 ))
