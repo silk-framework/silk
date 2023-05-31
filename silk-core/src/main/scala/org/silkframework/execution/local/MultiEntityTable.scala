4c5f7b2e19d42e9d70ab8fc1cc314b182f669a99
Robert Isele <mail@robertisele.com>
2023-05-31 11:06:16 +0200

    MultiEntityTable: Consider recursively nested tables.

--- silk-core/src/main/scala/org/silkframework/execution/local/MultiEntityTable.scala
+++ silk-core/src/main/scala/org/silkframework/execution/local/MultiEntityTable.scala
@@ -10,6 +10,18 @@ case class MultiEntityTable(entities: CloseableIterator[Entity],
                             subTables: Seq[LocalEntities],
                             override val globalErrors: Seq[String] = Seq.empty) extends LocalEntities {
 
+  /**
+    * Collects this entity table and all (recursively) nested entity tables.
+    */
+  def allTables: Seq[LocalEntities] = {
+    this +: subTables.flatMap {
+      case mt: MultiEntityTable =>
+        mt.allTables
+      case et: LocalEntities =>
+        Seq(et)
+    }
+  }
+
   override def updateEntities(newEntities: CloseableIterator[Entity], newSchema: EntitySchema): LocalEntities = {
     MultiEntityTable(newEntities, newSchema, task, subTables)
   }
