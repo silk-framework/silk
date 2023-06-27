6364cc1fd7efb8c8a77735968572a63345a253fd
Robert Isele <mail@robertisele.com>
2023-06-27 09:57:27 +0200

    ClassPluginDescription: Forward original exception.

--- silk-core/src/main/scala/org/silkframework/runtime/plugin/ClassPluginDescription.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/plugin/ClassPluginDescription.scala
@@ -155,7 +155,7 @@ object ClassPluginDescription {
       )
     } catch {
       case ex: InvalidPluginException =>
-        throw new InvalidPluginException(s"Cannot extract plugin description for plugin class '${pluginClass.getCanonicalName}'. Details: ${ex.getMessage}")
+        throw new InvalidPluginException(s"Cannot extract plugin description for plugin class '${pluginClass.getCanonicalName}'. Details: ${ex.getMessage}", ex)
     }
   }
 
