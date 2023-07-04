13a4b2271639ca7caf4db0adfe14d8ac7c37d3e9
Michael Haschke <michael.haschke@eccenca.com>
2023-07-04 09:05:21 +0200

    update test regarding renamed element

--- workspace/test/HierarchicalMapping/components/AutoComplete.test.jsx
+++ workspace/test/HierarchicalMapping/components/AutoComplete.test.jsx
@@ -1,29 +1,26 @@
 import React from "react";
-import { shallow } from 'enzyme';
-import AutoComplete from '../../../src/app/views/pages/MappingEditor/HierarchicalMapping/components/AutoComplete';
-import { AutoCompleteField } from "@eccenca/gui-elements";
+import { shallow } from "enzyme";
+import AutoComplete from "../../../src/app/views/pages/MappingEditor/HierarchicalMapping/components/AutoComplete";
+import { SuggestField } from "@eccenca/gui-elements";
 
 const props = {
-    input: 'text',
-    ruleId: 'rule',
+    input: "text",
+    ruleId: "rule",
 };
 const autocompleteAsyncMock = jest.fn();
-jest.doMock('../../../src/app/views/pages/MappingEditor/HierarchicalMapping/store', () => autocompleteAsyncMock);
+jest.doMock("../../../src/app/views/pages/MappingEditor/HierarchicalMapping/store", () => autocompleteAsyncMock);
 
-const getWrapper = (renderer = shallow) => renderer(
-    <AutoComplete {...props} />
-);
-
+const getWrapper = (renderer = shallow) => renderer(<AutoComplete {...props} />);
 
 describe("AutoComplete Component", () => {
-    describe("on component mounted, ",() => {
+    describe("on component mounted, ", () => {
         let wrapper;
         beforeEach(() => {
             wrapper = getWrapper(shallow);
         });
 
         it("should render AutoCompleteBox component", () => {
-            expect(wrapper.find(AutoCompleteField)).toHaveLength(1);
+            expect(wrapper.find(SuggestField)).toHaveLength(1);
         });
 
         afterEach(() => {
