aa1bd9ad0be4472a5453d49abec57081c75c70a1
Michael Haschke <michael.haschke@eccenca.com>
2023-07-10 14:25:06 +0200

    forward gui elements,minimal adjust element usage

--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/TargetList.tsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/TargetList.tsx
@@ -11,7 +11,7 @@ import {
 } from "@eccenca/gui-elements";
 import { ITargetWithSelected } from "../suggestion.typings";
 import { SuggestionListContext } from "../SuggestionContainer";
-import {extractSearchWords, matchesAllWords} from "@eccenca/gui-elements/src/components/Typography/Highlighter";
+import { extractSearchWords, matchesAllWords } from "@eccenca/gui-elements/src/components/Typography/Highlighter";
 
 // Select<T> is a generic component to work with your data types.
 // In TypeScript, you must first obtain a non-generic reference:
@@ -22,14 +22,14 @@ interface IProps {
 
     onChange(uri: ITargetWithSelected);
 }
-export default function TargetList({targets, onChange}: IProps) {
+export default function TargetList({ targets, onChange }: IProps) {
     const context = useContext(SuggestionListContext);
 
     const [items, setItems] = useState<ITargetWithSelected[]>(targets);
 
-    const [inputQuery, setInputQuery] = useState<string>('');
+    const [inputQuery, setInputQuery] = useState<string>("");
     // There must always be one item selected from the target list, e.g. auto-generated
-    const selected = targets.find(t => t._selected) as ITargetWithSelected;
+    const selected = targets.find((t) => t._selected) as ITargetWithSelected;
 
     useEffect(() => {
         setItems(items);
@@ -42,97 +42,117 @@ export default function TargetList({targets, onChange}: IProps) {
         if (!inputQuery) {
             setItems(targets);
         } else {
-            const words = extractSearchWords(inputQuery, true)
-            const filtered = targets.filter(o => {
-                    const searchIn = `${o?.uri || ""} ${o?.label || ""} ${o?.description || ""}`.toLowerCase()
-                    return matchesAllWords(searchIn, words)
-                }
-            );
-            const existingUris = new Set(filtered.map(f => f.uri))
-            if(suggestVocabularyProperties && context.fetchTargetPropertySuggestions) {
+            const words = extractSearchWords(inputQuery, true);
+            const filtered = targets.filter((o) => {
+                const searchIn = `${o?.uri || ""} ${o?.label || ""} ${o?.description || ""}`.toLowerCase();
+                return matchesAllWords(searchIn, words);
+            });
+            const existingUris = new Set(filtered.map((f) => f.uri));
+            if (suggestVocabularyProperties && context.fetchTargetPropertySuggestions) {
                 const timeout: number = window.setTimeout(async () => {
                     // For some reason the compiler cannot infer that context.fetchTargetPropertySuggestions is defined
-                    if(context.fetchTargetPropertySuggestions) {
+                    if (context.fetchTargetPropertySuggestions) {
                         // Search in all target properties
-                        const propertySuggestions = await context.fetchTargetPropertySuggestions(inputQuery)
+                        const propertySuggestions = await context.fetchTargetPropertySuggestions(inputQuery);
                         const propertySuggestionsWithSelect = propertySuggestions
-                            .filter(ps => !existingUris.has(ps.uri))
-                            .map(ps => ({
+                            .filter((ps) => !existingUris.has(ps.uri))
+                            .map((ps) => ({
                                 ...ps,
-                                _selected: false
-                            }))
-                        setItems([...filtered, ...propertySuggestionsWithSelect])
+                                _selected: false,
+                            }));
+                        setItems([...filtered, ...propertySuggestionsWithSelect]);
                     }
-                }, 200)
-                return () => clearTimeout(timeout)
+                }, 200);
+                return () => clearTimeout(timeout);
             } else {
                 setItems(filtered);
             }
         }
-    }, [inputQuery])
+    }, [inputQuery]);
 
-    const suggestVocabularyProperties = context.isFromDataset
+    const suggestVocabularyProperties = context.isFromDataset;
 
     const areTargetsEqual = (targetA: ITargetWithSelected, targetB: ITargetWithSelected) => {
         // Compare only the titles (ignoring case) just for simplicity.
         return targetA.uri?.toLowerCase() === targetB.uri?.toLowerCase();
-    }
+    };
 
     const handleSelectTarget = (target: ITargetWithSelected) => {
         onChange(target);
-    }
+    };
 
-    const itemLabel = (target: ITargetWithSelected, search: string) => <OverviewItem>
-        <OverviewItemDescription>
-            {target.label && <OverviewItemLine><OverflowText><Highlighter label={target.label} searchValue={search} /></OverflowText></OverviewItemLine>}
-            {target.uri && <OverviewItemLine small={!!target.label ? true : false }><OverflowText ellipsis={"reverse"}><Highlighter label={target.uri} searchValue={search} /></OverflowText></OverviewItemLine>}
-            {target.description && <OverviewItemLine small={true}><OverflowText><Highlighter label={target.description} searchValue={search} /></OverflowText></OverviewItemLine>}
-        </OverviewItemDescription>
-    </OverviewItem>;
+    const itemLabel = (target: ITargetWithSelected, search: string) => (
+        <OverviewItem>
+            <OverviewItemDescription>
+                {target.label && (
+                    <OverviewItemLine>
+                        <OverflowText>
+                            <Highlighter label={target.label} searchValue={search} />
+                        </OverflowText>
+                    </OverviewItemLine>
+                )}
+                {target.uri && (
+                    <OverviewItemLine small={!!target.label ? true : false}>
+                        <OverflowText ellipsis={"reverse"}>
+                            <Highlighter label={target.uri} searchValue={search} />
+                        </OverflowText>
+                    </OverviewItemLine>
+                )}
+                {target.description && (
+                    <OverviewItemLine small={true}>
+                        <OverflowText>
+                            <Highlighter label={target.description} searchValue={search} />
+                        </OverflowText>
+                    </OverviewItemLine>
+                )}
+            </OverviewItemDescription>
+        </OverviewItem>
+    );
 
-    const itemRenderer = (target: ITargetWithSelected, {handleClick}) => {
-        return <MenuItem
-            text={<div style={{width: "40rem", maxWidth: "90vw"}}>{itemLabel(target, inputQuery)}</div>}
-            key={target.uri}
-            onClick={handleClick}
-            active={target.uri === selected.uri}
-        />
-    }
+    const itemRenderer = (target: ITargetWithSelected, { handleClick }) => {
+        return (
+            <MenuItem
+                text={<div style={{ width: "40rem", maxWidth: "90vw" }}>{itemLabel(target, inputQuery)}</div>}
+                key={target.uri}
+                onClick={handleClick}
+                active={target.uri === selected.uri}
+            />
+        );
+    };
 
     const handleQueryChange = (value) => {
         if (!value) {
-            setInputQuery('');
+            setInputQuery("");
         } else {
-            setInputQuery(value)
+            setInputQuery(value);
         }
-    }
+    };
 
-    return <TargetSelect
-        className={"ecc-silk-mapping__suggestionlist__target-select"}
-        filterable={suggestVocabularyProperties || targets.length > 1}
-        onItemSelect={handleSelectTarget}
-        items={items}
-        itemRenderer={itemRenderer}
-        itemsEqual={areTargetsEqual}
-        resetOnSelect={true}
-        resetOnClose={true}
-        inputProps={{
-            placeholder: context.isFromDataset ? "Enter text to search in all target properties..." : "Filter candidates...",
-            className: "ecc-silk-mapping__suggestionlist__target-property-search"
-        }}
-        popoverProps={{
-            minimal: true,
-            popoverClassName: "ecc-silk-mapping__suggestionlist__target-dropdown",
-            portalContainer: context.portalContainer
-        }}
-        onQueryChange={handleQueryChange}
-        query={inputQuery}
-    >
-        <Button
-            fill={true}
-            rightIcon="toggler-caret"
-            text={itemLabel(selected, context.search)}
-        />
-    </TargetSelect>
-
+    return (
+        <TargetSelect
+            className={"ecc-silk-mapping__suggestionlist__target-select"}
+            filterable={suggestVocabularyProperties || targets.length > 1}
+            onItemSelect={handleSelectTarget}
+            items={items}
+            itemRenderer={itemRenderer}
+            itemsEqual={areTargetsEqual}
+            resetOnSelect={true}
+            resetOnClose={true}
+            inputProps={{
+                placeholder: context.isFromDataset
+                    ? "Enter text to search in all target properties..."
+                    : "Filter candidates...",
+                className: "ecc-silk-mapping__suggestionlist__target-property-search",
+            }}
+            contextOverlayProps={{
+                minimal: true,
+                popoverClassName: "ecc-silk-mapping__suggestionlist__target-dropdown",
+                portalContainer: context.portalContainer,
+            }}
+            onQueryChange={handleQueryChange}
+            query={inputQuery}
+        >
+            <Button fill={true} rightIcon="toggler-caret" text={itemLabel(selected, context.search)} />
+        </TargetSelect>
+    );
 }
--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/TypesList.tsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/TypesList.tsx
@@ -7,7 +7,7 @@ import { SuggestionTypeValues } from "../suggestion.typings";
 // In TypeScript, you must first obtain a non-generic reference:
 const TypesSelect = Select.ofType<string>();
 
-const TYPES = ['value', 'object'];
+const TYPES = ["value", "object"];
 
 interface IProps {
     selected: SuggestionTypeValues;
@@ -15,37 +15,30 @@ interface IProps {
     onChange(value: SuggestionTypeValues);
 }
 
-export default function TypesList({onChange, selected}: IProps) {
+export default function TypesList({ onChange, selected }: IProps) {
     const context = useContext(SuggestionListContext);
 
     const areTypesEqual = (typeA: string, typeB: string) => {
         return typeA.toLowerCase() === typeB.toLowerCase();
-    }
+    };
 
-    const itemRenderer = (type: string, {handleClick}) => {
-        return <MenuItem
-            text={type}
-            key={type}
-            onClick={handleClick}
-            active={selected === type}
-        />
-    }
+    const itemRenderer = (type: string, { handleClick }) => {
+        return <MenuItem text={type} key={type} onClick={handleClick} active={selected === type} />;
+    };
 
-    return <TypesSelect
-        filterable={false}
-        onItemSelect={onChange}
-        items={TYPES}
-        itemRenderer={itemRenderer}
-        itemsEqual={areTypesEqual}
-        popoverProps={{
-            minimal: true,
-            portalContainer: context.portalContainer
-        }}
-    >
-        <Button
-            rightIcon="toggler-caret"
-            text={selected}
-        />
-    </TypesSelect>
-
+    return (
+        <TypesSelect
+            filterable={false}
+            onItemSelect={onChange}
+            items={TYPES}
+            itemRenderer={itemRenderer}
+            itemsEqual={areTypesEqual}
+            contextOverlayProps={{
+                minimal: true,
+                portalContainer: context.portalContainer,
+            }}
+        >
+            <Button rightIcon="toggler-caret" text={selected} />
+        </TypesSelect>
+    );
 }
