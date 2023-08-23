a3b28cda8b2fbd4b9967b96ddb46feae8267af3b
Andreas Schultz <a.schultz@fu-berlin.de>
2023-08-23 11:50:15 +0200

    Support ENTER key in delete modals to submit the deletion, also works together with confirmation

--- workspace/src/app/views/shared/VariablesWidget/VariablesWidget.tsx
+++ workspace/src/app/views/shared/VariablesWidget/VariablesWidget.tsx
@@ -129,9 +129,10 @@ const VariablesWidget: React.FC<VariableWidgetProps> = ({ projectId, taskId }) =
     );
 
     const renderDeleteVariable = React.useCallback(() => {
+        const varname = selectedVariable?.name ?? ""
         return (
             <div>
-                {t("widget.VariableWidget.modalMessages.deletePrompt", { varname: selectedVariable?.name ?? "" })}
+                {t("widget.VariableWidget.modalMessages.deletePrompt", { varname })}
             </div>
         );
     }, [selectedVariable]);
