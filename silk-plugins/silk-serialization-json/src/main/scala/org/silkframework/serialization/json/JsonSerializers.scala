fe82518605f8c5b9f08c82116e30c72d69982c31
Robert Isele <mail@robertisele.com>
2023-08-29 11:48:36 +0200

    ComparisonJsonFormat: Threshold needs to be optional because it does not have to be set for boolean distance measures.

--- silk-plugins/silk-serialization-json/src/main/scala/org/silkframework/serialization/json/JsonSerializers.scala
+++ silk-plugins/silk-serialization-json/src/main/scala/org/silkframework/serialization/json/JsonSerializers.scala
@@ -25,9 +25,9 @@ import org.silkframework.serialization.json.LinkingSerializers._
 import org.silkframework.serialization.json.MetaDataSerializers._
 import org.silkframework.serialization.json.PluginSerializers.{ParameterValuesJsonFormat, PluginJsonFormat}
 import org.silkframework.util.{DPair, Identifier, IdentifierUtils, Uri}
-import org.silkframework.workspace.{LoadedTask, TaskLoadingError}
 import org.silkframework.workspace.activity.transform.{CachedEntitySchemata, VocabularyCacheValue}
 import org.silkframework.workspace.annotation.{StickyNote, UiAnnotations}
+import org.silkframework.workspace.{LoadedTask, TaskLoadingError}
 import play.api.libs.json._
 
 import scala.reflect.ClassTag
@@ -811,7 +811,7 @@ object JsonSerializers {
       Comparison(
         id = identifier(value, "comparison"),
         weight = numberValue(value, WEIGHT).intValue,
-        threshold = numberValue(value, THRESHOLD).doubleValue,
+        threshold = numberValueOption(value, THRESHOLD).map(_.doubleValue).getOrElse(0.0),
         indexing = booleanValue(value, INDEXING),
         metric = metric,
         inputs =
