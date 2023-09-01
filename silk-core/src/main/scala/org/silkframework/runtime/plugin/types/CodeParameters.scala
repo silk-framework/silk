bdcd6f0d17875abc3bb0f94dc3e404a9d27d7d2d
Andreas Schultz <a.schultz@fu-berlin.de>
2023-09-01 13:50:32 +0200

    Add implicit conversion from String to new code parameters

--- silk-core/src/main/scala/org/silkframework/runtime/plugin/types/CodeParameters.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/plugin/types/CodeParameters.scala
@@ -16,12 +16,28 @@ object CodeParameter {
 
 case class Jinja2CodeParameter(var str: String) extends CodeParameter
 
+object Jinja2CodeParameter {
+  implicit def str2parameter(str: String): Jinja2CodeParameter = Jinja2CodeParameter(str)
+}
+
 case class JsonCodeParameter(var str: String) extends CodeParameter
 
 case class SparqlCodeParameter(var str: String) extends CodeParameter
 
+object SparqlCodeParameter {
+  implicit def str2parameter(str: String): SparqlCodeParameter = SparqlCodeParameter(str)
+}
+
 case class SqlCodeParameter(var str: String) extends CodeParameter
 
+object SqlCodeParameter {
+  implicit def str2parameter(str: String): SqlCodeParameter = SqlCodeParameter(str)
+}
+
 case class XmlCodeParameter(var str: String) extends CodeParameter
+
+object XmlCodeParameter {
+  implicit def str2parameter(str: String): XmlCodeParameter = XmlCodeParameter(str)
+}
 
 case class YamlCodeParameter(var str: String) extends CodeParameter
