2ba8709b771529cc28f7f01592ebb9608d091997
Michael Haschke <michael.haschke@eccenca.com>
2023-10-18 14:14:24 +0200

    fix whitespace a bit

--- workspace/src/app/views/layout/Header/KeyboardShortcutsModal.tsx
+++ workspace/src/app/views/layout/Header/KeyboardShortcutsModal.tsx
@@ -97,7 +97,7 @@ export const KeyboardShortcutsModal = () => {
         >
             <OverviewItemList hasDivider columns={1}>
                 {sectionKeys.map((sectionKey) => (
-                    <section key={sectionKey}>
+                    <section key={sectionKey} style={{ margin: "0.5em 0" }}>
                         <TitleSubsection>
                             {t(`header.keyboardShortcutsModal.categories.${sectionKey}.label`)}
                         </TitleSubsection>
