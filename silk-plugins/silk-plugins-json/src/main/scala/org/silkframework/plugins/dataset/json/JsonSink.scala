0fc570772cf6ee637b6b286b1f3ee906ea671bb0
Robert Isele <mail@robertisele.com>
2024-03-19 15:55:20 +0100

    Improved HierarchicalSink so it does not require a resource.

--- silk-plugins/silk-plugins-json/src/main/scala/org/silkframework/plugins/dataset/json/JsonSink.scala
+++ silk-plugins/silk-plugins-json/src/main/scala/org/silkframework/plugins/dataset/json/JsonSink.scala
@@ -1,16 +1,32 @@
 package org.silkframework.plugins.dataset.json
 
+import org.silkframework.dataset.DirtyTrackingFileDataSink
 import org.silkframework.plugins.dataset.hierarchical.{HierarchicalEntityWriter, HierarchicalSink}
+import org.silkframework.runtime.activity.UserContext
 import org.silkframework.runtime.resource.WritableResource
 
 import java.io.OutputStream
 
 class JsonSink (val resource: WritableResource,
                 template: JsonTemplate = JsonTemplate.default,
-                override val maxDepth: Int = HierarchicalSink.DEFAULT_MAX_SIZE) extends HierarchicalSink {
+                override val maxDepth: Int = HierarchicalSink.DEFAULT_MAX_SIZE) extends HierarchicalSink with DirtyTrackingFileDataSink {
 
-  override protected def createWriter(outputStream: OutputStream): HierarchicalEntityWriter = {
-    new JsonEntityWriter(outputStream, template)
+  override protected def outputEntities(writeOutput: HierarchicalEntityWriter => Unit): Unit = {
+    resource.write() { outputStream =>
+      val writer = new JsonEntityWriter(outputStream, template)
+      try {
+        writeOutput(writer)
+      } finally {
+        writer.close()
+      }
+    }
+  }
+
+  /**
+   * Makes sure that the next write will start from an empty dataset.
+   */
+  override def clear()(implicit userContext: UserContext): Unit = {
+    resource.delete()
   }
 
 }
