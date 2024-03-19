bb7c7de492eba6baaf1bb5c962b360cde8083693
Robert Isele <mail@robertisele.com>
2024-03-19 14:52:13 +0100

    Template full evaluation now checks the memory limit for hierarchical entities.

--- silk-core/src/main/scala/org/silkframework/runtime/templating/TemplateEngine.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/templating/TemplateEngine.scala
@@ -52,15 +52,6 @@ trait CompiledTemplate {
   }
 
   /**
-   * Evaluates this template using an iterator over entities.
-   *
-   * @throws TemplateEvaluationException If the evaluation failed.
-   */
-  def evaluate(entities: Iterator[Entity], writer: Writer): Unit = {
-    evaluate(Map("entities" -> loadEntitiesIntoMemory(entities)), writer)
-  }
-
-  /**
    * Converts an entity to a sequence of template variables.
    */
   protected def entityToMap(entity: Entity): Seq[TemplateVariableValue] = {
@@ -86,28 +77,6 @@ trait CompiledTemplate {
         Seq((scope, nestedValues.toMap.asJava))
       }
     }
-  }
-
-  /**
-   * Loads an iterator of entities into a Java map.
-   */
-  private def loadEntitiesIntoMemory(entities: Iterator[Entity]): java.util.List[java.util.Map[String, AnyRef]] = {
-    val buffer = mutable.Buffer[java.util.Map[String, AnyRef]]()
-    val maxCount = Resource.maxInMemorySize()
-    var byteCount = 0
-    for (entity <- entities) {
-      byteCount += entity.uri.uri.length * 2
-      for (values <- entity.values; value <- values) {
-        byteCount += value.length * 2
-      }
-      if (byteCount > maxCount) {
-        throw new ResourceTooLargeException("Tried to load an entities into memory that are larger than the configured maximum " +
-          s"(size: $byteCount, maximum size: $maxCount}). " +
-          s"Configure '${Resource.maxInMemorySizeParameterName}' in order to increase this limit.")
-      }
-      buffer.append(convertValues(entityToMap(entity)).asJava)
-    }
-    buffer.asJava
   }
 
 }
