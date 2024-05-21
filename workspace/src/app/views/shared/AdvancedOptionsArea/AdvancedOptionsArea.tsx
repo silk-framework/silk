72391f90c8eec0fedd1ce1143f704242cfaa2cb5
Andreas Schultz <a.schultz@fu-berlin.de>
2024-05-21 15:31:05 +0200

    Make advanced section in rule editor boxed more compact

--- workspace/src/app/views/shared/AdvancedOptionsArea/AdvancedOptionsArea.tsx
+++ workspace/src/app/views/shared/AdvancedOptionsArea/AdvancedOptionsArea.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { Accordion, AccordionItem, TitleSubsection } from "@eccenca/gui-elements";
 import { useTranslation } from "react-i18next";
+import {compact} from "lodash";
 
-export function AdvancedOptionsArea({ children, open = false, ...otherProps }: any) {
+interface AdvancedOptionsAreaProps {
+    children: any
+    open?: boolean
+    compact?: boolean
+}
+export function AdvancedOptionsArea({ children, open = false, compact = false }: AdvancedOptionsAreaProps) {
     const [t] = useTranslation();
 
     return (
@@ -11,6 +17,8 @@ export function AdvancedOptionsArea({ children, open = false, ...otherProps }: a
                 label={<TitleSubsection>{t("common.words.advancedOptions", "Advanced options")}</TitleSubsection>}
                 fullWidth
                 elevated
+                noBorder={compact}
+                condensed={compact}
                 open={open}
             >
                 {children}
