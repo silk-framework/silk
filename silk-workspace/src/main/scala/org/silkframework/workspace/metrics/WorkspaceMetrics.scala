cda9a8e64a0658c689124174ba48e0e7d1a956cf
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2024-07-18 14:30:34 +0200

    Metrics for a Workspace instance.

---
+++ silk-workspace/src/main/scala/org/silkframework/workspace/metrics/WorkspaceMetrics.scala
@@ -0,0 +1,23 @@
+package org.silkframework.workspace.metrics
+
+import io.micrometer.core.instrument.binder.MeterBinder
+import io.micrometer.core.instrument.{Gauge, MeterRegistry}
+import org.silkframework.runtime.activity.UserContext
+import org.silkframework.workspace.Workspace
+
+import scala.util.Try
+
+/**
+ * Metrics for a Workspace instance.
+ *
+ * @param workspace Workspace to monitor.
+ * @param userContext User context needed to retrieve the projects, etc.
+ */
+class WorkspaceMetrics(workspace: Workspace)(implicit userContext: UserContext) extends MeterBinder {
+  override def bindTo(registry: MeterRegistry): Unit = {
+    Gauge.builder("workspace.metrics", workspace, (w: Workspace) => Try(w.projects.size.toDouble).getOrElse(0.0))
+      .tags("project", "size")
+      .description("Workspace metrics: project size")
+      .register(registry)
+  }
+}
