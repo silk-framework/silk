e9d38f1e1168647c2f3353de8ce152269ea20785
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2024-08-08 15:48:05 +0200

    Simplify the CSVSanitizer to the main cause of CSV injection.

--- silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CSVSanitizer.scala
+++ silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CSVSanitizer.scala
@@ -1,17 +1,14 @@
 package org.silkframework.plugins.dataset.csv
 
 /**
- * Sanitizer for CSV rows. It deals with the causes for CSV Injection, as documented in
+ * Sanitizer for CSV rows. It deals with the main cause for CSV Injection, as documented in
  * https://owasp.org/www-community/attacks/CSV_Injection.
  */
 object CSVSanitizer {
   def sanitize(str: String): String = {
-    if (str.isEmpty) "\"\"" // there are CSV parsers that only accept "" as an escaped quote
-    else if (str.startsWith("=") || str.startsWith("+") || str.startsWith("-")
+    if (str.startsWith("=") || str.startsWith("+") || str.startsWith("-")
         || str.startsWith("@") || str.startsWith("\\u000D") || str.startsWith("\\u0009")
     ) "\"'" + doubleQuoteMask(str) + "\""
-    else if (str.contains("\"") || str.contains(",") || str.contains("\r") || str.contains("\n"))
-      doubleQuoteMask(str)
     else str
   }
 
