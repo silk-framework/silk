fa838c74a13c6e248646061e00b9ff12ef385dae
Robert Isele <mail@robertisele.com>
2024-09-03 13:53:55 +0200

    Added test for FileHashTransformer

--- silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/dataset/FileHashTransformerTest.scala
+++ silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/dataset/FileHashTransformerTest.scala
@@ -2,6 +2,10 @@ package org.silkframework.rule.plugins.transformer.dataset
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import org.silkframework.config.PlainTask
+import org.silkframework.dataset.DatasetSpec
+import org.silkframework.plugins.dataset.csv.CsvDataset
+import org.silkframework.rule.TaskContext
 import org.silkframework.runtime.resource.InMemoryResourceManager
 
 class FileHashTransformerTest extends AnyFlatSpec with Matchers {
@@ -12,6 +16,16 @@ class FileHashTransformerTest extends AnyFlatSpec with Matchers {
     val resource = InMemoryResourceManager().get("test")
     resource.writeString("ABC")
     val transformer = FileHashTransformer(Some(resource))
+    transformer(Seq.empty) shouldBe Seq("3c01bdbb26f358bab27f267924aa2c9a03fcfdb8")
+  }
+
+  it should "generate the hash sum of the input dataset" in {
+    val resource = InMemoryResourceManager().get("test")
+    resource.writeString("ABC")
+    val dataset = PlainTask("input", DatasetSpec(CsvDataset(file = resource)))
+    val taskContext = TaskContext(Seq(dataset))
+
+    val transformer = FileHashTransformer(None).withContext(taskContext)
     transformer(Seq.empty) shouldBe Seq("3c01bdbb26f358bab27f267924aa2c9a03fcfdb8")
   }
 
