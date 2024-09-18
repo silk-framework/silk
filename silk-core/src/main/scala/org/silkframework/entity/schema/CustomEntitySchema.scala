8fca2168e8037eddbf7b23cd61a277e23b4c08bf
Robert Isele <mail@robertisele.com>
2024-09-18 16:46:43 +0200

    Added Scaladoc.

--- silk-core/src/main/scala/org/silkframework/entity/schema/CustomEntitySchema.scala
+++ silk-core/src/main/scala/org/silkframework/entity/schema/CustomEntitySchema.scala
@@ -4,7 +4,7 @@ import org.silkframework.config.{SilkVocab, Task, TaskSpec}
 import org.silkframework.entity.paths.{TypedPath, UntypedPath}
 import org.silkframework.entity.schema.FileType.FileType
 import org.silkframework.entity.{Entity, EntitySchema, ValueType}
-import org.silkframework.execution.local.LocalEntities
+import org.silkframework.execution.local.{GenericEntityTable, LocalEntities}
 import org.silkframework.runtime.iterator.CloseableIterator
 import org.silkframework.runtime.plugin.PluginContext
 import org.silkframework.runtime.resource.{FileResource, WritableResource}
@@ -15,16 +15,32 @@ import java.io.File
 
 //TODO move this to another package as LocalEntities is also in execution?
 
+/**
+ * A custom entity schema that holds entities of a specific type (e.g. files).
+ *
+ * @tparam EntityType The type of entities to be held.
+ */
 abstract class CustomEntitySchema[EntityType] {
 
+  /**
+   * The fixed schema for this type.
+   * Entities will be associated with this custom type based on the type URI of the schema.
+   */
   def schema: EntitySchema
 
+  /**
+   * Creates a generic entity from a custom value.
+   */
   def toEntity(v: EntityType)(implicit pluginContext: PluginContext): Entity
 
+  /**
+   * Creates a custom value from a generic entity.
+   */
   def fromEntity(entity: Entity)(implicit pluginContext: PluginContext): EntityType
 
   def unapply(entities: LocalEntities)(implicit pluginContext: PluginContext): Option[CustomEntities[EntityType]] = {
     entities match {
+      //TODO type erasure?
       case customEntities: CustomEntities[EntityType] =>
         Some(customEntities)
       case _ if entities.entitySchema.typeUri == schema.typeUri =>
@@ -62,10 +78,16 @@ class CustomEntities[EntityType](val customEntities: CloseableIterator[EntityTyp
   }
 
   override def updateEntities(newEntities: CloseableIterator[Entity], newSchema: EntitySchema): LocalEntities = {
-    //TODO implement correctly
-    this // Changing entities has no effect
+    if(newSchema == entitySchema) {
+      new CustomEntities[EntityType](
+        customEntities = newEntities.map(customEntitySchema.fromEntity),
+        customEntitySchema = customEntitySchema,
+        task = task
+      )
+    } else {
+      new GenericEntityTable(newEntities, newSchema, task)
+    }
   }
-
 }
 
 object FileEntitySchema extends CustomEntitySchema[FileEntity] {
@@ -118,10 +140,20 @@ object FileEntitySchema extends CustomEntitySchema[FileEntity] {
   }
 }
 
+/**
+ * A file entity that can be held in a `FileEntitySchema`.
+ *
+ * @param file The file to be exchanged.
+ * @param fileType The file type. Affects how the file will be serialized to a generic entity table.
+ * @param contentType Optional content type.
+ */
 case class FileEntity(file: WritableResource, fileType: FileType, contentType: Option[String] = None)
 
 object FileEntity {
 
+  /**
+   * Creates a temporary file entity.
+   */
   def createTemp(prefix: String, suffix: String = ".tmp"): FileEntity = {
     val tempFile = File.createTempFile(prefix, suffix)
     tempFile.deleteOnExit()
@@ -132,6 +164,12 @@ object FileEntity {
 
 }
 
+/**
+ * Determines where a `FileEntity` is held.
+ * One of:
+ * - Project: Files that are part of the project resources. Identified by the relative path within the project resources.
+ * - Local: File on the local file system. Identified by the full local path.
+ */
 object FileType extends Enumeration {
   type FileType = Value
   val Project, Local = Value
