18ce4ede69f8121f6673c42f9f905d77cf68b679
Andreas Schultz <a.schultz@fu-berlin.de>
2024-10-01 09:03:29 +0200

    Disable all parameter anchor links in the task plugin markdown modal

--- workspace/src/app/views/shared/modals/CreateArtefactModal/TaskDocumentationModal.tsx
+++ workspace/src/app/views/shared/modals/CreateArtefactModal/TaskDocumentationModal.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import { Button, HtmlContentBlock, Markdown, SimpleDialog, CLASSPREFIX as eccgui } from "@eccenca/gui-elements";
-import { ArtefactDocumentation } from "./CreateArtefactModal";
+import {Button, HtmlContentBlock, Markdown, SimpleDialog, CLASSPREFIX as eccgui} from "@eccenca/gui-elements";
+import {ArtefactDocumentation} from "./CreateArtefactModal";
 
 interface TaskDocumentationModalProps {
     documentationToShow: ArtefactDocumentation;
@@ -11,7 +11,7 @@ interface TaskDocumentationModalProps {
 const testId = "artefact-documentation-modal";
 const headerRegex = /^h[0-9]+/;
 
-// Finds a heading that is eiter parent of this element or is a sibling before one of its parent, i.e. a sibling of a parent must be a heading.
+// Finds a heading that is either parent of this element or is a sibling before one of its parent, i.e. a sibling of a parent must be a heading.
 const findHeadingBefore = (element: Element): Element | undefined => {
     let upperParent: Element | null = element;
     const isHeader = (element: Element): boolean => headerRegex.test(element.tagName.toLowerCase());
@@ -33,31 +33,41 @@ const findHeadingBefore = (element: Element): Element | undefined => {
 };
 
 /** Modal that shows the detailed task documentation, e.g. the Markdown. */
-export const TaskDocumentationModal = ({ documentationToShow, onClose }: TaskDocumentationModalProps) => {
+export const TaskDocumentationModal = ({documentationToShow, onClose}: TaskDocumentationModalProps) => {
     const [initialized, setInitialized] = React.useState(false);
 
     React.useEffect(() => {
         // If an anchor is defined, jump to it
-        if (initialized && documentationToShow.markdownDocumentation && documentationToShow.namedAnchor) {
-            const element = document.querySelector(
-                `[data-test-id="${testId}"] a[id="${documentationToShow.namedAnchor}"]`
-            );
-            if (element) {
-                element.classList.add(`${eccgui}-typography--spothighlight`);
-                const previousHeader = findHeadingBefore(element);
-                if (previousHeader) {
-                    // Scroll to previous header to have the context heading in view, link will be highlighted in addition
-                    setTimeout(
-                        // use minimal timeout to scroll via JS after the browser scrolled to the local anchor
-                        function () {
-                            previousHeader.scrollIntoView({ block: "start", inline: "nearest", behavior: "smooth" });
-                        },
-                        1
-                    );
-                } else {
-                    setTimeout(function () {
-                        element.scrollIntoView({ block: "center", inline: "nearest", behavior: "smooth" });
-                    }, 1);
+        if (initialized && documentationToShow.markdownDocumentation) {
+            // Make anchor links non-functional
+            const elements = document.querySelectorAll(`[data-test-id=${testId}] a[id^=parameter_doc_]`);
+            elements.forEach(link => {
+                // Disable anchor links
+                const l = link as HTMLLinkElement
+                l.onclick = () => false;
+            })
+            // Highlight named anchor
+            if (documentationToShow.namedAnchor) {
+                const element = document.querySelector(
+                    `[data-test-id="${testId}"] a[id="${documentationToShow.namedAnchor}"]`
+                );
+                if (element) {
+                    element.classList.add(`${eccgui}-typography--spothighlight`);
+                    const previousHeader = findHeadingBefore(element);
+                    if (previousHeader) {
+                        // Scroll to previous header to have the context heading in view, link will be highlighted in addition
+                        setTimeout(
+                            // use minimal timeout to scroll via JS after the browser scrolled to the local anchor
+                            function () {
+                                previousHeader.scrollIntoView({block: "start", inline: "nearest", behavior: "smooth"});
+                            },
+                            1
+                        );
+                    } else {
+                        setTimeout(function () {
+                            element.scrollIntoView({block: "center", inline: "nearest", behavior: "smooth"});
+                        }, 1);
+                    }
                 }
             }
         }
@@ -71,11 +81,11 @@ export const TaskDocumentationModal = ({ documentationToShow, onClose }: TaskDoc
             enforceFocus={true}
             onClose={onClose}
             title={documentationToShow.title ?? "Documentation"}
-            actions={<Button text="Close" onClick={onClose} />}
+            actions={<Button text="Close" onClick={onClose}/>}
             size="large"
         >
             <HtmlContentBlock>
-                <span ref={() => setInitialized(true)} />
+                <span ref={() => setInitialized(true)}/>
                 <Markdown allowHtml>
                     {documentationToShow.markdownDocumentation || documentationToShow.description || ""}
                 </Markdown>
