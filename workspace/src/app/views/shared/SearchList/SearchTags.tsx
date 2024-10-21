fa83aedac7567d4dbc9b24200175b9fc51bd178c
Michael Haschke <michael.haschke@eccenca.com>
2024-10-21 16:40:09 +0200

    improve rendering of SearchTags, prevent display issue in extra element

--- workspace/src/app/views/shared/SearchList/SearchTags.tsx
+++ workspace/src/app/views/shared/SearchList/SearchTags.tsx
@@ -1,29 +1,29 @@
-import {Highlighter, Spacing, Tag} from "@eccenca/gui-elements"
-import {SpacingProps} from "@eccenca/gui-elements/src/components/Separation/Spacing"
+import { Highlighter, Spacing, Tag } from "@eccenca/gui-elements";
+import { SpacingProps } from "@eccenca/gui-elements/src/components/Separation/Spacing";
 import React from "react";
 
 interface SearchTagsProps {
-    searchTags?: string[]
-    searchText?: string
+    searchTags?: string[];
+    searchText?: string;
     // Adds a tiny spacing between the tags
-    withSpacing?: SpacingProps["size"] | "none"
+    withSpacing?: SpacingProps["size"] | "none";
 }
 
 /** Displays search tags. */
-export const SearchTags = ({searchTags, searchText, withSpacing = "tiny"}: SearchTagsProps) => {
+export const SearchTags = ({ searchTags, searchText, withSpacing = "tiny" }: SearchTagsProps) => {
     if (!searchTags || searchTags.length === 0) {
-        return null
+        return null;
     }
     return (
         <>
-            {searchTags.map(searchTag => (
-                <div key={searchTag}>
-                    <Tag emphasis="weaker">
-                        <Highlighter label={searchTag} searchValue={searchText}/>
+            {searchTags.map((searchTag) => (
+                <>
+                    <Tag emphasis="weaker" key={searchTag}>
+                        <Highlighter label={searchTag} searchValue={searchText} />
                     </Tag>
-                    {withSpacing !== "none" ? <Spacing size={withSpacing} vertical/> : null}
-                </div>
+                    {withSpacing !== "none" ? <Spacing size={withSpacing} vertical /> : null}
+                </>
             ))}
         </>
     );
-}
+};
