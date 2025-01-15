5dcb256d1b5d0fa602aca6a100490752ae811b83
Andreas Schultz <a.schultz@fu-berlin.de>
2025-01-15 17:42:57 +0100

    Get frontend init data via hook inside TargetInfoBox

--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/StableRow/TargetInfoBox.tsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/StableRow/TargetInfoBox.tsx
@@ -2,40 +2,48 @@ import React, { useContext } from "react";
 import { IPageSuggestion, ITargetWithSelected } from "../../suggestion.typings";
 import { SuggestionListContext } from "../../SuggestionContainer";
 import { InfoBoxOverlay } from "./InfoBoxOverlay";
+import { useInitFrontend } from "../../../../../api/silkRestApi.hooks";
 
 interface IProps {
     selectedTarget: ITargetWithSelected | IPageSuggestion;
 }
 
-export default function TargetInfoBox({selectedTarget}: IProps) {
-    const context = useContext(SuggestionListContext);
-    const dmBaseUrl = context.frontendInitData?.dmBaseUrl
+export default function TargetInfoBox({ selectedTarget }: IProps) {
+    const initData = useInitFrontend();
+    const dmBaseUrl = initData?.dmBaseUrl;
     const dmResourceLink = (graph: string, dmBaseUrl: string) => {
-        return `${dmBaseUrl}/explore?resource=${encodeURIComponent(selectedTarget.uri)}&graph=${encodeURIComponent(graph)}`
-    }
+        return `${dmBaseUrl}/explore?resource=${encodeURIComponent(selectedTarget.uri)}&graph=${encodeURIComponent(
+            graph
+        )}`;
+    };
 
-    return <InfoBoxOverlay
-        data={[
-            {
-                key: "Label",
-                value: selectedTarget.label,
-            },
-            {
-                key: "URI",
-                value: (<span style={{ wordBreak: "break-all" }}>{ selectedTarget.uri }</span>),
-            },
-            {
-                key: "Explore in DataManager",
-                value: (dmBaseUrl && selectedTarget.graph) ? <a href={dmResourceLink(selectedTarget.graph, dmBaseUrl)}
-                                                                rel="noreferrer"
-                                                                target='_blank'>
-                    {selectedTarget.label}
-                </a> : '',
-            },
-            {
-                key: "Description",
-                value: selectedTarget.description,
-            }
-        ]}
-    />;
+    return (
+        <InfoBoxOverlay
+            data={[
+                {
+                    key: "Label",
+                    value: selectedTarget.label,
+                },
+                {
+                    key: "URI",
+                    value: <span style={{ wordBreak: "break-all" }}>{selectedTarget.uri}</span>,
+                },
+                {
+                    key: "Explore in DataManager",
+                    value:
+                        dmBaseUrl && selectedTarget.graph ? (
+                            <a href={dmResourceLink(selectedTarget.graph, dmBaseUrl)} rel="noreferrer" target="_blank">
+                                {selectedTarget.label}
+                            </a>
+                        ) : (
+                            ""
+                        ),
+                },
+                {
+                    key: "Description",
+                    value: selectedTarget.description,
+                },
+            ]}
+        />
+    );
 }
