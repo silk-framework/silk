cd109161fbae6b25bde78c5cd7fa17416a2c613d
Michael Haschke <michael.haschke@eccenca.com>
2025-03-17 16:16:33 +0100

    forward sb element properties and add new option how to display the data

--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/StableRow/InfoBoxOverlay.tsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/StableRow/InfoBoxOverlay.tsx
@@ -4,12 +4,17 @@ import {
     IconButton,
     Label,
     OverflowText,
+    PropertyName,
+    PropertyValue,
+    PropertyValueList,
+    PropertyValuePair,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableRow,
     WhiteSpaceContainer,
+    WhiteSpaceContainerProps,
 } from "@eccenca/gui-elements";
 import { SuggestionListContext } from "../../SuggestionContainer";
 import { TestableComponent } from "@eccenca/gui-elements/src/components/interfaces";
@@ -21,69 +26,107 @@ interface IDataStackItem {
 
 interface IDataStack extends TestableComponent {
     data: IDataStackItem[];
-    /** If true the content will be returned directly instead being shown as context overlay. */
-    embed?: boolean
 }
 
-export function InfoBoxOverlay({data, embed = false,...otherProps}: IDataStack) {
+export interface InfoBoxOverlayDisplayProps {
+    /** If true the content will be returned directly instead being shown as context overlay. */
+    embed?: boolean;
+    /** how it is displayed */
+    dislayType?: "table" | "propertylist";
+    /** forwading or overwriting container properties */
+    whiteSpaceContainerProps?: Omit<WhiteSpaceContainerProps, "children">;
+}
+
+export function InfoBoxOverlay({
+    data,
+    embed = false,
+    dislayType = "table",
+    whiteSpaceContainerProps,
+    ...otherProps
+}: IDataStack & InfoBoxOverlayDisplayProps) {
     const { portalContainer } = useContext(SuggestionListContext);
-    const dataTestId = (suffix: string) => otherProps["data-test-id"] ? otherProps["data-test-id"] + suffix : undefined
-    const Content = () => <WhiteSpaceContainer
-        style={{
-            width: "36rem",
-            maxWidth: "90vw",
-            maxHeight: "40vh",
-            overflow: "auto",
-        }}
-        paddingTop="small"
-        paddingRight="small"
-        paddingBottom="small"
-        paddingLeft="small"
-        data-test-id={dataTestId("-overlay")}
-    >
-        <TableContainer>
-            <Table size="small">
-                <colgroup>
-                    <col
-                        style={{
-                            width: "4rem",
-                        }}
-                    />
-                    <col
-                        style={{
-                            width: "20rem",
-                        }}
-                    />
-                </colgroup>
-                <TableBody>
+    const dataTestId = (suffix: string) =>
+        otherProps["data-test-id"] ? otherProps["data-test-id"] + suffix : undefined;
+    const Content = () => {
+        return (
+            <WhiteSpaceContainer
+                style={{
+                    width: "36rem",
+                    maxWidth: "90vw",
+                    maxHeight: "40vh",
+                    overflow: "auto",
+                }}
+                paddingTop="small"
+                paddingRight="small"
+                paddingBottom="small"
+                paddingLeft="small"
+                {...whiteSpaceContainerProps}
+                data-test-id={dataTestId("-overlay")}
+            >
+                {dislayType === "table" ? (
+                    <TableContainer>
+                        <Table size="small">
+                            <colgroup>
+                                <col
+                                    style={{
+                                        width: "4rem",
+                                    }}
+                                />
+                                <col
+                                    style={{
+                                        width: "20rem",
+                                    }}
+                                />
+                            </colgroup>
+                            <TableBody>
                                 {data.map((item, idx) =>
                                     item.value ? (
                                         <TableRow
                                             key={item.key ?? idx}
                                             data-test-id={`info-box-row-${item.key ?? idx}`}
                                         >
-                                    <TableCell key={"label"}>
-                                        <OverflowText passDown={true}>
+                                            <TableCell key={"label"}>
+                                                <OverflowText passDown={true}>
                                                     <Label text={item.key} isLayoutForElement="span" />
-                                        </OverflowText>
-                                    </TableCell>
-                                    <TableCell key={"description"} data-test-id={"info-box-row-values"}>
-                                        {item.value}
-                                    </TableCell>
+                                                </OverflowText>
+                                            </TableCell>
+                                            <TableCell key={"description"} data-test-id={"info-box-row-values"}>
+                                                {item.value}
+                                            </TableCell>
                                         </TableRow>
                                     ) : null
                                 )}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    </WhiteSpaceContainer>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                ) : (
+                    <PropertyValueList singleColumn>
+                        {data.map((item, idx) =>
+                            item.value ? (
+                                <PropertyValuePair
+                                    key={item.key ?? idx}
+                                    data-test-id={`info-box-row-${item.key ?? idx}`}
+                                >
+                                    <PropertyName nowrap>{item.key}</PropertyName>
+                                    <PropertyValue>{item.value}</PropertyValue>
+                                </PropertyValuePair>
+                            ) : null
+                        )}
+                    </PropertyValueList>
+                )}
+            </WhiteSpaceContainer>
+        );
+    };
 
-    return embed?
-        <Content/> :
-        <ContextOverlay
-            portalContainer={portalContainer}
-            content={<Content/>}
-        >
-            <IconButton name="item-info" text="Show more info" data-test-id={dataTestId("-btn")}/>
+    if (data.length === 0) {
+        return null;
+    }
+
+    return embed ? (
+        <Content />
+    ) : (
+        <ContextOverlay portalContainer={portalContainer} content={<Content />}>
+            <IconButton name="item-info" text="Show more info" data-test-id={dataTestId("-btn")} />
         </ContextOverlay>
+    );
 }
--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/StableRow/SourcePathInfoBox.tsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/containers/SuggestionNew/SuggestionTable/StableRow/SourcePathInfoBox.tsx
@@ -1,101 +1,111 @@
 import React, { useContext } from "react";
 import { HtmlContentBlock } from "@eccenca/gui-elements";
-import {ITargetWithSelected, SuggestionTypeValues} from "../../suggestion.typings";
+import { ITargetWithSelected, SuggestionTypeValues } from "../../suggestion.typings";
 import { SuggestionListContext } from "../../SuggestionContainer";
-import { InfoBoxOverlay } from "./InfoBoxOverlay";
-import {useTranslation} from "react-i18next";
+import { InfoBoxOverlay, InfoBoxOverlayDisplayProps } from "./InfoBoxOverlay";
+import { useTranslation } from "react-i18next";
 
 interface IProps {
     source?: string | ITargetWithSelected[];
-    pathType?: SuggestionTypeValues
+    pathType?: SuggestionTypeValues;
     objectInfo?: {
-        dataTypeSubPaths: string[]
-        objectSubPaths: string[]
-    }
-    embed?: boolean
-    exampleValues?: string[]
+        dataTypeSubPaths: string[];
+        objectSubPaths: string[];
+    };
+    exampleValues?: string[];
+    infoBoxOverlayProps?: InfoBoxOverlayDisplayProps;
 }
 
 /** Shows additional information for a dataset source path, e.g. examples values. */
-export function SourcePathInfoBox({source, pathType, objectInfo, embed, exampleValues}: IProps) {
+export function SourcePathInfoBox({ source, pathType, objectInfo, infoBoxOverlayProps, exampleValues }: IProps) {
     const context = useContext(SuggestionListContext);
-    const {exampleValues: contextExampleValues} = context;
-    const [t] = useTranslation()
+    const { exampleValues: contextExampleValues } = context;
+    const [t] = useTranslation();
 
     let examples: string[] | undefined = [];
-    let sourcePath: string = ""
-    if(exampleValues) {
-        examples = exampleValues
-    } else if (typeof source === 'string') {
-        sourcePath = source
+    let sourcePath: string = "";
+    if (exampleValues) {
+        examples = exampleValues;
+    } else if (typeof source === "string") {
+        sourcePath = source;
         examples = contextExampleValues[source as string];
     } else if (Array.isArray(source)) {
         // There is always one item selected from the target list
-        const selected = source.find(t => t._selected) as ITargetWithSelected;
-        sourcePath = selected.uri
+        const selected = source.find((t) => t._selected) as ITargetWithSelected;
+        sourcePath = selected.uri;
         if (selected && contextExampleValues[selected.uri]) {
-            examples = contextExampleValues[selected.uri]
+            examples = contextExampleValues[selected.uri];
         }
     }
 
-    const simpleStringValue = (str: string) => (<span style={{ wordBreak: "break-all" }}>{ str }</span>)
+    const simpleStringValue = (str: string) => (str ? <span style={{ wordBreak: "break-all" }}>{str}</span> : null);
 
     const infoBoxProperties = [
         {
             key: t("MappingSuggestion.SourceElement.infobox.sourcePath"),
-            value: simpleStringValue(sourcePath)
-        }
-    ]
+            value: simpleStringValue(sourcePath),
+        },
+    ];
 
-    if(examples && examples.length > 0) {
+    if (examples && examples.length > 0) {
         infoBoxProperties.push({
             key: t("MappingSuggestion.SourceElement.infobox.exampleData"),
-            value: <code>
-                <InfoList items={Array.from(new Set(examples)).sort().slice(0, 9)} />
-            </code>,
-        })
+            value: (
+                <code>
+                    <InfoList items={Array.from(new Set(examples)).sort().slice(0, 9)} />
+                </code>
+            ),
+        });
     }
 
-    if(pathType) {
+    if (pathType) {
         infoBoxProperties.push({
             key: "Path type",
-            value: simpleStringValue(pathType === "object" ?
-                t("MappingSuggestion.SourceElement.infobox.objectPath") :
-                t("MappingSuggestion.SourceElement.infobox.valuePath"))
-        })
+            value: simpleStringValue(
+                pathType === "object"
+                    ? t("MappingSuggestion.SourceElement.infobox.objectPath")
+                    : t("MappingSuggestion.SourceElement.infobox.valuePath")
+            ),
+        });
     }
 
-    if(objectInfo) {
+    if (objectInfo) {
         infoBoxProperties.push({
-            key: `${t("MappingSuggestion.SourceElement.infobox.dataTypeSubPaths")} (${objectInfo.dataTypeSubPaths.length})`,
-            value: <div>
-                <InfoList items={objectInfo.dataTypeSubPaths} />
-            </div>
-        })
+            key: `${t("MappingSuggestion.SourceElement.infobox.dataTypeSubPaths")} (${
+                objectInfo.dataTypeSubPaths.length
+            })`,
+            value: (
+                <div>
+                    <InfoList items={objectInfo.dataTypeSubPaths} />
+                </div>
+            ),
+        });
         infoBoxProperties.push({
-            key: `${t("MappingSuggestion.SourceElement.infobox.objectTypeSubPaths")} (${objectInfo.objectSubPaths.length})`,
-            value: <div>
-                <InfoList items={objectInfo.objectSubPaths} />
-            </div>
-        })
+            key: `${t("MappingSuggestion.SourceElement.infobox.objectTypeSubPaths")} (${
+                objectInfo.objectSubPaths.length
+            })`,
+            value: (
+                <div>
+                    <InfoList items={objectInfo.objectSubPaths} />
+                </div>
+            ),
+        });
     }
 
-    return <InfoBoxOverlay
-        data={infoBoxProperties}
-        data-test-id={"source-path-infobox"}
-        embed={embed}
-    />;
+    return <InfoBoxOverlay data={infoBoxProperties} data-test-id={"source-path-infobox"} {...infoBoxOverlayProps} />;
 }
 
 interface InfoListProps {
-    items: string[]
+    items: string[];
 }
-const InfoList = ({items}: InfoListProps) => {
-    return <HtmlContentBlock>
-        <ul>
-            {items.map((item) => {
-                return <li key={item}>{item}</li>;
-            })}
-        </ul>
-    </HtmlContentBlock>
-}
+const InfoList = ({ items }: InfoListProps) => {
+    return items.length > 0 ? (
+        <HtmlContentBlock>
+            <ul>
+                {items.map((item) => {
+                    return <li key={item}>{item}</li>;
+                })}
+            </ul>
+        </HtmlContentBlock>
+    ) : null;
+};
