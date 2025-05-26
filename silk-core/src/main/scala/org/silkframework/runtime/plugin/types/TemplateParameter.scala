d941a4ba0e420dd7233dca4cb873eca03d33d7cc
Robert Isele <mail@robertisele.com>
2025-05-26 18:24:10 +0200

    TemplateParameter: Add implicit conversion from string and remove superfluous conversions.

--- silk-core/src/main/scala/org/silkframework/runtime/plugin/types/TemplateParameter.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/plugin/types/TemplateParameter.scala
@@ -1,3 +1,10 @@
 package org.silkframework.runtime.plugin.types
 
+import scala.language.implicitConversions
+
 case class TemplateParameter(templateStr: String)
+
+object TemplateParameter {
+  implicit def str2parameter(str: String): TemplateParameter = TemplateParameter(str)
+  implicit def templateParameter2str(templateParameter: TemplateParameter): String = templateParameter.templateStr
+}
