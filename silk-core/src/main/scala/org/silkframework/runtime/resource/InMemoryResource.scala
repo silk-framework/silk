ca98c8a5894a631d9731196796f5907fee4919ca
Robert Isele <mail@robertisele.com>
2025-06-23 14:26:14 +0200

    Add InMemoryResource class.

---
+++ silk-core/src/main/scala/org/silkframework/runtime/resource/InMemoryResource.scala
@@ -0,0 +1,35 @@
+package org.silkframework.runtime.resource
+
+import java.io.{ByteArrayInputStream, InputStream}
+import java.time.Instant
+
+class InMemoryResource(override val name: String, override val path: String, bytes: Array[Byte]) extends Resource {
+
+  /**
+   * In-memory resources are always considered to exist.
+   */
+  override def exists: Boolean = true
+
+  /**
+   * Returns the size of this resource in bytes.
+   */
+  override def size: Option[Long] = {
+    Some(bytes.length)
+  }
+
+  /**
+   * The time that the resource was last modified.
+   * Returns None if the time is not known.
+   */
+  override def modificationTime: Option[Instant] = None
+
+  /**
+   * Creates an input stream for reading the resource.
+   *
+   * @return An input stream for reading the resource.
+   *         The caller is responsible for closing the stream after reading.
+   */
+  override def inputStream: InputStream = {
+    new ByteArrayInputStream(bytes)
+  }
+}
