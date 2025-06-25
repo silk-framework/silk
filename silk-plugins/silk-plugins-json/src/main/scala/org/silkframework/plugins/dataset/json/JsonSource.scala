a6fb44b6b8555781240ef0697c3419270e8058bf
Robert Isele <mail@robertisele.com>
2025-06-25 15:35:17 +0200

    Add navigateIntoArrays to "Parse JSON" operator as well

--- silk-plugins/silk-plugins-json/src/main/scala/org/silkframework/plugins/dataset/json/JsonSource.scala
+++ silk-plugins/silk-plugins-json/src/main/scala/org/silkframework/plugins/dataset/json/JsonSource.scala
@@ -112,7 +112,7 @@ abstract class JsonSource(taskId: Identifier, protected val basePath: String, pr
 
 object JsonSource {
 
-  def apply(file: Resource, basePath: String, uriPattern: String, navigateIntoArrays: Boolean): JsonSource = {
+  def apply(file: Resource, basePath: String, uriPattern: String, navigateIntoArrays: Boolean = false): JsonSource = {
     new JsonSourceStreaming(Identifier.fromAllowed(file.name), file, basePath, uriPattern, navigateIntoArrays)
   }
 
