8d127bc4f0e99a68e33d57f01b998ea9a98667e5
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-07-01 11:40:06 +0200

    Allow the processing of non-printable characters in the CsvWriter of uniVocity-parsers.

--- silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvWriter.scala
+++ silk-plugins/silk-plugins-csv/src/main/scala/org/silkframework/plugins/dataset/csv/CsvWriter.scala
@@ -46,6 +46,10 @@ class CsvWriter(resource: WritableResource, properties: Seq[TypedProperty], sett
     */
   private def createCsvWriter() = {
     val writerSettings = new CsvWriterSettings
+
+    // Allow the processing of non-printable characters.
+    writerSettings.trimValues(false)
+
     writerSettings.getFormat.setLineSeparator("\n")
     if(properties.nonEmpty) {
       writerSettings.setHeaders(properties.map(_.propertyUri.uri): _*)
