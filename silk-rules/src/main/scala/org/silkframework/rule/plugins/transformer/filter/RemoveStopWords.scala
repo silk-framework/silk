b43abff7c0b241115fd016e38d456aa7009e5cca
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-07-28 16:27:36 +0200

    Extend the stopWords (flexible) with a default list of stop words, read from a resource file (plain text).

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/filter/RemoveStopWords.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/filter/RemoveStopWords.scala
@@ -2,6 +2,7 @@ package org.silkframework.rule.plugins.transformer.filter
 
 import org.silkframework.rule.input.SimpleTransformer
 
+import scala.io.{BufferedSource, Source}
 import scala.util.matching.Regex
 
 /**
@@ -10,13 +11,27 @@ import scala.util.matching.Regex
  * @param separator Regular Expressions for the separator between single words.
  */
 abstract class RemoveStopWords(separator: String = "[\\s-]+") extends SimpleTransformer {
+  private val defaultStopWords: Set[String] = {
+    def loadStopWords: Set[String] = {
+      val txt = Source.fromResource("stopWords.txt")
+      try {
+        txt.getLines.toSet
+      } finally {
+        txt.close()
+      }
+    }
+    loadStopWords
+  }
+
   def stopWords: Set[String]
+
+  private def allStopWords: Set[String] = defaultStopWords ++ stopWords
 
   private val regex: Regex = separator.r
 
   override def evaluate(value: String): String = {
     val result = new StringBuilder
-    for(word <- regex.split(value) if !stopWords.contains(word)) {
+    for(word <- regex.split(value) if !allStopWords.contains(word)) {
       result.append(word)
       result.append(" ")
     }
