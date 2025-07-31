e4b1b2ce3e194170493fc97ec67085dc2f740297
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-07-31 13:55:52 +0200

    Review: Default stop words as a parameter.

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/filter/RemoveStopWords.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/filter/RemoveStopWords.scala
@@ -9,20 +9,11 @@ import scala.util.matching.Regex
  * Transformer which removes the stop words provided by its implementations, additionally to the default stop word list.
  *
  * @param separator Regular Expressions for the separator between single words.
+ * @param defaultStopWords List of default stop words. If not set, a sensible default (= base) stop word list is used.
  */
-abstract class RemoveStopWords(separator: String = "[\\s-]+") extends SimpleTransformer {
-  private val defaultStopWords: Set[String] = {
-    def loadDefaultStopWords: Set[String] = {
-      val txt = Source.fromResource("org/silkframework/rule/plugins/transformer/filter/stopWords.txt")
-      try {
-        txt.getLines.toSet
-      } finally {
-        txt.close()
-      }
-    }
-    loadDefaultStopWords
-  }
-
+abstract class RemoveStopWords(separator: String = "[\\s-]+",
+                               defaultStopWords: Set[String] = RemoveStopWords.loadDefaultStopWords
+                              ) extends SimpleTransformer {
   def stopWords: Set[String]
 
   private def allStopWords: Set[String] = defaultStopWords ++ stopWords
@@ -36,5 +27,16 @@ abstract class RemoveStopWords(separator: String = "[\\s-]+") extends SimpleTran
       result.append(" ")
     }
     result.toString()
+  }
+}
+
+object RemoveStopWords {
+  private def loadDefaultStopWords: Set[String] = {
+    val txt = Source.fromResource("org/silkframework/rule/plugins/transformer/filter/stopWords.txt")
+    try {
+      txt.getLines.toSet
+    } finally {
+      txt.close()
+    }
   }
 }
