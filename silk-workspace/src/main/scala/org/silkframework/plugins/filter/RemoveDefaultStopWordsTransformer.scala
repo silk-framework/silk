faa4711b062b855ba08101d5c173819e7ba10917
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-08-08 15:51:11 +0200

    Add TransformExamples to RemoveDefaultStopWordsTransformer.

--- silk-workspace/src/main/scala/org/silkframework/plugins/filter/RemoveDefaultStopWordsTransformer.scala
+++ silk-workspace/src/main/scala/org/silkframework/plugins/filter/RemoveDefaultStopWordsTransformer.scala
@@ -1,5 +1,6 @@
 package org.silkframework.plugins.filter
 
+import org.silkframework.rule.annotations.{TransformExample, TransformExamples}
 import org.silkframework.rule.plugins.transformer.filter.RemoveStopWords
 import org.silkframework.runtime.plugin.annotations.Plugin
 
@@ -13,4 +14,14 @@ import org.silkframework.runtime.plugin.annotations.Plugin
     " Should you want to provide an own stop word list, either as a resource (e.g. a file) or a remote URL," +
     " see the filters 'removeStopWords' and 'removeRemoteStopWords'."
 )
+@TransformExamples(Array(
+  new TransformExample(
+    input1 = Array("To be or not to be", "that is the question"),
+    output = Array("To", "question")
+  ),
+  new TransformExample(
+    input1 = Array("It always seems impossible", "until it's done"),
+    output = Array("It impossible", "")
+  )
+))
 case class RemoveDefaultStopWordsTransformer() extends RemoveStopWords()
