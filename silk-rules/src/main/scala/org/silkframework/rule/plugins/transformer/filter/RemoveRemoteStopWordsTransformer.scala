a1548d0ae86dda9709432eefbd35ee78ccc78485
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-08-08 11:40:04 +0200

    Review change: Improve the documentation of RemoveStopWordsTransformer and RemoveRemoteStopWordsTransformer.

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/filter/RemoveRemoteStopWordsTransformer.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/filter/RemoveRemoteStopWordsTransformer.scala
@@ -5,10 +5,20 @@ import org.silkframework.runtime.plugin.annotations.Plugin
 import scala.io.Source
 
 @Plugin(
-  id = "removeRemoteStopwords",
+  id = "removeRemoteStopWords",
   categories = Array("Filter"),
-  label = "Remove stopwords (remote stopword list)",
-  description = "Removes stopwords from all values. The stopword list is retrieved via a http connection (e.g. https://sites.google.com/site/kevinbouge/stopwords-lists/stopwords_de.txt). Each line in the stopword list contains a stopword. The separator defines a regex that is used for detecting words."
+  label = "Remove stop words (remote stop word list)",
+  description =
+    "Removes stop words from all values." +
+      " The stop word list is retrieved via a HTTP connection" +
+      " (e.g. https://raw.githubusercontent.com/stopwords-iso/stopwords-de/refs/heads/master/stopwords-de.txt)." +
+      " Such an overridable stop word list file is useful, for instance, for specifying the stop words of a different" +
+      " language, such as German instead of the" +
+      " [default](https://gist.githubusercontent.com/rg089/35e00abf8941d72d419224cfd5b5925d/raw/12d899b70156fd0041fa9778d657330b024b959c/stopwords.txt)" +
+      " for the English language." +
+      " Each line in the stop word list should contain a single stop word." +
+      " The separator defines a regular expression (regex) that is used for detecting words." +
+      " By default, the separator is a regular expression for non-whitespace characters."
 )
 case class RemoveRemoteStopWordsTransformer(stopWordListUrl: String = RemoveRemoteStopWordsTransformer.defaultStopWordListUrl,
                                             separator: String = "[\\s-]+")
