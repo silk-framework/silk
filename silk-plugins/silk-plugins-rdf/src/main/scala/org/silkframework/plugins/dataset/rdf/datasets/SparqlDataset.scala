dfb6fc127f62f8ee43f5bdf9f57f84b70997c4dd
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-08-21 14:29:50 +0200

    Improved description of the parameters of the SPARQL endpoint.

--- silk-plugins/silk-plugins-rdf/src/main/scala/org/silkframework/plugins/dataset/rdf/datasets/SparqlDataset.scala
+++ silk-plugins/silk-plugins-rdf/src/main/scala/org/silkframework/plugins/dataset/rdf/datasets/SparqlDataset.scala
@@ -14,7 +14,7 @@ import org.silkframework.runtime.plugin.types.{MultilineStringParameter, Passwor
   categories = Array(DatasetCategories.remote),
   description = "Connects to an existing SPARQL endpoint.")
 case class SparqlDataset(
-  @Param(label = "Endpoint URI", value = "The URI of the SPARQL endpoint, e.g., http://dbpedia.org/sparql")
+  @Param(label = "Endpoint URI", value = "The URI of the SPARQL endpoint, e.g. `http://dbpedia.org/sparql`")
   endpointURI: String,
   @Param("Login required for authentication")
   login: String = null,
@@ -22,29 +22,37 @@ case class SparqlDataset(
   password: PasswordParameter = PasswordParameter.empty,
   @Param("The URI of a named graph. If set, the SPARQL endpoint will only retrieve entities from that specific graph.")
   graph: String = null,
-  @Param(value = "The number of entities to be retrieved per SPARQL query. This is the page size while paging.", advanced = true)
+  @Param(value = "The number of entities to be retrieved per SPARQL query. This is the page size used when paging.", advanced = true)
   pageSize: Int = 1000,
-  @Param(value = "A list of entities to be retrieved. If not given, all entities will be retrieved. Multiple entities are separated by whitespace.", advanced = true)
+  @Param(value = "An optional list of entities to be retrieved. If not specified, all entities will be retrieved." +
+    " Multiple entities need to be separated by whitespace.", advanced = true)
   entityList: MultilineStringParameter = MultilineStringParameter(""),
   @Param(value = "The number of milliseconds to wait between subsequent queries", advanced = true)
   pauseTime: Int = 0,
-  @Param(value = "The number of retries if a query fails", advanced = true)
+  @Param(value = "The total number of retries to execute a (repeatedly) failing query", advanced = true)
   retryCount: Int = 3,
-  @Param(value = "The number of milliseconds to wait until a failed query is retried.", advanced = true)
+  @Param(value = "The number of milliseconds to wait until a previously failed query is executed again", advanced = true)
   retryPause: Int = 1000,
-  @Param(value = "Additional parameters to be appended to every request e.g. &soft-limit=1", advanced = true)
+  @Param(value = "Additional parameters to be appended to every query, e.g. `&soft-limit=1`", advanced = true)
   queryParameters: String = "",
-  @Param("The strategy use for retrieving entities: simple: Retrieve all entities using a single query; subQuery: Use a single query, but wrap it for improving the performance on Virtuoso; parallel: Use a separate Query for each entity property.")
+  @Param("The strategy for retrieving entities. There are three options:" +
+    " `simple` retrieves all entities using a single query;" +
+    " `subQuery` also uses a single query, which is optimized for Virtuoso;" +
+    " `parallel` executes multiple queries in parallel, one for each entity property.")
   strategy: EntityRetrieverStrategy = EntityRetrieverStrategy.parallel,
-  @Param("Include useOrderBy in queries to enforce correct order of values.")
+  @Param("Enforces the correct ordering of values, if set to `true` (default).")
   useOrderBy: Boolean = true,
   @Param(label = "Clear graph before workflow execution",
-    value = "If set to true this will clear the specified graph before executing a workflow that writes to it.")
+    value = "If set to `true`, this will clear the specified graph before executing a workflow that writes into it.")
   clearGraphBeforeExecution: Boolean = false,
   @Param(
     label = "SPARQL query timeout (ms)",
-    value = "SPARQL query timeout (select/update) in milliseconds. A value of zero means that the timeout configured via " +
-        "property is used (e.g. configured via silk.remoteSparqlEndpoint.defaults.read.timeout.ms). To overwrite the configured value specify a value greater than zero.")
+    value = "SPARQL query timeout in milliseconds." +
+      " By default, a value of zero is used." +
+      " This zero value has a symbolic character: it means that the timeout of SPARQL select and update queries is" +
+      " configured via the properties `silk.remoteSparqlEndpoint.defaults.connection.timeout.ms and" +
+      " `silk.remoteSparqlEndpoint.defaults.read.timeout.ms` for the default connection and read timeouts." +
+      " To overwrite these configured values, specify a (common) timeout greater than zero.")
   sparqlTimeout: Int = 0) extends RdfDataset with TripleSinkDataset {
 
   private val params =
