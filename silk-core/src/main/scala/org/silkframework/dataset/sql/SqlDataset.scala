12e3eef72bde09dd37928da7afbf4b6362b6afe7
Robert Isele <mail@robertisele.com>
2025-09-12 10:59:36 +0200

    Update naming of SQL datasets

--- silk-core/src/main/scala/org/silkframework/dataset/sql/SqlDataset.scala
+++ silk-core/src/main/scala/org/silkframework/dataset/sql/SqlDataset.scala
@@ -2,6 +2,9 @@ package org.silkframework.dataset.sql
 
 import org.silkframework.dataset.Dataset
 
+/**
+ * A dataset that provides access to a SQL endpoint.
+ */
 trait SqlDataset extends Dataset {
   def sqlEndpoint: SqlEndpoint
 }
