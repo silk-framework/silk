ed71f68db173df9dda2743aacb99ac8f10cf455a
Eduard Fugarolas <eduard.fugarolas@eccenca.com>
2025-09-29 14:34:24 +0200

    Correct recurring grammatical error.

--- silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XSLTOperator.scala
+++ silk-plugins/silk-plugins-xml/src/main/scala/org/silkframework/plugins/dataset/xml/XSLTOperator.scala
@@ -12,19 +12,19 @@ import org.silkframework.workspace.resources.ResourceAutoCompletionProvider
   description =
       """A task that converts an XML resource via an XSLT script and writes the transformed output into a file resource."""
 )
-case class XSLTOperator(@Param(value = "The XSLT file to be used for transforming XML.",
-                               autoCompletionProvider = classOf[ResourceAutoCompletionProvider], allowOnlyAutoCompletedValues = true)
-                        file: Resource) extends CustomTask {
+case class XSLTOperator(
+  @Param(
+    value = "The XSLT file to be used for transforming XML.",
+    autoCompletionProvider = classOf[ResourceAutoCompletionProvider],
+    allowOnlyAutoCompletedValues = true
+  ) file: Resource
+) extends CustomTask {
 
-  override def inputPorts: InputPorts = {
-    FixedNumberOfInputs(Seq(FixedSchemaPort(FileEntitySchema.schema)))
-  }
+  override def inputPorts: InputPorts = FixedNumberOfInputs(Seq(FixedSchemaPort(FileEntitySchema.schema)))
 
   /** Outputs the resulting XML file as a [[org.silkframework.runtime.resource.Resource]] which overwrites the
     * target dataset's resource. */
-  override lazy val outputPort: Option[Port] = {
-    Some(FixedSchemaPort(FileEntitySchema.schema))
-  }
+  override lazy val outputPort: Option[Port] = Some(FixedSchemaPort(FileEntitySchema.schema))
 
   override def referencedResources: Seq[Resource] = Seq(file)
 
