f11a470e760a5108e2391d34e056303d98832d6b
Robert Isele <mail@robertisele.com>
2025-12-08 13:05:44 +0100

    Added glue parameter to zip transformer

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/combine/ZipTransformer.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/combine/ZipTransformer.scala
@@ -38,15 +38,35 @@ import org.silkframework.runtime.plugin.annotations.{Param, Plugin}
     input2 = Array("1", "2"),
     output = Array("a1", "b2", "c-")
   ),
+  new TransformExample(
+    description = "Zipping two inputs using a glue string.",
+    parameters = Array("glue", "-"),
+    input1 = Array("a", "b"),
+    input2 = Array("1", "2"),
+    output = Array("a-1", "b-2")
+  ),
+  new TransformExample(
+    description = "Zipping two inputs using a glue string with escape characters.",
+    parameters = Array("glue", """\n"""),
+    input1 = Array("a", "b"),
+    input2 = Array("1", "2"),
+    output = Array("a\n1", "b\n2")
+  )
 ))
 case class ZipTransformer(@Param(value = "Placeholder to be used if the first input provides fewer values than the second one.")
                           firstPlaceholder: String = "",
                           @Param(value = "Placeholder to be used if the second input provides fewer values than the first one.")
-                          secondPlaceholder: String = "") extends Transformer {
+                          secondPlaceholder: String = "",
+                          @Param(value = ConcatTransformer.glueDescription)
+                          glue: String = "") extends Transformer {
+
+  // glue with escaped char sequences (\\, \n, \t) converted to actual character.
+  lazy val parsedGlue: String = ConcatTransformer.parseGlue(glue)
+
   override def apply(values: Seq[Seq[String]]): Seq[String] = {
     require(values.size == 2, "Zip transformer requires exactly two inputs.")
     val firstValues = values.head
     val secondValues = values(1)
-    firstValues.zipAll(secondValues, firstPlaceholder, secondPlaceholder) map { case (v1, v2) => v1 + v2 }
+    firstValues.zipAll(secondValues, firstPlaceholder, secondPlaceholder) map { case (v1, v2) => v1 + parsedGlue + v2 }
   }
 }
