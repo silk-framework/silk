f67ef82cf6dc36f1a094efaa4234de65968f9d61
Michael Haschke <michael.haschke@eccenca.com>
2026-01-28 16:33:49 +0100

    replace deprecated Icon component

--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/components/ThingIcon.jsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/components/ThingIcon.jsx
@@ -1,29 +1,32 @@
-import { MAPPING_RULE_TYPE_COMPLEX, MAPPING_RULE_TYPE_DIRECT, MAPPING_RULE_TYPE_OBJECT } from '../utils/constants';
-import { Icon } from 'gui-elements-deprecated';
-import React from 'react';
+import { MAPPING_RULE_TYPE_COMPLEX, MAPPING_RULE_TYPE_DIRECT, MAPPING_RULE_TYPE_OBJECT } from "../utils/constants";
+import { Icon, Spacing } from "@eccenca/gui-elements";
+import React from "react";
 
-export const ThingIcon = ({type, status, message}) => {
-    let iconName = 'help_outline';
-    let tooltip = '';
+export const ThingIcon = ({ type, status, message }) => {
+    let iconName = "item-question";
+    let tooltip = "";
     switch (type) {
         case MAPPING_RULE_TYPE_DIRECT:
         case MAPPING_RULE_TYPE_COMPLEX:
-            tooltip = 'Value mapping';
-            iconName = 'insert_drive_file';
+            tooltip = "Value mapping";
+            iconName = "artefact-file";
             break;
         case MAPPING_RULE_TYPE_OBJECT:
-            tooltip = 'Object mapping';
-            iconName = 'folder';
+            tooltip = "Object mapping";
+            iconName = "artefact-project";
             break;
         default:
-            iconName = 'help_outline';
+            iconName = "item-question";
     }
-    
+
     return (
-        <Icon
-            className="ecc-silk-mapping__ruleitem-icon"
-            name={status === 'error' ? 'warning' : iconName}
-            tooltip={status === 'error' ? `${tooltip} (${message})` : tooltip}
-        />
+        <>
+            <Icon
+                className="ecc-silk-mapping__ruleitem-icon"
+                name={status === "error" ? "warning" : iconName}
+                tooltip={status === "error" ? `${tooltip} (${message})` : tooltip}
+            />
+            <Spacing vertical size={"tiny"} />
+        </>
     );
 };
