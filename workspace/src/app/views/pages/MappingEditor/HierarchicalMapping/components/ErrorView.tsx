b3fbae8f40264eefbdeca5882059064e524c15a8
Michael Haschke <michael.haschke@eccenca.com>
2026-01-28 16:09:33 +0100

    replace deprecated alerts by Notification component

--- workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/components/ErrorView.tsx
+++ workspace/src/app/views/pages/MappingEditor/HierarchicalMapping/components/ErrorView.tsx
@@ -1,13 +1,12 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import { Error } from 'gui-elements-deprecated';
-import _ from 'lodash';
-import {Spacing} from "@eccenca/gui-elements";
+import _ from "lodash";
+import { Spacing, Notification, IconButton, HtmlContentBlock } from "@eccenca/gui-elements";
 
 export const ErrorCause = ({ errorCause }) => (
-    <ul className="ecc-hierarchical-mapping-error-list">
+    <ul>
         {_.map(errorCause, ({ title, detail, cause }) => {
-            let renderedCause: React.ReactElement | undefined = undefined
+            let renderedCause: React.ReactElement | undefined = undefined;
 
             if (_.isArray(cause)) {
                 renderedCause = <ErrorCause errorCause={cause} />;
@@ -15,8 +14,12 @@ export const ErrorCause = ({ errorCause }) => (
 
             return (
                 <li key={title}>
-                    <p>{title}</p>
-                    <p>{detail}</p>
+                    <HtmlContentBlock>
+                        <p>
+                            <strong>{title}</strong>
+                        </p>
+                        <p>{detail}</p>
+                    </HtmlContentBlock>
                     {renderedCause}
                 </li>
             );
@@ -25,10 +28,12 @@ export const ErrorCause = ({ errorCause }) => (
 );
 
 export const ErrorIssue = ({ errorCause }) => (
-    <ul className="ecc-hierarchical-mapping-error-list">
+    <ul>
         {_.map(errorCause, ({ message }) => (
             <li>
-                <p>{message}</p>
+                <HtmlContentBlock>
+                    <p>{message}</p>
+                </HtmlContentBlock>
             </li>
         ))}
     </ul>
@@ -36,73 +41,75 @@ export const ErrorIssue = ({ errorCause }) => (
 
 interface IProps {
     // Error title
-    title: string
+    title: string;
     // Error detail
-    detail: string
+    detail: string;
     // it may contain a list for errors with title and detail itself
-    cause?: any[]
+    cause?: any[];
     // it may contain a list for errors with title and detail itself, too
-    issues?: any[]
+    issues?: any[];
     // True if the problem is a HTTP request related problem
-    isHTTPProblem?: boolean
+    isHTTPProblem?: boolean;
     // The status code of the HTTP request if this is a HTTP error
-    status?: number
+    status?: number;
     // An title prefix
-    titlePrefix?: string
+    titlePrefix?: string;
 }
 
 /** A component to show error with expandable details. */
-export function ErrorView({title, detail, cause, issues, isHTTPProblem, status, titlePrefix = ""}: IProps) {
-    const [errorExpanded, setErrorExpanded] = useState<boolean>(false)
+export function ErrorView({ title, detail, cause, issues, isHTTPProblem, status, titlePrefix = "" }: IProps) {
+    const [errorExpanded, setErrorExpanded] = useState<boolean>(false);
 
     const toggleExpansion = () => {
-        setErrorExpanded(prev => !prev)
-    }
+        setErrorExpanded((prev) => !prev);
+    };
 
-    let shownTitle = titlePrefix + title
+    let shownTitle = titlePrefix + title;
     if (isHTTPProblem && !status) {
-        shownTitle = "There has been a connection problem."
+        shownTitle = "There has been a connection problem.";
     }
-    const errorClassName = errorExpanded
-        ? ''
-        : 'mdl-alert--narrowed';
 
-    let causesHtml: React.ReactElement | undefined = undefined
-    let issuesHtml: React.ReactElement | undefined = undefined
+    let causesHtml: React.ReactElement | undefined = undefined;
+    let issuesHtml: React.ReactElement | undefined = undefined;
 
     if (errorExpanded && cause) {
-        if(_.isArray(cause)) {
-            causesHtml = <ErrorCause errorCause={cause}/>;
-        } else if(_.isObject(cause)) {
-            causesHtml = <ErrorCause errorCause={[cause]}/>
+        if (_.isArray(cause)) {
+            causesHtml = <ErrorCause errorCause={cause} />;
+        } else if (_.isObject(cause)) {
+            causesHtml = <ErrorCause errorCause={[cause]} />;
         }
     }
 
     if (errorExpanded && _.isArray(issues)) {
-        issuesHtml = <ErrorIssue errorCause={issues}/>;
+        issuesHtml = <ErrorIssue errorCause={issues} />;
     }
 
-    const detailHtml = title !== detail ?
-        <><Spacing size="small" /><p>{detail}</p></>
-        : undefined
+    const detailHtml =
+        title !== detail ? (
+            <>
+                <Spacing size="small" />
+                <p>{detail}</p>
+            </>
+        ) : undefined;
 
-    return <Error
-        border
-        className={errorClassName}
-        handlerDismiss={toggleExpansion}
-        labelDismiss={
-            errorExpanded ? 'Show less' : 'Show more'
-        }
-        iconDismiss={
-            errorExpanded ? 'expand_less' : 'expand_more'
-        }
-    >
-        <strong>{shownTitle}</strong>
-        {detailHtml}
-        {causesHtml}
-        {issuesHtml}
-    </Error>
-
+    return (
+        <Notification
+            intent={"danger"}
+            actions={
+                <IconButton
+                    name={errorExpanded ? "toggler-showless" : "toggler-showmore"}
+                    text={errorExpanded ? "Show less" : "Show more"}
+                    onClick={toggleExpansion}
+                />
+            }
+        >
+            <strong>{shownTitle}</strong>
+            <br />
+            {detailHtml}
+            {causesHtml}
+            {issuesHtml}
+        </Notification>
+    );
 }
 
 export default ErrorView;
