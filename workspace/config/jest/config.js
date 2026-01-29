5a23f4703e08f1dfbdff0ac40b1c43cd4496f13b
Michael Haschke <michael.haschke@eccenca.com>
2026-01-29 15:43:03 +0100

    do not use TypeScript for jest config

---
+++ workspace/config/jest/config.js
@@ -0,0 +1,40 @@
+/* import type {Config} from "jest"; */
+
+module.exports = {
+    testEnvironment: "jsdom",
+    testEnvironmentOptions: {
+        url: "http://localhost/",
+        globalsCleanup: "on",
+    },
+    globals: {
+        __DEBUG__: false,
+    },
+    roots: ["<rootDir>/src/", "<rootDir>/test/"],
+    collectCoverageFrom: ["test/**/*.{js,jsx,ts,tsx}", "src/**/*.{js,jsx,ts,tsx}", "!src/**/*.d.ts"],
+    resolver: "jest-pnp-resolver",
+    setupFiles: ["react-app-polyfill/jsdom"],
+    setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+    testMatch: [
+        "<rootDir>/src/**/*(*.)@(spec|test).{js,jsx,ts,tsx}",
+        "<rootDir>/test/**/*(*.)@(spec|test).{js,jsx,ts,tsx}",
+    ],
+    transform: {
+        "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
+        "^.+\\.css$": "<rootDir>/config/jest/cssTransform.js",
+        "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "<rootDir>/config/jest/fileTransform.js",
+    },
+    transformIgnorePatterns: [
+        "[/\\\\]node_modules[/\\\\](?!react-markdown|vfile|unist-util-stringify-position|).+\\.(js|jsx|ts|tsx)$",
+        "^.+\\.module\\.(css|sass|scss)$",
+    ],
+    moduleNameMapper: {
+        "^react-native$": "react-native-web",
+        "^react-markdown$": "<rootDir>/../node_modules/react-markdown",
+        "^@eccenca/gui-elements$": "<rootDir>/../node_modules/@eccenca/gui-elements",
+        "^@eccenca/gui-elements/(.*)$": "<rootDir>/../node_modules/@eccenca/gui-elements/$1",
+        "^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+        "@ducks(.*)$": "<rootDir>/src/app/store/ducks/$1",
+    },
+    moduleFileExtensions: ["web.js", "js", "web.ts", "ts", "web.tsx", "tsx", "json", "web.jsx", "jsx", "node"],
+    watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
+};
--- workspace/config/jest/subproject.js
+++ workspace/config/jest/subproject.js
@@ -1,14 +1,12 @@
-import type {Config} from "jest";
-import orgconfig from "./config.ts"
+/* import type {Config} from "jest"; */
+const orgconfig = require("./config.js");
 
-const subconfig: Config = {
+module.exports = {
     ...orgconfig,
-    "moduleNameMapper": {
+    moduleNameMapper: {
         ...orgconfig.moduleNameMapper,
         "^react-markdown$": "<rootDir>/../../node_modules/react-markdown",
         "^@eccenca/gui-elements$": "<rootDir>/../../node_modules/@eccenca/gui-elements",
         "^@eccenca/gui-elements/(.*)$": "<rootDir>/../../node_modules/@eccenca/gui-elements/$1",
     },
 };
-
-export default subconfig;
