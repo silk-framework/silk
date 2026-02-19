01e7cc5eee4bbcb9bd8576e88afdda26d29d4c55
Robert Isele <mail@robertisele.com>
2026-02-19 10:12:53 +0100

    LowerCaseTransformer: remove accidental commit

--- silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/normalize/LowerCaseTransformer.scala
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/normalize/LowerCaseTransformer.scala
@@ -23,8 +23,7 @@ import org.silkframework.runtime.plugin.annotations.Plugin
   id = "lowerCase",
   categories = Array("Normalize", PluginCategories.recommended),
   label = "Lower case",
-  description = "Converts a string to lower case.",
-  deprecation = "dddd" //TODO remove
+  description = "Converts a string to lower case."
 )
 @TransformExamples(Array(
   new TransformExample(
