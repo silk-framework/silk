5de4a6aca0ffdbb47c42855ae1acb4778c8b3feb
Robert Isele <mail@robertisele.com>
2026-03-30 15:40:08 +0200

    Add converstions for TemplateVariables

--- silk-core/src/main/scala/org/silkframework/runtime/templating/TemplateVariableConversions.scala
+++ silk-core/src/main/scala/org/silkframework/runtime/templating/TemplateVariableConversions.scala
@@ -2,12 +2,15 @@ package org.silkframework.runtime.templating
 
 import org.silkframework.config.{Task, TaskSpec}
 import org.silkframework.entity.Entity
-import org.silkframework.runtime.plugin.{AnyPlugin, ParameterValues, PluginContext, PluginDescription, PluginObjectParameterTypeTrait, StringParameterType}
+import org.silkframework.runtime.plugin.{ParameterValues, PluginContext, PluginObjectParameterTypeTrait, StringParameterType}
 
 object TemplateVariableConversions {
 
   /**
    * Converts an entity to a sequence of template variables.
+   *
+   * @param entity The entity to convert.
+   * @param scope  The scope to assign to all resulting variables.
    */
   def fromEntity(entity: Entity, scope: Seq[String] = Seq.empty): Seq[TemplateVariableValue] = {
     for((path, value) <- entity.schema.typedPaths zip entity.values if value.nonEmpty) yield {
@@ -15,8 +18,25 @@ object TemplateVariableConversions {
     }
   }
 
-  def fromTask(task: Task[_ <: TaskSpec]): Seq[TemplateVariableValue] = {
-
+  /**
+   * Converts a task's parameters to a sequence of template variables.
+   * Nested plugin parameters are placed into nested scopes using the parameter key.
+   *
+   * @param task  The task whose parameters to convert.
+   * @param scope The base scope. Nested parameters extend this scope with the parameter key.
+   */
+  def fromTask(task: Task[_ <: TaskSpec], scope: Seq[String] = Seq.empty)(implicit pluginContext: PluginContext): Seq[TemplateVariableValue] = {
+    fromPluginParameters(task.data.parameters, scope)
   }
 
+  private def fromPluginParameters(values: ParameterValues, scope: Seq[String] = Seq.empty)(implicit pluginContext: PluginContext): Seq[TemplateVariableValue] = {
+    for((key, value) <- values.values) yield {
+      value match {
+        case _: StringParameterType[_] =>
+          Seq(new TemplateVariableValue(key, scope, Seq(value.toString)))
+        case pt: PluginObjectParameterTypeTrait =>
+          fromPluginParameters(pt.parameters, scope :+ key)
+      }
+    }
+  }.flatten.toSeq
 }
