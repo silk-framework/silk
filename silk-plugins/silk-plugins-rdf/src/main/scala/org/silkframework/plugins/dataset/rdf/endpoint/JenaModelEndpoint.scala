ba4d2ab0dc5522393eb0531d4bfa2d90b0961f8f
Robert Isele <mail@robertisele.com>
2026-04-28 18:37:16 +0200

    JenaModelEndpoint: Limit the memory that can be written

--- silk-plugins/silk-plugins-rdf/src/main/scala/org/silkframework/plugins/dataset/rdf/endpoint/JenaModelEndpoint.scala
+++ silk-plugins/silk-plugins-rdf/src/main/scala/org/silkframework/plugins/dataset/rdf/endpoint/JenaModelEndpoint.scala
@@ -1,16 +1,32 @@
 package org.silkframework.plugins.dataset.rdf.endpoint
 
 import org.apache.jena.query.{DatasetFactory, Query, QueryExecution, QueryExecutionFactory}
-import org.apache.jena.rdf.model.Model
-import org.apache.jena.sparql.core.DatasetGraphFactory
+import org.apache.jena.rdf.listeners.StatementListener
+import org.apache.jena.rdf.model.{Model, Statement}
 import org.apache.jena.update.{UpdateExecutionFactory, UpdateFactory, UpdateProcessor}
 import org.silkframework.dataset.rdf.{QuadIterator, SparqlEndpoint, SparqlParams, SparqlResults}
 import org.silkframework.runtime.activity.UserContext
+import org.silkframework.runtime.resource.Resource
 
 /**
  * A SPARQL endpoint which executes all queries on a Jena Model.
  */
 class JenaModelEndpoint(model: Model) extends JenaEndpoint {
+
+  private val byteLimit: Long = Resource.maxInMemorySize()
+  @volatile private var estimatedBytesWritten: Long = 0L
+
+  model.register(new StatementListener {
+    override def addedStatement(s: Statement): Unit =
+      estimatedBytesWritten += statementBytes(s)
+    override def removedStatement(s: Statement): Unit =
+      estimatedBytesWritten = math.max(0L, estimatedBytesWritten - statementBytes(s))
+  })
+
+  private def statementBytes(s: Statement): Long =
+    s.getSubject.toString.length.toLong +
+    s.getPredicate.toString.length.toLong +
+    s.getObject.toString.length.toLong
 
   override protected def createQueryExecution(query: Query): QueryExecution = {
     QueryExecutionFactory.create(query, model)
@@ -45,6 +61,14 @@ class JenaModelEndpoint(model: Model) extends JenaEndpoint {
                      (implicit userContext: UserContext): Unit = {
     this.synchronized {
       super.update(query)
+      if (estimatedBytesWritten > byteLimit) {
+        throw new RuntimeException(
+          s"In-memory Knowledge Graph has exceeded the size limit of $byteLimit bytes " +
+          s"(estimated bytes written: $estimatedBytesWritten). " +
+          s"Reduce the amount of data written or increase the limit by configuring " +
+          s"'${Resource.maxInMemorySizeParameterName}'."
+        )
+      }
     }
   }
 
