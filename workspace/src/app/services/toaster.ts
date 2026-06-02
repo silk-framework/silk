74f3fcad3e622bca9bcb4b985c761d567217b258
Michael Haschke <michael.haschke@eccenca.com>
2026-06-02 17:34:33 +0200

    remove unused code

--- workspace/src/app/services/toaster.ts
+++
@@ -1,7 +0,0 @@
-import Toaster from "@eccenca/gui-elements/blueprint/toaster";
-import { Position } from "@eccenca/gui-elements/blueprint/constants";
-
-/** Singleton toaster instance. Create separate instances for different options. */
-export const AppToaster = Toaster.create({
-    position: Position.BOTTOM_RIGHT,
-});
