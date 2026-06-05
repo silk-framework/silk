09280e8de0d4091c8b5c8879335b4a13c07e720f
Robert Isele <mail@robertisele.com>
2026-06-05 16:30:43 +0200

    Add SetExecutionVariableExecution transformer and test

---
+++ silk-rules/src/main/scala/org/silkframework/rule/plugins/transformer/variable/SetExecutionVariableTransformer.scala
@@ -0,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.silkframework.rule.plugins.transformer.variable
+
+import org.silkframework.rule.TaskContext
+import org.silkframework.rule.input.{Transformer, TransformerExecution}
+import org.silkframework.runtime.plugin.annotations.{Param, Plugin}
+import org.silkframework.runtime.templating.{PluginTemplateVariables, TemplateVariable, TemplateVariableScopes}
+import org.silkframework.runtime.validation.ValidationException
+
+@Plugin(
+  id = "setExecutionVariable",
+  categories = Array("Variables"),
+  label = "Set execution variable",
+  description =
+    "Sets an execution variable to the first value of the (single) input and passes the input values through unchanged. " +
+    "The variable is written to the 'execution' scope and can be read downstream as 'execution.<name>'. " +
+    "Only works while running inside a workflow execution."
+)
+case class SetExecutionVariableTransformer(@Param("Name of the execution variable to set. " +
+                                                  "It is written to the 'execution' scope and addressed downstream as 'execution.<name>'.")
+                                           variableName: String = "myVariable") extends Transformer {
+
+  override def execution(taskContext: TaskContext): TransformerExecution = {
+    new SetExecutionVariableExecution(variableName, taskContext.pluginContext.templateVariables)
+  }
+}
+
+/**
+  * Captures the execution-time template variables so the variable can be set while values flow through.
+  */
+private class SetExecutionVariableExecution(variableName: String,
+                                            templateVariables: PluginTemplateVariables) extends TransformerExecution {
+
+  override def apply(values: Seq[Seq[String]]): Seq[String] = {
+    if (values.size != 1) {
+      throw new ValidationException(
+        s"Set execution variable expects exactly one input, but got ${values.size}.")
+    }
+    val inputValues = values.head
+    inputValues.headOption.foreach { firstValue =>
+      templateVariables.setExecutionVariable(
+        TemplateVariable(name = variableName, value = firstValue, scope = TemplateVariableScopes.execution))
+    }
+    // Output the input values unchanged
+    inputValues
+  }
+}
---
+++ silk-rules/src/test/scala/org/silkframework/rule/plugins/transformer/variable/SetExecutionVariableTransformerTest.scala
@@ -0,0 +1,99 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.silkframework.rule.plugins.transformer.variable
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+import org.silkframework.config.Prefixes
+import org.silkframework.rule.TaskContext
+import org.silkframework.runtime.activity.UserContext
+import org.silkframework.runtime.plugin.PluginContext
+import org.silkframework.runtime.resource.InMemoryResourceManager
+import org.silkframework.runtime.templating.{PluginTemplateVariables, TemplateVariable, TemplateVariableScopes, TemplateVariables}
+import org.silkframework.runtime.validation.ValidationException
+
+class SetExecutionVariableTransformerTest extends AnyFlatSpec with Matchers {
+
+  behavior of "SetExecutionVariableTransformer"
+
+  it should "set the execution variable to the first value of the single input and pass the input through unchanged" in {
+    val variables = new RecordingTemplateVariables()
+    val transformer = SetExecutionVariableTransformer("myVar").execution(taskContext(variables))
+
+    val result = transformer(Seq(Seq("first", "second")))
+
+    result shouldBe Seq("first", "second")
+    variables.get("myVar").value shouldBe "first"
+    variables.get("myVar").scope shouldBe TemplateVariableScopes.execution
+  }
+
+  it should "overwrite the variable on each invocation, so the last processed value wins" in {
+    val variables = new RecordingTemplateVariables()
+    val transformer = SetExecutionVariableTransformer("myVar").execution(taskContext(variables))
+
+    transformer(Seq(Seq("entity1")))
+    transformer(Seq(Seq("entity2")))
+
+    variables.get("myVar").value shouldBe "entity2"
+    variables.all.variables should have size 1
+  }
+
+  it should "leave the variable untouched but still pass through when the input has no values" in {
+    val variables = new RecordingTemplateVariables()
+    val transformer = SetExecutionVariableTransformer("myVar").execution(taskContext(variables))
+
+    val result = transformer(Seq(Seq.empty))
+
+    result shouldBe Seq.empty
+    variables.all.variables shouldBe empty
+  }
+
+  it should "throw a ValidationException when not exactly one input is connected" in {
+    val variables = new RecordingTemplateVariables()
+    val transformer = SetExecutionVariableTransformer("myVar").execution(taskContext(variables))
+
+    an[ValidationException] should be thrownBy transformer(Seq(Seq("a"), Seq("b")))
+    an[ValidationException] should be thrownBy transformer(Seq.empty)
+  }
+
+  private def taskContext(variables: PluginTemplateVariables): TaskContext = {
+    val pluginContext = PluginContext(
+      prefixes = Prefixes.empty,
+      resources = InMemoryResourceManager(),
+      user = UserContext.Empty,
+      templateVariables = variables
+    )
+    TaskContext(Seq.empty, pluginContext)
+  }
+
+  /**
+    * Minimal mutable [[PluginTemplateVariables]] for the test, mirroring the replace-on-collision behaviour
+    * of the real execution-variable holder used during workflow runs.
+    */
+  private class RecordingTemplateVariables extends PluginTemplateVariables {
+
+    private var current: Seq[TemplateVariable] = Seq.empty
+
+    override def scopes: Set[Seq[String]] = Set(TemplateVariableScopes.execution)
+
+    override def all: TemplateVariables = TemplateVariables(current)
+
+    override def setExecutionVariable(variable: TemplateVariable): Unit = {
+      require(variable.scope == TemplateVariableScopes.execution,
+        s"Expected execution scope but got '${variable.scope.mkString(".")}'")
+      current = variable +: current.filterNot(_.name == variable.name)
+    }
+  }
+}
